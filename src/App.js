78084403da1d1c7c266bc79e6bef749ad483f805
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-02 10:05:21 +0100

    Adding set funds and get balance options of created safe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form } from 'react-final-form'
+import { Form, Field } from 'react-final-form'
 import Safe from '../gnosis-safe-contracts/build/contracts/GnosisSafe.json'
 import getWeb3, { promisify } from './utils/getWeb3'
 import contract from 'truffle-contract'
@@ -14,7 +14,9 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: undefined,
+      safeAddress: undefined,
+      funds: undefined,
     }
 
     this.safe = contract(Safe)
@@ -41,12 +43,30 @@ class App extends Component {
       // console.log("Transaction" + JSON.stringify(transaction, 2, 0))
       const transactionReceipt = await promisify(cb => web3.eth.getTransactionReceipt(transactionHash, cb))
       console.log("Transaction Receipt" + JSON.stringify(transactionReceipt, 2, 0))
+      this.setState({ safeAddress: safeInstance.address})
     } catch (error) {
       console.log("Error while creating the Safe")
     }
   }
 
+  onAddFunds = async (values) => {
+    const fundsToAdd = values.funds
+    try {
+      const { web3, safeAddress } = this.state
+      const accounts = await promisify(cb => web3.eth.getAccounts(cb))
+      const txData = {from: accounts[0], to: safeAddress, value: web3.toWei(fundsToAdd, 'ether')}
+      await promisify(cb => web3.eth.sendTransaction(txData, cb))
+      const funds = await promisify( cb => web3.eth.getBalance(safeAddress, cb))
+      const fundsInEther = funds ? web3.fromWei(funds.toNumber(), 'ether') : 0
+      this.setState({funds: fundsInEther})
+    } catch (error) {
+      console.log("Errog adding funds to safe" + error)
+    }
+  }
+
   render() {
+    const { safeAddress, funds } = this.state
+
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
@@ -60,11 +80,31 @@ class App extends Component {
                 <h2>Create a new Safe instance for testing purposes</h2>
                 <div>
                   <button style={{ marginLeft: '10px', border: '1px solid #ccc' }} type="submit">
-                    Submit
+                    Create Safe
                   </button>
                 </div> 
               </form>
             )} />
+            <Form
+              onSubmit={this.onAddFunds}
+              render={({ handleSubmit, pristine, invalid }) => (
+                <form onSubmit={handleSubmit}>
+                  <h2>Add Funds to the safe</h2>
+                  <div style={{ margin: '10px 0px'}}>
+                    <label style={{ marginRight: '10px' }}>{safeAddress ? safeAddress : 'Not safe detected'}</label>
+                  </div>
+                  { safeAddress && <div>
+                    <Field name="funds" component="input" placeholder="ETH to add" />
+                    <button style={{ marginLeft: '10px', border: '1px solid #ccc' }} type="submit" disabled={ !safeAddress || pristine || invalid}>
+                      Add funds
+                    </button>
+                  </div> }
+                  { safeAddress && <div style={{ margin: '10px 0px'}}>
+                    Total funds in this safe: { funds ? funds : 0 } ETH
+                  </div> }
+                </form>
+              )}
+            />
         </main>
       </div>
     );
