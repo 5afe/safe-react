07607a6c4a9142f3af19fdd42e7c91eea5fc87ed
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-06 18:01:03 +0100

    Downgrading web3 and truffle-contract for better bundle size

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const webpack = require('webpack');
@@ -71,6 +72,19 @@ module.exports = {
   mode: 'production',
   // Don't attempt to continue if there are any errors.
   bail: true,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: "vendor",
+          chunks: "all",
+          enforce: true,
+          minSize: 1
+        }
+      }
+    }
+  },
   entry: [
     require.resolve('./polyfills'),
     paths.appIndexJs
@@ -163,7 +177,8 @@ module.exports = {
     // having to parse `index.html`.
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
-    })
+    }),
+    // new BundleAnalyzerPlugin()
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
