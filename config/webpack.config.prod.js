39057f87be4b7cd4097a940749fb85c9790f2d4a
apanizo <adolfo.panizo@gmail.com>
2018-03-10 10:26:19 +0100

    Adding polyfill

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,18 +1,19 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const autoprefixer = require('autoprefixer');
-const cssvars = require('postcss-simple-vars');
-const webpack = require('webpack');
+require('@babel/polyfill')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const autoprefixer = require('autoprefixer')
+const cssvars = require('postcss-simple-vars')
+const webpack = require('webpack')
 
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const url = require('url');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const url = require('url')
 
-var paths = require('./paths');
-var getClientEnvironment = require('./env');
+const paths = require('./paths')
+const getClientEnvironment = require('./env')
 
-var cssvariables = require(paths.appSrc + '/theme/variables');
+const cssvariables = require(`${paths.appSrc}/theme/variables`)
 
 const postcssPlugins = [
   autoprefixer({
@@ -21,25 +22,24 @@ const postcssPlugins = [
       'last 4 versions',
       'Firefox ESR',
       'not ie < 9', // React doesn't support IE8 anyway
-    ]
+    ],
   }),
   cssvars({
-    variables: function () {
-        return Object.assign({}, cssvariables);
+    variables() {
+      return Object.assign({}, cssvariables)
     },
-    silent: true
+    silent: true,
   }),
-];
+]
 
 function ensureSlash(path, needsSlash) {
-  var hasSlash = path.endsWith('/');
+  const hasSlash = path.endsWith('/')
   if (hasSlash && !needsSlash) {
-    return path.substr(path, path.length - 1);
+    return path.substr(path, path.length - 1)
   } else if (!hasSlash && needsSlash) {
-    return path + '/';
-  } else {
-    return path;
+    return `${path}/`
   }
+  return path
 }
 
 // We use "homepage" field to infer "public path" at which the app is served.
@@ -47,23 +47,23 @@ function ensureSlash(path, needsSlash) {
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-var homepagePath = require(paths.appPackageJson).homepage;
+const homepagePath = require(paths.appPackageJson).homepage
 //  var homepagePathname = homepagePath ? url.parse(homepagePath).pathname : '/';
-var homepagePathname = '/';
+const homepagePathname = '/'
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-var publicPath = ensureSlash(homepagePathname, true);
+const publicPath = ensureSlash(homepagePathname, true)
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-var publicUrl = ensureSlash(homepagePathname, false);
+const publicUrl = ensureSlash(homepagePathname, false)
 // Get environment variables to inject into our app.
-var env = getClientEnvironment(publicUrl);
+const env = getClientEnvironment(publicUrl)
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
 if (env['process.env'].NODE_ENV !== '"production"') {
-  throw new Error('Production builds must have NODE_ENV=production.');
+  throw new Error('Production builds must have NODE_ENV=production.')
 }
 
 // This is the production configuration.
@@ -78,17 +78,17 @@ module.exports = {
       cacheGroups: {
         vendor: {
           test: /node_modules/,
-          name: "vendor",
-          chunks: "all",
+          name: 'vendor',
+          chunks: 'all',
           enforce: true,
-          minSize: 1
-        }
-      }
-    }
+          minSize: 1,
+        },
+      },
+    },
   },
   entry: [
     require.resolve('./polyfills'),
-    paths.appIndexJs
+    paths.appIndexJs,
   ],
   output: {
     // The build folder.
@@ -99,14 +99,14 @@ module.exports = {
     filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: publicPath
+    publicPath,
   },
   resolve: {
     modules: [
       paths.appSrc,
       'node_modules',
       paths.appContracts,
-    ], 
+    ],
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
@@ -115,7 +115,7 @@ module.exports = {
     alias: {
       '~': paths.appSrc,
       '#': paths.appContracts,
-    }
+    },
   },
 
   module: {
@@ -124,33 +124,33 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(scss|css)$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
-            { 
+            {
               loader: 'css-loader',
               options: {
                 importLoaders: 1,
                 modules: true,
-                minimize: true
-              }
+                minimize: true,
+              },
             },
             {
               loader: 'postcss-loader',
               options: {
                 sourceMap: true,
                 plugins: postcssPlugins,
-              }
+              },
             },
           ],
-        })
+        }),
       },
-    ]
+    ],
   },
   plugins: [
     // Generates an `index.html` file with the <script> injected.
@@ -167,8 +167,8 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
-      }
+        minifyURLs: true,
+      },
     }),
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
@@ -181,7 +181,7 @@ module.exports = {
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
     new ManifestPlugin({
-      fileName: 'asset-manifest.json'
+      fileName: 'asset-manifest.json',
     }),
     // new BundleAnalyzerPlugin()
   ],
@@ -190,6 +190,6 @@ module.exports = {
   node: {
     fs: 'empty',
     net: 'empty',
-    tls: 'empty'
-  }
-};
+    tls: 'empty',
+  },
+}
