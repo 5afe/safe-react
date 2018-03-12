7df67af45da9f3de921fe971583d00e159e50493
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-12 16:42:24 +0100

    Fixing lint problems

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,14 @@
-import { addDecorator, configure } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import * as React from 'react';
-import StoryRouter from 'storybook-router';
+import { addDecorator, configure } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import * as React from 'react'
+import StoryRouter from 'storybook-router'
 
 (function (global) {
     //Useful for adding data and libraries to window object.
 })(typeof window !== 'undefined' ? window : {});
 
 addDecorator(withKnobs);
-addDecorator(StoryRouter());
+addDecorator(StoryRouter())
 
 addDecorator((story) => (
     <div>
@@ -20,7 +20,7 @@ const components = require.context('../src/components', true, /\.stories\.((js|t
 const routes = require.context('../src/routes', true, /\.stories\.((js|ts)x?)$/)
 
 function loadStories() {
-    components.keys().forEach((filename) => components(filename));
+    components.keys().forEach((filename) => components(filename))
     routes.keys().forEach((filename) => routes(filename))
 }
 
