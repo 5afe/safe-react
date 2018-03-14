99cbee3a844e95240d9d9c10aaf1261d60450078
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-14 08:51:14 +0100

    Updated styles veriable with constants

--- src/theme/variables.js
+++ src/theme/variables.js
@@ -1,10 +1,23 @@
 // @flow
 const primary = '#1798cc'
-const secondary = '#6b7c93'
-const footer = '#13222b'
+const secondary = '#13222b'
+const tertiary = '#f6f9fc'
 
 module.exports = Object.assign({}, {
   primary,
   secondary,
-  footer,
+  tertiary,
+  xs: '8px',
+  md: '16px',
+  xl: '24px',
+  xxl: '42px',
+  regularFontWeight: 400,
+  regularFontSize: '14px',
+  screenXs: 480,
+  screenXsMax: 767,
+  screenSm: 768,
+  screenSmMax: 991,
+  screenMd: 992,
+  screenMdMax: 1199,
+  screenLg: 1200,
 })
