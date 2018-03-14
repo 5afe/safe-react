3032fc5c28c46c66deaa546e27d38f04b4fd2fe5
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-14 12:03:24 +0100

    Using yarn pck manager and update deploy script including storybook

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -9,6 +9,7 @@ REPO_OWNER=${REPO_SLUG_ARRAY[0]}
 REPO_NAME=${REPO_SLUG_ARRAY[1]}
 
 DEPLOY_PATH=./build_webpack
+DEPLOY_PATH_STORYBOOK=./build_storybook
 
 DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
 
@@ -58,16 +59,18 @@ do
   echo $DEPLOY_SUBDOMAIN
 
   DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
+  DEPLOY_STORYBOOK=https://storybook-${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
 
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
-
+  surge --project ${DEPLOY_PATH_STORYBOOK} --domain $DEPLOY_STORYBOOK
+  
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
     # Using the Issues api instead of the PR api
     # Done so because every PR is an issue, and the issues api allows to post general comments,
     # while the PR api requires that comments are made to specific files and specific commits
     GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
-    curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+    curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}' \r\n Storybook book automatic deployment: '${DEPLOY_STORYBOOK}'"}'
   fi
 done
 
