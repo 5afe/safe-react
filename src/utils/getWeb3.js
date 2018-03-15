d22bf8623aad81acc63c16c42e4828cf3b22beab
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-15 14:30:15 +0100

    Fixing eslint problems on getWeb3 helper function

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,50 +1,42 @@
+// @flow
 import Web3 from 'web3'
 
-let getWeb3 = new Promise(function(resolve, reject) {
+const getWeb3 = new Promise((resolve) => {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  window.addEventListener('load', function() {
-    var results
-    var web3 = window.web3
+  window.addEventListener('load', () => {
+    let { web3 } = window
 
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider)
 
-      results = {
-        web3: web3
-      }
+      // eslint-disable-next-line
+      console.log('Injected web3 detected.')
 
-      console.log('Injected web3 detected.');
-
-      resolve(results)
+      resolve({ web3 })
     } else {
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
+      const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
 
       web3 = new Web3(provider)
 
-      results = {
-        web3: web3
-      }
+      // eslint-disable-next-line
+      console.log('No web3 instance injected, using Local web3.')
 
-      console.log('No web3 instance injected, using Local web3.');
-
-      resolve(results)
+      resolve({ web3 })
     }
   })
 })
 
-export const promisify = (inner) =>
+export const promisify = inner =>
   new Promise((resolve, reject) =>
     inner((err, res) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve(res);
-        }
-    })
-)
+      if (err) {
+        reject(err)
+      } else {
+        resolve(res)
+      }
+    }))
 
 export default getWeb3
