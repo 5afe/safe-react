8acebfb63be525ac84ec983d00ca430c6130397a
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-15 16:37:39 +0100

    WA-229 Adding redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
+// @flow
 import 'babel-polyfill'
+import { createBrowserHistory } from 'history'
 import { MuiThemeProvider } from 'material-ui/styles'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter, routerMiddleware, routerReducer } from 'react-router-redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import theme from '~/theme/mui'
-import App from './App'
+import AppRoutes from '~/routes'
+import './index.scss'
+
+const history = createBrowserHistory()
+const enhancers = [
+  applyMiddleware(
+    thunk,
+    routerMiddleware(history),
+  ),
+]
+const finalCreateStore = compose(...enhancers)
+
+const reducers = combineReducers({
+  routing: routerReducer,
+})
+
+const store = createStore(reducers, finalCreateStore)
 
 const Root = () => (
-  <MuiThemeProvider theme={theme}>
-    <App />
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+      <ConnectedRouter history={history}>
+        <AppRoutes />
+      </ConnectedRouter>
+    </MuiThemeProvider>
+  </Provider>
 )
 
 ReactDOM.render(
