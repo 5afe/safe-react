3bd89747f7ba14cf9e87f706b3d998f1fa890c5f
apanizo <adolfo.panizo@gmail.com>
2018-03-18 13:35:46 +0100

    WA-229 refactor Header component

---
+++ src/components/Header/NotConnected.jsx
@@ -0,0 +1,5 @@
+// @flow
+import React from 'react'
+import Span from '~/components/layout/Span'
+
+export default () => <Span>Not Connected</Span>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,53 +1,26 @@
 // @flow
 import React from 'react'
 import Col from '~/components/layout/Col'
-import Span from '~/components/layout/Span'
 import Row from '~/components/layout/Row'
 import Img from '~/components/layout/Img'
-import { upperFirst } from '~/utils/css'
+import Connected from './Connected'
+import NotConnected from './NotConnected'
 
-const logo = require('./gnosis_logo.svg')
-const IconParity = require('./icon_parity.svg')
-const IconMetamask = require('./icon_metamask.svg')
+const logo = require('./assets/gnosis_logo.svg')
 
 type Props = {
-  provider?: string,
+  provider: string,
 }
 
-const PROVIDER_METAMASK = 'METAMASK'
-const PROVIDER_PARITY = 'PARITY'
-
-const PROVIDER_ICONS = {
-  [PROVIDER_METAMASK]: IconMetamask,
-  [PROVIDER_PARITY]: IconParity,
-}
-
-const Connected = ({ provider }: string) => (
-  <React.Fragment>
-    <Img
-      height={40}
-      src={PROVIDER_ICONS[provider]}
-      title={`You are using ${upperFirst(provider.toLowerCase())} to connect to your Safe`}
-    />
-    <Span>Connected</Span>
-  </React.Fragment>
+const Header = ({ provider }: Props) => (
+  <Row>
+    <Col xs={12} center="xs" sm={6} start="sm" margin="lg">
+      <Img src={logo} height={40} alt="Gnosis Safe" />
+    </Col>
+    <Col xs={12} center="xs" sm={6} end="sm" margin="lg">
+      { provider ? <Connected provider={provider} /> : <NotConnected /> }
+    </Col>
+  </Row>
 )
-
-const NotConnected = () => <Span>Not Connected</Span>
-
-const Header = ({ provider }: Props) => {
-  const connected = !!provider && PROVIDER_ICONS[provider]
-  return (
-    <Row>
-      <Col xs={12} center="xs" sm={6} start="sm" margin="lg">
-        <Img src={logo} height={40} />
-      </Col>
-      <Col xs={12} center="xs" sm={6} end="sm" margin="lg">
-        { connected ? <Connected provider={provider} /> : <NotConnected /> }
-      </Col>
-    </Row>
-  )
-}
-
 
 export default Header
