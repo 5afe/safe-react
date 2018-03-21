2c5a1375c6fd5cda85e6b1114b855d625341b5c6
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-21 09:53:53 +0100

    WA-229 Adding logic for obtaining the web3 provider

--- src/wallets/store/test/builder/index.builder.js
+++ src/wallets/store/test/builder/index.builder.js
@@ -1,22 +1,25 @@
 // @flow
+import type { Provider } from '~/wallets/store/model/provider'
 import { makeProvider } from '~/wallets/store/model/provider'
 
 class ProviderBuilder {
+  provider: Provider
+
   constructor() {
     this.provider = makeProvider()
   }
 
-  withName(name) {
+  withName(name: string) {
     this.provider = this.provider.set('name', name)
     return this
   }
 
-  withLoaded(loaded) {
+  withLoaded(loaded: boolean) {
     this.provider = this.provider.set('loaded', loaded)
     return this
   }
 
-  withAvailable(available) {
+  withAvailable(available: boolean) {
     this.provider = this.provider.set('available', available)
     return this
   }
