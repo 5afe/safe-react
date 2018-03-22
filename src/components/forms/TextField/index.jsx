9f005f1544b1b350a1a5ac1ab99d7cc60ac534a5
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-22 11:09:12 +0100

    WA-229 Adding Safe name and number of confirmations as required fields

--- src/components/forms/TextField/index.jsx
+++ src/components/forms/TextField/index.jsx
@@ -10,7 +10,6 @@ const TextField = ({
   render,
   text,
   ...rest
-<<<<<<< b007d561f6319d9dadc9b39b0a0517b028e2e1d7
 }: TextFieldProps) => {
   const helperText = value ? text : undefined
   const showError = meta.touched && !meta.valid
@@ -28,19 +27,5 @@ const TextField = ({
     />
   )
 }
-=======
-}: TextFieldProps) => (
-  <MuiTextField
-    {...rest}
-    name={name}
-    helperText={meta.touched ? meta.error : undefined}
-    error={meta.error && meta.touched}
-    inputProps={restInput}
-    onChange={onChange}
-    value={value}
-    fullWidth
-  />
-)
->>>>>>> WA-229 Adding fullWidth property to TextField by default
 
 export default TextField
