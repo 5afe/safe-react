55a013152a2d1680574c005c4149017656acb5aa
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-22 11:09:12 +0100

    WA-229 Adding Safe name and number of confirmations as required fields

--- src/routes/open/components/Layout.jsx
+++ src/routes/open/components/Layout.jsx
@@ -1,9 +1,11 @@
 // @flow
 import Button from 'material-ui/Button'
 import * as React from 'react'
-import { Form, Field } from 'react-final-form'
+import { Field } from 'react-final-form'
 import Block from '~/components/layout/Block'
 import TextField from '~/components/forms/TextField'
+import GnoForm from '~/components/forms/GnoForm'
+import { composeValidators, minValue, mustBeNumber, required } from '~/components/forms/validator'
 
 type Props = {
   onCallSafeContractSubmit: Function,
@@ -16,28 +18,33 @@ const Open = ({
   funds, safeAddress, onCallSafeContractSubmit, onAddFunds,
 }: Props) => (
   <React.Fragment>
-    <Form
-      onSubmit={onCallSafeContractSubmit}
-      render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <Block margin="md">
-            <h2>Create a new Safe instance for testing purposes</h2>
-            <Block margin="sm">
-              <Field name="name" component={TextField} type="text" placeholder="Safe name" />
-            </Block>
-            <Block margin="sm">
-              <Button variant="raised" color="primary" type="submit">
-                Create Safe
-              </Button>
-            </Block>
+    <GnoForm onSubmit={onCallSafeContractSubmit} width="500">
+      { () => (
+        <Block margin="md">
+          <h2>Deploy a new Safe</h2>
+          <Block margin="sm">
+            <Field name="name" component={TextField} type="text" validate={required} placeholder="Safe name*" />
           </Block>
-        </form>
+          <Block margin="sm">
+            <Field
+              name="confirmations"
+              component={TextField}
+              type="text"
+              validate={composeValidators(required, mustBeNumber, minValue(1))}
+              placeholder="Required confirmations*"
+            />
+          </Block>
+          <Block margin="sm">
+            <Button variant="raised" color="primary" type="submit">
+              Create Safe
+            </Button>
+          </Block>
+        </Block>
       )}
-    />
-    <Form
-      onSubmit={onAddFunds}
-      render={({ handleSubmit, pristine, invalid }) => (
-        <form onSubmit={handleSubmit}>
+    </GnoForm>
+    <GnoForm onSubmit={onAddFunds} width="500">
+      {(pristine, invalid) => (
+        <Block margin="md">
           <h2>Add Funds to the safe</h2>
           <div style={{ margin: '10px 0px' }}>
             <label style={{ marginRight: '10px' }}>{safeAddress || 'Not safe detected'}</label>
@@ -55,9 +62,9 @@ const Open = ({
               Total funds in this safe: { funds || 0 } ETH
             </div>
           }
-        </form>
+        </Block>
       )}
-    />
+    </GnoForm>
   </React.Fragment>
 )
 
