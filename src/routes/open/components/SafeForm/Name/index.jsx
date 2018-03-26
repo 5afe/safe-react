82c03f820505ee00c80f17d85dfb3df3fe263a57
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-26 14:21:16 +0200

    WA-279 Moving local private Open Safe Form components

---
+++ src/routes/open/components/SafeForm/Name/index.jsx
@@ -0,0 +1,21 @@
+// @flow
+import * as React from 'react'
+import Field from '~/components/forms/Field'
+import TextField from '~/components/forms/TextField'
+import { required } from '~/components/forms/validator'
+import Block from '~/components/layout/Block'
+
+const Name = () => (
+  <Block margin="md">
+    <Field
+      name="name"
+      component={TextField}
+      type="text"
+      validate={required}
+      placeholder="Safe name*"
+      text="Safe name"
+    />
+  </Block>
+)
+
+export default Name
