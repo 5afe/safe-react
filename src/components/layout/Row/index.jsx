6cf8b8a086438af3a8a6cbbf22894390ac591e84
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-27 15:57:04 +0200

    WA-279 Adding margin to Row layout component

--- src/components/layout/Row/index.jsx
+++ src/components/layout/Row/index.jsx
@@ -1,6 +1,7 @@
 // @flow
 import classNames from 'classnames/bind'
 import React from 'react'
+import { capitalize } from '~/utils/css'
 import styles from './index.scss'
 
 const cx = classNames.bind(styles)
@@ -8,12 +9,23 @@ const cx = classNames.bind(styles)
 type Props = {
   className?: string,
   children: React$Node,
+  margin?: 'sm' | 'md' | 'lg' | 'xl',
 }
 
-const Row = ({ children, className, ...props }: Props) => (
-  <div className={cx(styles.row, className)} {...props}>
-    { children }
-  </div>
-)
+const Row = ({
+  children, className, margin, ...props
+}: Props) => {
+  const rowClassNames = cx(
+    styles.row,
+    margin ? capitalize(margin, 'margin') : undefined,
+    className,
+  )
+
+  return (
+    <div className={rowClassNames} {...props}>
+      { children }
+    </div>
+  )
+}
 
 export default Row
