120179ee3eab0821076df1601074684afa11534b
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-28 11:00:40 +0200

    WA-279 adding grow and align properties to Row layout column

--- src/components/layout/Row/index.jsx
+++ src/components/layout/Row/index.jsx
@@ -10,14 +10,18 @@ type Props = {
   className?: string,
   children: React$Node,
   margin?: 'sm' | 'md' | 'lg' | 'xl',
+  align?: 'center' | 'end' | 'start',
+  grow?: boolean,
 }
 
 const Row = ({
-  children, className, margin, ...props
+  children, className, margin, align, grow, ...props
 }: Props) => {
   const rowClassNames = cx(
     styles.row,
     margin ? capitalize(margin, 'margin') : undefined,
+    align ? capitalize(align, 'align') : undefined,
+    { grow },
     className,
   )
 
