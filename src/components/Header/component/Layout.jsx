547738e83545ea57d3e7a4108b00f36cf3165872
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-03-29 13:21:47 +0200

    WA-280 renaming from Loader to Refresh

--- src/components/Header/component/Layout.jsx
+++ src/components/Header/component/Layout.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Col from '~/components/layout/Col'
 import Img from '~/components/layout/Img'
-import Loader from '~/components/Loader'
+import Refresh from '~/components/Refresh'
 import Row from '~/components/layout/Row'
 
 import Connected from './Connected'
@@ -22,7 +22,7 @@ const Header = ({ provider, reloadWallet }: Props) => (
     </Col>
     <Col xs={12} center="xs" sm={6} end="sm" margin="lg">
       { provider ? <Connected provider={provider} /> : <NotConnected /> }
-      <Loader callback={reloadWallet} />
+      <Refresh callback={reloadWallet} />
     </Col>
   </Row>
 )
