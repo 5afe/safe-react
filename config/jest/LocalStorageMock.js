57d56232bd17c8a01caed0d76964772a38a32081
apanizo <adolfo.panizo>
2018-04-03 18:56:29 +0200

    Adding local storage mock to JEST as a global instance

---
+++ config/jest/LocalStorageMock.js
@@ -0,0 +1,26 @@
+// @flow
+class LocalStorageMock {
+  store: Object
+
+  constructor() {
+    this.store = {}
+  }
+
+  clear() {
+    this.store = {}
+  }
+
+  getItem(key) {
+    return this.store[key] || null
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString()
+  }
+
+  removeItem(key) {
+    delete this.store[key]
+  }
+}
+
+global.localStorage = new LocalStorageMock()
