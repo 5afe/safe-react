0ac1a20396a7ba62af931d54b0a5b7e994578fe9
apanizo <adolfo.panizo>
2018-04-03 18:58:08 +0200

    WA-280 Loading persisted safes in route->safe reducer store

--- src/routes/safe/store/reducer/safe.js
+++ src/routes/safe/store/reducer/safe.js
@@ -1,14 +1,32 @@
 // @flow
-import { Map } from 'immutable'
+import { Map, List } from 'immutable'
 import { handleActions, type ActionType } from 'redux-actions'
 import addSafe, { ADD_SAFE } from '~/routes/safe/store/actions/addSafe'
+import { makeOwner } from '~/routes/safe/store/model/owner'
 import { type Safe, makeSafe } from '~/routes/safe/store/model/safe'
+import { loadSafes, saveSafes } from '~/utils/localStorage'
 
-export const REDUCER_ID = 'safes'
+export const SAFE_REDUCER_ID = 'safes'
 
-type State = Map<string, Safe>
+export type State = Map<string, Safe>
 
-const defaultProps: State = Map()
+const buildSafesFrom = (loadedSafes: Object): State => {
+  const safes: State = Map()
+
+  return safes.withMutations((map: State) => {
+    Object.keys(loadedSafes).forEach((address) => {
+      const safe = loadedSafes[address]
+      safe.owners = List(safe.owners.map((owner => makeOwner(owner))))
+      return map.set(address, makeSafe(safe))
+    })
+  })
+}
+
+export const defaultProps = (): State => {
+  const storedSafes = loadSafes()
+
+  return storedSafes ? buildSafesFrom(storedSafes) : Map()
+}
 
 /*
 type Action<T> = {
@@ -22,6 +40,9 @@ action: AddSafeType
 */
 
 export default handleActions({
-  [ADD_SAFE]: (state: State, action: ActionType<typeof addSafe>): State =>
-    state.set(action.payload.address, makeSafe(action.payload)),
-}, defaultProps)
+  [ADD_SAFE]: (state: State, action: ActionType<typeof addSafe>): State => {
+    const safes = state.set(action.payload.address, makeSafe(action.payload))
+    saveSafes(safes.toJSON())
+    return safes
+  },
+}, defaultProps())
--- src/routes/safe/store/test/safe.reducer.js
+++ src/routes/safe/store/test/safe.reducer.js
@@ -1,7 +1,7 @@
 // @flow
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import safeReducer, { REDUCER_ID } from '~/routes/safe/store/reducer/safe'
+import safeReducer, { defaultProps, SAFE_REDUCER_ID } from '~/routes/safe/store/reducer/safe'
 import addSafe from '~/routes/safe/store/actions/addSafe'
 import * as SafeFields from '~/routes/open/components/fields'
 import { getAccountsFrom, getNamesFrom } from '~/routes/open/utils/safeDataExtractor'
@@ -12,7 +12,7 @@ const providerReducerTests = () => {
     let store
     beforeEach(() => {
       const reducers = combineReducers({
-        [REDUCER_ID]: safeReducer,
+        [SAFE_REDUCER_ID]: safeReducer,
       })
       const middlewares = [
         thunk,
@@ -43,10 +43,37 @@ const providerReducerTests = () => {
         getNamesFrom(formValues),
         getAccountsFrom(formValues),
       ))
-      const safes = store.getState()[REDUCER_ID]
+      const safes = store.getState()[SAFE_REDUCER_ID]
 
       // THEN
       expect(safes.get(address)).toEqual(SafeFactory.oneOwnerSafe)
+    })
+
+    it('reducer loads information from localStorage', () => {
+      // GIVEN
+      const address = '0x03db1a8b26d08df23337e9276a36b474510f0025'
+      const formValues = {
+        [SafeFields.FIELD_NAME]: 'Adol ICO Safe',
+        [SafeFields.FIELD_CONFIRMATIONS]: 1,
+        [SafeFields.FIELD_OWNERS]: 1,
+        [SafeFields.getOwnerAddressBy(0)]: '0x03db1a8b26d08df23337e9276a36b474510f0023',
+        [SafeFields.getOwnerNameBy(0)]: 'Adol Metamask',
+        address,
+      }
+
+      // WHEN
+      store.dispatch(addSafe(
+        formValues[SafeFields.FIELD_NAME],
+        formValues.address,
+        formValues[SafeFields.FIELD_CONFIRMATIONS],
+        getNamesFrom(formValues),
+        getAccountsFrom(formValues),
+      ))
+
+      const safes = store.getState()[SAFE_REDUCER_ID]
+
+      // THEN
+      expect(defaultProps()).toEqual(safes)
     })
   })
 }
