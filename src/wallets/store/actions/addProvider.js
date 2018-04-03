ef1727962e423af558eaf0805f471f9a63592269
apanizo <adolfo.panizo>
2018-04-03 17:50:25 +0200

    WA-280 Using Flow identity recommendations when creatingAction in addProvider

--- src/wallets/store/actions/addProvider.js
+++ src/wallets/store/actions/addProvider.js
@@ -1,6 +1,9 @@
 // @flow
 import { createAction } from 'redux-actions'
+import { type Provider } from '~/wallets/store/model/provider'
 
 export const ADD_PROVIDER = 'ADD_PROVIDER'
 
-export default createAction(ADD_PROVIDER)
+const addProvider = createAction(ADD_PROVIDER, (provider: Provider) => provider)
+
+export default addProvider
