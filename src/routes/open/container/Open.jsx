31cf970027ceb4316bfa1f858f6e72d65c017acf
apanizo <adolfo.panizo>
2018-04-04 09:47:02 +0200

    WA-280 call addSafe action when a safe is created a blockchain level

--- src/routes/open/container/Open.jsx
+++ src/routes/open/container/Open.jsx
@@ -4,14 +4,15 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import contract from 'truffle-contract'
 import PageFrame from '~/components/layout/PageFrame'
-import { getAccountsFrom, getThresholdFrom } from '~/routes/open/utils/safeDataExtractor'
+import { getAccountsFrom, getThresholdFrom, getNamesFrom, getSafeNameFrom } from '~/routes/open/utils/safeDataExtractor'
 import { getWeb3 } from '~/wallets/getWeb3'
 import { promisify } from '~/utils/promisify'
 import Safe from '#/GnosisSafe.json'
 import selector from './selector'
+import actions, { type Actions } from './actions'
 import Layout from '../components/Layout'
 
-type Props = {
+type Props = Actions & {
   provider: string,
   userAccount: string,
 }
@@ -35,20 +36,21 @@ class Open extends React.Component<Props, State> {
 
   onCallSafeContractSubmit = async (values) => {
     try {
-      const { userAccount } = this.props
+      const { userAccount, addSafe } = this.props
       const accounts = getAccountsFrom(values)
       const numConfirmations = getThresholdFrom(values)
+      const name = getSafeNameFrom(values)
+      const owners = getNamesFrom(values)
 
       const web3 = getWeb3()
       this.safe.setProvider(web3.currentProvider)
 
       const safeInstance = await this.safe.new(accounts, numConfirmations, 0, 0, { from: userAccount, gas: '5000000' })
-      const { transactionHash } = safeInstance
-
+      const { address, transactionHash } = safeInstance
       const transactionReceipt = await promisify(cb => web3.eth.getTransactionReceipt(transactionHash, cb))
-      // eslint-disable-next-line
-      console.log(`Transaction Receipt${JSON.stringify(transactionReceipt)}`)
-      this.setState({ safeAddress: safeInstance.address, safeTx: transactionReceipt })
+
+      addSafe(name, address, numConfirmations, owners, accounts)
+      this.setState({ safeAddress: address, safeTx: transactionReceipt })
     } catch (error) {
       // eslint-disable-next-line
       console.log('Error while creating the Safe' + error)
@@ -75,4 +77,4 @@ class Open extends React.Component<Props, State> {
   }
 }
 
-export default connect(selector)(Open)
+export default connect(selector, actions)(Open)
