3dea564a636383076e6c0853a19a6719ffb5cab9
apanizo <adolfo.panizo@gmail.com>
2018-04-11 12:21:35 +0200

    WA-280 Transforming TextField into PureComponent class for allowing JEST transversal selectors

--- src/components/forms/TextField/index.jsx
+++ src/components/forms/TextField/index.jsx
@@ -2,30 +2,33 @@
 import React from 'react'
 import MuiTextField, { TextFieldProps } from 'material-ui/TextField'
 
-const TextField = ({
-  input: {
-    name, onChange, value, ...restInput
-  },
-  meta,
-  render,
-  text,
-  ...rest
-}: TextFieldProps) => {
-  const helperText = value ? text : undefined
-  const showError = (meta.touched || !meta.pristine) && !meta.valid
+class TextField extends React.PureComponent<TextFieldProps> {
+  render() {
+    const {
+      input: {
+        name, onChange, value, ...restInput
+      },
+      meta,
+      render,
+      text,
+      ...rest
+    } = this.props
+    const helperText = value ? text : undefined
+    const showError = (meta.touched || !meta.pristine) && !meta.valid
 
-  return (
-    <MuiTextField
-      {...rest}
-      name={name}
-      helperText={showError ? meta.error : helperText}
-      error={meta.error && (meta.touched || !meta.pristine)}
-      inputProps={restInput}
-      onChange={onChange}
-      value={value}
-      fullWidth
-    />
-  )
+    return (
+      <MuiTextField
+        {...rest}
+        name={name}
+        helperText={showError ? meta.error : helperText}
+        error={meta.error && (meta.touched || !meta.pristine)}
+        inputProps={restInput}
+        onChange={onChange}
+        value={value}
+        fullWidth
+      />
+    )
+  }
 }
 
 export default TextField
