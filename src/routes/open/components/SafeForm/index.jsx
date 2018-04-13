47110948bc72f31a04094ba991b5d0de7e6e266b
apanizo <adolfo.panizo@gmail.com>
2018-04-13 09:40:05 +0200

    WA-280 Sending to validator a copy of already introduced addresses

--- src/routes/open/components/SafeForm/index.jsx
+++ src/routes/open/components/SafeForm/index.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import Block from '~/components/layout/Block'
 import Heading from '~/components/layout/Heading'
+import { getAccountsFrom } from '~/routes/open/utils/safeDataExtractor'
 import Name from './Name'
 import Owners from './Owners'
 import Confirmations from './Confirmations'
@@ -22,7 +23,7 @@ export default () => ({ values }: Object) => (
   <Block margin="md">
     <Heading tag="h2" margin="lg">Deploy a new Safe</Heading>
     <Name />
-    <Owners numOwners={values.owners} />
+    <Owners numOwners={values.owners} otherAccounts={getAccountsFrom(values)} />
     <Confirmations />
   </Block>
 )
