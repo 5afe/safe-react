4b39ebf98778f455e8673a51f1be5a4e6f0cd33e
apanizo <adolfo.panizo@gmail.com>
2018-04-26 17:52:39 +0200

    WA-238 Refactor Safe component leaving space for user interaction

---
+++ src/routes/safe/component/Safe/index.jsx
@@ -0,0 +1,58 @@
+// @flow
+import * as React from 'react'
+import Block from '~/components/layout/Block'
+import Col from '~/components/layout/Col'
+import Bold from '~/components/layout/Bold'
+import Paragraph from '~/components/layout/Paragraph'
+import Row from '~/components/layout/Row'
+import { type Safe } from '~/routes/safe/store/model/safe'
+import List from 'material-ui/List'
+
+import Address from './Address'
+import Balance from './Balance'
+import Owners from './Owners'
+import Confirmations from './Confirmations'
+
+type SafeProps = {
+  safe: Safe,
+  balance: string,
+}
+
+const listStyle = {
+  width: '100%',
+}
+
+class GnoSafe extends React.PureComponent<SafeProps> {
+  render() {
+    const { safe, balance } = this.props
+
+    return (
+      <Row>
+        <Col xs={12} top="xs" sm={4} margin="xl">
+          <List style={listStyle}>
+            <Balance balance={balance} />
+            <Owners owners={safe.owners} />
+            <Confirmations confirmations={safe.get('confirmations')} />
+            <Address address={safe.get('address')} />
+          </List>
+        </Col>
+        <Col xs={12} center="xs" sm={8} margin="xl" layout="block">
+          <Block margin="xl">
+            <Paragraph size="lg" noMargin align="right">
+              <Bold>{safe.name.toUpperCase()}</Bold>
+            </Paragraph>
+          </Block>
+          <Block>
+            Extra info will be placed here
+          </Block>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+/*
+        <Paragraph size="lg">
+          <Bold>{safe.name.toUpperCase()}</Bold>
+*/
+export default GnoSafe
