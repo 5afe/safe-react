b47214253641ce6343d7981130fd99beb4171bd5
apanizo <adolfo.panizo@gmail.com>
2018-05-08 13:45:00 +0200

    WA-238 Withdrawn Review component

---
+++ src/routes/safe/component/Withdrawn/Review.jsx
@@ -0,0 +1,25 @@
+// @flow
+import * as React from 'react'
+import Block from '~/components/layout/Block'
+import Bold from '~/components/layout/Bold'
+import Heading from '~/components/layout/Heading'
+import Paragraph from '~/components/layout/Paragraph'
+import { DESTINATION_PARAM, VALUE_PARAM } from './withdrawn'
+
+type FormProps = {
+  values: Object,
+}
+
+const Review = () => ({ values }: FormProps) => (
+  <Block>
+    <Heading tag="h2">Review the Withdrawn Operation</Heading>
+    <Paragraph>
+      <Bold>Destination: </Bold> {values[DESTINATION_PARAM]}
+    </Paragraph>
+    <Paragraph>
+      <Bold>Value in ETH: </Bold> {values[VALUE_PARAM]}
+    </Paragraph>
+  </Block>
+)
+
+export default Review
