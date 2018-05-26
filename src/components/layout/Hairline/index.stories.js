48a58040516231f1eecf02810861e6a35ef69c46
apanizo <adolfo.panizo@gmail.com>
2018-05-26 09:36:59 +0200

    WA-238 Hairline layout component

---
+++ src/components/layout/Hairline/index.stories.js
@@ -0,0 +1,14 @@
+// @flow
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import { host } from 'storybook-host'
+import Component from './index'
+
+storiesOf('Components', module)
+  .addDecorator(host({
+    title: 'Hairline',
+    align: 'center',
+    height: 5,
+    width: '100%',
+  }))
+  .add('Hairline', () => <Component />)
