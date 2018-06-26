847033a5c8560ec65ff98fec8f747b53ee0fab5c
apanizo <adolfo.panizo@gmail.com>
2018-06-26 13:46:17 +0200

    WA-232 Fix Flow errors regarding final form validator Field

--- src/components/forms/GnoForm/index.jsx
+++ src/components/forms/GnoForm/index.jsx
@@ -3,12 +3,14 @@ import * as React from 'react'
 import { type FormApi } from 'final-form'
 import { Form } from 'react-final-form'
 
+export type OnSubmit = (
+  values: Object,
+  form: FormApi,
+  callback: ?(errors: ?Object) => ?Object
+) => ?Object | Promise<?Object> | void
+
 type Props = {
-  onSubmit: (
-    values: Object,
-    form: FormApi,
-    callback: ?(errors: ?Object) => ?Object
-  ) => ?Object | Promise<?Object> | void,
+  onSubmit: OnSubmit,
   children: Function,
   padding: number,
   validation?: (values: Object) => Object | Promise<Object>,
