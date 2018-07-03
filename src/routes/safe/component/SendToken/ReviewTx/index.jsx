269aa54f37766c8af3e1678912f9edeb185fd9a8
apanizo <adolfo.panizo@gmail.com>
2018-07-03 13:20:14 +0200

    WA-232 Creating SendToken component

---
+++ src/routes/safe/component/SendToken/ReviewTx/index.jsx
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Block from '~/components/layout/Block'
+import Bold from '~/components/layout/Bold'
+import Heading from '~/components/layout/Heading'
+import Paragraph from '~/components/layout/Paragraph'
+import { TKN_DESTINATION_PARAM, TKN_VALUE_PARAM } from '~/routes/safe/component/SendToken/SendTokenForm/index'
+
+type FormProps = {
+  values: Object,
+  submitting: boolean,
+}
+
+const spinnerStyle = {
+  minHeight: '50px',
+}
+
+const ReviewTx = (symbol: string) => ({ values, submitting }: FormProps) => (
+  <Block>
+    <Heading tag="h2">Review the move token funds</Heading>
+    <Paragraph align="left">
+      <Bold>Destination: </Bold> {values[TKN_DESTINATION_PARAM]}
+    </Paragraph>
+    <Paragraph align="left">
+      <Bold>{`Amount to transfer: ${values[TKN_VALUE_PARAM]} ${symbol}`}</Bold>
+    </Paragraph>
+    <Block style={spinnerStyle}>
+      { submitting && <CircularProgress size={50} /> }
+    </Block>
+  </Block>
+)
+
+export default ReviewTx
