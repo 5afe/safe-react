c7a2a2ae5261267e4bc7537e8de67dadf20dffa5
apanizo <adolfo.panizo@gmail.com>
2018-07-13 09:28:04 +0200

    WA-232 Disabling cog while tokens get loaded

--- src/routes/safe/component/Safe/BalanceInfo.jsx
+++ src/routes/safe/component/Safe/BalanceInfo.jsx
@@ -50,7 +50,7 @@ const BalanceComponent = openHoc(({
         </Avatar>
         <ListItemText primary="Balance" secondary="List of different token balances" />
         <ListItemIcon>
-          <IconButton to={settingsUrl} component={Link} className={classes.button}>
+          <IconButton to={settingsUrl} disabled={!hasBalances} component={Link} className={classes.button}>
             <Settings />
           </IconButton>
         </ListItemIcon>
