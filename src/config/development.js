73099b38d632df82b82d7aa25abf7b5e83d13e61
apanizo <adolfo.panizo@gmail.com>
2018-08-16 13:43:35 +0200

    WA-521 Adding a feature flag for kwnowing when modules are accepted in TX-history service

--- src/config/development.js
+++ src/config/development.js
@@ -1,8 +1,9 @@
 // @flow
-import { TX_SERVICE_HOST } from '~/config/names'
+import { TX_SERVICE_HOST, ENABLED_TX_SERVICE_MODULES } from '~/config/names'
 
 const devConfig = {
   [TX_SERVICE_HOST]: 'https://safe-transaction-history.dev.gnosisdev.com/api/v1/',
+  [ENABLED_TX_SERVICE_MODULES]: false,
 }
 
 export default devConfig
--- src/config/names.js
+++ src/config/names.js
@@ -1,3 +1,4 @@
 // @flow
 
 export const TX_SERVICE_HOST = 'tsh'
+export const ENABLED_TX_SERVICE_MODULES = 'tsm'
