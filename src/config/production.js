b960ecefe8cabe9041a21de73e075250858a32c1
apanizo <adolfo.panizo@gmail.com>
2018-08-22 13:38:35 +0200

    WA-521 Adding feature flag for use metamask provider as signer

--- src/config/production.js
+++ src/config/production.js
@@ -1,10 +1,16 @@
 // @flow
-import { TX_SERVICE_HOST, ENABLED_TX_SERVICE_MODULES, ENABLED_TX_SERVICE_REMOVAL_SENDER } from '~/config/names'
+import {
+  TX_SERVICE_HOST,
+  ENABLED_TX_SERVICE_MODULES,
+  ENABLED_TX_SERVICE_REMOVAL_SENDER,
+  SIGNATURES_VIA_METAMASK,
+} from '~/config/names'
 
 const prodConfig = {
   [TX_SERVICE_HOST]: 'https://safe-transaction-history.dev.gnosisdev.com/api/v1/',
   [ENABLED_TX_SERVICE_MODULES]: false,
   [ENABLED_TX_SERVICE_REMOVAL_SENDER]: false,
+  [SIGNATURES_VIA_METAMASK]: true,
 }
 
 export default prodConfig
