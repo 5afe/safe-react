8707ba91042ffec221a69db19fd678c5d70e58b3
apanizo <adolfo.panizo@gmail.com>
2018-08-31 12:23:51 +0200

    Making no round button as default

--- src/components/layout/Button/index.jsx
+++ src/components/layout/Button/index.jsx
@@ -1,4 +1,11 @@
 // @flow
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
 
-export default Button
+const styles = {
+  root: {
+    borderRadius: 0,
+  },
+}
+
+export default withStyles(styles)(Button)
