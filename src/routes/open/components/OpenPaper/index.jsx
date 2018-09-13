29c9b786faf96259f957cbe73af0781712ffe97c
apanizo <adolfo.panizo@gmail.com>
2018-09-13 17:08:30 +0200

    Creating a OpenPaper component for integration it in the Open Safe Stepper

---
+++ src/routes/open/components/OpenPaper/index.jsx
@@ -0,0 +1,32 @@
+// @flow
+import * as React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Block from '~/components/layout/Block'
+import { lg } from '~/theme/variables'
+
+const styles = () => ({
+  root: {
+    padding: lg,
+    maxWidth: '770px',
+  },
+  container: {
+    maxWidth: '600px',
+    letterSpacing: '-0.5px',
+  },
+})
+
+type Props = {
+  classes: Object,
+  children: React$Node,
+}
+
+const OpenPaper = ({ classes, children }: Props) => (
+  <Paper className={classes.root} elevation={1}>
+    <Block className={classes.container}>
+      {children}
+    </Block>
+  </Paper>
+)
+
+export default withStyles(styles)(OpenPaper)
