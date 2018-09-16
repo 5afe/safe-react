b594d3098c8431d2c345a27561abe18c6d7a239b
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-09-16 09:46:16 +0200

    Adding undefined to getSize helper function

--- src/components/layout/Hairline/index.js
+++ src/components/layout/Hairline/index.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { type Size, getSize } from '~/theme/size'
 import { border } from '~/theme/variables'
 
-const calculateStyleFrom = (margin: Size) => ({
+const calculateStyleFrom = (margin?: Size) => ({
   width: '100%',
   height: '1px',
   backgroundColor: border,
