166ea9c1ca7e9eef7af3e20e820b1d566d2cba4e
apanizo <adolfo.panizo@gmail.com>
2018-09-28 09:55:43 +0200

    Enabling EIP 712 in production

--- src/config/production.js
+++ src/config/production.js
@@ -10,7 +10,7 @@ const prodConfig = {
   [TX_SERVICE_HOST]: 'https://safe-transaction-history.dev.gnosisdev.com/api/v1/',
   [ENABLED_TX_SERVICE_MODULES]: false,
   [ENABLED_TX_SERVICE_REMOVAL_SENDER]: false,
-  [SIGNATURES_VIA_METAMASK]: false,
+  [SIGNATURES_VIA_METAMASK]: true,
 }
 
 export default prodConfig
