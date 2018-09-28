a4ea9e055c6722ac4548ca63ed6b4751caeb3d41
apanizo <adolfo.panizo@gmail.com>
2018-09-28 14:26:14 +0200

    Adding storybook instance for opening route

---
+++ src/routes/opening/Layout.stories.js
@@ -0,0 +1,22 @@
+// @flow
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import styles from '~/components/layout/PageFrame/index.scss'
+import { ETHEREUM_NETWORK } from '~/logic/wallets/getWeb3'
+import Component from './component'
+
+const FrameDecorator = story => (
+  <div className={styles.frame}>
+    { story() }
+  </div>
+)
+
+storiesOf('Routes /opening', module)
+  .addDecorator(FrameDecorator)
+  .add('View while safe is being deployed', () => (
+    <Component
+      name="Super Vault 2000"
+      tx="0xed163e50e2e85695f5edafeba51d6be1758549858d12611ed4dcc96feaa19fc9"
+      network={ETHEREUM_NETWORK.RINKEBY}
+    />
+  ))
