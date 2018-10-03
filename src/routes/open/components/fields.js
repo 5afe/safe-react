75edd7262ee2042b44919ff9f2e71a463e169253
apanizo <adolfo.panizo@gmail.com>
2018-10-03 17:39:09 +0200

    Helper function to calculate num owners introduced

--- src/routes/open/components/fields.js
+++ src/routes/open/components/fields.js
@@ -6,3 +6,9 @@ export const FIELD_DAILY_LIMIT: string = 'limit'
 
 export const getOwnerNameBy = (index: number) => `owner${index}Name`
 export const getOwnerAddressBy = (index: number) => `owner${index}Address`
+
+export const getNumOwnersFrom = (values: Object) => {
+  const accounts = Object.keys(values).sort().filter(key => /^owner\d+Name$/.test(key))
+
+  return accounts.length
+}
