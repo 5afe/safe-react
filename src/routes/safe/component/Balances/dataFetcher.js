22a5da172287138863c8c7d192e75ad34f584ffd
apanizo <adolfo.panizo@gmail.com>
2018-10-24 16:19:23 +0200

    Remove the value column in Safe Overview

--- src/routes/safe/component/Balances/dataFetcher.js
+++ src/routes/safe/component/Balances/dataFetcher.js
@@ -18,44 +18,32 @@ export const getBalanceData = (): Array<BalanceRow> => [
     [BALANCE_TABLE_ASSET_ID]: 'CVL Journalism',
     [BALANCE_TABLE_BALANCE_ID]: '234 CVL',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 234,
-    [BALANCE_TABLE_VALUE_ID]: '$6700',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 6700,
   },
   {
     [BALANCE_TABLE_ASSET_ID]: 'ABC Periodico',
     [BALANCE_TABLE_BALANCE_ID]: '1.394 ABC',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 1.394,
-    [BALANCE_TABLE_VALUE_ID]: '$3.1',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 3.1,
   },
   {
     [BALANCE_TABLE_ASSET_ID]: 'Ethereum',
     [BALANCE_TABLE_BALANCE_ID]: '9.394 ETH',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 9.394,
-    [BALANCE_TABLE_VALUE_ID]: '$539.45',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 539.45,
     [FIXED]: true,
   },
   {
     [BALANCE_TABLE_ASSET_ID]: 'Gnosis',
     [BALANCE_TABLE_BALANCE_ID]: '0.599 GNO',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 0.559,
-    [BALANCE_TABLE_VALUE_ID]: '$23.11',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 23.11,
   },
   {
     [BALANCE_TABLE_ASSET_ID]: 'OmiseGO',
     [BALANCE_TABLE_BALANCE_ID]: '39.922 OMG',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 39.922,
-    [BALANCE_TABLE_VALUE_ID]: '$2930.89',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 2930.89,
   },
   {
     [BALANCE_TABLE_ASSET_ID]: 'Moe Feo',
     [BALANCE_TABLE_BALANCE_ID]: '0 MOE',
     [buildOrderFieldFrom(BALANCE_TABLE_BALANCE_ID)]: 0,
-    [BALANCE_TABLE_VALUE_ID]: '$0',
-    [buildOrderFieldFrom(BALANCE_TABLE_VALUE_ID)]: 0,
   },
 ]
 
@@ -78,15 +66,6 @@ export const generateColumns = () => {
     custom: false,
   }
 
-  const valueRow: Column = {
-    id: BALANCE_TABLE_VALUE_ID,
-    order: true,
-    numeric: true,
-    disablePadding: false,
-    label: 'Value',
-    custom: false,
-  }
-
   const actions: Column = {
     id: 'actions',
     order: false,
@@ -96,7 +75,7 @@ export const generateColumns = () => {
     custom: true,
   }
 
-  return List([assetRow, balanceRow, valueRow, actions])
+  return List([assetRow, balanceRow, actions])
 }
 
 export const filterByZero = (data: Array<BalanceRow>, hideZero: boolean): Array<BalanceRow> =>
