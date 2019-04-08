6e9799bd17cc8e2283f2ca08d043836405f09180
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-08 11:19:39 +0400

    remove cookie store and session storage

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -1,9 +1,12 @@
 // @flow
 import {
-  ImmortalStorage, CookieStore, IndexedDbStore, LocalStorageStore, SessionStorageStore,
+  ImmortalStorage, IndexedDbStore, LocalStorageStore,
 } from 'immortal-db'
 
-const stores = [CookieStore, IndexedDbStore, LocalStorageStore, SessionStorageStore]
+// Don't use sessionStorage and cookieStorage
+// https://github.com/gruns/ImmortalDB/issues/22
+// https://github.com/gruns/ImmortalDB/issues/6
+const stores = [IndexedDbStore, LocalStorageStore]
 export const storage = new ImmortalStorage(stores)
 
 const PREFIX = 'v1'
