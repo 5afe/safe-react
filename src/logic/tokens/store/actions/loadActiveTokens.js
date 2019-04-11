3530d2f3056a2d5a1ddcf63a437177ad5f7c2d96
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-11 20:24:14 +0400

    remove eth from list of active tokens

--- src/logic/tokens/store/actions/loadActiveTokens.js
+++ src/logic/tokens/store/actions/loadActiveTokens.js
@@ -4,19 +4,14 @@ import { List, Map } from 'immutable'
 import { type TokenProps, type Token, makeToken } from '~/logic/tokens/store/model/token'
 import { type GlobalState } from '~/store/index'
 import { getActiveTokens } from '~/logic/tokens/utils/tokensStorage'
-import { getEthAsToken } from '~/logic/tokens/utils/tokenHelpers'
 import saveTokens from './saveTokens'
 
 const loadActiveTokens = (safeAddress: string) => async (dispatch: ReduxDispatch<GlobalState>) => {
   try {
     const tokens: List<TokenProps> = await getActiveTokens(safeAddress)
 
-    // ETH is active by default and cannot be disabled
-    const eth = await getEthAsToken(safeAddress)
-
     const tokenRecords: Map<string, Token> = Map().withMutations((map) => {
       tokens.forEach(token => map.set(token.address, makeToken(token)))
-      map.set(eth.address, eth)
     })
 
     dispatch(saveTokens(safeAddress, tokenRecords))
--- src/logic/tokens/utils/tokenHelpers.js
+++ src/logic/tokens/utils/tokenHelpers.js
@@ -7,20 +7,20 @@ import { makeToken, type Token } from '~/logic/tokens/store/model/token'
 export const ETH_ADDRESS = '0x000'
 export const isEther = (symbol: string) => symbol === 'ETH'
 
-export const getEthAsToken = async (safeAddress: string) => {
-  const balance = await getBalanceInEtherOf(safeAddress)
+// export const getEthAsToken = async (safeAddress: string) => {
+//   const balance = await getBalanceInEtherOf(safeAddress)
 
-  const ethBalance = makeToken({
-    address: ETH_ADDRESS,
-    name: 'Ether',
-    symbol: 'ETH',
-    decimals: 18,
-    logoUri: logo,
-    funds: balance,
-  })
+//   const ethBalance = makeToken({
+//     address: ETH_ADDRESS,
+//     name: 'Ether',
+//     symbol: 'ETH',
+//     decimals: 18,
+//     logoUri: logo,
+//     funds: balance,
+//   })
 
-  return ethBalance
-}
+//   return ethBalance
+// }
 
 export const calculateActiveErc20TokensFrom = (tokens: List<Token>) => {
   const activeTokens = List().withMutations(list => tokens.forEach((token: Token) => {
