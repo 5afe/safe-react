2c8a79e58289f17c9888e682bab86292bf3a0f52
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-15 22:21:39 +0400

    remove cdu, otherwise it will fail on many token updates because of inconsistent request finishes, debouncing tbd

--- src/routes/safe/store/actions/updateActiveTokens.js
+++ src/routes/safe/store/actions/updateActiveTokens.js
@@ -6,7 +6,8 @@ import type { SafeToken } from '~/routes/safe/store/models/safeToken'
 import SafeTokenRecord from '~/routes/safe/store/models/safeToken'
 import { safeTokensSelector } from '~/routes/safe/store/selectors/index'
 import { SAFE_PARAM_ADDRESS } from '~/routes/routes'
-import updateSafe from './updateSafe'
+import type { Token } from '~/logic/tokens/store/model/token'
+import updateSafe from '~/routes/safe/store/actions/updateSafe'
 
 // the selector uses ownProps argument/router props to get the address of the safe
 // so in order to use it I had to recreate the same structure
@@ -18,11 +19,11 @@ const generateMatchProps = (safeAddress: string) => ({
   },
 })
 
-const updateActiveTokens = (safeAddress: string, tokenAddress: string) => async (
+const updateActiveTokens = (safeAddress: string, token: Token) => async (
   dispatch: ReduxDispatch<GlobalState>,
   getState: GetState<GlobalState>,
 ) => {
-  const token: SafeToken = SafeTokenRecord({ address: tokenAddress })
+  const safeTokenInstance: SafeToken = SafeTokenRecord({ address: token.address })
   const state = getState()
   const safeTokens: List<SafeToken> = safeTokensSelector(state, generateMatchProps(safeAddress))
   const index = safeTokens.findIndex(safeToken => safeToken.address === token.address)
@@ -31,7 +32,7 @@ const updateActiveTokens = (safeAddress: string, tokenAddress: string) => async 
   if (index !== -1) {
     updatedTokens = safeTokens.delete(index)
   } else {
-    updatedTokens = safeTokens.push(token)
+    updatedTokens = safeTokens.push(safeTokenInstance)
   }
 
   dispatch(updateSafe({ address: safeAddress, tokens: updatedTokens }))
