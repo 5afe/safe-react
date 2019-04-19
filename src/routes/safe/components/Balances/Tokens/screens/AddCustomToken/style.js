8adc1ee25a0af8a4f9e1101e1d714bacc0391c4d
mmv <mmvsha73@gmail.com>
2019-04-19 17:55:23 +0400

    font size fix

--- src/routes/safe/components/Balances/Tokens/screens/AddCustomToken/style.js
+++ src/routes/safe/components/Balances/Tokens/screens/AddCustomToken/style.js
@@ -3,7 +3,8 @@ import { lg, bolderFont } from '~/theme/variables'
 
 export const styles = () => ({
   title: {
-    padding: `${lg}`,
+    padding: `${lg} 20px 20px`,
     fontWeight: bolderFont,
+    fontSize: '16px',
   },
 })
