4b1a0f785d45ce82be72d83cc387a5ed07fe6111
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-23 13:54:10 +0400

    pass token address as a param in fetch token function

--- src/logic/tokens/api/fetchToken.js
+++ src/logic/tokens/api/fetchToken.js
@@ -4,9 +4,13 @@ import { getRelayUrl } from '~/config/index'
 
 const fetchToken = (tokenAddress: string) => {
   const apiUrl = getRelayUrl()
-  const url = `${apiUrl}/tokens/${tokenAddress}`
+  const url = `${apiUrl}/tokens/`
 
-  return axios.get(url)
+  return axios.get(url, {
+    params: {
+      address: tokenAddress,
+    },
+  })
 }
 
 export default fetchToken
