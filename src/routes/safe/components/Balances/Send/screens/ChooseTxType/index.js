f6f9076fab7d60b495c7e60af3e894551a834421
mmv <mmvsha73@gmail.com>
2019-04-29 13:03:25 +0400

    wip chooseTxType screen

--- src/routes/safe/components/Balances/Send/screens/ChooseTxType/index.js
+++ src/routes/safe/components/Balances/Send/screens/ChooseTxType/index.js
@@ -5,12 +5,14 @@ import Close from '@material-ui/icons/Close'
 import IconButton from '@material-ui/core/IconButton'
 import Paragraph from '~/components/layout/Paragraph'
 import Row from '~/components/layout/Row'
+import Hairline from '~/components/layout/Hairline'
 import { lg, md } from '~/theme/variables'
 
 const styles = () => ({
   heading: {
     padding: `${md} ${lg}`,
     justifyContent: 'space-between',
+    maxHeight: '75px',
   },
   manage: {
     fontSize: '24px',
@@ -29,7 +31,7 @@ type Props = {
 const Send = ({ classes, onClose }: Props) => (
   <React.Fragment>
     <Row align="center" grow className={classes.heading}>
-      <Paragraph className={classes.manage} noMargin>Send Funds</Paragraph>
+      <Paragraph className={classes.manage} noMargin>Send</Paragraph>
       <IconButton onClick={onClose} disableRipple>
         <Close className={classes.close} />
       </IconButton>
