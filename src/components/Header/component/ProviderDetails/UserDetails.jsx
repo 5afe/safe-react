98aeccf718be3bd02f03efd9f4c491b7cf6b226e
Germán Martínez <germaga92@gmail.com>
2019-06-03 13:38:12 +0200

    Show Safe logo when Safe is connected

--- src/components/Header/component/ProviderDetails/UserDetails.jsx
+++ src/components/Header/component/ProviderDetails/UserDetails.jsx
@@ -21,7 +21,8 @@ import { shortVersionOf } from '~/logic/wallets/ethAddresses'
 import { getEtherScanLink } from '~/logic/wallets/getWeb3'
 import CircleDot from '~/components/Header/component/CircleDot'
 
-const metamask = require('../../assets/metamask.svg')
+const metamaskIcon = require('../../assets/metamask-icon.svg')
+const safeIcon = require('../../assets/gnosis-safe-icon.svg')
 const dot = require('../../assets/dotRinkeby.svg')
 
 type Props = {
@@ -145,7 +146,10 @@ const UserDetails = ({
           Client
         </Paragraph>
         <Spacer />
-        <Img className={classes.logo} src={metamask} height={14} alt="Metamask client" />
+        {provider === 'safe'
+          ? <Img className={classes.logo} src={safeIcon} height={14} alt="Safe client" />
+          : <Img className={classes.logo} src={metamaskIcon} height={14} alt="Metamask client" />
+        }
         <Paragraph noMargin align="right" weight="bolder" className={classes.labels}>
           {upperFirst(provider)}
         </Paragraph>
