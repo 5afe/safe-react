0932f34a6808c349f79e2710986225c2ab03f3c9
Mikhail Mikheev <mmvsha73@gmail.com>
2019-09-05 14:06:04 +0400

    increase minHeight for settings to avoid jumping

--- src/routes/safe/components/Settings/style.js
+++ src/routes/safe/components/Settings/style.js
@@ -7,7 +7,7 @@ export const styles = () => ({
   root: {
     backgroundColor: 'white',
     boxShadow: '1px 2px 10px 0 rgba(212, 212, 211, 0.59)',
-    minHeight: '400px',
+    minHeight: '505px',
     display: 'flex',
     borderRadius: '8px',
   },
