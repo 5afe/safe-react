053d87e0b0e53670bcadca4c6687eb730939bfb1
Germán Martínez <germaga92@gmail.com>
2019-09-10 13:08:32 +0200

    Update transaction history endpoints

--- src/config/development.js
+++ src/config/development.js
@@ -6,7 +6,7 @@ import {
 } from '~/config/names'
 
 const devConfig = {
-  [TX_SERVICE_HOST]: 'https://safe-transaction-service.staging.gnosisdev.com/api/v1/',
+  [TX_SERVICE_HOST]: 'https://safe-transaction.staging.gnosisdev.com/api/v1/',
   [SIGNATURES_VIA_METAMASK]: false,
   [RELAY_API_URL]: 'https://safe-relay.staging.gnosisdev.com/api/v1/',
 }
