957c06879f4ec7a25871776b0179fba8475ea84e
mmv <mmvsha73@gmail.com>
2019-09-23 18:53:13 +0400

    Add default safe test

--- src/test/safe.dom.sidebar.test.js
+++ src/test/safe.dom.sidebar.test.js
@@ -6,6 +6,7 @@ import { renderSafeView } from '~/test/builder/safe.dom.utils'
 import '@testing-library/jest-dom/extend-expect'
 import { TOGGLE_SIDEBAR_BTN_TESTID } from '~/components/Header/component/SafeListHeader'
 import { SIDEBAR_SAFELIST_ROW_TESTID } from '~/components/Sidebar/SafeList'
+import { sleep } from '~/utils/timer'
 
 describe('DOM > Feature > Sidebar', () => {
   let store
@@ -26,5 +27,29 @@ describe('DOM > Feature > Sidebar', () => {
     expect(safes.length).toBe(1)
 
     expect(safes[0]).toContainElement(SafeDom.getByText('default'))
+  })
+
+  it('Changes default safe', async () => {
+    const SafeDom = await renderSafeView(store, safeAddress)
+    await aMinedSafe(store)
+
+    await sleep(2000)
+
+    act(() => {
+      fireEvent.click(SafeDom.getByTestId(TOGGLE_SIDEBAR_BTN_TESTID))
+    })
+
+    const safes = SafeDom.getAllByTestId(SIDEBAR_SAFELIST_ROW_TESTID)
+    expect(safes.length).toBe(2)
+
+    expect(safes[1]).toContainElement(SafeDom.getByText('default'))
+    expect(safes[0]).toContainElement(SafeDom.getByText('Make default'))
+
+    act(() => {
+      fireEvent.click(SafeDom.getByText('Make default'))
+    })
+
+    expect(safes[0]).toContainElement(SafeDom.getByText('default'))
+    expect(safes[1]).toContainElement(SafeDom.getByText('Make default'))
   })
 })
