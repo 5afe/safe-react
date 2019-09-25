550fdef729f66628624c50282c0cffcd54584dd6
Mikhail Mikheev <mmvsha73@gmail.com>
2019-09-25 14:23:59 +0400

    update web3connect to beta.22

--- src/components/ConnectButton/index.jsx
+++ src/components/ConnectButton/index.jsx
@@ -1,20 +1,32 @@
 // @flow
 import * as React from 'react'
 import Web3Connect from 'web3connect'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import Portis from '@portis/web3'
+import Fortmatic from 'fortmatic'
 import Button from '~/components/layout/Button'
 import Web3Integration from '~/logic/wallets/web3Integration'
 
 const web3Connect = new Web3Connect.Core({
   network: 'rinkeby',
   providerOptions: {
-    walletConnect: {
-      infuraId: process.env.INFURA_ID,
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId: process.env.INFURA_ID,
+      },
     },
     portis: {
-      id: '852b763d-f28b-4463-80cb-846d7ec5806b',
+      package: Portis,
+      options: {
+        id: '852b763d-f28b-4463-80cb-846d7ec5806b',
+      },
     },
     fortmatic: {
-      key: 'pk_test_43A53775AE976718',
+      package: Fortmatic,
+      options: {
+        key: 'pk_test_43A53775AE976718',
+      },
     },
   },
 })
