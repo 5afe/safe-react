f5d324fff8d1e727752d37d93b34876feacc903a
Mikhail Mikheev <mmvsha73@gmail.com>
2019-10-01 19:31:25 +0400

    formatAmounts wip

--- src/logic/tokens/utils/formatAmount.js
+++ src/logic/tokens/utils/formatAmount.js
@@ -1,9 +1,10 @@
 // @flow
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
 export const formatAmount = (number: string | number) => {
   let numberFloat = parseFloat(number)
 
   if (numberFloat < 999.999999) {
-    numberFloat = numberFloat.toFixed(2).toLocaleString()
+    numberFloat = numberFloat.toFixed(5).toLocaleString()
   }
 
   return numberFloat
