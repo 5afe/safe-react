4af205fbe3105c8387b4c9aa22326bde6d15da57
apane <agustin.pane@gmail.com>
2019-11-25 10:10:43 -0300

    Finish cookie banner implementation

--- src/logic/cookies/store/selectors/index.js
+++ src/logic/cookies/store/selectors/index.js
@@ -2,4 +2,4 @@
 import { type GlobalState } from '~/store'
 import { COOKIE_REDUCER_ID } from '~/logic/cookies/store/reducer/cookies'
 
-export const cookiesSelector = (state: GlobalState) => state[COOKIE_REDUCER_ID]
+export const cookiesSelector = (state: GlobalState) => state[COOKIE_REDUCER_ID].toJS()
