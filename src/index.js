26b420742db77d3ae1ec834ad60de05a44533f3c
apane <agustin.pane@gmail.com>
2019-11-26 11:03:00 -0300

    Fix cookieStore deletion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from '~/components/Root'
 import loadActiveTokens from '~/logic/tokens/store/actions/loadActiveTokens'
-import loadCookiesFromStorage from '~/logic/cookies/store/actions/loadCookiesFromStorage'
 import loadDefaultSafe from '~/routes/safe/store/actions/loadDefaultSafe'
 import loadSafesFromStorage from '~/routes/safe/store/actions/loadSafesFromStorage'
 import { store } from '~/store'
@@ -19,7 +18,6 @@ if (process.env.NODE_ENV !== 'production') {
 store.dispatch(loadActiveTokens())
 store.dispatch(loadSafesFromStorage())
 store.dispatch(loadDefaultSafe())
-store.dispatch(loadCookiesFromStorage())
 
 const root = document.getElementById('root')
 
