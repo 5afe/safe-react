d1ea3821bb948354773b98e2adb049ee59034d07
fernandomg <fernando.greco@gmail.com>
2019-11-28 11:33:42 -0300

    Retrieve balance from API

--- src/logic/tokens/store/actions/activeTokensByBalance.js
+++ src/logic/tokens/store/actions/activeTokensByBalance.js
@@ -1,41 +1,41 @@
 // @flow
 import type { Dispatch as ReduxDispatch } from 'redux'
-import BigNumber from 'bignumber.js'
-import { List, Set } from 'immutable'
+import { Set } from 'immutable'
 import { type GetState, type GlobalState } from '~/store'
-import { tokenListSelector } from '~/logic/tokens/store/selectors'
-import { calculateBalanceOf } from '~/routes/safe/store/actions/fetchTokenBalances'
 import updateActiveTokens from '~/routes/safe/store/actions/updateActiveTokens'
 import {
   safeActiveTokensSelectorBySafe,
   safeBlacklistedTokensSelectorBySafe,
   safesMapSelector,
 } from '~/routes/safe/store/selectors'
+import fetchTokenBalanceList from '~/logic/tokens/api/fetchTokenBalanceList'
+import updateSafe from '~/routes/safe/store/actions/updateSafe'
 
 const activeTokensByBalance = (safeAddress: string) => async (dispatch: ReduxDispatch<GlobalState>, getState: GetState) => {
   try {
-    const state = getState()
-    const tokens = tokenListSelector(state)
-    const safes = safesMapSelector(state)
-    const activeTokens = safeActiveTokensSelectorBySafe(safeAddress, safes)
+    const result = await fetchTokenBalanceList(safeAddress)
+    const safes = safesMapSelector(getState())
+    const alreadyActiveTokens = safeActiveTokensSelectorBySafe(safeAddress, safes)
     const blacklistedTokens = safeBlacklistedTokensSelectorBySafe(safeAddress, safes)
 
-    const activeByBalance = (await Promise.all(
-      tokens.map(async (token) => {
-        const balance = await calculateBalanceOf(token.address, safeAddress, token.decimals)
-        return {
-          address: token.address,
-          balance,
-        }
-      }),
-    ))
-      .filter(({ address }) => !blacklistedTokens.includes(address))
-      .filter(({ address, balance }) => activeTokens.includes(address) || BigNumber(balance).gt(0))
-      .map(({ address }) => address)
+    // addresses: potentially active tokens by balance
+    // balances: tokens' balance returned by the backend
+    const { addresses, balances } = result.data.reduce((acc, { tokenAddress, balance }) => ({
+      addresses: [...acc.addresses, tokenAddress],
+      balances: [...acc.balances, balance],
+    }), { addresses: [], balances: [] })
 
-    dispatch(updateActiveTokens(safeAddress, Set(activeByBalance)))
+    // update balance list for the safe
+    dispatch(updateSafe({ address: safeAddress, balances: Set(balances) }))
 
-    return List(activeByBalance)
+    // active tokens by balance, excluding those already blacklisted and the `null` address
+    const activeByBalance = addresses.filter((address) => address !== null && !blacklistedTokens.includes(address))
+
+    // need to persist those already active tokens, despite its balances
+    const activeTokens = alreadyActiveTokens.toSet().union(activeByBalance)
+
+    // update list of active tokens
+    dispatch(updateActiveTokens(safeAddress, activeTokens))
   } catch (err) {
     console.error('Error fetching token list', err)
 
