eb8adb501d38d6b4542e4577fefd42201ecbee82
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-11-28 15:40:37 -0300

    (add) dropdown styles

--- src/routes/safe/components/DropdownCurrency/index.jsx
+++ src/routes/safe/components/DropdownCurrency/index.jsx
@@ -1,30 +1,91 @@
 // @flow
-import React, { useState } from 'react'
-import { withStyles } from '@material-ui/core'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import { useDispatch, useSelector } from 'react-redux'
-import Menu, { MenuProps } from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import Button from '@material-ui/core/Button'
-import { setCurrencySelected } from '~/logic/currencyValues/store/actions/setCurrencySelected'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import React, { useState } from 'react'
+import style from 'currency-flags/dist/currency-flags.min.css'
+import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles'
+import { useDispatch, useSelector } from 'react-redux'
+import { DropdownListTheme } from '~/theme/mui'
 import { currencyValuesListSelector } from '~/logic/currencyValues/store/selectors'
+import { setCurrencySelected } from '~/logic/currencyValues/store/actions/setCurrencySelected'
+import CheckIcon from './img/check.svg'
 
-const styles = () => ({
-  dropdownContainer: {
-
+const buttonWidth = '140px'
+const useStyles = makeStyles({
+  listItem: {
+    minWidth: buttonWidth,
+  },
+  localFlag: {
+    backgroundPosition: '50% 50%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    height: '20px !important',
+    width: '26px !important',
+  },
+  iconLeft: {
+    marginRight: '10px',
+  },
+  iconRight: {
+    marginLeft: '18px',
+  },
+  button: {
+    backgroundColor: '#e8e7e6',
+    border: 'none',
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    color: '#5d6d74',
+    cursor: 'pointer',
+    fontSize: '12px',
+    fontWeight: 'normal',
+    height: '24px',
+    lineHeight: '1.33',
+    marginRight: '20px',
+    minWidth: buttonWidth,
+    outline: 'none',
+    padding: '0',
+    textAlign: 'left',
+    '&:active': {
+      opacity: '0.8',
+    },
+  },
+  buttonInner: {
+    boxSizing: 'border-box',
+    display: 'block',
+    height: '100%',
+    lineHeight: '24px',
+    padding: '0 22px 0 8px',
+    position: 'relative',
+    width: '100%',
+    '&::after': {
+      borderLeft: '5px solid transparent',
+      borderRight: '5px solid transparent',
+      borderTop: '5px solid #5d6d74',
+      content: '""',
+      height: '0',
+      position: 'absolute',
+      right: '8px',
+      top: '9px',
+      width: '0',
+    },
+  },
+  openMenuButton: {
+    '&::after': {
+      borderBottom: '5px solid #5d6d74',
+      borderLeft: '5px solid transparent',
+      borderRight: '5px solid transparent',
+      borderTop: 'none',
+    },
   },
 })
 
-const DropdownCurrency = ({
-  classes,
-}: Props) => {
+const DropdownCurrency = () => {
   const currencyPairList = useSelector(currencyValuesListSelector)
   const dispatch = useDispatch()
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const [currentCurrency, setCurrentCurrency] = useState('USD')
+  const classes = useStyles()
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
@@ -50,44 +111,49 @@ const DropdownCurrency = ({
   }
 
   return (
-    <div className={classes.dropdownContainer}>
-      <Button
-        aria-controls="customized-menu"
-        aria-haspopup="true"
-        color="primary"
-        onClick={handleClick}
-        variant="contained"
-      >
-        {currentCurrency}
-      </Button>
-      <Menu
-        anchorEl={anchorEl}
-        elevation={0}
-        getContentAnchorEl={null}
-        id="customizedMenu"
-        keepMounted
-        onClose={handleClose}
-        open={Boolean(anchorEl)}
-        anchorOrigin={{
-          horizontal: 'center',
-          vertical: 'bottom',
-        }}
-        transformOrigin={{
-          horizontal: 'center',
-          vertical: 'top',
-        }}
-      >
-        {currencyPairList.valueSeq().map((dropdownValue) => (
-          <MenuItem key={dropdownValue.currencyName} value={dropdownValue.currencyName} onClick={() => onCurrentCurrencyChangedHandler(dropdownValue.currencyName)}>
-            <ListItemIcon>
-              <div>ICO - </div>
-            </ListItemIcon>
-            <ListItemText primary={dropdownValue.currencyName} />
-          </MenuItem>
-        ))}
-      </Menu>
-    </div>
+    <MuiThemeProvider theme={DropdownListTheme}>
+      <>
+        <button
+          className={classes.button}
+          onClick={handleClick}
+          type="button"
+        >
+          <span className={`${classes.buttonInner} ${anchorEl ? classes.openMenuButton : ''}`}>
+            {currentCurrency}
+          </span>
+        </button>
+        <Menu
+          anchorEl={anchorEl}
+          elevation={0}
+          rounded={0}
+          getContentAnchorEl={null}
+          id="customizedMenu"
+          keepMounted
+          onClose={handleClose}
+          open={Boolean(anchorEl)}
+          square
+          anchorOrigin={{
+            horizontal: 'center',
+            vertical: 'bottom',
+          }}
+          transformOrigin={{
+            horizontal: 'center',
+            vertical: 'top',
+          }}
+        >
+          {currencyPairList.valueSeq().map((dropdownValue) => (
+            <MenuItem className={classes.listItem} key={dropdownValue.currencyName} value={dropdownValue.currencyName} onClick={() => onCurrentCurrencyChangedHandler(dropdownValue.currencyName)}>
+              <ListItemIcon className={classes.iconLeft}>
+                <div className={`${classes.localFlag} ${style['currency-flag']} ${style['currency-flag-lg']} ${style[`currency-flag-${dropdownValue.currencyName.toLowerCase()}`]}`} />
+              </ListItemIcon>
+              <ListItemText primary={dropdownValue.currencyName} />
+              {dropdownValue.currencyName === currentCurrency ? <ListItemIcon className={classes.iconRight}><img src={CheckIcon} alt="" /></ListItemIcon> : null}
+            </MenuItem>
+          ))}
+        </Menu>
+      </>
+    </MuiThemeProvider>
   )
 }
 
-export default withStyles(styles)(DropdownCurrency)
+export default DropdownCurrency
