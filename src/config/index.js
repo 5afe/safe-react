f02434fe26e6845a4a8b3d3beb84e76e3f82b1d8
Mikhail Mikheev <mmvsha73@gmail.com>
2019-12-02 17:00:11 +0400

    Add env variable for production intercom id

--- src/config/index.js
+++ src/config/index.js
@@ -46,4 +46,4 @@ export const signaturesViaMetamask = () => {
   return config[SIGNATURES_VIA_METAMASK]
 }
 
-export const getIntercomId = () => (process.env.NODE_ENV === 'production' ? process.env.REACT_APP_INTERCOM_ID_PRODUCTION : process.env.REACT_APP_INTERCOM_ID_NON_PRODUCTION)
+export const getIntercomId = () => (process.env.REACT_APP_ENV === 'production' ? process.env.REACT_APP_INTERCOM_ID : 'km54f6ih')
