cd6ead93ae5bced035d5573e47bd9dc1c95efe98
Mikhail Mikheev <mmvsha73@gmail.com>
2019-12-06 18:15:06 +0400

    add 'disconnect' event listener for web3connect

--- src/logic/wallets/store/actions/removeProvider.js
+++ src/logic/wallets/store/actions/removeProvider.js
@@ -1,15 +1,16 @@
 // @flow
 import { createAction } from 'redux-actions'
 import type { Dispatch as ReduxDispatch } from 'redux'
-import { NOTIFICATIONS, showSnackbar } from '~/logic/notifications'
+import { NOTIFICATIONS, enhanceSnackbarForAction } from '~/logic/notifications'
 import { getWeb3, resetWeb3 } from '~/logic/wallets/getWeb3'
+import enqueueSnackbar from '~/logic/notifications/store/actions/enqueueSnackbar'
 
 export const REMOVE_PROVIDER = 'REMOVE_PROVIDER'
 
 const removeProvider = createAction<string, *, *>(REMOVE_PROVIDER)
 
-export default (enqueueSnackbar: Function, closeSnackbar: Function) => (dispatch: ReduxDispatch<*>) => {
-  showSnackbar(NOTIFICATIONS.WALLET_DISCONNECTED_MSG, enqueueSnackbar, closeSnackbar)
+export default () => (dispatch: ReduxDispatch<*>) => {
+  dispatch(enqueueSnackbar(enhanceSnackbarForAction(NOTIFICATIONS.WALLET_DISCONNECTED_MSG)))
 
   const web3 = getWeb3()
 
