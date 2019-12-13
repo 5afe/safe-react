509000b9e6c9ced69869932cb58070337745f8fb
Mikhail Mikheev <mmvsha73@gmail.com>
2019-12-13 21:15:13 +0400

    Feature: Activate fortmatic (#339)
    * Add fortmatic integration to web3connect
    
    * add fortmatic
    
    * Safe open form improvements: limit calling initContracts to 1 time
    
    * update .env.example

--- src/routes/open/container/Open.jsx
+++ src/routes/open/container/Open.jsx
@@ -8,7 +8,7 @@ import {
   getAccountsFrom, getThresholdFrom, getNamesFrom, getSafeNameFrom, getOwnersFrom,
 } from '~/routes/open/utils/safeDataExtractor'
 import { buildSafe } from '~/routes/safe/store/actions/fetchSafe'
-import { getGnosisSafeInstanceAt, deploySafeContract, initContracts } from '~/logic/contracts/safeContracts'
+import { getGnosisSafeInstanceAt, deploySafeContract } from '~/logic/contracts/safeContracts'
 import { checkReceiptStatus } from '~/logic/wallets/ethTransactions'
 import { history } from '~/store'
 import { OPENING_ADDRESS, stillInOpeningView, SAFELIST_ADDRESS } from '~/routes/routes'
@@ -56,8 +56,6 @@ export const createSafe = async (values: Object, userAccount: string, addSafe: A
   const ownersNames = getNamesFrom(values)
   const ownerAddresses = getAccountsFrom(values)
 
-  await initContracts()
-
   const safe = await deploySafeContract(ownerAddresses, numConfirmations, userAccount)
   await checkReceiptStatus(safe.tx)
 
@@ -85,10 +83,6 @@ export const createSafe = async (values: Object, userAccount: string, addSafe: A
 }
 
 class Open extends React.Component<Props> {
-  async componentDidMount() {
-    await initContracts()
-  }
-
   onCallSafeContractSubmit = async (values) => {
     try {
       const { userAccount, addSafe } = this.props
