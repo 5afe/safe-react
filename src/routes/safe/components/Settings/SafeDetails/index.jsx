99520d85fa750c4a74a6f1587fc9c870eaf8e0aa
fernandomg <fernando.greco@gmail.com>
2019-12-17 15:02:51 -0300

    Add Safe version to Safe Details

--- src/routes/safe/components/Settings/SafeDetails/index.jsx
+++ src/routes/safe/components/Settings/SafeDetails/index.jsx
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import { withSnackbar } from 'notistack'
+import semverLessThan from 'semver/functions/lt'
 import Block from '~/components/layout/Block'
 import Col from '~/components/layout/Col'
 import Field from '~/components/forms/Field'
@@ -15,12 +16,12 @@ import Button from '~/components/layout/Button'
 import { getNotificationsFromTxType, showSnackbar } from '~/logic/notifications'
 import { TX_NOTIFICATION_TYPES } from '~/logic/safe/transactions'
 import { styles } from './style'
+import { getSafeMasterContract } from '~/logic/contracts/safeContracts'
 
 export const SAFE_NAME_INPUT_TEST_ID = 'safe-name-input'
 export const SAFE_NAME_SUBMIT_BTN_TEST_ID = 'change-safe-name-btn'
 
 type Props = {
-  classes: Object,
   safeAddress: string,
   safeName: string,
   updateSafe: Function,
@@ -28,9 +29,13 @@ type Props = {
   closeSnackbar: Function,
 }
 
-const ChangeSafeName = (props: Props) => {
+const useStyles = makeStyles(styles)
+
+const SafeDetails = (props: Props) => {
+  const classes = useStyles()
+  const [safeVersions, setSafeVersions] = React.useState({ current: '0.0.0', latest: '0.0.0', needUpdate: false })
   const {
-    classes, safeAddress, safeName, updateSafe, enqueueSnackbar, closeSnackbar,
+    safeAddress, safeName, updateSafe, enqueueSnackbar, closeSnackbar,
   } = props
 
   const handleSubmit = (values) => {
@@ -40,11 +45,32 @@ const ChangeSafeName = (props: Props) => {
     showSnackbar(notification.afterExecution.noMoreConfirmationsNeeded, enqueueSnackbar, closeSnackbar)
   }
 
+  React.useEffect(() => {
+    const getVersion = async () => {
+      const safeMaster = await getSafeMasterContract()
+      const current = await safeMaster.VERSION()
+      const latest = process.env.REACT_APP_LATEST_SAFE_VERSION
+      const needUpdate = semverLessThan(current, latest)
+
+      setSafeVersions({ current, latest, needUpdate })
+    }
+    getVersion()
+  }, [])
+
   return (
     <>
       <GnoForm onSubmit={handleSubmit}>
         {() => (
           <>
+            <Block className={classes.formContainer}>
+              <Heading tag="h2">Safe Version</Heading>
+              <Row align="end" grow>
+                <Block className={classes.root}>
+                  {safeVersions.current}
+                  {safeVersions.needUpdate && ` (there's a newer version: ${safeVersions.latest})`}
+                </Block>
+              </Row>
+            </Block>
             <Block className={classes.formContainer}>
               <Heading tag="h2">Modify Safe name</Heading>
               <Paragraph>
@@ -85,4 +111,4 @@ const ChangeSafeName = (props: Props) => {
   )
 }
 
-export default withStyles(styles)(withSnackbar(ChangeSafeName))
+export default withSnackbar(SafeDetails)
