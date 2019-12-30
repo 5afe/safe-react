a9561223037084202c501764b54e7dd33ac4760b
apane <agustin.pane@gmail.com>
2019-12-30 08:43:28 -0300

    Replaces updateAddressBook with addAddressBook

--- src/logic/addressBook/store/reducer/addressBook.js
+++ src/logic/addressBook/store/reducer/addressBook.js
@@ -2,7 +2,7 @@
 import { Map } from 'immutable'
 import { handleActions, type ActionType } from 'redux-actions'
 import type { Cookie } from '~/logic/cookies/model/cookie'
-import { UPDATE_ADDRESS_BOOK } from '~/logic/addressBook/store/actions/updateAddressBook'
+import { ADD_ADDRESS_BOOK } from '~/logic/addressBook/store/actions/addAddressBook'
 
 export const ADDRESS_BOOK_REDUCER_ID = 'addressBook'
 
@@ -10,7 +10,7 @@ export type State = Map<string, Map<string, Cookie>>
 
 export default handleActions<State, *>(
   {
-    [UPDATE_ADDRESS_BOOK]: (state: State, action: ActionType<Function>): State => {
+    [ADD_ADDRESS_BOOK]: (state: State, action: ActionType<Function>): State => {
       const { addressBook } = action.payload
 
       return state.set('addressBook', addressBook)
