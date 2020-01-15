f0db8792722bb46aadf022eaee77d9411527f1c2
fernandomg <fernando.greco@gmail.com>
2020-01-15 16:29:35 -0300

    Create `currentSession` state

---
+++ src/logic/currentSession/store/actions/loadCurrentSessionFromStorage.js
@@ -0,0 +1,14 @@
+// @flow
+import { type Dispatch as ReduxDispatch } from 'redux'
+import { type GlobalState } from '~/store'
+import { getCurrentSessionFromStorage } from '~/logic/currentSession/utils'
+import loadCurrentSession from '~/logic/currentSession/store/actions/loadCurrentSession'
+import { makeCurrentSession } from '~/logic/currentSession/store/model/currentSession'
+
+const loadCurrentSessionFromStorage = () => async (dispatch: ReduxDispatch<GlobalState>) => {
+  const currentSession = (await getCurrentSessionFromStorage())
+
+  dispatch(loadCurrentSession(makeCurrentSession(currentSession ? currentSession.currentSession : {})))
+}
+
+export default loadCurrentSessionFromStorage
