b739674bfa49f9365db8326f807038e85e38d07a
Mikhail Mikheev <mmvsha73@gmail.com>
2020-01-20 13:59:46 +0400

    V1.6.3 (#450)
    * Cherry-pick aa063aa92acb829b25fa1f6efb0f5d688ea7e19b, conflict fix
    
    * Update package.json
    
    * Delete package lock

--- src/components/ConnectButton/index.jsx
+++ src/components/ConnectButton/index.jsx
@@ -5,7 +5,6 @@ import Torus from '@toruslabs/torus-embed'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import Fortmatic from 'fortmatic'
 import Portis from '@portis/web3'
-import Squarelink from 'squarelink'
 import Authereum from 'authereum'
 import Button from '~/components/layout/Button'
 import { fetchProvider, removeProvider } from '~/logic/wallets/store/actions'
@@ -15,7 +14,7 @@ import { store } from '~/store'
 const isMainnet = process.env.REACT_APP_NETWORK === 'mainnet'
 
 const PORTIS_DAPP_ID = isMainnet ? process.env.REACT_APP_PORTIS_ID : '852b763d-f28b-4463-80cb-846d7ec5806b'
-const SQUARELINK_CLIENT_ID = isMainnet ? process.env.REACT_APP_SQUARELINK_ID : '46ce08fe50913cfa1b78'
+// const SQUARELINK_CLIENT_ID = isMainnet ? process.env.REACT_APP_SQUARELINK_ID : '46ce08fe50913cfa1b78'
 const FORTMATIC_API_KEY = isMainnet ? process.env.REACT_APP_FORTMATIC_KEY : 'pk_test_CAD437AA29BE0A40'
 
 export const web3Connect = new Web3Connect.Core({
@@ -31,12 +30,6 @@ export const web3Connect = new Web3Connect.Core({
       package: Portis,
       options: {
         id: PORTIS_DAPP_ID,
-      },
-    },
-    squarelink: {
-      package: Squarelink,
-      options: {
-        id: SQUARELINK_CLIENT_ID,
       },
     },
     fortmatic: {
