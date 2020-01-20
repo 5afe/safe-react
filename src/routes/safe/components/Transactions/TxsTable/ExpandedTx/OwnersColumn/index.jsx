41f569712a2ffc805b2ecdd5d765332101a3c18c
fernandomg <fernando.greco@gmail.com>
2020-01-20 11:05:04 -0300

    Fix execute icon display conditions

--- src/routes/safe/components/Transactions/TxsTable/ExpandedTx/OwnersColumn/index.jsx
+++ src/routes/safe/components/Transactions/TxsTable/ExpandedTx/OwnersColumn/index.jsx
@@ -220,7 +220,7 @@ const OwnersColumn = ({
           }
         />
         <div className={classes.iconState}>
-          {(!tx.isExecuted || (cancelTx && !cancelTx.isExecuted)) && (
+          {(!tx.isExecuted && (cancelTx && !cancelTx.isExecuted)) && (
             <Img src={ConfirmLargeGreyIcon} alt="Confirm / Execute tx" />
           )}
           {tx.isExecuted && (
