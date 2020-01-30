b099caf001f7f784b8c14d9c926a48735911ca25
fernandomg <fernando.greco@gmail.com>
2020-01-30 09:21:59 -0300

    Verify tx nonce by using `Number.isInteger`

--- src/routes/safe/components/Transactions/TxsTable/columns.js
+++ src/routes/safe/components/Transactions/TxsTable/columns.js
@@ -106,7 +106,7 @@ export const getTxTableData = (
 
     return getTransactionTableData(
       tx,
-      ![undefined, null, ''].includes(tx.nonce) ? cancelTxsByNonce.get(tx.nonce) : undefined,
+      Number.isInteger(tx.nonce) ? cancelTxsByNonce.get(tx.nonce) : undefined,
     )
   })
 }
--- src/routes/safe/store/actions/createTransaction.js
+++ src/routes/safe/store/actions/createTransaction.js
@@ -78,7 +78,7 @@ const createTransaction = ({
   const from = userAccountSelector(state)
   const safeInstance = await getGnosisSafeInstanceAt(safeAddress)
   const threshold = await safeInstance.getThreshold()
-  const nonce = [undefined, null, ''].includes(txNonce) ? await getLastPendingTxNonce(safeAddress) : txNonce
+  const nonce = !Number.isInteger(txNonce) ? await getLastPendingTxNonce(safeAddress) : txNonce
   const isExecution = threshold.toNumber() === 1 || shouldExecute
 
   // https://gnosis-safe.readthedocs.io/en/latest/contracts/signatures.html#pre-validated-signatures
