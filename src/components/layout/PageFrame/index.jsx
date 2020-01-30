316939759437d09dd5d22af2190e4dd561a9440d
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-01-30 19:15:58 -0300

    (fix) notifications' visual inconsistencies

--- src/components/layout/PageFrame/index.jsx
+++ src/components/layout/PageFrame/index.jsx
@@ -1,72 +1,31 @@
 // @flow
 import * as React from 'react'
-import { connect } from 'react-redux'
 import { SnackbarProvider } from 'notistack'
+import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
-import { getNetwork } from '~/config'
-import { ETHEREUM_NETWORK } from '~/logic/wallets/getWeb3'
-import SidebarProvider from '~/components/Sidebar'
-import Header from '~/components/Header'
 import Backdrop from '~/components/layout/Backdrop'
+import CookiesBanner from '~/components/CookiesBanner'
+import Header from '~/components/Header'
 import Img from '~/components/layout/Img'
 import Notifier from '~/components/Notifier'
+import SidebarProvider from '~/components/Sidebar'
+import { ETHEREUM_NETWORK } from '~/logic/wallets/getWeb3'
+import { getNetwork } from '~/config'
 import { networkSelector } from '~/logic/wallets/store/selectors'
 import AlertIcon from './assets/alert.svg'
 import CheckIcon from './assets/check.svg'
 import ErrorIcon from './assets/error.svg'
 import InfoIcon from './assets/info.svg'
-import CookiesBanner from '~/components/CookiesBanner'
 import styles from './index.scss'
-import { fontColor } from '~/theme/variables'
-
-const notificationStyles = {
-  success: {
-    background: '#ffffff',
-    fontFamily: 'Averta',
-    fontSize: '14px',
-    lineHeight: 1.43,
-    color: fontColor,
-    minHeight: '58px',
-    boxShadow: '0 0 10px 0 rgba(212, 212, 211, 0.59)',
-  },
-  error: {
-    background: '#ffe6ea',
-    fontFamily: 'Averta',
-    fontSize: '14px',
-    lineHeight: 1.43,
-    color: fontColor,
-    minHeight: '58px',
-    boxShadow: '0 0 10px 0 rgba(212, 212, 211, 0.59)',
-  },
-  warning: {
-    background: '#fff3e2',
-    fontFamily: 'Averta',
-    fontSize: '14px',
-    lineHeight: 1.43,
-    color: fontColor,
-    minHeight: '58px',
-    boxShadow: '0 0 10px 0 rgba(212, 212, 211, 0.59)',
-  },
-  info: {
-    background: '#ffffff',
-    fontFamily: 'Averta',
-    fontSize: '14px',
-    lineHeight: 1.43,
-    color: fontColor,
-    minHeight: '58px',
-    boxShadow: '0 0 10px 0 rgba(212, 212, 211, 0.59)',
-  },
-}
 
 type Props = {
   children: React.Node,
-  classes: Object,
   currentNetwork: string,
 }
 
 const desiredNetwork = getNetwork()
 
-const PageFrame = ({ children, classes, currentNetwork }: Props) => {
+const PageFrame = ({ children, currentNetwork }: Props) => {
   const isWrongNetwork = currentNetwork !== ETHEREUM_NETWORK.UNKNOWN && currentNetwork !== desiredNetwork
 
   return (
@@ -75,17 +34,11 @@ const PageFrame = ({ children, classes, currentNetwork }: Props) => {
       <SnackbarProvider
         maxSnack={5}
         anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        classes={{
-          variantSuccess: classes.success,
-          variantError: classes.error,
-          variantWarning: classes.warning,
-          variantInfo: classes.info,
-        }}
         iconVariant={{
-          success: <Img src={CheckIcon} alt="Success" />,
           error: <Img src={ErrorIcon} alt="Error" />,
-          warning: <Img src={AlertIcon} alt="Warning" />,
           info: <Img src={InfoIcon} alt="Info" />,
+          success: <Img src={CheckIcon} alt="Success" />,
+          warning: <Img src={AlertIcon} alt="Warning" />,
         }}
       >
         <Notifier />
@@ -99,11 +52,4 @@ const PageFrame = ({ children, classes, currentNetwork }: Props) => {
   )
 }
 
-export default withStyles(notificationStyles)(
-  connect(
-    (state) => ({
-      currentNetwork: networkSelector(state),
-    }),
-    null,
-  )(PageFrame),
-)
+export default connect((state) => ({ currentNetwork: networkSelector(state) }), null)(PageFrame)
