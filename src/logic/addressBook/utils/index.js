d8b033f97c9c59a0c486018312f105180b342b76
fernandomg <fernando.greco@gmail.com>
2020-01-31 09:59:33 -0300

    Rename box flag to a most representative name `use3BoxStorage`

--- src/logic/addressBook/utils/index.js
+++ src/logic/addressBook/utils/index.js
@@ -1,22 +1,27 @@
 // @flow
 import { useSelector } from 'react-redux'
 import type { AddressBook, AddressBookProps } from '~/logic/addressBook/model/addressBook'
-import { loadFrom3Box, loadFromStorage, saveTo3Box, saveToStorage } from '~/utils/storage'
+import {
+  loadFrom3Box,
+  loadFromStorage,
+  saveTo3Box,
+  saveToStorage,
+} from '~/utils/storage'
 import { getAddressBook } from '~/logic/addressBook/store/selectors'
 import type { Owner } from '~/routes/safe/store/models/owner'
 
 const ADDRESS_BOOK_STORAGE_KEY = 'ADDRESS_BOOK_STORAGE_KEY'
 
-export const getAddressBookFromStorage = async (box?: boolean): Promise<AddressBookProps | []> => {
-  const loadHandler = box ? loadFrom3Box : loadFromStorage
+export const getAddressBookFromStorage = async (use3BoxStorage?: boolean): Promise<AddressBookProps | []> => {
+  const loadHandler = use3BoxStorage ? loadFrom3Box : loadFromStorage
   const data = await loadHandler(ADDRESS_BOOK_STORAGE_KEY)
 
   return data || []
 }
 
-export const saveAddressBook = async (addressBook: AddressBook, box?: boolean) => {
+export const saveAddressBook = async (addressBook: AddressBook, use3BoxStorage?: boolean) => {
   try {
-    const saveHandler = box ? saveTo3Box : saveToStorage
+    const saveHandler = use3BoxStorage ? saveTo3Box : saveToStorage
     await saveHandler(ADDRESS_BOOK_STORAGE_KEY, addressBook.toJS())
   } catch (err) {
     console.error('Error storing addressBook in localstorage', err)
