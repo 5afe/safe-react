2887d5fa9641eedfe4c15fdbce55ca16f9d7d920
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-02-04 15:36:01 -0300

    (change) icon's names

--- src/routes/safe/components/assets/address-book.jsx
+++
@@ -1,6 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-    <g fill="none" fill-rule="evenodd">
-        <path d="M0 0h16v16H0z"/>
-        <path fill="#B2B5B2" fill-rule="nonzero" d="M2 10V6a1 1 0 1 1 0-2V3a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-1a1 1 0 0 1 0-2zm2 0a1 1 0 0 1 0 2v1h8V3H4v1a1 1 0 1 1 0 2v4z"/>
-    </g>
-</svg>
