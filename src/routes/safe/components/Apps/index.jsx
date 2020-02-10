9fe97a7fcca20db9563aa1be4f098f50196ba71e
nicosampler <nf.dominguez.87@gmail.com>
2020-02-10 11:47:18 -0300

    Apps tabs: refactor and styling

--- src/routes/safe/components/Apps/index.jsx
+++ src/routes/safe/components/Apps/index.jsx
@@ -2,12 +2,17 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
 
+import ButtonLink from '../../../../components/layout/ButtonLink'
+import Loader from './ListContentLayout/Loader'
 import sendTransactions from './sendTransactions'
+import { Wrapper, Menu, Content, Footer, Nav } from './ListContentLayout/Layout'
+import List from './ListContentLayout/List'
 
-const AppsWrapper = styled.div`
-  margin: 15px;
+const StyledIframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  display: ${props => (props.shouldDisplay ? 'block' : 'none')};
 `
-
 const operations = {
   SEND_TRANSACTIONS: 'sendTransactions',
   GET_TRANSACTIONS: 'getTransactions',
@@ -19,23 +24,65 @@ type Props = {
   web3: any,
   safeAddress: String,
   network: String,
-  createTransaction: any
+  createTransaction: any,
 }
 
-function Apps({
-  web3, safeAddress, network, createTransaction,
-}: Props) {
-  const iframeUrl = 'http://localhost:3002'
+function Apps({ web3, safeAddress, network, createTransaction }: Props) {
+  const apps = [
+    {
+      id: 1,
+      name: 'Compound',
+      // url: 'http://localhost:3002',
+      url: 'https://gnosis-apps.netlify.com/',
+      iconUrl: 'https://compound.finance/images/compound-mark.svg',
+      description: '',
+      providedBy: { name: 'Gnosis', url: '' },
+    },
+    {
+      id: 2,
+      name: 'ENS Manager',
+      url: '',
+      iconUrl: 'https://app.ens.domains/static/media/ensIconLogo.4d995d23.svg',
+      description: '',
+      providedBy: { name: 'Gnosis', url: '' },
+    },
+    {
+      id: 3,
+      name: 'Uniswap',
+      url: '',
+      iconUrl:
+        'https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-LNun-MDdANv-PeRglM0%2Favatar.png?generation=1538584950851432&alt=media',
+      description: '',
+      providedBy: { name: 'Gnosis', url: '' },
+    },
+    {
+      id: 4,
+      name: 'Nexus Mutual',
+      url: '',
+      iconUrl:
+        'https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-LK136DM17k-0Gl82Q9B%2Favatar.png?generation=1534411701476772&alt=media',
+      description: '',
+      providedBy: {
+        name: 'Gnosis',
+        url: '',
+      },
+    },
+  ]
 
-  const [selectedApp, setSelectedApp] = useState(null)
-  const [appIsLoading, setAppIsLoading] = useState(false)
-  const [iframeEl, seIframeEl] = useState(null)
+  const [selectedApp, setSelectedApp] = useState(1)
+  const [appIsLoading, setAppIsLoading] = useState(true)
+  const [iframeEl, setframeEl] = useState(null)
+
+  const getSelectedApp = () => apps.find(e => e.id === selectedApp)
 
   const sendMessageToIframe = (messageId, data) => {
-    iframeEl.contentWindow.postMessage({ messageId, data }, iframeUrl)
+    iframeEl.contentWindow.postMessage(
+      { messageId, data },
+      getSelectedApp().url
+    )
   }
 
-  const handleIframeMessage = async (data) => {
+  const handleIframeMessage = async data => {
     if (!data || !data.messageId) {
       console.warn('iframe: message without messageId')
       return
@@ -47,7 +94,7 @@ function Apps({
           web3,
           createTransaction,
           safeAddress,
-          data.data,
+          data.data
         )
 
         if (txHash) {
@@ -69,15 +116,15 @@ function Apps({
     }
   }
 
-  const iframeRef = useCallback((node) => {
+  const iframeRef = useCallback(node => {
     if (node !== null) {
-      seIframeEl(node)
+      setframeEl(node)
     }
   }, [])
 
   useEffect(() => {
     const onIframeMessage = async ({ origin, data }) => {
-      if (origin !== iframeUrl) {
+      if (origin !== getSelectedApp().url) {
         return
       }
 
@@ -110,30 +157,53 @@ function Apps({
     }
   }, [iframeEl])
 
-  const selectApp = () => {
+  const onSelectApp = appId => {
     setAppIsLoading(true)
-    setSelectedApp({ url: `${iframeUrl}?id=2` })
+    setSelectedApp(appId)
+  }
+
+  const getContent = () => {
+    if (!selectedApp) {
+      return null
+    }
+
+    return (
+      <>
+        {appIsLoading && <Loader />}
+        <StyledIframe
+          shouldDisplay={!appIsLoading}
+          id="iframeId"
+          frameBorder="0"
+          title="app"
+          ref={iframeRef}
+          src={getSelectedApp().url}
+        />
+      </>
+    )
   }
 
   return (
-    <AppsWrapper>
-      <button type="button" onClick={selectApp}>
-        load app
-      </button>
-
-      {selectedApp && appIsLoading && 'Loading app...'}
-
-      {selectedApp && (
-        <iframe
-          id="iframeId"
-          title="app"
-          ref={iframeRef}
-          src={selectedApp.url}
-          height="350"
-          width="100%"
-        />
-      )}
-    </AppsWrapper>
+    <Wrapper>
+      <Nav>
+        <ButtonLink size="lg" onClick={() => {}} testId="manage-tokens-btn">
+          Manage Apps
+        </ButtonLink>
+      </Nav>
+      <Menu>
+        <List items={apps} activeItem={selectedApp} onItemClick={onSelectApp} />
+      </Menu>
+      <Content>{getContent()}</Content>
+      <Footer>
+        This App is provided by{' '}
+        <ButtonLink
+          size="lg"
+          onClick={() => window.open(getSelectedApp().providedBy.url, '_blank')}
+          testId="manage-tokens-btn"
+        >
+          {getSelectedApp().providedBy.name}
+        </ButtonLink>
+      </Footer>
+    </Wrapper>
   )
 }
 
