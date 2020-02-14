88f63944ead00f812ac6e3390fce7c67a3776201
Mati Dastugue <matias.dastugue@altoros.com>
2020-02-14 01:35:24 -0300

    Update onboardjs version fix ledger connection

--- src/components/ConnectButton/index.jsx
+++ src/components/ConnectButton/index.jsx
@@ -70,7 +70,7 @@ export const onboard = new Onboard({
         // can also instantiate your contracts with the web3 instance
         setWeb3(wallet.provider)
         providerName = wallet.name
-        //store.dispatch(fetchProvider(providerName))
+        // store.dispatch(fetchProvider(providerName))
       }
     },
     address: address => {
