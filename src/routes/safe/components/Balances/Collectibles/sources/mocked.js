92a55d76e345ba6ed8be71584c148c76abf38363
fernandomg <fernando.greco@gmail.com>
2020-02-14 17:14:12 -0300

    Create collectible source classes
    - source from mocked data and opensea, it's extendable to import information from other sources

---
+++ src/routes/safe/components/Balances/Collectibles/sources/mocked.js
@@ -0,0 +1,145 @@
+// @flow
+import type {
+  CollectibleData,
+  CollectibleMetadataSource,
+} from '~/routes/safe/components/Balances/Collectibles/types'
+
+const kittyAssetContract = {
+  address: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+  asset_contract_type: 'non-fungible',
+  created_date: '2018-01-23T04:50:03.948541',
+  name: 'CryptoKittiesRinkeby',
+  nft_version: '1.0',
+  opensea_version: null,
+  owner: 8995,
+  schema_name: 'ERC721',
+  symbol: 'KITTYR',
+  total_supply: null,
+  description: 'The Rinkeby version of CryptoKitties',
+  external_link: null,
+  image_url:
+    'https://storage.googleapis.com/opensea-static/cryptokitties-logo.png',
+  default_to_fiat: false,
+  dev_buyer_fee_basis_points: 0,
+  dev_seller_fee_basis_points: 0,
+  only_proxied_transfers: false,
+  opensea_buyer_fee_basis_points: 0,
+  opensea_seller_fee_basis_points: 250,
+  buyer_fee_basis_points: 0,
+  seller_fee_basis_points: 250,
+  payout_address: null,
+}
+
+const allCollectibles: CollectibleData[] = [
+  {
+    image:
+      'https://storage.googleapis.com/opensea-static/cryptokitties-logo.png',
+    title: 'CryptoKittiesRinkeby',
+    data: [
+      {
+        tokenId: '0',
+        name: 'Glitter',
+        order: null,
+        color: '#F6FEFC',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888670/6_w93q19.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '1',
+        name: 'Furbeard',
+        order: null,
+        color: '#F6C68A',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888668/9_xunbhn.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '2',
+        name: 'Glasswalker',
+        order: null,
+        color: '#CAFAF7',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888668/10_iqm4un.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '3',
+        name: 'Ande',
+        order: null,
+        color: '#B8F1B9',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888667/5_sxqrol.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '4',
+        name: 'Squib',
+        order: null,
+        color: '#CFD4F9',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888664/1_sz6sji.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '10',
+        name: 'Negato',
+        order: null,
+        color: '#D7BBF3',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888661/8_qjebni.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '11',
+        name: 'DuCat',
+        order: null,
+        color: '#D6DDD8',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888654/2_yndavu.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+      {
+        tokenId: '12',
+        name: 'Berry',
+        order: null,
+        color: '#F7B4D5',
+        image:
+          'https://res.cloudinary.com/ddklsa6jc/image/upload/v1556888653/4_do9hzd.png',
+        assetUrl: 'https://www.cryptokitties.co/',
+        description: '',
+        assetAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        asset: kittyAssetContract,
+      },
+    ],
+  },
+]
+
+class Mocked implements CollectibleMetadataSource {
+  async fetchAllUserCollectiblesAsync(): Promise<CollectibleData[]> {
+    return allCollectibles
+  }
+}
+
+export default Mocked
---
+++ src/routes/safe/components/Balances/Collectibles/sources/opensea.js
@@ -0,0 +1,105 @@
+// @flow
+import { RateLimit } from 'async-sema'
+import { OPENSEA_API_KEY } from '~/utils/constants'
+import type {
+  AssetCollectible,
+  CollectibleData,
+  CollectibleMetadataSource,
+  OpenSeaAsset,
+} from '~/routes/safe/components/Balances/Collectibles/types'
+import { ETHEREUM_NETWORK } from '~/logic/wallets/getWeb3'
+
+type GroupedCollectibles = {
+  [key: string]: AssetCollectible[],
+}
+
+class OpenSea implements CollectibleMetadataSource {
+  _rateLimit = async () => {}
+
+  _endpointsUrls: { [key: string]: string } = {
+    [ETHEREUM_NETWORK.MAINNET]: 'https://api.opensea.io/api/v1',
+    [ETHEREUM_NETWORK.RINKEBY]: 'https://rinkeby-api.opensea.io/api/v1',
+  }
+
+  _fetch = async (url: string) => {
+    // eslint-disable-next-line no-underscore-dangle
+    await this._rateLimit()
+    return fetch(url, {
+      headers: { 'X-API-KEY': OPENSEA_API_KEY || '' },
+    })
+  }
+
+  /**
+   * OpenSea class constructor
+   * @param {object} options
+   * @param {number} options.rps - requests per second
+   */
+  constructor(options: { rps: number }) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._rateLimit = RateLimit(options.rps)
+  }
+
+  static getAssetsAsCollectible(assets: OpenSeaAsset[]): AssetCollectible[] {
+    return assets.map(asset => OpenSea.getAssetAsCollectible(asset))
+  }
+
+  static getAssetAsCollectible(asset: OpenSeaAsset): AssetCollectible {
+    return {
+      tokenId: asset.token_id,
+      name: asset.name || `${asset.asset_contract.name} - #${asset.token_id}`,
+      color: asset.background_color ? `#${asset.background_color}` : '',
+      image: asset.image_url,
+      assetUrl: asset.external_link,
+      description: asset.name,
+      order: null,
+      assetAddress: asset.asset_contract.address,
+      asset: asset.asset_contract,
+    }
+  }
+
+  /**
+   * Fetches from OpenSea the list of collectibles for the provided Safe Address in the specified Network
+   * @param {string} safeAddress
+   * @param {string} networkName
+   * @returns {Promise<Array<CollectibleData>>}
+   */
+  async fetchAllUserCollectiblesAsync(
+    safeAddress: string,
+    networkName: string
+  ) {
+    // eslint-disable-next-line no-underscore-dangle
+    const metadataSourceUrl = this._endpointsUrls[networkName]
+    const url = `${metadataSourceUrl}/assets?owner=${safeAddress}`
+    // eslint-disable-next-line no-underscore-dangle
+    const assetsResponse = await this._fetch(url)
+    const assetsResponseJson = await assetsResponse.json()
+    const groupedCollectibles: GroupedCollectibles = OpenSea.getAssetsAsCollectible(
+      assetsResponseJson.assets
+    ).reduce((acc, el) => {
+      const family = acc[el.assetAddress]
+
+      if (family) {
+        acc[el.assetAddress] = [...family, el]
+      } else {
+        acc[el.assetAddress] = [el]
+      }
+
+      return acc
+    }, {})
+
+    return Object.keys(groupedCollectibles).map<CollectibleData>(
+      collectibleAddress => {
+        const collectibles = groupedCollectibles[collectibleAddress]
+        const { image_url: image, name: title } = collectibles[0].asset
+
+        return {
+          image,
+          title,
+          data: collectibles,
+        }
+      }
+    )
+  }
+}
+
+export default OpenSea
