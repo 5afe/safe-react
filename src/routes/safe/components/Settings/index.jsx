37a8a016173835629946993d4ca01191721d54df
Agustin Pane <agustin.pane@gmail.com>
2020-02-26 04:55:28 -0300

    Fix #596: Notification when safe is already updated (#599)
    * Fix notification of update if the safe is already updated
    
    * Makes the notification clickable
    Displays the notification for owners only
    
    * Identify upgrade tx
    
    * Add red badge to Settings tab
    
    * Fixs Padding
    Removes the red dot if the user is not an owner
    
    Co-authored-by: Fernando <fernando.greco@gmail.com>

--- src/routes/safe/components/Settings/index.jsx
+++ src/routes/safe/components/Settings/index.jsx
@@ -143,13 +143,7 @@ class Settings extends React.Component<Props, State> {
                 onClick={this.handleChange(1)}
               >
                 <SafeDetailsIcon />
-                <Badge
-                  badgeContent=" "
-                  variant="dot"
-                  invisible={!this.state.needUpdate || !granted}
-                  color="error"
-                  style={{ paddingRight: '10px' }}
-                >
+                <Badge badgeContent=" " variant="dot" invisible={!this.state.needUpdate || !granted} color="error" style={{paddingRight: '10px'}}>
                   Safe details
                 </Badge>
               </Row>
