e4c009f284bc936cac08e4262b8a3fb3557d9099
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-03-10 18:16:43 +0100

    Fix conditional statement at travis build.sh file

--- config/travis/build.sh
+++ config/travis/build.sh
@@ -4,7 +4,7 @@ export NODE_ENV=production;
 
 if [[ -n "$TRAVIS_TAG" ]]; then
     export REACT_APP_ENV='production';
-elif [[ "$TRAVIS_BRANCH" == "development" ]];
+elif [[ "$TRAVIS_BRANCH" == "development" ]]; then
     export NODE_ENV=development;
 fi
 
