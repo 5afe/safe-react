a37926ecc5344051049bede1d8ff36bf56bc812e
apane <agustin.pane@gmail.com>
2020-03-11 10:39:35 -0300

    Implements feedback, now the displayed txHash are stored on localstorage and once the first time we notify the user about it, they won't never appear again

--- src/routes/safe/store/middleware/notificationsMiddleware.js
+++ src/routes/safe/store/middleware/notificationsMiddleware.js
@@ -18,8 +18,41 @@ import { ADD_TRANSACTIONS } from '~/routes/safe/store/actions/addTransactions'
 import updateSafe from '~/routes/safe/store/actions/updateSafe'
 import { safeParamAddressFromStateSelector, safesMapSelector } from '~/routes/safe/store/selectors'
 import { type GlobalState } from '~/store/'
+import { loadFromStorage, saveToStorage } from '~/utils/storage'
 
 const watchedActions = [ADD_TRANSACTIONS, ADD_INCOMING_TRANSACTIONS, ADD_SAFE]
+
+const sendAwaitingTransactionNotification = async (
+  dispatch: Function,
+  safeAddress: string,
+  awaitingTransactionList: List[],
+  notificationKey: string,
+  notificationClickedCb: Function,
+) => {
+  const NOTIFICATIONS_DISPLAYED_STORAGE_ID = 'NOTIFICATIONS_DISPLAYED_STORAGE_ID'
+  if (!dispatch || !safeAddress || !awaitingTransactionList || !notificationKey) {
+    return
+  }
+  if (awaitingTransactionList.size === 0) {
+    return
+  }
+  let displayedNotifications = (await loadFromStorage(NOTIFICATIONS_DISPLAYED_STORAGE_ID)) || []
+  let newDisplayedNotificationsArray = displayedNotifications
+  const filteredDuplicatedAwaitingTxList = awaitingTransactionList.filter(txHash => {
+    const includes = displayedNotifications.includes(txHash)
+    if (!includes) {
+      newDisplayedNotificationsArray.push(txHash)
+    }
+    return !includes
+  })
+  if (filteredDuplicatedAwaitingTxList.size === 0) {
+    return
+  }
+  dispatch(
+    enqueueSnackbar(enhanceSnackbarForAction(NOTIFICATIONS.TX_WAITING_MSG, notificationKey, notificationClickedCb)),
+  )
+  await saveToStorage(NOTIFICATIONS_DISPLAYED_STORAGE_ID, newDisplayedNotificationsArray)
+}
 
 const notificationsMiddleware = (store: Store<GlobalState>) => (next: Function) => async (action: Action<*>) => {
   const handledAction = next(action)
@@ -41,23 +74,25 @@ const notificationsMiddleware = (store: Store<GlobalState>) => (next: Function) 
           cancellationTransactionsByNonce,
           userAddress,
         )
-        const awaitingTransactionsList = awaitingTransactions.get(safeAddress, List([]))
+        const awaitingTxsHashList = awaitingTransactions.get(safeAddress, List([])).map(tx => tx.safeTxHash)
         const safes = safesMapSelector(state)
         const currentSafe = safes.get(safeAddress)
 
-        if (!isUserOwner(currentSafe, userAddress) || awaitingTransactionsList.size === 0) {
+        if (!isUserOwner(currentSafe, userAddress) || awaitingTxsHashList.size === 0) {
           break
         }
-
         const notificationKey = `${safeAddress}-awaiting`
         const onNotificationClicked = () => {
           dispatch(closeSnackbarAction({ key: notificationKey }))
           dispatch(push(`/safes/${safeAddress}/transactions`))
         }
-        dispatch(
-          enqueueSnackbar(
-            enhanceSnackbarForAction(NOTIFICATIONS.TX_WAITING_MSG, notificationKey, onNotificationClicked),
-          ),
+
+        await sendAwaitingTransactionNotification(
+          dispatch,
+          safeAddress,
+          awaitingTxsHashList,
+          notificationKey,
+          onNotificationClicked,
         )
 
         break
