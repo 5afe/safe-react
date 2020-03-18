b10d728fc9e1dcf970b30014543f56826d1c2f6a
Mati Dastugue <matias.dastugue@altoros.com>
2020-03-18 15:59:29 -0300

    Added desktop support

--- src/components/ConnectButton/index.jsx
+++ src/components/ConnectButton/index.jsx
@@ -8,52 +8,16 @@ import { getWeb3, setWeb3 } from '~/logic/wallets/getWeb3'
 import { fetchProvider } from '~/logic/wallets/store/actions'
 import transactionDataCheck from '~/logic/wallets/transactionDataCheck'
 import { store } from '~/store'
+import { selectWallets } from '~/utils/desktop'
 
 const isMainnet = process.env.REACT_APP_NETWORK === 'mainnet'
 
 const BLOCKNATIVE_API_KEY = isMainnet ? process.env.REACT_APP_BLOCKNATIVE_KEY : '7fbb9cee-7e97-4436-8770-8b29a9a8814c'
-const PORTIS_DAPP_ID = isMainnet ? process.env.REACT_APP_PORTIS_ID : '852b763d-f28b-4463-80cb-846d7ec5806b'
-// const SQUARELINK_CLIENT_ID = isMainnet ? process.env.REACT_APP_SQUARELINK_ID : '46ce08fe50913cfa1b78'
-const FORTMATIC_API_KEY = isMainnet ? process.env.REACT_APP_FORTMATIC_KEY : 'pk_test_CAD437AA29BE0A40'
-
-const wallets = [
-  { walletName: 'metamask', preferred: true },
-  {
-    walletName: 'walletConnect',
-    preferred: true,
-    infuraKey: process.env.REACT_APP_INFURA_TOKEN,
-  },
-  {
-    walletName: 'trezor',
-    appUrl: 'gnosis-safe.io',
-    preferred: true,
-    email: 'safe@gnosis.io',
-    rpcUrl: 'https://rinkeby.infura.io/v3/b42c928da8fd4c1f90374b18aa9ac6ba',
-  },
-  {
-    walletName: 'ledger',
-    preferred: true,
-    rpcUrl: 'https://rinkeby.infura.io/v3/b42c928da8fd4c1f90374b18aa9ac6ba',
-  },
-  { walletName: 'trust', preferred: true },
-  { walletName: 'dapper' },
-  {
-    walletName: 'fortmatic',
-    apiKey: FORTMATIC_API_KEY,
-  },
-  {
-    walletName: 'portis',
-    apiKey: PORTIS_DAPP_ID,
-    label: 'Login with Email',
-  },
-  { walletName: 'authereum' },
-  { walletName: 'coinbase' },
-  { walletName: 'opera' },
-  { walletName: 'operaTouch' },
-]
 
 let lastUsedAddress = ''
 let providerName
+
+const wallets = selectWallets()
 
 export const onboard = new Onboard({
   dappId: BLOCKNATIVE_API_KEY,
---
+++ src/utils/desktop.js
@@ -0,0 +1,53 @@
+// @flow
+
+const isMainnet = process.env.REACT_APP_NETWORK === 'mainnet'
+
+const PORTIS_DAPP_ID = isMainnet ? process.env.REACT_APP_PORTIS_ID : '852b763d-f28b-4463-80cb-846d7ec5806b'
+// const SQUARELINK_CLIENT_ID = isMainnet ? process.env.REACT_APP_SQUARELINK_ID : '46ce08fe50913cfa1b78'
+const FORTMATIC_API_KEY = isMainnet ? process.env.REACT_APP_FORTMATIC_KEY : 'pk_test_CAD437AA29BE0A40'
+const wallets = [
+  { walletName: 'metamask', preferred: true, desktop: false },
+  {
+    walletName: 'walletConnect',
+    preferred: true,
+    infuraKey: process.env.REACT_APP_INFURA_TOKEN,
+  },
+  {
+    walletName: 'trezor',
+    appUrl: 'gnosis-safe.io',
+    preferred: true,
+    email: 'safe@gnosis.io',
+    rpcUrl: 'https://rinkeby.infura.io/v3/b42c928da8fd4c1f90374b18aa9ac6ba',
+  },
+  {
+    walletName: 'ledger',
+    preferred: true,
+    rpcUrl: 'https://rinkeby.infura.io/v3/b42c928da8fd4c1f90374b18aa9ac6ba',
+    LedgerTransport: window.TransportNodeHid,
+  },
+  { walletName: 'trust', preferred: true, desktop: false },
+  { walletName: 'dapper', desktop: false },
+  {
+    walletName: 'fortmatic',
+    apiKey: FORTMATIC_API_KEY,
+  },
+  {
+    walletName: 'portis',
+    apiKey: PORTIS_DAPP_ID,
+    label: 'Login with Email',
+  },
+  { walletName: 'authereum', desktop: false },
+  { walletName: 'coinbase', desktop: false },
+  { walletName: 'opera', desktop: false },
+  { walletName: 'operaTouch', desktop: false },
+]
+
+const isDesktop = () => window && window.process && window.process.type
+
+export const selectWallets = () => {
+  const desktopMode = isDesktop()
+  /* eslint-disable no-unused-vars */
+  if (desktopMode) return wallets.filter(wallet => wallet.desktop).map(({ desktop, ...rest }) => rest)
+
+  return wallets.map(({ desktop, ...rest }) => rest)
+}
