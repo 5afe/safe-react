0be7981b3c93b6e24f786d8ebd47ada5e65a1c91
fernandomg <fernando.greco@gmail.com>
2020-03-25 17:35:26 -0300

    Batch request tokens balances

--- src/routes/safe/store/actions/fetchTokenBalances.js
+++ src/routes/safe/store/actions/fetchTokenBalances.js
@@ -8,7 +8,33 @@ import updateSafe from './updateSafe'
 import { getStandardTokenContract } from '~/logic/tokens/store/actions/fetchTokens'
 import { type Token } from '~/logic/tokens/store/model/token'
 import { ETH_ADDRESS } from '~/logic/tokens/utils/tokenHelpers'
+import { getWeb3 } from '~/logic/wallets/getWeb3'
 import { type GlobalState } from '~/store/index'
+
+const getBatchBalances = async (tokens: List<Token>, safeAddress: string) => {
+  const erc20Token = await getStandardTokenContract()
+  const web3 = getWeb3()
+  const batch = new web3.BatchRequest()
+
+  const balances = tokens
+    .toJS()
+    .filter(({ address }) => address !== ETH_ADDRESS)
+    .map(async ({ address, decimals }: any) => {
+      const tokenInstance = await erc20Token.at(address)
+      const request = tokenInstance.balanceOf(safeAddress).then(balance => ({
+        address,
+        balance: new BigNumber(balance).div(10 ** decimals).toString(),
+      }))
+
+      batch.add(request)
+
+      return request
+    })
+
+  batch.execute()
+
+  return Promise.all(balances)
+}
 
 export const calculateBalanceOf = async (tokenAddress: string, safeAddress: string, decimals: number = 18) => {
   if (tokenAddress === ETH_ADDRESS) {
@@ -34,15 +60,7 @@ const fetchTokenBalances = (safeAddress: string, tokens: List<Token>) => async (
     return
   }
   try {
-    const withBalances = await Promise.all(
-      tokens.map(async token => {
-        const balance = await calculateBalanceOf(token.address, safeAddress, token.decimals)
-        return {
-          address: token.address,
-          balance,
-        }
-      }),
-    )
+    const withBalances = await getBatchBalances(tokens, safeAddress)
 
     const balances = Map().withMutations(map => {
       withBalances.forEach(({ address, balance }) => {
