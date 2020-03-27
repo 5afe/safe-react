9ea25e9135cb2ec573031d8af6ff5b74e4a9f299
fernandomg <fernando.greco@gmail.com>
2020-03-27 12:24:24 -0300

    Standardize non-standard ERC20 tokens discovery

--- src/routes/safe/store/actions/fetchTokenBalances.js
+++ src/routes/safe/store/actions/fetchTokenBalances.js
@@ -9,8 +9,22 @@ import { getOnlyBalanceToken, getStandardTokenContract } from '~/logic/tokens/st
 import { type Token } from '~/logic/tokens/store/model/token'
 import { ETH_ADDRESS } from '~/logic/tokens/utils/tokenHelpers'
 import { sameAddress } from '~/logic/wallets/ethAddresses'
-import { getWeb3 } from '~/logic/wallets/getWeb3'
+import { ETHEREUM_NETWORK, getWeb3 } from '~/logic/wallets/getWeb3'
 import { type GlobalState } from '~/store/index'
+import { NETWORK } from '~/utils/constants'
+
+// List of all the non-standard ERC20 tokens
+const nonStandardERC20 = [
+  // DATAcoin
+  { network: ETHEREUM_NETWORK.RINKEBY, address: '0x0cf0ee63788a0849fe5297f3407f701e122cc023' },
+]
+
+// This is done due to an issues with DATAcoin contract in Rinkeby
+// https://rinkeby.etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
+// It doesn't have a `balanceOf` method implemented.
+const isStandardERC20 = (address: string): boolean => {
+  return !!nonStandardERC20.find(token => sameAddress(address, token.address) && sameAddress(NETWORK, token.network))
+}
 
 const getBatchBalances = (tokens: List<Token>, safeAddress: string) => {
   const web3 = getWeb3()
@@ -21,11 +35,8 @@ const getBatchBalances = (tokens: List<Token>, safeAddress: string) => {
     const onlyBalanceToken = getOnlyBalanceToken()
     onlyBalanceToken.options.address = address
 
-    // This is done due to issues with DATAcoin contract in Rinkeby
-    // https://rinkeby.etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
-    // It doesn't have a `balanceOf` method implemented.
     // As a fallback, we're using `balances`
-    const method = sameAddress(address, '0x0cf0ee63788a0849fe5297f3407f701e122cc023') ? 'balances' : 'balanceOf'
+    const method = isStandardERC20(address) ? 'balanceOf' : 'balances'
 
     return new Promise(resolve => {
       const request = onlyBalanceToken.methods[method](safeAddress).call.request((error, balance) => {
