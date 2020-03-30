c73dafe3ce468075571ac03dbf3fff2439f7ad0e
Agustin Pane <agustin.pane@gmail.com>
2020-03-30 13:14:04 -0300

    Feature: #528 optimize network requests (#680)
    * Generates a cache to avoid multiples getHumanFriendlyToken() for the same token address
    
    * Adds etags implementation for transactions
    
    * Caches outgoing and incoming safe transactions based on etag value
    
    * Removes cachedSafeTransactions, cachedSafeIncommingTransactions
    
    * Refactors getTokenInstance
    
    * Avoid recreating tokens on fetchTokens() once we have them in redux
    
    * Fixs error on catch
    
    * Batch request tokens balances
    
    * Fixs missing token names
    Changes the tokens limit from 300 to 3000
    
    * fix: failed to instantiate non-standard ERC-20 tokens
    
    For the batchRequest of balances, we're just using the `balanceOf` method call. So having a simple ABI with that only method prevents errors with non-standard ERC-20 Tokens.
    
    * Removes unnecessary action updateSafeThreshold
    Removes unnecessary action fetchEtherBalance
    
    * Updated comments in code
    Replaces constant with directly dispatching action
    
    * BatchRequest done right
    
    * fix: invalid action name `savedToken` -> `saveToken`
    
    * Renames getTokenInstance to getTokenInfos
    Fixs first load of transactions are empty
    
    * Move fetchTokenBalances to `Balances` and `SendModal` components
    
    * fix: Incoming transaction type
    
    Backend now changed the type from 'incoming' to one of: `'ERC721_TRANSFER', 'ERC20_TRANSFER', 'ETHER_TRANSFER'`
    
    * fix: tokenInstance `symbol` and `decimal` extraction
    
    * Fix property name `decimals` instead of `tokenDecimals`
    
    * Standardize non-standard ERC20 tokens discovery
    
    * fix: isStandardERC20
    
    * Revert "Move fetchTokenBalances to `Balances` and `SendModal` components"
    
    This reverts commit ed84bd92
    
    * Fixs Typo INCOMING_TX_TYPES
    Renames tokenInstance with localToken
    
    * Renames getBatchBalances to getTokenBalances
    Returns saved tokens instead of tokenInstance in getTokenInfos
    
    * Remove promise returns
    
    Co-authored-by: fernandomg <fernando.greco@gmail.com>

--- src/routes/safe/store/actions/fetchEtherBalance.js
+++ src/routes/safe/store/actions/fetchEtherBalance.js
@@ -3,13 +3,17 @@ import type { Dispatch as ReduxDispatch } from 'redux'
 
 import { getBalanceInEtherOf } from '~/logic/wallets/getWeb3'
 import updateSafe from '~/routes/safe/store/actions/updateSafe'
+import type { Safe } from '~/routes/safe/store/models/safe'
 import { type GlobalState } from '~/store/index'
 
-const fetchEtherBalance = (safeAddress: string) => async (dispatch: ReduxDispatch<GlobalState>) => {
+const fetchEtherBalance = (safe: Safe) => async (dispatch: ReduxDispatch<GlobalState>) => {
   try {
-    const ethBalance = await getBalanceInEtherOf(safeAddress)
+    const { address, ethBalance } = safe
+    const newEthBalance = await getBalanceInEtherOf(address)
 
-    dispatch(updateSafe({ address: safeAddress, ethBalance }))
+    if (newEthBalance !== ethBalance) {
+      dispatch(updateSafe({ address, newEthBalance }))
+    }
   } catch (err) {
     // eslint-disable-next-line
     console.error('Error when fetching Ether balance:', err)
