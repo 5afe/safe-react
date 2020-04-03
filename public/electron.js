cedd2474403ffec035fc90786df9a712eed3c3c3
Mati Dastugue <matias.dastugue@mercadolibre.com>
2020-04-03 18:08:38 -0300

    Added support for Portis

--- public/electron.js
+++ public/electron.js
@@ -9,6 +9,7 @@ const autoUpdater = require('./auto-updater');
 
 const url = require('url');
 const app = electron.app;
+const session = electron.session;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require("path");
@@ -32,6 +33,50 @@ const createServer = () => {
 
 let mainWindow;
 
+function getOpenedWindow(url,options) {
+  let display = electron.screen.getPrimaryDisplay();
+  let width = display.bounds.width;
+  let height = display.bounds.height;
+
+  // filter all requests to trezor-bridge and change origin to make it work
+   const filter = {
+    urls: ['http://127.0.0.1:21325/*']
+  };
+
+  options.webPreferences.affinity = 'main-window';
+
+  if(url.includes('about:blank')){
+    /*
+    session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+      details.requestHeaders['Origin'] = 'https://electron.trezor.io';
+      callback({cancel: false, requestHeaders: details.requestHeaders});
+    });
+  }
+ */
+  if(url.includes('wallet.portis') || url.includes('about:blank')){
+    const win = new BrowserWindow({
+      width:300,
+      height:700,
+      x: width - 1300,
+      parent:mainWindow,
+      y: height - 200,
+      webContents: options.webContents, // use existing webContents if provided
+      fullscreen: false,
+      show: false,
+    });
+
+    win.once('ready-to-show', () => win.show());
+
+    if(!options.webPreferences){
+      win.loadURL(url);
+    }
+    return win
+  }
+
+  return null;
+
+
+}
 function createWindow() {
   mainWindow = new BrowserWindow({
     show: false,
@@ -39,8 +84,9 @@ function createWindow() {
     height: 768,
     webPreferences: {
       preload: path.join(__dirname, '../scripts/preload.js'),
-      nodeIntegration: false,
-      allowRunningInsecureContent: true
+      allowRunningInsecureContent: true,
+      nodeIntegration: true, // dev settings to be able to use "require" in main.js, could be set to false in production build
+      nativeWindowOpen: true, // need to be set in order to display modal
     },
     icon: path.join(__dirname, './build/safe.png'),
   });
@@ -61,12 +107,21 @@ function createWindow() {
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
   }
 
-  //mainWindow.setMenu(null);
-  //mainWindow.setMenuBarVisibility(false)
+  mainWindow.setMenu(null);
+  mainWindow.setMenuBarVisibility(false);
 
-  mainWindow.webContents.on('new-window', function(event, url){
+  mainWindow.webContents.on('new-window', function(event, url, frameName, disposition, options){
     event.preventDefault();
-    open(url);
+    const win = getOpenedWindow(url,options);
+    if(win){
+      win.once('ready-to-show', () => win.show());
+
+      if(!options.webPreferences){
+        win.loadURL(url);
+      }
+
+      event.newGuest = win
+    } else open(url);
   });
 
   mainWindow.webContents.on('did-finish-load', () => {
@@ -84,7 +139,7 @@ function createWindow() {
 app.commandLine.appendSwitch('ignore-certificate-errors');
 app.on("ready", () =>{
   // Hide the menu
-  Menu.setApplicationMenu(null);
+  //Menu.setApplicationMenu(null);
   if(!isDev) createServer();
   createWindow();
 });
