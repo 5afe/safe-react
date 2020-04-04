e9b888439712320db73b98053ca5981127f37c39
nicosampler <nf.dominguez.87@gmail.com>
2020-04-04 00:42:44 -0300

    remove unused files

--- config/jest/Web3Mock.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import Web3 from 'web3'
-
-const window = global.window || {}
-window.web3 = window.web3 || {}
-window.web3.currentProvider = new Web3.providers.HttpProvider('http://localhost:8545')
-
-global.window = window
