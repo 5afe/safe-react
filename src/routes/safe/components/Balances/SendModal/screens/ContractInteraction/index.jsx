b36f0daffb8a7e1e7ab52f87dabc1bb6a48be66e
fernandomg <fernando.greco@gmail.com>
2020-04-28 10:43:51 -0300

    feature: (WIP) Load ABI and methods from Etherscan service for an specified contract address

--- src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/index.jsx
+++ src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/index.jsx
@@ -1,8 +1,16 @@
 // @flow
 import IconButton from '@material-ui/core/IconButton'
 import InputAdornment from '@material-ui/core/InputAdornment'
-import { makeStyles } from '@material-ui/core/styles'
+import InputBase from '@material-ui/core/InputBase'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles'
 import Close from '@material-ui/icons/Close'
+import SearchIcon from '@material-ui/icons/Search'
+import classNames from 'classnames'
+import createDecorator from 'final-form-calculate'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
@@ -11,15 +19,20 @@ import ArrowDown from '../assets/arrow-down.svg'
 import { styles } from './style'
 
 import QRIcon from '~/assets/icons/qrcode.svg'
-import CopyBtn from '~/components/CopyBtn'
-import EtherscanBtn from '~/components/EtherscanBtn'
-import Identicon from '~/components/Identicon'
 import ScanQRModal from '~/components/ScanQRModal'
+import GnoCheckbox from '~/components/forms/Checkbox'
 import Field from '~/components/forms/Field'
 import GnoForm from '~/components/forms/GnoForm'
 import TextField from '~/components/forms/TextField'
 import TextareaField from '~/components/forms/TextareaField'
-import { composeValidators, maxValue, mustBeFloat } from '~/components/forms/validator'
+import {
+  composeValidators,
+  maxValue,
+  mustBeEthereumAddress,
+  mustBeEthereumContractAddress,
+  mustBeFloat,
+  required,
+} from '~/components/forms/validator'
 import Block from '~/components/layout/Block'
 import Button from '~/components/layout/Button'
 import ButtonLink from '~/components/layout/ButtonLink'
@@ -28,40 +41,164 @@ import Hairline from '~/components/layout/Hairline'
 import Img from '~/components/layout/Img'
 import Paragraph from '~/components/layout/Paragraph'
 import Row from '~/components/layout/Row'
+import { getNetwork } from '~/config'
+import { getConfiguredSource } from '~/logic/contractInteraction/sources'
+import EtherscanService from '~/logic/contractInteraction/sources/EtherscanService'
 import SafeInfo from '~/routes/safe/components/Balances/SendModal/SafeInfo'
-import AddressBookInput from '~/routes/safe/components/Balances/SendModal/screens/AddressBookInput'
+import CheckIcon from '~/routes/safe/components/DropdownCurrency/img/check.svg'
+import { useDropdownStyles } from '~/routes/safe/components/DropdownCurrency/style'
 import { safeSelector } from '~/routes/safe/store/selectors'
+import { DropdownListTheme } from '~/theme/mui'
 import { sm } from '~/theme/variables'
 
 type Props = {
   initialValues: Object,
   onClose: () => void,
   onNext: (any) => void,
-  recipientAddress: string,
+  contractAddress?: string,
 }
 
 const useStyles = makeStyles(styles)
 
-const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress }: Props) => {
+const abiExtractor = createDecorator({
+  field: 'contractAddress',
+  updates: {
+    abi: (contractAddress) => {
+      if (!contractAddress) {
+        return 'no contract'
+      }
+      const network = getNetwork()
+      const source = getConfiguredSource()
+      return source.getContractABI(contractAddress, network)
+    },
+  },
+})
+
+const formMutators = {
+  setMax: (args, state, utils) => {
+    utils.changeValue(state, 'value', () => args[0])
+  },
+  setContractAddress: (args, state, utils) => {
+    utils.changeValue(state, 'contractAddress', () => args[0])
+  },
+}
+
+const MENU_WIDTH = '452px'
+const MethodsDropdown = ({ abi }: { abi: ?string }) => {
+  const classes = useDropdownStyles({ buttonWidth: MENU_WIDTH })
+  const [methodSelected, setMethodSelected] = useState('')
+  const [methodsList, setMethodsList] = useState([])
+  const [methodsListFiltered, setMethodsListFiltered] = useState([])
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [searchParams, setSearchParams] = useState('')
+
+  React.useEffect(() => {
+    if (abi) {
+      try {
+        setMethodsList(EtherscanService.extractUsefulMethods(JSON.parse(abi)))
+      } catch (e) {
+        setMethodsList([])
+      }
+    }
+  }, [abi])
+
+  React.useMemo(() => {
+    setMethodsListFiltered(methodsList.filter(({ name }) => name.toLowerCase().includes(searchParams.toLowerCase())))
+  }, [methodsList, searchParams])
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const onMethodSelectedChanged = (newMethodSelected) => {
+    setMethodSelected(newMethodSelected)
+    handleClose()
+  }
+
+  return (
+    <MuiThemeProvider theme={DropdownListTheme}>
+      <>
+        <button className={classes.button} onClick={handleClick} type="button">
+          <span className={classNames(classes.buttonInner, anchorEl && classes.openMenuButton)}>{methodSelected}</span>
+        </button>
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            horizontal: 'center',
+            vertical: 'bottom',
+          }}
+          elevation={0}
+          getContentAnchorEl={null}
+          id="customizedMenu"
+          keepMounted
+          onClose={handleClose}
+          open={!!anchorEl}
+          PaperProps={{ style: { width: MENU_WIDTH } }}
+          rounded={0}
+          transformOrigin={{
+            horizontal: 'center',
+            vertical: 'top',
+          }}
+        >
+          <MenuItem className={classes.listItemSearch} key="0">
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                onChange={(event) => setSearchParams(event.target.value)}
+                placeholder="Search…"
+                value={searchParams}
+              />
+            </div>
+          </MenuItem>
+          <div className={classes.dropdownItemsScrollWrapper}>
+            {methodsListFiltered.map(({ action, name }) => (
+              <MenuItem
+                className={classes.listItem}
+                key={name}
+                onClick={() => onMethodSelectedChanged(name)}
+                value={name}
+              >
+                <ListItemText primary={name} />
+                <ListItemIcon className={classes.iconRight}>
+                  {name === methodSelected ? <img alt="checked" src={CheckIcon} /> : null}
+                </ListItemIcon>
+                <ListItemIcon className={classes.iconRight}>
+                  <div>{action}</div>
+                </ListItemIcon>
+              </MenuItem>
+            ))}
+          </div>
+        </Menu>
+      </>
+    </MuiThemeProvider>
+  )
+}
+
+const ContractInteraction = ({ contractAddress, initialValues, onClose, onNext }: Props) => {
   const classes = useStyles()
   const { address: safeAddress, ethBalance, name: safeName } = useSelector(safeSelector)
   const [qrModalOpen, setQrModalOpen] = useState<boolean>(false)
-  const [selectedEntry, setSelectedEntry] = useState<Object | null>({
-    address: recipientAddress || initialValues.recipientAddress,
-    name: '',
-  })
-  const [pristine, setPristine] = useState<boolean>(true)
-  const [isValidAddress, setIsValidAddress] = useState<boolean>(true)
 
   React.useMemo(() => {
-    if (selectedEntry === null && pristine) {
-      setPristine(false)
+    if (contractAddress) {
+      initialValues.contractAddress = contractAddress
     }
-  }, [selectedEntry, pristine])
+  }, [contractAddress])
 
-  const handleSubmit = (values: Object) => {
-    if (values.data || values.value) {
-      onNext(values)
+  const handleSubmit = ({ contractAddress, methodArguments, methodCalled, value }: {}) => {
+    if (value || (methodCalled && methodArguments)) {
+      onNext({ contractAddress, value, methodCalled, methodArguments })
     }
   }
 
@@ -71,15 +208,6 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
 
   const closeQrModal = () => {
     setQrModalOpen(false)
-  }
-
-  const formMutators = {
-    setMax: (args, state, utils) => {
-      utils.changeValue(state, 'value', () => ethBalance)
-    },
-    setRecipient: (args, state, utils) => {
-      utils.changeValue(state, 'recipientAddress', () => args[0])
-    },
   }
 
   return (
@@ -94,14 +222,15 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
         </IconButton>
       </Row>
       <Hairline />
-      <GnoForm formMutators={formMutators} initialValues={initialValues} onSubmit={handleSubmit}>
-        {(...args) => {
-          const mutators = args[3]
-
-          let shouldDisableSubmitButton = !isValidAddress
-          if (selectedEntry) {
-            shouldDisableSubmitButton = !selectedEntry.address
-          }
+      <GnoForm
+        decorators={[abiExtractor]}
+        formMutators={formMutators}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {(submitting, validating, rest) => {
+          const { form, valid } = rest
+          const { mutators } = form
 
           const handleScan = (value) => {
             let scannedAddress = value
@@ -110,7 +239,7 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
               scannedAddress = scannedAddress.replace('ethereum:', '')
             }
 
-            mutators.setRecipient(scannedAddress)
+            mutators.setContractAddress(scannedAddress)
             closeQrModal()
           }
 
@@ -126,87 +255,36 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
                     <Hairline />
                   </Col>
                 </Row>
-                {selectedEntry && selectedEntry.address ? (
-                  <div
-                    onKeyDown={(e) => {
-                      if (e.keyCode !== 9) {
-                        setSelectedEntry(null)
-                      }
-                    }}
-                    role="listbox"
-                    tabIndex="0"
-                  >
-                    <Row margin="xs">
-                      <Paragraph color="disabled" noMargin size="md" style={{ letterSpacing: '-0.5px' }}>
-                        Recipient
-                      </Paragraph>
-                    </Row>
-                    <Row align="center" margin="md">
-                      <Col xs={1}>
-                        <Identicon address={selectedEntry.address} diameter={32} />
-                      </Col>
-                      <Col layout="column" xs={11}>
-                        <Block justify="left">
-                          <Block>
-                            <Paragraph
-                              className={classes.selectAddress}
-                              noMargin
-                              onClick={() => setSelectedEntry(null)}
-                              weight="bolder"
-                            >
-                              {selectedEntry.name}
-                            </Paragraph>
-                            <Paragraph
-                              className={classes.selectAddress}
-                              noMargin
-                              onClick={() => setSelectedEntry(null)}
-                              weight="bolder"
-                            >
-                              {selectedEntry.address}
-                            </Paragraph>
-                          </Block>
-                          <CopyBtn content={selectedEntry.address} />
-                          <EtherscanBtn type="address" value={selectedEntry.address} />
-                        </Block>
-                      </Col>
-                    </Row>
-                  </div>
-                ) : (
-                  <>
-                    <Row margin="md">
-                      <Col xs={11}>
-                        <AddressBookInput
-                          fieldMutator={mutators.setRecipient}
-                          isCustomTx
-                          pristine={pristine}
-                          setIsValidAddress={setIsValidAddress}
-                          setSelectedEntry={setSelectedEntry}
-                        />
-                      </Col>
-                      <Col center="xs" className={classes} middle="xs" xs={1}>
-                        <Img
-                          alt="Scan QR"
-                          className={classes.qrCodeBtn}
-                          height={20}
-                          onClick={() => {
-                            openQrModal()
-                          }}
-                          role="button"
-                          src={QRIcon}
-                        />
-                      </Col>
-                    </Row>
-                  </>
-                )}
-                <Row margin="xs">
-                  <Col between="lg">
-                    <Paragraph color="disabled" noMargin size="md" style={{ letterSpacing: '-0.5px' }}>
-                      Value
-                    </Paragraph>
-                    <ButtonLink onClick={mutators.setMax} weight="bold">
-                      Send max
-                    </ButtonLink>
+                <Row margin="md">
+                  <Col xs={11}>
+                    <Field
+                      component={TextField}
+                      name="contractAddress"
+                      placeholder="Contract Address*"
+                      testId="contractInteraction-contractAddress"
+                      text="Contract Address*"
+                      type="text"
+                      validate={composeValidators(required, mustBeEthereumAddress, mustBeEthereumContractAddress)}
+                    />
                   </Col>
+                  <Col center="xs" className={classes} middle="xs" xs={1}>
+                    <Img
+                      alt="Scan QR"
+                      className={classes.qrCodeBtn}
+                      height={20}
+                      onClick={openQrModal}
+                      role="button"
+                      src={QRIcon}
+                    />
+                  </Col>
+                </Row>
+                <Row className={classes.fullWidth} margin="xs">
+                  <Paragraph color="disabled" noMargin size="md" style={{ letterSpacing: '-0.5px' }}>
+                    Value
+                  </Paragraph>
+                  <ButtonLink onClick={() => mutators.setMax(ethBalance)} weight="bold">
+                    Send max
+                  </ButtonLink>
                 </Row>
                 <Row margin="md">
                   <Col>
@@ -217,8 +295,8 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
                         endAdornment: <InputAdornment position="end">ETH</InputAdornment>,
                       }}
                       name="value"
-                      placeholder="Value*"
-                      text="Value*"
+                      placeholder="Value"
+                      text="Value"
                       type="text"
                       validate={composeValidators(mustBeFloat, maxValue(ethBalance))}
                     />
@@ -226,12 +304,26 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
                 </Row>
                 <Row margin="sm">
                   <Col>
-                    <TextareaField
-                      name="data"
-                      placeholder="Data (hex encoded)*"
-                      text="Data (hex encoded)*"
-                      type="text"
-                    />
+                    <TextareaField name="abi" placeholder="ABI*" text="ABI*" type="text" />
+                  </Col>
+                </Row>
+                <Row margin="sm">
+                  <Col end="sm">
+                    <Field name="abi" subscription={{ value: true }}>
+                      {({ input: { value } }) => (
+                        <label htmlFor="interactWithABI">
+                          Interact with Contract?{' '}
+                          <Field component={GnoCheckbox} disabled={!value} name="interactWithABI" type="checkbox" />
+                        </label>
+                      )}
+                    </Field>
+                  </Col>
+                </Row>
+                <Row margin="sm">
+                  <Col>
+                    <Field name="interactWithABI" subscription={{ value: true }}>
+                      {({ input: { value } }) => (value === true ? <MethodsDropdown abi={rest.values.abi} /> : null)}
+                    </Field>
                   </Col>
                 </Row>
               </Block>
@@ -244,7 +336,7 @@ const ContractInteraction = ({ initialValues, onClose, onNext, recipientAddress 
                   className={classes.submitButton}
                   color="primary"
                   data-testid="review-tx-btn"
-                  disabled={shouldDisableSubmitButton}
+                  disabled={submitting || validating || !valid}
                   minWidth={140}
                   type="submit"
                   variant="contained"
