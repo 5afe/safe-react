4567e07d700db1ec11cab2994ce9cbb941c9b8e2
Mati Dastugue <matias.dastugue@altoros.com>
2020-04-30 16:26:36 -0300

    Added logs for desktop app

--- scripts/preload.js
+++ scripts/preload.js
@@ -2,11 +2,21 @@
 // It has the same sandbox as a Chrome extension.
 
 const TransportNodeHid = require("@ledgerhq/hw-transport-node-hid").default;
+const log = require('electron-log');
 window.TransportNodeHid = TransportNodeHid;
 
 window.isDesktop = true;
 
 window.addEventListener('DOMContentLoaded', () => {
+  console.error = (...args) => {
+    log.error(...args)
+  }
+  console.warn = (...args) => {
+    log.warn(...args)
+  }
+  console.log = (...args) => {
+    log.info(...args)
+  }
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
