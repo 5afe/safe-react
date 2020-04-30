69ecbf8072a141fee61eca20edc871f47a0c4b6e
Agustín Longoni <agustin.longoni@altoros.com>
2020-04-30 12:02:09 -0300

    Update Layout.jsx

--- src/components-v2/layouts/ListContentLayout/Layout.jsx
+++ src/components-v2/layouts/ListContentLayout/Layout.jsx
@@ -11,7 +11,6 @@ export const Wrapper = styled.div`
     box-shadow: 1px 2px 10px 0 rgba(212, 212, 211, 0.59);
     background-color: white;
     }
-  }
 `
 export const Nav = styled.div`
   grid-column: 1/3;
