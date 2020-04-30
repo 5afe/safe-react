15e633998320bee2ccd6a027962ce8b80e8acc05
Agustín Longoni <agustin.longoni@altoros.com>
2020-04-30 11:39:47 -0300

    add scroll and styles

--- src/components-v2/layouts/ListContentLayout/Layout.jsx
+++ src/components-v2/layouts/ListContentLayout/Layout.jsx
@@ -10,6 +10,7 @@ export const Wrapper = styled.div`
   .background {
     box-shadow: 1px 2px 10px 0 rgba(212, 212, 211, 0.59);
     background-color: white;
+    }
   }
 `
 export const Nav = styled.div`
@@ -28,6 +29,22 @@ export const Menu = styled.div.attrs(() => ({ className: 'background' }))`
   border-top-left-radius: 8px;
   border-bottom-left-radius: 8px;
   background-color: white;
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 0.7em !important;
+    scroll-behavior: smooth !important;
+  }
+
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3) !important;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: darkgrey !important;
+    outline: 1px solid slategrey !important;
+    border-radius: 10px !important;
+  }
 `
 
 export const Content = styled.div.attrs(() => ({ className: 'background' }))`
