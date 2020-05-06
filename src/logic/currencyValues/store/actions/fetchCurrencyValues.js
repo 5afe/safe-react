a98652abe464ac740a891a8b9896805d2201b6a3
Agustin Pane <agustin.pane@gmail.com>
2020-05-06 17:03:59 -0300

    (Fix) - 772 safe zero balances (#832)
    * Refactor currency values:
    - Creates middleware to save currency values on actions
    - Replaces currency values model to be safeAddress-dependant
    -
    
    * - Adds all the safes balances on safe load
    - Saves the currency values status to localStorage on the middleware
    
    * Fixs equals comparation used to avoid unnecessary dispatchs
    
    * Update naming
    
    * Fixs rate type
    
    * Fixs wrong variable usage
    
    * Removes currencyBalances from localStorage, just saves currencyRate and currencyValueSelected
    
    * Renames currencyValueSelected to currencyValueSelected
    
    * Refactors currencyValueSelected to selectedCurrencyValue
    
    * Refactors selectedCurrencyValue to selectedCurrency

--- src/logic/currencyValues/store/actions/fetchCurrencyValues.js
+++ src/logic/currencyValues/store/actions/fetchCurrencyValues.js
@@ -1,36 +1,42 @@
 // @flow
+import { List } from 'immutable'
 import { batch } from 'react-redux'
 import type { Dispatch as ReduxDispatch } from 'redux'
 
-import fetchCurrencySelectedValue from '~/logic/currencyValues/store/actions/fetchCurrencySelectedValue'
-import { CURRENCY_SELECTED_KEY } from '~/logic/currencyValues/store/actions/saveCurrencySelected'
+import fetchCurrencyRate from '~/logic/currencyValues/store/actions/fetchCurrencyRate'
+import { setCurrencyBalances } from '~/logic/currencyValues/store/actions/setCurrencyBalances'
 import { setCurrencyRate } from '~/logic/currencyValues/store/actions/setCurrencyRate'
-import { setCurrencySelected } from '~/logic/currencyValues/store/actions/setCurrencySelected'
+import { setSelectedCurrency } from '~/logic/currencyValues/store/actions/setSelectedCurrency'
 import { AVAILABLE_CURRENCIES } from '~/logic/currencyValues/store/model/currencyValues'
+import { loadCurrencyValues } from '~/logic/currencyValues/store/utils/currencyValuesStorage'
+import fetchSafeTokens from '~/logic/tokens/store/actions/fetchSafeTokens'
 import type { GlobalState } from '~/store'
-import { loadFromStorage } from '~/utils/storage'
 
-export const fetchCurrencyValues = () => async (dispatch: ReduxDispatch<GlobalState>) => {
+export const fetchCurrencyValues = (safeAddress: string) => async (dispatch: ReduxDispatch<GlobalState>) => {
   try {
-    const currencyStored = await loadFromStorage(CURRENCY_SELECTED_KEY)
-
-    if (!currencyStored) {
+    const storedCurrencies = await loadCurrencyValues()
+    const storedCurrency = storedCurrencies[safeAddress]
+    if (!storedCurrency) {
       return batch(() => {
-        dispatch(setCurrencySelected(AVAILABLE_CURRENCIES.USD))
-        dispatch(setCurrencyRate(1))
+        dispatch(setCurrencyBalances(safeAddress, List([])))
+        dispatch(setSelectedCurrency(safeAddress, AVAILABLE_CURRENCIES.USD))
+        dispatch(setCurrencyRate(safeAddress, 1))
       })
     }
-
-    const { currencyValueSelected } = currencyStored
-
-    batch(() => {
-      dispatch(setCurrencySelected(currencyValueSelected))
-      dispatch(fetchCurrencySelectedValue(currencyValueSelected))
+    // Loads the stored state on redux
+    Object.entries(storedCurrencies).forEach((kv) => {
+      const safeAddr = kv[0]
+      const value = kv[1]
+      const { currencyRate, selectedCurrency } = value
+      batch(() => {
+        dispatch(setSelectedCurrency(safeAddr, selectedCurrency))
+        dispatch(setCurrencyRate(safeAddr, currencyRate))
+        dispatch(fetchCurrencyRate(safeAddr, selectedCurrency))
+        dispatch(fetchSafeTokens(safeAddress))
+      })
     })
   } catch (err) {
-    console.error('Error fetching tokens price list', err)
+    console.error('Error fetching currency values', err)
   }
   return Promise.resolve()
 }
-
-export default fetchCurrencyValues
