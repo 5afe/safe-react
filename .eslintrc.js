7f23177b4bde898d993f69e3df3147c3a283e8e8
nicosampler <nf.dominguez.87@gmail.com>
2020-05-19 10:48:03 -0300

    some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
     react: {
