a07f194c2e4771fe4fd02b1d9411881d5e1eea08
Agustin Pane <agustin.pane@gmail.com>
2020-05-22 11:44:46 -0300

    Merge branch 'development' into 623-show-more
    # Conflicts:
    #	src/routes/safe/components/Transactions/TxsTable/ExpandedTx/TxDescription/index.tsx

--- src/routes/safe/components/Settings/SafeDetails/style.ts
+++ src/routes/safe/components/Settings/SafeDetails/style.ts
@@ -1,4 +1,4 @@
-import { boldFont, border, lg, sm } from 'src/theme/variables'
+import { boldFont, border, lg, sm, fontColor } from 'src/theme/variables'
 
 export const styles = () => ({
   formContainer: {
@@ -24,7 +24,7 @@ export const styles = () => ({
     height: '21px',
   },
   link: {
-    color: '#001428',
-    borderBottom: '1px solid #001428',
+    color: `${fontColor}`,
+    borderBottom: `1px solid ${fontColor}`,
   },
 })
