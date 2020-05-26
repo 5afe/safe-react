30a70d47a6e390c61e375d0d66c6e71b31bea959
Agustin Pane <agustin.pane@gmail.com>
2020-05-26 12:02:40 -0300

    Add copy and etherscanlinks to creation safe details

--- src/routes/safe/components/Transactions/TxsTable/ExpandedTx/CreationTx/index.tsx
+++ src/routes/safe/components/Transactions/TxsTable/ExpandedTx/CreationTx/index.tsx
@@ -3,11 +3,31 @@ import React from 'react'
 import { formatDate } from '../../columns'
 import Bold from '../../../../../../../components/layout/Bold'
 import Paragraph from '../../../../../../../components/layout/Paragraph'
+import EtherscanLink from '../../../../../../../components/EtherscanLink'
+import { makeStyles } from '@material-ui/core/styles'
+import Block from '../../../../../../../components/layout/Block'
+
+const useStyles = makeStyles({
+  address: {
+    height: '20px',
+  },
+  txData: {
+    alignItems: 'center',
+    display: 'flex',
+    lineHeight: '1.6',
+  },
+  txHash: {
+    paddingRight: '3px',
+  },
+})
 
 export const CreationTx = (props) => {
   const { tx } = props
+  const classes = useStyles()
   if (!tx) return null
   const isCreationTx = tx.type === 'creation'
+
+  console.log('Classes', classes)
 
   return !isCreationTx ? null : (
     <>
@@ -15,18 +35,18 @@ export const CreationTx = (props) => {
         <Bold>Created: </Bold>
         {formatDate(tx.created)}
       </Paragraph>
-      <Paragraph noMargin>
-        <Bold>Creator: </Bold>
-        {tx.creator}
-      </Paragraph>
-      <Paragraph noMargin>
-        <Bold>Factory: </Bold>
-        {tx.factoryAddress}
-      </Paragraph>
-      <Paragraph noMargin>
-        <Bold>Mastercopy: </Bold>
-        {tx.masterCopy}
-      </Paragraph>
+      <Block align="left" className={classes.txData}>
+        <Bold className={classes.txHash}>Creator:</Bold>
+        {tx.creator ? <EtherscanLink cut={8} type="address" value={tx.creator} /> : 'n/a'}
+      </Block>
+      <Block align="left" className={classes.txData}>
+        <Bold className={classes.txHash}>Factory:</Bold>
+        {tx.factoryAddress ? <EtherscanLink cut={8} type="address" value={tx.factoryAddress} /> : 'n/a'}
+      </Block>
+      <Block align="left" className={classes.txData}>
+        <Bold className={classes.txHash}>Mastercopy:</Bold>
+        {tx.masterCopy ? <EtherscanLink cut={8} type="address" value={tx.masterCopy} /> : 'n/a'}
+      </Block>
     </>
   )
 }
