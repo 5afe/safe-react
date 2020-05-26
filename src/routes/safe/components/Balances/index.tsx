96ddfb5f3548dc695470624c7475b040fbfe6b21
Agustin Pane <agustin.pane@gmail.com>
2020-05-26 12:25:23 -0300

    Fix hooks import

--- src/routes/safe/components/Balances/index.tsx
+++ src/routes/safe/components/Balances/index.tsx
@@ -18,7 +18,7 @@ import CurrencyDropdown from 'src/routes/safe/components/CurrencyDropdown'
 import { safeFeaturesEnabledSelector, safeParamAddressFromStateSelector } from 'src/routes/safe/store/selectors'
 import { history } from 'src/store/index'
 import { wrapInSuspense } from 'src/utils/wrapInSuspense'
-import { useFetchTokens } from '../../container/hooks/useFetchTokens'
+import { useFetchTokens } from '../../container/Hooks/useFetchTokens'
 
 const Collectibles = React.lazy(() => import('src/routes/safe/components/Balances/Collectibles'))
 const Coins = React.lazy(() => import('src/routes/safe/components/Balances/Coins'))
