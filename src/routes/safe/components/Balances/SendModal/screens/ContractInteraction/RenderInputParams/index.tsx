20d6260c7b52429b8959dc84a385f454293c58c0
Agustin Pane <agustin.pane@gmail.com>
2020-06-02 12:14:31 -0300

    Refactor input params to use checkbox component for boolean types

--- src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/RenderInputParams/index.tsx
+++ src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/RenderInputParams/index.tsx
@@ -6,6 +6,7 @@ import TextField from 'src/components/forms/TextField'
 import { composeValidators, mustBeEthereumAddress, required } from 'src/components/forms/validator'
 import Col from 'src/components/layout/Col'
 import Row from 'src/components/layout/Row'
+import Checkbox from 'src/components/forms/Checkbox'
 
 const RenderInputParams = () => {
   const {
@@ -16,26 +17,63 @@ const RenderInputParams = () => {
   }: any = useField('selectedMethod', { value: true })
   const renderInputs = validABI && !!method && method.inputs.length
 
+  const renderInputComponent = (type, name, key, placeholder) => {
+    if (!type) {
+      return null
+    }
+    switch (type) {
+      case 'bool': {
+        const inputProps = {
+          'data-testid': key,
+        }
+        return (
+          <Col>
+            <Field component={Checkbox} name={key} label={placeholder} type="checkbox" inputProps={inputProps} />
+          </Col>
+        )
+      }
+      case 'address': {
+        return (
+          <Col>
+            <Field
+              component={TextField}
+              name={key}
+              placeholder={placeholder}
+              testId={key}
+              text={placeholder}
+              type="text"
+              validate={composeValidators(required, mustBeEthereumAddress)}
+            />
+          </Col>
+        )
+      }
+      default: {
+        return (
+          <Col>
+            <Field
+              component={TextField}
+              name={key}
+              placeholder={placeholder}
+              testId={key}
+              text={placeholder}
+              type="text"
+              validate={required}
+            />
+          </Col>
+        )
+      }
+    }
+  }
+
   return !renderInputs
     ? null
     : method.inputs.map(({ name, type }, index) => {
         const placeholder = name ? `${name} (${type})` : type
         const key = `methodInput-${method.name}_${index}_${type}`
-        const validate = type === 'address' ? composeValidators(required, mustBeEthereumAddress) : required
 
         return (
           <Row key={key} margin="sm">
-            <Col>
-              <Field
-                component={TextField}
-                name={key}
-                placeholder={placeholder}
-                testId={key}
-                text={placeholder}
-                type="text"
-                validate={validate}
-              />
-            </Col>
+            {renderInputComponent(type, name, key, placeholder)}
           </Row>
         )
       })
