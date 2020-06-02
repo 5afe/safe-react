48ee6599741b4f47405c1afd9d11c6dad3b85dfb
Agustin Pane <agustin.pane@gmail.com>
2020-06-02 13:48:12 -0300

    Fix ts issues

--- src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/RenderInputParams/index.tsx
+++ src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/RenderInputParams/index.tsx
@@ -11,7 +11,7 @@ import Checkbox from 'src/components/forms/Checkbox'
 const RenderInputParams = () => {
   const {
     meta: { valid: validABI },
-  } = useField('abi', { valid: true } as Record<string, boolean>)
+  } = useField('abi', { value: true })
   const {
     input: { value: method },
   }: any = useField('selectedMethod', { value: true })
