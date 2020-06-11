78f0a50467e4c896c70b1fb28acdf499ebbe6822
Agustin Pane <agustin.pane@gmail.com>
2020-06-11 10:08:54 -0300

    Simplify identicon logic

--- src/components/Identicon/index.tsx
+++ src/components/Identicon/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
 import makeBlockie from 'ethereum-blockies-base64'
-import { useEffect, useState } from 'react'
 
 type Props = {
   address: string
@@ -12,49 +11,22 @@ type Props = {
 type StyleProps = {
   width: number
   height: number
+  borderRadius: string
 }
 
 const getStyleFrom = (diameter: number): StyleProps => ({
   width: diameter,
   height: diameter,
+  borderRadius: `${diameter / 2}px`,
 })
 
-const generateBlockieIdenticon = (address: string, diameter: number): HTMLImageElement => {
-  const image = new window.Image()
-  image.src = makeBlockie(address)
-  image.height = diameter
-  image.width = diameter
-  image.style.borderRadius = `${diameter / 2}px`
-
-  return image
-}
-
 const Identicon = (props: Props) => {
-  const { address, diameter, className } = props
+  const { address, diameter } = props
   const style: StyleProps = getStyleFrom(diameter)
 
-  const [identicon, setIdenticon] = useState(null)
+  const iconSrc = React.useMemo(() => makeBlockie(address), [address])
 
-  useEffect(() => {
-    setIdenticon(React.createRef<HTMLImageElement>())
-  }, [])
-
-  useEffect(() => {
-    const image = generateBlockieIdenticon(address, diameter)
-
-    if (!identicon || !identicon.current) {
-      return
-    }
-
-    const { children } = identicon.current
-    for (let i = 0; i < children.length; i += 1) {
-      identicon.current.removeChild(children[i])
-    }
-
-    identicon.current.appendChild(image)
-  }, [address, diameter, identicon])
-
-  return <div className={className} ref={identicon} style={style} />
+  return <img src={iconSrc} style={style} {...props} />
 }
 
 export default Identicon
