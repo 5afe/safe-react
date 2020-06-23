8f0cccdb5917dcab5f77c8c0cccebd2c1faddd5e
Mikhail Mikheev <mmvsha73@gmail.com>
2020-06-23 19:03:02 +0400

    Fix token storage type errors, remove unused functions

--- src/logic/tokens/utils/tokensStorage.ts
+++ src/logic/tokens/utils/tokensStorage.ts
@@ -1,6 +1,7 @@
-import { List } from 'immutable'
+import { Map } from 'immutable'
 
 import { loadFromStorage, saveToStorage } from 'src/utils/storage'
+import { TokenProps, Token } from './../store/model/token'
 
 export const ACTIVE_TOKENS_KEY = 'ACTIVE_TOKENS'
 export const CUSTOM_TOKENS_KEY = 'CUSTOM_TOKENS'
@@ -9,7 +10,7 @@ export const CUSTOM_TOKENS_KEY = 'CUSTOM_TOKENS'
 // to avoid iterating a large amount of data of tokens from the backend
 // Custom tokens should be saved too unless they're deleted (marking them as inactive doesn't count)
 
-export const saveActiveTokens = async (tokens) => {
+export const saveActiveTokens = async (tokens: Map<string, Token>): Promise<void> => {
   try {
     await saveToStorage(ACTIVE_TOKENS_KEY, tokens.toJS())
   } catch (err) {
@@ -17,34 +18,8 @@ export const saveActiveTokens = async (tokens) => {
   }
 }
 
-export const getActiveTokens = async () => {
-  const data = await loadFromStorage(ACTIVE_TOKENS_KEY)
+export const getActiveTokens = async (): Promise<Record<string, TokenProps> | undefined> => {
+  const data = await loadFromStorage<Record<string, TokenProps>>(ACTIVE_TOKENS_KEY)
 
-  return data || {}
-}
-
-export const getCustomTokens = async () => {
-  const data = await loadFromStorage(CUSTOM_TOKENS_KEY)
-
-  return data ? List(data) : List()
-}
-
-export const removeTokenFromStorage = async (safeAddress, token) => {
-  const data = await getCustomTokens()
-
-  try {
-    const index = data.indexOf(token)
-    await saveToStorage(CUSTOM_TOKENS_KEY, data.remove(index))
-  } catch (err) {
-    console.error('Error removing token in localstorage', err)
-  }
-}
-
-export const removeFromActiveTokens = async (safeAddress, token) => {
-  const activeTokens = await getActiveTokens()
-  const index = activeTokens.findIndex((activeToken) => activeToken.name === token.name)
-
-  if (index !== -1) {
-    await saveActiveTokens(safeAddress)
-  }
+  return data
 }
