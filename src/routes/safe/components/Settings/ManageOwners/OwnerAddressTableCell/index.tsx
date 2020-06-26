c9d6d70754406eb9a26702a854a3ac05c3ef8268
Agustin Pane <agustin.pane@gmail.com>
2020-06-26 11:08:50 -0300

    Adds useWindowDimensions hook Uses useWindowDimensions hook to render the address on ownerAddressTable depending of the size of the screen

--- src/routes/safe/components/Settings/ManageOwners/OwnerAddressTableCell/index.tsx
+++ src/routes/safe/components/Settings/ManageOwners/OwnerAddressTableCell/index.tsx
@@ -4,16 +4,34 @@ import EtherScanLink from 'src/components/EtherscanLink'
 import Identicon from 'src/components/Identicon'
 import Block from 'src/components/layout/Block'
 import Paragraph from 'src/components/layout/Paragraph'
+import { screenMd, screenSm, screenLg } from 'src/theme/variables'
+import { useWindowDimensions } from '../../../../container/hooks/useWindowDimensions'
+import { useEffect, useState } from 'react'
 
 const OwnerAddressTableCell = (props) => {
   const { address, knownAddress, showLinks, userName } = props
+  const [cut, setCut] = useState(undefined)
+  const { width } = useWindowDimensions()
+
+  useEffect(() => {
+    if (width >= screenLg) {
+      setCut(12)
+    } else if (width >= screenMd) {
+      setCut(undefined)
+    } else if (width >= screenSm) {
+      setCut(8)
+    } else {
+      setCut(6)
+    }
+  }, [width])
+
   return (
     <Block justify="left">
       <Identicon address={address} diameter={32} />
       {showLinks ? (
         <div style={{ marginLeft: 10, flexShrink: 1, minWidth: 0 }}>
           {userName}
-          <EtherScanLink knownAddress={knownAddress} type="address" value={address} />
+          <EtherScanLink knownAddress={knownAddress} type="address" value={address} cut={cut} />
         </div>
       ) : (
         <Paragraph style={{ marginLeft: 10 }}>{address}</Paragraph>
