1dbe2e76f5268802888871729b13ec2d8b1c1960
fernandomg <fernando.greco@gmail.com>
2020-07-08 17:30:18 -0300

    dataDecoded helper functions

---
+++ src/routes/safe/store/actions/transactions/utils/multiSendDecodedDetails.ts
@@ -0,0 +1,77 @@
+import { TransferDetails } from './transferDetailsTypes'
+import {
+  MultiSigTransaction,
+  Operation,
+  Parameter,
+  Transfer,
+  TransferType,
+  TxConstants,
+  DataDecoded,
+} from 'src/routes/safe/store/models/types/transactions'
+import {
+  extractERC20TransferDetails,
+  extractERC721TransferDetails,
+  extractETHTransferDetails,
+  extractUnknownTransferDetails,
+} from './transferDetails'
+import { isMultiSendParameter } from './newTransactionHelpers'
+
+export type MultiSendDetails = {
+  operation: keyof typeof Operation
+  to: string
+  data: DataDecoded | null
+  value: number
+}
+
+type TxDetails = MultiSendDetails | DataDecoded
+
+export type MultiSendDecodedData = {
+  txDetails?: TxDetails[] | null
+  transfersDetails: TransferDetails[]
+}
+
+export const isMultiSendDetails = (details: TxDetails): details is MultiSendDetails => {
+  return !!(details as MultiSendDetails)?.operation
+}
+
+export const extractTransferDetails = (transfer: Transfer): TransferDetails => {
+  switch (TransferType[transfer.type]) {
+    case TransferType.ERC20_TRANSFER:
+      return extractERC20TransferDetails(transfer)
+    case TransferType.ERC721_TRANSFER:
+      return extractERC721TransferDetails(transfer)
+    case TransferType.ETHER_TRANSFER:
+      return extractETHTransferDetails(transfer)
+    default:
+      return extractUnknownTransferDetails(transfer)
+  }
+}
+
+export const extractMultiSendDetails = (parameter: Parameter): MultiSendDetails[] | null => {
+  if (isMultiSendParameter(parameter)) {
+    const multiSendDetails = parameter.decodedValue.map((decodedValue) => {
+      return {
+        operation: decodedValue.operation,
+        to: decodedValue.to,
+        value: decodedValue.value,
+        data: decodedValue?.decodedData ?? null,
+      }
+    })
+    console.log({ multiSendDetails })
+    return multiSendDetails
+  }
+
+  return null
+}
+
+export const extractMultiSendDecodedData = (tx: MultiSigTransaction): MultiSendDecodedData => {
+  const transfersDetails = null ?? tx.transfers?.map(extractTransferDetails)
+
+  const txDetails =
+    tx.dataDecoded?.method === TxConstants.MULTI_SEND
+      ? extractMultiSendDetails(tx.dataDecoded?.parameters[0])
+      : [tx.dataDecoded]
+
+  console.log({ txDetails, transfersDetails })
+  return { txDetails, transfersDetails }
+}
---
+++ src/routes/safe/store/actions/transactions/utils/transferDetails.ts
@@ -0,0 +1,88 @@
+import { Transfer, TxConstants } from 'src/routes/safe/store/models/types/transactions'
+import { sameAddress } from 'src/logic/wallets/ethAddresses'
+import { store } from 'src/store'
+import { safeParamAddressFromStateSelector } from 'src/routes/safe/store/selectors'
+import { BigNumber } from 'bignumber.js'
+import {
+  ERC20TransferDetails,
+  ERC721TransferDetails,
+  ETHTransferDetails,
+  UnknownTransferDetails,
+} from './transferDetailsTypes'
+
+const isIncomingTransfer = (transfer: Transfer): boolean => {
+  const state = store.getState()
+  const safeAddress = safeParamAddressFromStateSelector(state)
+  return sameAddress(transfer.to, safeAddress)
+}
+
+const humanReadableValue = (value: string, decimals = 18): string => {
+  return new BigNumber(value).times(`1e-${decimals}`).toFixed()
+}
+
+export const extractERC20TransferDetails = (transfer: Transfer): ERC20TransferDetails => {
+  const erc20TransferDetails = {
+    tokenAddress: transfer.tokenInfo?.address || TxConstants.UNKNOWN,
+    value: humanReadableValue(transfer.value, transfer.tokenInfo?.decimals),
+    name: transfer.tokenInfo?.name || transfer.tokenInfo?.symbol || TxConstants.UNKNOWN,
+    txHash: transfer.transactionHash,
+  }
+
+  if (isIncomingTransfer(transfer)) {
+    return {
+      ...erc20TransferDetails,
+      from: transfer.from,
+    }
+  }
+
+  return {
+    ...erc20TransferDetails,
+    to: transfer.to,
+  }
+}
+
+export const extractERC721TransferDetails = (transfer: Transfer): ERC721TransferDetails => {
+  const erc721TransferDetails = {
+    tokenAddress: transfer.tokenAddress,
+    tokenId: transfer.tokenId,
+    txHash: transfer.transactionHash,
+  }
+  if (isIncomingTransfer(transfer)) {
+    return {
+      ...erc721TransferDetails,
+      from: transfer.from,
+    }
+  }
+
+  return {
+    ...erc721TransferDetails,
+    to: transfer.to,
+  }
+}
+
+export const extractETHTransferDetails = (transfer: Transfer): ETHTransferDetails => {
+  const ethTransferDetails = {
+    value: humanReadableValue(transfer.value),
+    txHash: transfer.transactionHash,
+  }
+  if (isIncomingTransfer(transfer)) {
+    return {
+      ...ethTransferDetails,
+      from: transfer.from,
+    }
+  }
+
+  return {
+    ...ethTransferDetails,
+    to: transfer.to,
+  }
+}
+
+export const extractUnknownTransferDetails = (transfer: Transfer): UnknownTransferDetails => {
+  return {
+    value: transfer?.value || TxConstants.UNKNOWN,
+    txHash: transfer?.transactionHash || TxConstants.UNKNOWN,
+    to: transfer?.to || TxConstants.UNKNOWN,
+    from: transfer?.from || TxConstants.UNKNOWN,
+  }
+}
