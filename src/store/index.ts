c3149107131baecfd4bd4f065ee444a3d9fa37ce
fernandomg <fernando.greco@gmail.com>
2020-07-14 13:14:37 -0300

    use `TokenState` in AppReduxState

--- src/store/index.ts
+++ src/store/index.ts
@@ -17,7 +17,7 @@ import currencyValuesStorageMiddleware from 'src/logic/currencyValues/store/midd
 import currencyValues, { CURRENCY_VALUES_KEY } from 'src/logic/currencyValues/store/reducer/currencyValues'
 import currentSession, { CURRENT_SESSION_REDUCER_ID } from 'src/logic/currentSession/store/reducer/currentSession'
 import notifications, { NOTIFICATIONS_REDUCER_ID } from 'src/logic/notifications/store/reducer/notifications'
-import tokens, { TOKEN_REDUCER_ID } from 'src/logic/tokens/store/reducer/tokens'
+import tokens, { TOKEN_REDUCER_ID, TokenState } from 'src/logic/tokens/store/reducer/tokens'
 import providerWatcher from 'src/logic/wallets/store/middlewares/providerWatcher'
 import provider, { PROVIDER_REDUCER_ID } from 'src/logic/wallets/store/reducer/provider'
 import notificationsMiddleware from 'src/routes/safe/store/middleware/notificationsMiddleware'
@@ -31,7 +31,6 @@ import incomingTransactions, {
 import safe, { SAFE_REDUCER_ID, SafeReducerMap } from 'src/routes/safe/store/reducer/safe'
 import transactions, { TRANSACTIONS_REDUCER_ID } from 'src/routes/safe/store/reducer/transactions'
 import { ProviderRecord } from 'src/logic/wallets/store/model/provider'
-import { Token } from 'src/logic/tokens/store/model/token'
 import { NFTAssets, NFTTokens } from 'src/logic/collectibles/sources/OpenSea'
 
 export const history = createHashHistory({ hashType: 'slash' })
@@ -72,7 +71,7 @@ export type AppReduxState = CombinedState<{
   [SAFE_REDUCER_ID]: SafeReducerMap
   [NFT_ASSETS_REDUCER_ID]?: NFTAssets
   [NFT_TOKENS_REDUCER_ID]?: NFTTokens
-  [TOKEN_REDUCER_ID]?: Map<string, Token>
+  [TOKEN_REDUCER_ID]?: TokenState
   [TRANSACTIONS_REDUCER_ID]: Map<string, any>
   [CANCELLATION_TRANSACTIONS_REDUCER_ID]: Map<string, any>
   [INCOMING_TRANSACTIONS_REDUCER_ID]: Map<string, any>
