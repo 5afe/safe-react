67c67f4569d24e102c5c06f87bbd6e790d560ff3
fernandomg <fernando.greco@gmail.com>
2020-07-15 19:17:12 -0300

    set type for dispatch actions in hooks

--- src/routes/safe/container/hooks/useFetchTokens.tsx
+++ src/routes/safe/container/hooks/useFetchTokens.tsx
@@ -1,6 +1,8 @@
 import { useMemo } from 'react'
 import { batch, useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
+import { AnyAction } from 'redux'
+import { ThunkDispatch } from 'redux-thunk'
 
 import fetchCollectibles from 'src/logic/collectibles/store/actions/fetchCollectibles'
 import { fetchCurrencyValues } from 'src/logic/currencyValues/store/actions/fetchCurrencyValues'
@@ -8,9 +10,12 @@ import activateAssetsByBalance from 'src/logic/tokens/store/actions/activateAsse
 import fetchSafeTokens from 'src/logic/tokens/store/actions/fetchSafeTokens'
 import { fetchTokens } from 'src/logic/tokens/store/actions/fetchTokens'
 import { COINS_LOCATION_REGEX, COLLECTIBLES_LOCATION_REGEX } from 'src/routes/safe/components/Balances'
+import { AppReduxState } from 'src/store'
+
+type Dispatch = ThunkDispatch<AppReduxState, undefined, AnyAction>
 
 export const useFetchTokens = (safeAddress: string): void => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch<Dispatch>()
   const location = useLocation()
 
   useMemo(() => {
@@ -25,7 +30,7 @@ export const useFetchTokens = (safeAddress: string): void => {
 
     if (COLLECTIBLES_LOCATION_REGEX.test(location.pathname)) {
       batch(() => {
-        dispatch<any>(fetchCollectibles(safeAddress)).then(() => {
+        dispatch(fetchCollectibles(safeAddress)).then(() => {
           dispatch(activateAssetsByBalance(safeAddress))
         })
       })
