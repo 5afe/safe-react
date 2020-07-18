4648ac0079fa39b649c28f20710b7a61f6b080f4
Agustin Pane <agustin.pane@gmail.com>
2020-07-18 11:40:46 -0300

    Add types

--- src/logic/currencyValues/store/actions/fetchCurrencyRate.ts
+++ src/logic/currencyValues/store/actions/fetchCurrencyRate.ts
@@ -1,8 +1,11 @@
 import fetchCurrenciesRates from 'src/logic/currencyValues/api/fetchCurrenciesRates'
 import { setCurrencyRate } from 'src/logic/currencyValues/store/actions/setCurrencyRate'
 import { AVAILABLE_CURRENCIES } from 'src/logic/currencyValues/store/model/currencyValues'
+import { Dispatch } from 'redux'
 
-const fetchCurrencyRate = (safeAddress: string, selectedCurrency: AVAILABLE_CURRENCIES) => async (dispatch) => {
+const fetchCurrencyRate = (safeAddress: string, selectedCurrency: AVAILABLE_CURRENCIES) => async (
+  dispatch: Dispatch,
+): Promise<void> => {
   if (AVAILABLE_CURRENCIES.USD === selectedCurrency) {
     return dispatch(setCurrencyRate(safeAddress, 1))
   }
