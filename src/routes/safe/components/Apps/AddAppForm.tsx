2f309771b03018ff9ed6d835920cc25c165923a3
fernandomg <fernando.greco@gmail.com>
2020-07-23 13:01:51 -0300

    refactor AddAppForm to use the observable pattern

--- src/routes/safe/components/Apps/AddAppForm.tsx
+++ src/routes/safe/components/Apps/AddAppForm.tsx
@@ -1,20 +1,22 @@
 import { Checkbox, Text, TextField } from '@gnosis.pm/safe-react-components'
+import createDecorator from 'final-form-calculate'
 import memoize from 'lodash.memoize'
-import React, { useState } from 'react'
-import { FormSpy } from 'react-final-form'
+import React from 'react'
+import { useField, useFormState } from 'react-final-form'
 import styled from 'styled-components'
+import { useDebounce } from 'use-lodash-debounce'
+
+import { SafeApp } from './types'
+import { getAppInfoFromUrl } from './utils'
 
 import Field from 'src/components/forms/Field'
-import DebounceValidationField from 'src/components/forms/Field/DebounceValidationField'
 import GnoForm from 'src/components/forms/GnoForm'
-import { required } from 'src/components/forms/validator'
+import { composeValidators, required } from 'src/components/forms/validator'
 import Img from 'src/components/layout/Img'
 import { getContentFromENS } from 'src/logic/wallets/getWeb3'
+import { isValidEnsName } from 'src/logic/wallets/ethAddresses'
 import appsIconSvg from 'src/routes/safe/components/Transactions/TxsTable/TxType/assets/appsIcon.svg'
 import { isValid as isURLValid } from 'src/utils/url'
-
-import { getAppInfoFromUrl } from './utils'
-import { SafeApp } from './types'
 
 const APP_INFO: SafeApp = {
   id: undefined,
@@ -46,20 +48,7 @@ const StyledCheckbox = styled(Checkbox)`
   margin: 0;
 `
 
-const uniqueAppValidator = memoize((appList, value) => {
-  const exists = appList.some((a) => {
-    try {
-      const currentUrl = new URL(a.url)
-      const newUrl = new URL(value)
-      return currentUrl.href === newUrl.href
-    } catch (error) {
-      return 'There was a problem trying to validate the URL existence.'
-    }
-  })
-  return exists ? 'This app is already registered.' : undefined
-})
-
-const getIpfsLinkFromEns = memoize(async (name) => {
+const getIpfsLinkFromEns = memoize(async (name: string) => {
   try {
     const content = await getContentFromENS(name)
     if (content && content.protocolType === 'ipfs') {
@@ -71,81 +60,120 @@ const getIpfsLinkFromEns = memoize(async (name) => {
   }
 })
 
-const getUrlFromFormValue = memoize(async (value: string) => {
-  const isUrlValid = isURLValid(value)
-  let ensContent
-  if (!isUrlValid) {
-    ensContent = await getIpfsLinkFromEns(value)
-  }
-
-  if (!isUrlValid && ensContent === undefined) {
-    return undefined
-  }
-  return isUrlValid ? value : ensContent
-})
-
-const curriedSafeAppValidator = (appList) =>
-  memoize(async (value: string) => {
-    const url = await getUrlFromFormValue(value)
-
-    if (!url) {
-      return 'Provide a valid url or ENS name.'
-    }
-
-    const appExistsRes = uniqueAppValidator(appList, url)
-    if (appExistsRes) {
-      return appExistsRes
-    }
-
-    const appInfo = await getAppInfoFromUrl(url)
-    if (appInfo.error) {
-      return 'This is not a valid Safe app.'
-    }
-  })
-
-const composeValidatorsApps = (...validators) => (value, values, meta) => {
-  if (!meta.modified) {
-    return
-  }
-  return validators.reduce((error, validator) => error || validator(value), undefined)
-}
-
-type Props = {
-  formId: string
-  appList: Array<SafeApp>
+type AddAppFromProps = {
+  appList: SafeApp[]
   closeModal: () => void
+  formId: string
   onAppAdded: (app: SafeApp) => void
   setIsSubmitDisabled: (status: boolean) => void
 }
 
-const AddAppForm = ({ appList, formId, closeModal, onAppAdded, setIsSubmitDisabled }: Props) => {
-  const [appInfo, setAppInfo] = useState<SafeApp>(APP_INFO)
-  const safeAppValidator = curriedSafeAppValidator(appList)
+const uniqueApp = (appList: SafeApp[]) => (url: string) => {
+  const exists = appList.some((a) => {
+    try {
+      const currentUrl = new URL(a.url)
+      const newUrl = new URL(url)
+      return currentUrl.href === newUrl.href
+    } catch (error) {
+      console.error('There was a problem trying to validate the URL existence.', error.message)
+      return false
+    }
+  })
+  return exists ? 'This app is already registered.' : undefined
+}
+
+const AppInfoUpdater = ({ onAppInfo }: { onAppInfo: (appInfo: SafeApp) => void }): React.ReactElement => {
+  const {
+    input: { value: appUrl },
+  } = useField('appUrl', { subscription: { value: true } })
+  const debouncedValue = useDebounce(appUrl, 500)
+
+  React.useEffect(() => {
+    const updateAppInfo = async () => {
+      const appInfo = await getAppInfoFromUrl(debouncedValue)
+      onAppInfo({ ...appInfo })
+    }
+    updateAppInfo()
+  }, [debouncedValue, onAppInfo])
+
+  return null
+}
+
+const appUrlResolver = createDecorator({
+  field: 'appUrl',
+  updates: {
+    appUrl: async (appUrl: string): Promise<string | undefined> => {
+      const ensContent = !isURLValid(appUrl) && isValidEnsName(appUrl) && (await getIpfsLinkFromEns(appUrl))
+
+      if (ensContent) {
+        return ensContent
+      }
+
+      return appUrl
+    },
+  },
+})
+
+const validateUrl = (url: string): string | undefined => (isURLValid(url) ? undefined : 'Invalid URL')
+
+const AppUrl = ({ appList }: { appList: SafeApp[] }): React.ReactElement => (
+  <Field
+    label="App URL"
+    name="appUrl"
+    placeholder="App URL"
+    type="text"
+    component={TextField}
+    validate={composeValidators(required, validateUrl, uniqueApp(appList))}
+  />
+)
+
+const AppAgreement = (): React.ReactElement => (
+  <Field
+    component={StyledCheckbox}
+    label={
+      <Text size="xl">
+        This app is not a Gnosis product and I agree to use this app
+        <br />
+        at my own risk.
+      </Text>
+    }
+    name="agreement"
+    type="checkbox"
+    validate={required}
+  />
+)
+
+const SubmitButtonStatus = ({
+  appInfo,
+  isSubmitDisabled,
+}: {
+  appInfo: SafeApp
+  isSubmitDisabled: (disabled: boolean) => void
+}): React.ReactElement => {
+  const { valid, validating, values } = useFormState({ subscription: { valid: true, validating: true, values: true } })
+
+  React.useEffect(() => {
+    console.log(validating, valid, appInfo.error, appInfo.url, appInfo.name, values)
+    isSubmitDisabled(
+      validating || !valid || appInfo.error || !appInfo.url || !appInfo.name || appInfo.name === 'unknown',
+    )
+  }, [validating, valid, appInfo.error, appInfo.url, appInfo.name, values, isSubmitDisabled])
+
+  return null
+}
+
+const AddApp = ({
+  appList,
+  closeModal,
+  formId,
+  onAppAdded,
+  setIsSubmitDisabled,
+}: AddAppFromProps): React.ReactElement => {
+  const [appInfo, setAppInfo] = React.useState<SafeApp>(APP_INFO)
 
   const initialValues = {
     appUrl: '',
-    agreed: false,
-  }
-
-  const subscription = { submitting: true }
-
-  const onFormStatusChange = async ({ pristine, valid, validating, values, errors }) => {
-    if (!pristine) {
-      setIsSubmitDisabled(validating || !valid)
-    }
-
-    if (validating) {
-      return
-    }
-
-    if (!values.appUrl || !values.appUrl.length || errors.appUrl) {
-      setAppInfo(APP_INFO)
-      return
-    }
-
-    const url = await getUrlFromFormValue(values.appUrl)
-    const appInfo = await getAppInfoFromUrl(url)
-    setAppInfo({ ...appInfo })
+    agreement: false,
   }
 
   const handleSubmit = () => {
@@ -153,59 +181,33 @@ const AddAppForm = ({ appList, formId, closeModal, onAppAdded, setIsSubmitDisabl
     onAppAdded(appInfo)
   }
 
-  const validators = composeValidatorsApps(required, safeAppValidator)
-
   return (
     <GnoForm
+      decorators={[appUrlResolver]}
       initialValues={initialValues}
-      subscription={subscription}
-      // submit is triggered from ManageApps Component
       onSubmit={handleSubmit}
+      subscription={{ values: true }}
       testId={formId}
     >
       {() => (
         <>
           <StyledText size="xl">Add custom app</StyledText>
-          <DebounceValidationField
-            component={TextField}
-            label="App URL"
-            name="appUrl"
-            placeholder="App URL"
-            type="text"
-            validate={validators}
-          />
+
+          <AppUrl appList={appList} />
+          <AppInfoUpdater onAppInfo={setAppInfo} />
 
           <AppInfo>
             <Img alt="Token image" height={55} src={appInfo.iconUrl} />
             <StyledTextFileAppName label="App name" readOnly value={appInfo.name} onChange={() => {}} />
           </AppInfo>
 
-          <FormSpy
-            onChange={onFormStatusChange}
-            subscription={{
-              values: true,
-              valid: true,
-              errors: true,
-              pristine: true,
-              validating: true,
-            }}
-          />
+          <AppAgreement />
 
-          <Field
-            component={StyledCheckbox}
-            label={
-              <p>
-                This app is not a Gnosis product and I agree to use this app <br /> at my own risk.
-              </p>
-            }
-            name="agreed"
-            type="checkbox"
-            validate={required}
-          />
+          <SubmitButtonStatus isSubmitDisabled={setIsSubmitDisabled} appInfo={appInfo} />
         </>
       )}
     </GnoForm>
   )
 }
 
-export default AddAppForm
+export default AddApp
