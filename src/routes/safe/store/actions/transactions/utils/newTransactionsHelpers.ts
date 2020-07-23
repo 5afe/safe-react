ba8d55a8700a5213a62cc655c4a659ea3940f650
Agustin Pane <agustin.pane@gmail.com>
2020-07-23 11:28:08 -0300

    Replaces is with boolean

--- src/routes/safe/store/actions/transactions/utils/newTransactionsHelpers.ts
+++ src/routes/safe/store/actions/transactions/utils/newTransactionsHelpers.ts
@@ -1,17 +1,11 @@
-import {
-  EthereumTransaction,
-  ModuleTransaction,
-  MultiSigTransaction,
-  Transaction,
-  TxType,
-} from '../../../models/types/transactions'
+import { Transaction, TxType } from '../../../models/types/transactions'
 
-export const isMultiSigTx = (tx: Transaction): tx is MultiSigTransaction => {
+export const isMultiSigTx = (tx: Transaction): boolean => {
   return TxType[tx.txType] === TxType.MULTISIG_TRANSACTION
 }
-export const isModuleTx = (tx: Transaction): tx is ModuleTransaction => {
+export const isModuleTx = (tx: Transaction): boolean => {
   return TxType[tx.txType] === TxType.MODULE_TRANSACTION
 }
-export const isEthereumTx = (tx: Transaction): tx is EthereumTransaction => {
+export const isEthereumTx = (tx: Transaction): boolean => {
   return TxType[tx.txType] === TxType.ETHEREUM_TRANSACTION
 }
