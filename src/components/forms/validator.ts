f62bbffdd35f2fe70911402ab45899dded2395f9
Mikhail Mikheev <mmvsha73@gmail.com>
2020-07-27 14:31:13 +0400

    Tech Debt: Validator Type definitions (#1108)
    * type validators
    
    * safeSelector types
    
    * history 5.0.0 breaking changes adaptation
    
    * replace simpleMemoize with memoize from lodash because of typing issues
    
    * add type definitions for history and react-router-dom
    
    * type fixes
    
    * yarn lock update
    
    * fix router state
    
    * more type improvements
    
    * validator tests wip
    
    * add tests for validators, remove duplicated validators
    
    * add error messages to tests
    
    * fix minValue error message for inclusive param
    
    * Replace jsx.element with react.reactelement
    
    * Fix uniqueAddress validator argument type
    
    * remove comment in AddCustomToken validator
    
    * use absolute import for saferecord in safe paage container

--- src/components/forms/validator.ts
+++ src/components/forms/validator.ts
@@ -2,20 +2,14 @@ import { List } from 'immutable'
 
 import { sameAddress } from 'src/logic/wallets/ethAddresses'
 import { getWeb3 } from 'src/logic/wallets/getWeb3'
+import memoize from 'lodash.memoize'
 
-export const simpleMemoize = (fn) => {
-  let lastArg
-  let lastResult
-  return (arg, ...args) => {
-    if (arg !== lastArg) {
-      lastArg = arg
-      lastResult = fn(arg, ...args)
-    }
-    return lastResult
-  }
-}
+type ValidatorReturnType = string | undefined
+type GenericValidatorType = (...args: unknown[]) => ValidatorReturnType
+type AsyncValidator = (...args: unknown[]) => Promise<ValidatorReturnType>
+type Validator = GenericValidatorType | AsyncValidator
 
-export const required = (value?: string) => {
+export const required = (value?: string): ValidatorReturnType => {
   const required = 'Required'
 
   if (!value) {
@@ -29,30 +23,15 @@ export const required = (value?: string) => {
   return undefined
 }
 
-export const mustBeInteger = (value: string) =>
+export const mustBeInteger = (value: string): ValidatorReturnType =>
   !Number.isInteger(Number(value)) || value.includes('.') ? 'Must be an integer' : undefined
 
-export const mustBeFloat = (value: string) => (value && Number.isNaN(Number(value)) ? 'Must be a number' : undefined)
-
-export const greaterThan = (min: number | string) => (value: string) => {
-  if (Number.isNaN(Number(value)) || Number.parseFloat(value) > Number(min)) {
-    return undefined
-  }
-
-  return `Should be greater than ${min}`
-}
-
-export const equalOrGreaterThan = (min: number | string) => (value: string): undefined | string => {
-  if (Number.isNaN(Number(value)) || Number.parseFloat(value) >= Number(min)) {
-    return undefined
-  }
-
-  return `Should be equal or greater than ${min}`
-}
+export const mustBeFloat = (value: string): ValidatorReturnType =>
+  value && Number.isNaN(Number(value)) ? 'Must be a number' : undefined
 
 const regexQuery = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
 const url = new RegExp(regexQuery)
-export const mustBeUrl = (value: string) => {
+export const mustBeUrl = (value: string): ValidatorReturnType => {
   if (url.test(value)) {
     return undefined
   }
@@ -60,73 +39,68 @@ export const mustBeUrl = (value: string) => {
   return 'Please, provide a valid url'
 }
 
-export const minValue = (min: number | string) => (value: string) => {
-  if (Number.isNaN(Number(value)) || Number.parseFloat(value) >= Number(min)) {
+export const minValue = (min: number | string, inclusive = true) => (value: string): ValidatorReturnType => {
+  if (Number.parseFloat(value) > Number(min) || (inclusive && Number.parseFloat(value) >= Number(min))) {
     return undefined
   }
 
-  return `Should be at least ${min}`
+  return `Should be greater than ${inclusive ? 'or equal to ' : ''}${min}`
 }
 
-export const maxValueCheck = (max: number | string, value: string): string | undefined => {
-  if (!max || Number.isNaN(Number(value)) || parseFloat(value) <= parseFloat(max.toString())) {
+export const maxValue = (max: number | string) => (value: string): ValidatorReturnType => {
+  if (!max || parseFloat(value) <= parseFloat(max.toString())) {
     return undefined
   }
 
   return `Maximum value is ${max}`
 }
 
-export const maxValue = (max: number | string) => (value: string) => {
-  return maxValueCheck(max, value)
-}
+export const ok = (): undefined => undefined
 
-export const ok = () => undefined
+export const mustBeEthereumAddress = memoize(
+  (address: string): ValidatorReturnType => {
+    const startsWith0x = address.startsWith('0x')
+    const isAddress = getWeb3().utils.isAddress(address)
 
-export const mustBeEthereumAddress = simpleMemoize((address: string) => {
-  const startsWith0x = address.startsWith('0x')
-  const isAddress = getWeb3().utils.isAddress(address)
+    return startsWith0x && isAddress ? undefined : 'Address should be a valid Ethereum address or ENS name'
+  },
+)
 
-  return startsWith0x && isAddress ? undefined : 'Address should be a valid Ethereum address or ENS name'
-})
+export const mustBeEthereumContractAddress = memoize(
+  async (address: string): Promise<ValidatorReturnType> => {
+    const contractCode = await getWeb3().eth.getCode(address)
 
-export const mustBeEthereumContractAddress = simpleMemoize(async (address: string) => {
-  const contractCode = await getWeb3().eth.getCode(address)
+    return !contractCode || contractCode.replace('0x', '').replace(/0/g, '') === ''
+      ? 'Address should be a valid Ethereum contract address or ENS name'
+      : undefined
+  },
+)
 
-  return !contractCode || contractCode.replace('0x', '').replace(/0/g, '') === ''
-    ? 'Address should be a valid Ethereum contract address or ENS name'
-    : undefined
-})
-
-export const minMaxLength = (minLen, maxLen) => (value) =>
+export const minMaxLength = (minLen: number, maxLen: number) => (value: string): ValidatorReturnType =>
   value.length >= +minLen && value.length <= +maxLen ? undefined : `Should be ${minLen} to ${maxLen} symbols`
 
 export const ADDRESS_REPEATED_ERROR = 'Address already introduced'
 
-export const uniqueAddress = (addresses: string[] | List<string>) =>
-  simpleMemoize((value: string[]) => {
-    const addressAlreadyExists = addresses.some((address) => sameAddress(value, address))
-    return addressAlreadyExists ? ADDRESS_REPEATED_ERROR : undefined
-  })
+export const uniqueAddress = (addresses: string[] | List<string>): GenericValidatorType =>
+  memoize(
+    (value: string): ValidatorReturnType => {
+      const addressAlreadyExists = addresses.some((address) => sameAddress(value, address))
+      return addressAlreadyExists ? ADDRESS_REPEATED_ERROR : undefined
+    },
+  )
 
-export const composeValidators = (...validators) => (value) =>
-  validators.reduce((error, validator) => error || validator(value), undefined)
+export const composeValidators = (...validators: Validator[]) => (value: unknown): ValidatorReturnType =>
+  validators.reduce(
+    (error: string | undefined, validator: GenericValidatorType): ValidatorReturnType => error || validator(value),
+    undefined,
+  )
 
-export const inLimit = (limit, base, baseText, symbol = 'ETH') => (value) => {
-  const amount = Number(value)
-  const max = limit - base
-  if (amount <= max) {
-    return undefined
-  }
-
-  return `Should not exceed ${max} ${symbol} (amount to reach ${baseText})`
-}
-
-export const differentFrom = (diffValue: number | string) => (value: string) => {
+export const differentFrom = (diffValue: number | string) => (value: string): ValidatorReturnType => {
   if (value === diffValue.toString()) {
-    return `Value should be different than ${value}`
+    return `Value should be different than ${diffValue}`
   }
 
   return undefined
 }
 
-export const noErrorsOn = (name, errors) => errors[name] === undefined
+export const noErrorsOn = (name: string, errors: Record<string, unknown>): boolean => errors[name] === undefined
