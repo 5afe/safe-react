e7a9dbe9069bcbdc33c07eab47bb1948379089d5
Mati Dastugue <mdastugu@amazon.com>
2020-07-27 14:29:36 -0300

    Eslint error

--- src/routes/safe/components/Layout/index.tsx
+++ src/routes/safe/components/Layout/index.tsx
@@ -39,7 +39,6 @@ interface Props extends RouteComponentProps {
   onHide: (value: string) => void
   showSendFunds: (value: string) => void
   hideSendFunds: () => void
-  match: { path: string }
 }
 
 const useStyles = makeStyles(styles as any)
