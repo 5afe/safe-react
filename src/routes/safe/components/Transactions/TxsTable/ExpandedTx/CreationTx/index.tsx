877cf5ea4bb5c16920c1d42f2fe8df1ea20f4edb
nicosampler <nf.dominguez.87@gmail.com>
2020-08-03 12:38:02 -0300

    ExpandedTx - creation

--- src/routes/safe/components/Transactions/TxsTable/ExpandedTx/CreationTx/index.tsx
+++ src/routes/safe/components/Transactions/TxsTable/ExpandedTx/CreationTx/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { AddressInfo } from '@gnosis.pm/safe-react-components'
 import { getNetwork } from 'src/config'
 
+import { Transaction } from 'src/routes/safe/store/models/types/transaction'
 import { formatDate } from 'src/routes/safe/components/Transactions/TxsTable/columns'
 import Bold from 'src/components/layout/Bold'
 import Paragraph from 'src/components/layout/Paragraph'
@@ -23,7 +24,11 @@ const useStyles = makeStyles({
   },
 })
 
-export const CreationTx = ({ tx }) => {
+type Props = {
+  tx: Transaction
+}
+
+export const CreationTx = ({ tx }: Props): React.ReactElement => {
   const classes = useStyles()
 
   if (!tx) {
