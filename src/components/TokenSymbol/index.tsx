cce3bb75a935682dfafc8d6ebcc28ecfbca1c594
Agustin Pane <agustin.pane@gmail.com>
2020-08-05 12:20:10 -0300

    Move fetchTokenInfo logic to custom hook

--- src/components/TokenSymbol/index.tsx
+++ src/components/TokenSymbol/index.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { setImageToPlaceholder } from '../../routes/safe/components/Balances/utils'
 import Img from '../layout/Img'
-import { getTokenInfos } from '../../logic/tokens/store/actions/fetchTokens'
-import { Token } from '../../logic/tokens/store/model/token'
+import { useToken } from 'src/routes/safe/container/hooks/useToken'
 
 type Props = {
   tokenAddress: string
 }
 
 export const TokenSymbol = ({ tokenAddress }: Props): React.ReactElement => {
-  const [token, setToken] = useState<Token | null>(null)
-  useEffect(() => {
-    const fetchTokenInfo = async () => {
-      const tokenInfo = await getTokenInfos(tokenAddress)
-      setToken(tokenInfo)
-    }
-    fetchTokenInfo()
-  }, [tokenAddress])
-
+  const token = useToken(tokenAddress)
   if (!token) return null
   return <Img alt={token.name} height={26} onError={setImageToPlaceholder} src={token.logoUri} />
 }
