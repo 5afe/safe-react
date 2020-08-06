55411288dfc8d72f396cc0f893cfcd6b11c73132
Agustin Pane <agustin.pane@gmail.com>
2020-08-06 18:35:45 -0300

    Adds fetchSafeTokens.test.ts

---
+++ src/test/logic/currencyValues/api/fetchSafeTokens.test.ts
@@ -0,0 +1,42 @@
+import { aNewStore } from 'src/store'
+import fetchTokenCurrenciesBalances from 'src/logic/currencyValues/api/fetchTokenCurrenciesBalances'
+import axios from 'axios'
+import { getTxServiceHost } from 'src/config'
+
+jest.mock('axios')
+describe('fetchTokenCurrenciesBalances', () => {
+  let store
+  const safeAddress = '0xdfA693da0D16F5E7E78FdCBeDe8FC6eBEa44f1Cf'
+  beforeEach(async () => {
+    store = aNewStore()
+  })
+
+  it('Given a safeAddress, calls the api and gets all the balances token for that address',  async () => {
+    // given
+    const resultExpected = [{
+      balance: "849890000000000000",
+      balanceUsd: "337.2449",
+      token: null,
+      tokenAddress: null,
+      usdConversion: "396.81"
+    }, {
+      balance: "24698677800000000000",
+      balanceUsd: "29.3432",
+      token: {name: "Dai", symbol: "DAI", decimals: 18, logoUri: "https://gnosis-safe-token-logos.s3.amazonaws.com/0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa.png"},
+      tokenAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+      usdConversion: "1.188"
+    }]
+    const apiUrl = getTxServiceHost()
+
+    // @ts-ignore
+    axios.get.mockImplementationOnce(() => Promise.resolve(resultExpected))
+
+    // when
+    const result = await fetchTokenCurrenciesBalances(safeAddress)
+
+    // then
+    expect(result).toStrictEqual(resultExpected)
+    expect(axios.get).toHaveBeenCalled()
+    expect(axios.get).toBeCalledWith(`${apiUrl}safes/${safeAddress}/balances/usd/`, {"params": {"limit": 3000}})
+  })
+})
