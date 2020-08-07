5f524617db70103de29d92f6f4203e700bf28821
Agustin Pane <agustin.pane@gmail.com>
2020-08-07 12:22:51 -0300

    Placeholder for transactionHelpers tests

---
+++ src/test/routes/safe/store/actions/transactions/utils/transactionHelpers.test.ts
@@ -0,0 +1,193 @@
+
+describe('isInnerTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isCancelTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isPendingTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isModifySettingsTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isMultiSendTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isUpgradeTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isOutgoingTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isCustomTransaction', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('getRefundParams', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('getDecodedParams', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('isTransactionCancelled', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('calculateTransactionStatus', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('calculateTransactionType', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+
+describe('buildTx', async () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('updateStoredTransactionsStatus', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
+
+describe('generateSafeTxHash', () => {
+  it('',   () => {
+    // given
+
+    // when
+
+
+    // then
+
+  })
+})
