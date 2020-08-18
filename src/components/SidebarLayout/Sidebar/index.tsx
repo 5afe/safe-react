3b498ba6248fad1f20201a579961318e64343320
Agustín Longoni <agustin.longoni@altoros.com>
2020-08-18 15:17:54 -0300

    add Help Center link and fix margins

--- src/components/SidebarLayout/Sidebar/index.tsx
+++ src/components/SidebarLayout/Sidebar/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Divider, IconText } from '@gnosis.pm/safe-react-components'
+import { Divider, ButtonLink } from '@gnosis.pm/safe-react-components'
 
 import List, { ListItemType } from '../../List'
 import WalletInfo from '../WalletInfo'
 
 const StyledDivider = styled(Divider)`
-  margin: 16px -8px;
+  margin: 16px -8px 0;
 `
 
 const HelpContainer = styled.div`
-  margin-bottom: 8px;
+  height: 58px;
+`
+
+const StyledButtonLink = styled(ButtonLink)`
+  height: 40px;
+  width: 176px;
+  padding: 0 0 0 16px;
+  margin: 16px 0;
+  text-decoration: none;
+
+  &:hover {
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.colors.background};
+    box-sizing: border-box;
+  }
+  p {
+    font-family: ${({ theme }) => theme.fonts.fontFamily};
+    font-size: 0.76em;
+    font-weight: 600;
+    line-height: 1.5;
+    letter-spacing: 1px;
+    color: ${({ theme }) => theme.colors.placeHolder};
+    text-transform: uppercase;
+    padding: 0 0 0 8px;
+  }
 `
 type Props = {
   items: ListItemType[]
@@ -54,7 +78,13 @@ const Sidebar = ({
 
       <HelpContainer>
         <StyledDivider />
-        <IconText iconSize="md" textSize="sm" iconType="question" text="HELP CENTER" />
+        <StyledButtonLink
+          onClick={() => window.open('https://help.gnosis-safe.io/en/')}
+          color="placeHolder"
+          iconType="question"
+        >
+          HELP CENTER
+        </StyledButtonLink>
       </HelpContainer>
     </>
   )
