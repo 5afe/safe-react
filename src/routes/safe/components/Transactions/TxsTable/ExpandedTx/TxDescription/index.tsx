4a4d9bf9c8c5bd4f61d03c0a0761dbe279915b46
fernandomg <fernando.greco@gmail.com>
2020-08-24 20:23:16 -0300

    refactor: make it easy to identify what Tx is being displayed

--- src/routes/safe/components/Transactions/TxsTable/ExpandedTx/TxDescription/index.tsx
+++ src/routes/safe/components/Transactions/TxsTable/ExpandedTx/TxDescription/index.tsx
@@ -1,53 +1,51 @@
 import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 
-import { styles } from './styles'
-import { getTxData } from './utils'
-import SettingsDescription from './SettingsDescription'
-import CustomDescription from './CustomDescription'
-import TransferDescription from './TransferDescription'
-
-import { getTxAmount } from 'src/routes/safe/components/Transactions/TxsTable/columns'
 import Block from 'src/components/layout/Block'
-import { Transaction } from 'src/logic/safe/store/models/types/transaction'
+import { Transaction, TransactionTypes } from 'src/logic/safe/store/models/types/transaction'
+import { getTxAmount } from 'src/routes/safe/components/Transactions/TxsTable/columns'
+
+import CustomDescription from './CustomDescription'
+import SettingsDescription from './SettingsDescription'
+import { styles } from './styles'
+import TransferDescription from './TransferDescription'
+import { getTxData } from './utils'
 
 export const TRANSACTIONS_DESC_SEND_TEST_ID = 'tx-description-send'
 
 const useStyles = makeStyles(styles)
 
+const SettingsDescriptionTx = ({ tx }: { tx: Transaction }): React.ReactElement => {
+  const { action, addedOwner, module, newThreshold, removedOwner } = getTxData(tx)
+  return <SettingsDescription {...{ action, addedOwner, module, newThreshold, removedOwner }} />
+}
+
+const CustomDescriptionTx = ({ tx }: { tx: Transaction }): React.ReactElement => {
+  const amount = getTxAmount(tx, false)
+  const { data, recipient } = getTxData(tx)
+  return <CustomDescription {...{ amount, data, recipient }} storedTx={tx} />
+}
+
+const UpgradeDescriptionTx = ({ tx }: { tx: Transaction }): React.ReactElement => {
+  const { data } = getTxData(tx)
+  return <div>{data}</div>
+}
+
+const TransferDescriptionTx = ({ tx }: { tx: Transaction }): React.ReactElement => {
+  const amount = getTxAmount(tx, false)
+  const { recipient } = getTxData(tx)
+  return <TransferDescription {...{ amount, recipient }} />
+}
+
 const TxDescription = ({ tx }: { tx: Transaction }): React.ReactElement => {
   const classes = useStyles()
-  const {
-    action,
-    addedOwner,
-    cancellationTx,
-    creationTx,
-    customTx,
-    data,
-    modifySettingsTx,
-    module,
-    newThreshold,
-    recipient,
-    removedOwner,
-    upgradeTx,
-  }: any = getTxData(tx)
-  const amount = getTxAmount(tx, false)
+
   return (
     <Block className={classes.txDataContainer}>
-      {modifySettingsTx && action && (
-        <SettingsDescription
-          action={action}
-          addedOwner={addedOwner}
-          newThreshold={newThreshold}
-          removedOwner={removedOwner}
-          module={module}
-        />
-      )}
-      {!upgradeTx && customTx && <CustomDescription amount={amount} data={data} recipient={recipient} storedTx={tx} />}
-      {upgradeTx && <div>{data}</div>}
-      {!cancellationTx && !modifySettingsTx && !customTx && !creationTx && !upgradeTx && (
-        <TransferDescription amount={amount} recipient={recipient} />
-      )}
+      {tx.type === TransactionTypes.SETTINGS && <SettingsDescriptionTx tx={tx} />}
+      {tx.type === TransactionTypes.CUSTOM && <CustomDescriptionTx tx={tx} />}
+      {tx.type === TransactionTypes.UPGRADE && <UpgradeDescriptionTx tx={tx} />}
+      {[TransactionTypes.TOKEN, TransactionTypes.COLLECTIBLE].includes(tx.type) && <TransferDescriptionTx tx={tx} />}
     </Block>
   )
 }
