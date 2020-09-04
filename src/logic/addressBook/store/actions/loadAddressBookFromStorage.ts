c0868f7eae3d16d84c13010d073eb9fa67d32b49
Agustin Pane <agustin.pane@gmail.com>
2020-09-04 12:54:28 -0300

    Simplify loadAddressBookFromStorage

--- src/logic/addressBook/store/actions/loadAddressBookFromStorage.ts
+++ src/logic/addressBook/store/actions/loadAddressBookFromStorage.ts
@@ -1,29 +1,17 @@
 import { loadAddressBook } from 'src/logic/addressBook/store/actions/loadAddressBook'
 import { buildAddressBook } from 'src/logic/addressBook/store/reducer/addressBook'
 import { getAddressBookFromStorage } from 'src/logic/addressBook/utils'
-import { safesListSelector } from 'src/logic/safe/store/selectors'
 import { Dispatch } from 'redux'
-import { AppReduxState } from 'src/store'
-import { makeAddressBookEntry } from 'src/logic/addressBook/model/addressBook'
 
-const loadAddressBookFromStorage = () => async (dispatch: Dispatch, getState: () => AppReduxState): Promise<void> => {
+const loadAddressBookFromStorage = () => async (dispatch: Dispatch): Promise<void> => {
   try {
-    const state = getState()
     let storedAdBk = await getAddressBookFromStorage()
     if (!storedAdBk) {
       storedAdBk = []
     }
 
     const addressBook = buildAddressBook(storedAdBk)
-    // Fetch all the current safes, in case that we don't have a safe on the addressBook, we add it
-    const safes = safesListSelector(state)
-    safes.forEach((safe) => {
-      const { address } = safe
-      const found = addressBook.find((entry) => entry.address === address)
-      if (!found) {
-        addressBook.push(makeAddressBookEntry({ address }))
-      }
-    })
+
     dispatch(loadAddressBook(addressBook))
   } catch (err) {
     // eslint-disable-next-line
