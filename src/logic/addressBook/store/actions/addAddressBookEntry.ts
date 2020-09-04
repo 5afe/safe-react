62aec5b1fa2427608aa41c39bf1d3af96999afbc
Agustin Pane <agustin.pane@gmail.com>
2020-09-04 10:44:43 -0300

    Add types for addressBook actions Remove unused saveAndUpdateAddressBook action

--- src/logic/addressBook/store/actions/addAddressBookEntry.ts
+++ src/logic/addressBook/store/actions/addAddressBookEntry.ts
@@ -1,7 +1,9 @@
 import { createAction } from 'redux-actions'
+import { AddressBookEntry } from 'src/logic/addressBook/model/addressBook'
 
 export const ADD_ENTRY = 'ADD_ENTRY'
 
-export const addAddressBookEntry = createAction(ADD_ENTRY, (entry) => ({
+export const addAddressBookEntry = createAction(ADD_ENTRY, (entry: AddressBookEntry, isOwner?: boolean) => ({
   entry,
+  isOwner,
 }))
--- src/logic/addressBook/store/actions/loadAddressBook.ts
+++ src/logic/addressBook/store/actions/loadAddressBook.ts
@@ -1,7 +1,8 @@
 import { createAction } from 'redux-actions'
+import { AddressBookState } from 'src/logic/addressBook/model/addressBook'
 
 export const LOAD_ADDRESS_BOOK = 'LOAD_ADDRESS_BOOK'
 
-export const loadAddressBook = createAction(LOAD_ADDRESS_BOOK, (addressBook) => ({
+export const loadAddressBook = createAction(LOAD_ADDRESS_BOOK, (addressBook: AddressBookState) => ({
   addressBook,
 }))
