de6d3bd3bf17dba36d3d62d46682bd42b3ffba06
Agustin Pane <agustin.pane@gmail.com>
2020-09-10 09:25:16 -0300

    Remove onlyTrustedTokens param

--- src/logic/currencyValues/api/fetchTokenCurrenciesBalances.ts
+++ src/logic/currencyValues/api/fetchTokenCurrenciesBalances.ts
@@ -6,18 +6,20 @@ import { TokenProps } from 'src/logic/tokens/store/model/token'
 export type BalanceEndpoint = {
   balance: string
   balanceUsd: string
-  tokenAddress: string
+  tokenAddress?: string
   token?: TokenProps
   usdConversion: string
 }
 
 const fetchTokenCurrenciesBalances = (
-  safeAddress: string,
+  safeAddress?: string,
   excludeSpamTokens = true,
-  onlyTrustedTokens = true,
 ): Promise<AxiosResponse<BalanceEndpoint[]>> => {
+  if (!safeAddress) {
+    return null
+  }
   const apiUrl = getTxServiceHost()
-  const url = `${apiUrl}safes/${safeAddress}/balances/usd/?exclude_spam=${excludeSpamTokens}&&trusted=${onlyTrustedTokens}`
+  const url = `${apiUrl}safes/${safeAddress}/balances/usd/?exclude_spam=${excludeSpamTokens}`
 
   return axios.get(url, {
     params: {
