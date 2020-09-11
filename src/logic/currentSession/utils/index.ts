a70aeb0bf4e3ffd7db6ddbdd84e4392508da0e4a
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2020-09-11 09:22:07 +0200

    Rename SerializedSessionState to CurrentSessionState

--- src/logic/currentSession/utils/index.ts
+++ src/logic/currentSession/utils/index.ts
@@ -1,9 +1,9 @@
 import { loadFromStorage, saveToStorage } from 'src/utils/storage'
-import { SerializedSessionState } from 'src/logic/currentSession/store/model/currentSession'
+import { CurrentSessionState } from 'src/logic/currentSession/store/model/currentSession'
 
 const CURRENT_SESSION_STORAGE_KEY = 'CURRENT_SESSION'
 
-export const getCurrentSessionFromStorage = async (): Promise<SerializedSessionState | undefined> =>
+export const getCurrentSessionFromStorage = async (): Promise<CurrentSessionState | undefined> =>
   loadFromStorage(CURRENT_SESSION_STORAGE_KEY)
 
 export const saveCurrentSessionToStorage = async (currentSession) => {
