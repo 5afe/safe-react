06aa64042554d57e5b132619fb0a41ec76d33d57
Agustin Pane <agustin.pane@gmail.com>
2020-09-14 11:51:23 -0300

    Refactor Tokens to TokenList, makes it functional component also fix blacklisted addresses calculation

--- src/routes/safe/components/Balances/Tokens/screens/TokenList/index.tsx
+++ src/routes/safe/components/Balances/Tokens/screens/TokenList/index.tsx
@@ -1,9 +1,9 @@
 import CircularProgress from '@material-ui/core/CircularProgress'
 import MuiList from '@material-ui/core/List'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Search from '@material-ui/icons/Search'
 import cn from 'classnames'
-import { Set } from 'immutable'
+import { List, Set } from 'immutable'
 import SearchBar from 'material-ui-search-bar'
 import * as React from 'react'
 import { FixedSizeList } from 'react-window'
@@ -17,10 +17,15 @@ import Button from 'src/components/layout/Button'
 import Divider from 'src/components/layout/Divider'
 import Hairline from 'src/components/layout/Hairline'
 import Row from 'src/components/layout/Row'
+import { useEffect, useState } from 'react'
+import { Token } from 'src/logic/tokens/store/model/token'
+import { useDispatch } from 'react-redux'
+import updateBlacklistedTokens from 'src/logic/safe/store/actions/updateBlacklistedTokens'
+import updateActiveTokens from 'src/logic/safe/store/actions/updateActiveTokens'
 
 export const ADD_CUSTOM_TOKEN_BUTTON_TEST_ID = 'add-custom-token-btn'
 
-const filterBy = (filter, tokens) =>
+const filterBy = (filter: string, tokens: List<Token>): List<Token> =>
   tokens.filter(
     (token) =>
       !filter ||
@@ -28,166 +33,136 @@ const filterBy = (filter, tokens) =>
       token.name.toLowerCase().includes(filter.toLowerCase()),
   )
 
-// OPTIMIZATION IDEA (Thanks Andre)
-// Calculate active tokens on component mount, store it in component state
-// After user closes modal, dispatch an action so we don't have 100500 actions
-// And selectors don't recalculate
+const useStyles = makeStyles(styles)
 
-class Tokens extends React.Component<any> {
-  renderCount = 0
+type Props = {
+  setActiveScreen: (newScreen: string) => void
+  tokens: List<Token>
+  activeTokens: List<Token>
+  blacklistedTokens: Set<string>
+  safeAddress: string
+}
 
-  state = {
-    filter: '',
-    activeTokensAddresses: Set([]),
-    initialActiveTokensAddresses: Set([]),
-    blacklistedTokensAddresses: Set([]),
-    activeTokensCalculated: false,
-    blacklistedTokensCalculated: false,
+export const TokenList = (props: Props): React.ReactElement => {
+  const classes = useStyles()
+  const { setActiveScreen, tokens, activeTokens, blacklistedTokens, safeAddress } = props
+  const [activeTokensAddresses, setActiveTokensAddresses] = useState(Set(activeTokens.map(({ address }) => address)))
+  const [blacklistedTokensAddresses, setBlacklistedTokensAddresses] = useState<Set<string>>(blacklistedTokens)
+  const [filter, setFilter] = useState('')
+  const dispatch = useDispatch()
+
+  const onUpdateActiveTokens = () => {
+    dispatch(updateActiveTokens(safeAddress, activeTokensAddresses))
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // I moved this logic here because if placed in ComponentDidMount
-    // the user would see Switches switch and this method fires before the component mounts
+  const onUpdateBlacklistedTokens = () => {
+    dispatch(updateBlacklistedTokens(safeAddress, blacklistedTokensAddresses))
+  }
 
-    if (!prevState.activeTokensCalculated) {
-      const { activeTokens } = nextProps
-
-      return {
-        activeTokensAddresses: Set(activeTokens.map(({ address }) => address)),
-        initialActiveTokensAddresses: Set(activeTokens.map(({ address }) => address)),
-        activeTokensCalculated: true,
-      }
+  useEffect(() => {
+    return () => {
+      onUpdateActiveTokens()
+      onUpdateBlacklistedTokens()
     }
+  }, [onUpdateActiveTokens, onUpdateBlacklistedTokens])
 
-    if (!prevState.blacklistedTokensCalculated) {
-      const { blacklistedTokens } = nextProps
-
-      return {
-        blacklistedTokensAddresses: blacklistedTokens,
-        blacklistedTokensCalculated: true,
-      }
-    }
-
-    return null
+  const searchClasses = {
+    input: classes.searchInput,
+    root: classes.searchRoot,
+    iconButton: classes.searchIcon,
+    searchContainer: classes.searchContainer,
   }
 
-  componentWillUnmount() {
-    const { activeTokensAddresses, blacklistedTokensAddresses } = this.state
-    const { safeAddress, updateActiveTokens, updateBlacklistedTokens } = this.props
-
-    updateActiveTokens(safeAddress, activeTokensAddresses)
-    updateBlacklistedTokens(safeAddress, blacklistedTokensAddresses)
-  }
-
-  onCancelSearch = () => {
+  const onCancelSearch = () => {
+    setFilter('')
     this.setState(() => ({ filter: '' }))
   }
 
-  onChangeSearchBar = (value) => {
-    this.setState(() => ({ filter: value }))
+  const onChangeSearchBar = (value: string) => {
+    setFilter(value)
   }
 
-  onSwitch = (token) => () => {
-    this.setState((prevState: any) => {
-      let newActiveTokenAddresses
-      let newBlacklistedTokenAddresses
-      if (prevState.activeTokensAddresses.has(token.address)) {
-        newActiveTokenAddresses = prevState.activeTokensAddresses.remove(token.address)
-        newBlacklistedTokenAddresses = prevState.blacklistedTokensAddresses.add(token.address)
-      } else {
-        newActiveTokenAddresses = prevState.activeTokensAddresses.add(token.address)
-        newBlacklistedTokenAddresses = prevState.blacklistedTokensAddresses.remove(token.address)
-      }
-
-      return {
-        ...prevState,
-        activeTokensAddresses: newActiveTokenAddresses,
-        blacklistedTokensAddresses: newBlacklistedTokenAddresses,
-      }
-    })
-  }
-
-  createItemData = (tokens, activeTokensAddresses) => ({
-    tokens,
-    activeTokensAddresses,
-    onSwitch: this.onSwitch,
-  })
-
-  getItemKey = (index, { tokens }) => {
-    const token = tokens.get(index)
-
-    return token.address
-  }
-
-  render() {
-    const { classes, setActiveScreen, tokens } = this.props
-    const { activeTokensAddresses, filter } = this.state
-    const searchClasses = {
-      input: classes.searchInput,
-      root: classes.searchRoot,
-      iconButton: classes.searchIcon,
-      searchContainer: classes.searchContainer,
+  const onSwitch = (token: Token) => () => {
+    if (activeTokensAddresses.has(token.address)) {
+      const newTokens = activeTokensAddresses.remove(token.address)
+      setActiveTokensAddresses(newTokens)
+      setBlacklistedTokensAddresses(blacklistedTokensAddresses.add(token.address))
+    } else {
+      setActiveTokensAddresses(activeTokensAddresses.add(token.address))
+      setBlacklistedTokensAddresses(blacklistedTokensAddresses.remove(token.address))
     }
-    const switchToAddCustomTokenScreen = () => setActiveScreen('addCustomToken')
-
-    const filteredTokens = filterBy(filter, tokens)
-    const itemData = this.createItemData(filteredTokens, activeTokensAddresses)
-
-    return (
-      <>
-        <Block className={classes.root}>
-          <Row align="center" className={cn(classes.padding, classes.actions)}>
-            <Search className={classes.search} />
-            <SearchBar
-              classes={searchClasses}
-              onCancelSearch={this.onCancelSearch}
-              onChange={this.onChangeSearchBar}
-              placeholder="Search by name or symbol"
-              searchIcon={<div />}
-              value={filter}
-            />
-            <Spacer />
-            <Divider />
-            <Spacer />
-            <Button
-              classes={{ label: classes.addBtnLabel }}
-              className={classes.add}
-              color="primary"
-              onClick={switchToAddCustomTokenScreen}
-              size="small"
-              testId={ADD_CUSTOM_TOKEN_BUTTON_TEST_ID}
-              variant="contained"
-            >
-              + Add custom token
-            </Button>
-          </Row>
-          <Hairline />
-        </Block>
-        {!tokens.size && (
-          <Block className={classes.progressContainer} justify="center">
-            <CircularProgress />
-          </Block>
-        )}
-        {tokens.size > 0 && (
-          <MuiList className={classes.list}>
-            <FixedSizeList
-              height={413}
-              itemCount={filteredTokens.size}
-              itemData={itemData}
-              itemKey={this.getItemKey}
-              itemSize={51}
-              overscanCount={process.env.NODE_ENV === 'test' ? 100 : 10}
-              width={500}
-            >
-              {TokenRow}
-            </FixedSizeList>
-          </MuiList>
-        )}
-      </>
-    )
   }
+
+  const createItemData = (
+    tokens: List<Token>,
+    activeTokensAddresses: Set<string>,
+  ): { tokens: List<Token>; activeTokensAddresses: Set<string>; onSwitch: (token: Token) => void } => {
+    return {
+      tokens,
+      activeTokensAddresses,
+      onSwitch: onSwitch,
+    }
+  }
+
+  const switchToAddCustomTokenScreen = () => setActiveScreen('addCustomToken')
+
+  const getItemKey = (index: number, { tokens }): string => {
+    return tokens.get(index).address
+  }
+
+  const filteredTokens = filterBy(filter, tokens)
+  const itemData = createItemData(filteredTokens, activeTokensAddresses)
+
+  return (
+    <>
+      <Block className={classes.root}>
+        <Row align="center" className={cn(classes.padding, classes.actions)}>
+          <Search className={classes.search} />
+          <SearchBar
+            classes={searchClasses}
+            onCancelSearch={onCancelSearch}
+            onChange={onChangeSearchBar}
+            placeholder="Search by name or symbol"
+            searchIcon={<div />}
+            value={filter}
+          />
+          <Spacer />
+          <Divider />
+          <Spacer />
+          <Button
+            classes={{ label: classes.addBtnLabel }}
+            className={classes.add}
+            color="primary"
+            onClick={switchToAddCustomTokenScreen}
+            size="small"
+            testId={ADD_CUSTOM_TOKEN_BUTTON_TEST_ID}
+            variant="contained"
+          >
+            + Add custom token
+          </Button>
+        </Row>
+        <Hairline />
+      </Block>
+      {!tokens.size && (
+        <Block className={classes.progressContainer} justify="center">
+          <CircularProgress />
+        </Block>
+      )}
+      {tokens.size > 0 && (
+        <MuiList className={classes.list}>
+          <FixedSizeList
+            height={413}
+            itemCount={filteredTokens.size}
+            itemData={itemData}
+            itemKey={getItemKey}
+            itemSize={51}
+            overscanCount={process.env.NODE_ENV === 'test' ? 100 : 10}
+            width={500}
+          >
+            {TokenRow}
+          </FixedSizeList>
+        </MuiList>
+      )}
+    </>
+  )
 }
-
-const TokenComponent = withStyles(styles as any)(Tokens)
-
-export default TokenComponent
