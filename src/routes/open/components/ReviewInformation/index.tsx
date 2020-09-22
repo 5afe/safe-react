8708e2b726a72a1cc06bee3b5d2edf4a05e69575
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2020-09-22 10:40:45 +0200

    Update ReviewInformation components to remove type error

--- src/routes/open/components/ReviewInformation/index.tsx
+++ src/routes/open/components/ReviewInformation/index.tsx
@@ -1,7 +1,6 @@
 import TableContainer from '@material-ui/core/TableContainer'
-import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { FIELD_CONFIRMATIONS, FIELD_NAME, getNumOwnersFrom } from '../fields'
 
@@ -18,73 +17,11 @@ import { estimateGasForDeployingSafe } from 'src/logic/contracts/safeContracts'
 import { formatAmount } from 'src/logic/tokens/utils/formatAmount'
 import { getWeb3 } from 'src/logic/wallets/getWeb3'
 import { getAccountsFrom, getNamesFrom } from 'src/routes/open/utils/safeDataExtractor'
-import { background, border, lg, screenSm, sm } from 'src/theme/variables'
+import { useStyles } from './styles'
 
-const { useEffect, useState } = React
+const ReviewComponent = ({ userAccount, values }: any) => {
+  const classes = useStyles()
 
-const styles = () => ({
-  root: {
-    minHeight: '300px',
-    [`@media (min-width: ${screenSm}px)`]: {
-      flexDirection: 'row',
-    },
-  },
-  detailsColumn: {
-    minWidth: '100%',
-    [`@media (min-width: ${screenSm}px)`]: {
-      minWidth: '0',
-    },
-  },
-  ownersColumn: {
-    minWidth: '100%',
-    [`@media (min-width: ${screenSm}px)`]: {
-      minWidth: '0',
-    },
-  },
-  details: {
-    padding: lg,
-    borderRight: `solid 1px ${border}`,
-    height: '100%',
-  },
-  info: {
-    backgroundColor: background,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    padding: lg,
-    textAlign: 'center',
-  },
-  owners: {
-    padding: lg,
-  },
-  name: {
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  },
-  userName: {
-    whiteSpace: 'nowrap',
-  },
-  owner: {
-    alignItems: 'center',
-    minWidth: 'fit-content',
-    padding: sm,
-    paddingLeft: lg,
-  },
-  user: {
-    justifyContent: 'left',
-    '& > p': {
-      marginRight: sm,
-    },
-  },
-  open: {
-    paddingLeft: sm,
-    width: 'auto',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-})
-
-const ReviewComponent = ({ classes, userAccount, values }: any) => {
   const [gasCosts, setGasCosts] = useState('< 0.001')
   const names = getNamesFrom(values)
   const addresses = getAccountsFrom(values)
@@ -198,14 +135,15 @@ const ReviewComponent = ({ classes, userAccount, values }: any) => {
   )
 }
 
-const ReviewPage = withStyles(styles as any)(ReviewComponent)
-
-const Review = () => (controls, { values }) => (
-  <>
-    <OpenPaper controls={controls} padding={false}>
-      <ReviewPage values={values} />
-    </OpenPaper>
-  </>
-)
+const Review = () =>
+  function ReviewPage(controls, { values }): React.ReactElement {
+    return (
+      <>
+        <OpenPaper controls={controls} padding={false}>
+          <ReviewComponent values={values} />
+        </OpenPaper>
+      </>
+    )
+  }
 
 export default Review
