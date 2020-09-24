171b6d9ddc1c31235bf6f60cee7293b06a062c4b
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2020-09-24 16:04:35 +0200

    Update travis script to execute tests (#1382)
    * Update travis script to execute tests
    
    * Update ReviewInformation components to remove type error
    
    * Fix tests and import types without .d
    
    * Upgrade dependencies with minor changes

---
+++ src/routes/open/components/ReviewInformation/styles.ts
@@ -0,0 +1,66 @@
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import { background, border, lg, screenSm, sm } from 'src/theme/variables'
+
+export const useStyles = makeStyles(
+  createStyles({
+    root: {
+      minHeight: '300px',
+      [`@media (min-width: ${screenSm}px)`]: {
+        flexDirection: 'row',
+      },
+    },
+    detailsColumn: {
+      minWidth: '100%',
+      [`@media (min-width: ${screenSm}px)`]: {
+        minWidth: '0',
+      },
+    },
+    ownersColumn: {
+      minWidth: '100%',
+      [`@media (min-width: ${screenSm}px)`]: {
+        minWidth: '0',
+      },
+    },
+    details: {
+      padding: lg,
+      borderRight: `solid 1px ${border}`,
+      height: '100%',
+    },
+    info: {
+      backgroundColor: background,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      padding: lg,
+      textAlign: 'center',
+    },
+    owners: {
+      padding: lg,
+    },
+    name: {
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+    },
+    userName: {
+      whiteSpace: 'nowrap',
+    },
+    owner: {
+      alignItems: 'center',
+      minWidth: 'fit-content',
+      padding: sm,
+      paddingLeft: lg,
+    },
+    user: {
+      justifyContent: 'left',
+      '& > p': {
+        marginRight: sm,
+      },
+    },
+    open: {
+      paddingLeft: sm,
+      width: 'auto',
+      '&:hover': {
+        cursor: 'pointer',
+      },
+    },
+  }),
+)
