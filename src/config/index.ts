9a467d0f022058a34893a1e32eae73da518e996b
fernandomg <fernando.greco@gmail.com>
2020-10-02 10:40:59 -0300

    simplify config file and use CONSTANTS where possible
    Co-authored-by: Matias Dastugue <matias.dastugue@altoros.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -26,47 +26,21 @@ const configuration = (): Promise<NetworkSpecificConfiguration> => {
 
 const getConfig: () => NetworkSpecificConfiguration = ensureOnce(configuration)
 
-export const getTxServiceHost = (): string => {
-  const config = getConfig()
-  return config.txServiceUri
-}
+export const getTxServiceHost = (): string => getConfig()?.txServiceUri
 
-export const getRelayUrl = (): string | undefined => {
-  const config = getConfig()
-  return config.relayApiUri
-}
+export const getRelayUrl = (): string | undefined => getConfig()?.relayApiUri
 
-export const getGnosisSafeAppsUrl = (): string => {
-  const config = getConfig()
-  return config.safeAppsUri
-}
+export const getGnosisSafeAppsUrl = (): string => getConfig()?.safeAppsUri
 
-export const getTxServiceUriFrom = (safeAddress) =>
-  `safes/${safeAddress}/transactions/`
+export const getTxServiceUriFrom = (safeAddress) => `safes/${safeAddress}/transactions/`
 
-export const getIncomingTxServiceUriTo = (safeAddress) =>
-  `safes/${safeAddress}/incoming-transfers/`
+export const getIncomingTxServiceUriTo = (safeAddress: string) => `safes/${safeAddress}/incoming-transfers/`
 
-export const getAllTransactionsUriFrom = (safeAddress: string): string =>
-  `safes/${safeAddress}/all-transactions/`
+export const getAllTransactionsUriFrom = (safeAddress: string) => `safes/${safeAddress}/all-transactions/`
 
-export const getSafeCreationTxUri = (safeAddress) => `safes/${safeAddress}/creation/`
+export const getSafeCreationTxUri = (safeAddress: string) => `safes/${safeAddress}/creation/`
 
-export const getGoogleAnalyticsTrackingID = () => GOOGLE_ANALYTICS_ID[ETHEREUM_NETWORK[getNetwork()]]
-
-// TODO: replace this with an `INTERCOM_ID` constant?
-export const getIntercomId = () =>
-  process.env.REACT_APP_ENV === 'production'
-    ? process.env.REACT_APP_INTERCOM_ID
-    : 'plssl1fl'
-
-// TODO: handle this within one function? or expose both urls in respective constants?
-//  `EXCHANGE_RATE_URI`, `EXCHANGE_RATES_URI_FALLBACK`
-export const getExchangeRatesUrl = () => 'https://api.exchangeratesapi.io/latest'
-export const getExchangeRatesUrlFallback = () => 'https://api.coinbase.com/v2/exchange-rates'
-
-// TODO: This can be exposed directly in the `constants.ts` file
-export const getSafeLastVersion = () => process.env.REACT_APP_LATEST_SAFE_VERSION  || '1.1.1'
+export const getGoogleAnalyticsTrackingID = (): string => GOOGLE_ANALYTICS_ID[getNetworkName()]
 
 export const buildSafeCreationTxUrl = (safeAddress) => {
   const host = getTxServiceHost()
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,14 +3,16 @@ export const GOOGLE_ANALYTICS_ID = {
   RINKEBY: process.env.REACT_APP_GOOGLE_ANALYTICS_ID_RINKEBY,
   MAINNET: process.env.REACT_APP_GOOGLE_ANALYTICS_ID_MAINNET,
 }
-export const INTERCOM_ID = process.env.REACT_APP_INTERCOM_ID
+export const INTERCOM_ID = process.env.REACT_APP_ENV === 'production' ? process.env.REACT_APP_INTERCOM_ID : 'plssl1fl'
 export const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 export const SQUARELINK_ID = process.env.REACT_APP_SQUARELINK_ID
 export const FORTMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 export const INFURA_TOKEN = process.env.REACT_APP_INFURA_TOKEN || ''
-export const LATEST_SAFE_VERSION = process.env.REACT_APP_LATEST_SAFE_VERSION || 'not-defined'
+export const LATEST_SAFE_VERSION = process.env.REACT_APP_LATEST_SAFE_VERSION || '1.1.1'
 export const APP_VERSION = process.env.REACT_APP_APP_VERSION || 'not-defined'
 export const OPENSEA_API_KEY = process.env.REACT_APP_OPENSEA_API_KEY || ''
 export const COLLECTIBLES_SOURCE = process.env.REACT_APP_COLLECTIBLES_SOURCE || 'OpenSea'
 export const TIMEOUT = process.env.NODE_ENV === 'test' ? 1500 : 5000
 export const ETHERSCAN_API_KEY = process.env.REACT_APP_ETHERSCAN_API_KEY
+export const EXCHANGE_RATE_URL = 'https://api.exchangeratesapi.io/latest'
+export const EXCHANGE_RATE_URL_FALLBACK = 'https://api.coinbase.com/v2/exchange-rates'
