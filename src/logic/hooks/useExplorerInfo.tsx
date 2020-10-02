ccddaf16a41d5045dfa70cb08098dad39d08111b
Agustin Pane <agustin.pane@gmail.com>
2020-10-02 11:04:17 -0300

    Fix useExplorerInfo

--- src/logic/hooks/useExplorerInfo.tsx
+++ src/logic/hooks/useExplorerInfo.tsx
@@ -2,23 +2,25 @@ import { useSelector } from 'react-redux'
 import { providerSelector } from '../wallets/store/selectors'
 import { getNetworkConfig } from '../../config/utils'
 
-export type BlockScanInfo = {
-  alt: string
-  url: string
+export type BlockScanInfo = () => {
+  alt?: string
+  url?: string
 }
 
 // @todo (agustin) finish iimplementation
-export const useExplorerInfo = (hash: string): BlockScanInfo | null => {
+export const useExplorerInfo = (hash?: string | null): BlockScanInfo => {
   const provider = useSelector(providerSelector)
   const config = getNetworkConfig(provider.network)
 
-  const url = config?.environment.dev?.NETWORK_EXPLORER_URL
-  console.log('URL', url)
-  if (config && config.environment.dev) {
+  const url = config?.environment.dev?.networkExplorerUri
+  console.log('URL', url, 'etwork', provider.network)
+
+  const blockScanInfo = () => {
     return {
-      url: config.environment.dev.NETWORK_EXPLORER_URL,
-      alt: config.environment.dev.NETWORK_EXPLORER_NAME,
+      url: hash ? config?.environment.dev?.networkExplorerUri : '',
+      alt: config?.environment.dev?.networkExplorerName,
     }
   }
-  return null
+
+  return blockScanInfo
 }
