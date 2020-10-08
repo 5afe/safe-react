3a6d0eb568b0557bf905593caba558732e0bbc59
Agustin Pane <agustin.pane@gmail.com>
2020-10-08 12:32:53 -0300

    Uses networkInfo on NetworkLabel.tsx

--- src/components/AppLayout/Header/components/NetworkLabel.tsx
+++ src/components/AppLayout/Header/components/NetworkLabel.tsx
@@ -3,13 +3,8 @@ import * as React from 'react'
 
 import Col from 'src/components/layout/Col'
 import Paragraph from 'src/components/layout/Paragraph'
-import { getNetworkId } from 'src/config'
-import { ETHEREUM_NETWORK } from 'src/config/networks/network.d'
+import { getNetworkInfo } from 'src/config'
 import { border, md, screenSm, sm, xs } from 'src/theme/variables'
-
-const interfaceNetwork = getNetworkId()
-const formatNetwork = (network: number): string =>
-  ETHEREUM_NETWORK[network][0].toUpperCase() + ETHEREUM_NETWORK[network].substring(1).toLowerCase()
 
 const useStyles = makeStyles({
   container: {
@@ -33,18 +28,14 @@ const useStyles = makeStyles({
   },
 })
 
-interface NetworkLabelProps {
-  network?: number
-}
-
-const NetworkLabel = ({ network = interfaceNetwork }: NetworkLabelProps): React.ReactElement => {
+const NetworkLabel = (): React.ReactElement => {
   const classes = useStyles()
-  const formattedNetwork = formatNetwork(network)
+  const networkInfo = getNetworkInfo()
 
   return (
     <Col className={classes.container} middle="xs" start="xs">
       <Paragraph className={classes.text} size="xs">
-        {formattedNetwork}
+        {networkInfo.label}
       </Paragraph>
     </Col>
   )
