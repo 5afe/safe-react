c552b23dee8e7053c21fae5f9529c3b084ede052
Agustin Pane <agustin.pane@gmail.com>
2020-10-16 14:04:31 -0300

    Replace old ETH values with native coin

--- src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/Review/index.tsx
+++ src/routes/safe/components/Balances/SendModal/screens/ContractInteraction/Review/index.tsx
@@ -163,7 +163,7 @@ const ContractInteractionReview = ({ onClose, onPrev, tx }: Props): React.ReactE
         </Row>
         <Row>
           <Paragraph>
-            {`You're about to create a transaction and will have to confirm it with your currently connected wallet. Make sure you have ${gasCosts} (fee price) ETH in this wallet to fund this confirmation.`}
+            {`You're about to create a transaction and will have to confirm it with your currently connected wallet. Make sure you have ${gasCosts} (fee price) ${nativeCoin.name} in this wallet to fund this confirmation.`}
           </Paragraph>
         </Row>
       </Block>
