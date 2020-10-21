bd953f9f7cd90bb4f6e9c024230872063851cf68
nicosampler <nf.dominguez.87@gmail.com>
2020-10-21 17:04:36 -0300

    Add config for EWC and Volta

---
+++ src/config/networks/energy_web_chain.ts
@@ -0,0 +1,44 @@
+import EwcLogo from 'src/config/assets/token_ewc.svg'
+import { EnvironmentSettings, ETHEREUM_NETWORK, NetworkConfig } from 'src/config/networks/network.d'
+
+const baseConfig: EnvironmentSettings = {
+  txServiceUrl: 'https://safe-transaction.ewc.gnosis.io/api/v1',  
+  safeAppsUrl: 'https://safe-apps.dev.gnosisdev.com',
+  gasPriceOracleUrl: 'https://station.energyweb.org',
+  rpcServiceUrl: 'https://rpc.energyweb.org',
+  networkExplorerName: 'Energy web explorer',
+  networkExplorerUrl: 'https://explorer.energyweb.org',
+  networkExplorerApiUrl: 'http://explorer.energyweb.org/api',
+}
+
+const mainnet: NetworkConfig = {
+  environment: {
+    dev: {
+      ...baseConfig,
+    },
+    staging: {
+      ...baseConfig,
+      safeAppsUrl: 'https://safe-apps.staging.gnosisdev.com',
+    },
+    production: {
+      ...baseConfig,
+      safeAppsUrl: 'https://apps.gnosis-safe.io',
+    },
+  },
+  network: {
+    id: ETHEREUM_NETWORK.ENERGY_WEB_CHAIN,
+    backgroundColor: '#A566FF',
+    textColor: '#ffffff',
+    label: 'EWC',
+    isTestNet: false,
+    nativeCoin: {
+      address: '0x000',
+      name: 'Energy web token',
+      symbol: 'EWT',
+      decimals: 18,
+      logoUri: EwcLogo,
+    },
+  }
+}
+
+export default mainnet
---
+++ src/config/networks/volta.ts
@@ -0,0 +1,44 @@
+import EwcLogo from 'src/config/assets/token_ewc.svg'
+import { EnvironmentSettings, ETHEREUM_NETWORK, NetworkConfig } from 'src/config/networks/network.d'
+
+const baseConfig: EnvironmentSettings = {
+  txServiceUrl: 'https://safe-transaction.volta.gnosis.io/api/v1',
+  safeAppsUrl: 'https://safe-apps.dev.gnosisdev.com',
+  gasPriceOracleUrl: 'https://ethgasstation.info/json/ethgasAPI.json',
+  rpcServiceUrl: 'https://volta-rpc.energyweb.org',
+  networkExplorerName: 'Volta explorer',
+  networkExplorerUrl: 'https://volta-explorer.energyweb.org',
+  networkExplorerApiUrl: 'http://volta-explorer.energyweb.org/api',
+}
+
+const mainnet: NetworkConfig = {
+  environment: {
+    dev: {
+      ...baseConfig,
+    },
+    staging: {
+      ...baseConfig,
+      safeAppsUrl: 'https://safe-apps.staging.gnosisdev.com',
+    },
+    production: {
+      ...baseConfig,
+      safeAppsUrl: 'https://apps.gnosis-safe.io',
+    },
+  },
+  network: {
+    id: ETHEREUM_NETWORK.VOLTA,
+    backgroundColor: '#514989',
+    textColor: '#ffffff',
+    label: 'Volta',
+    isTestNet: true,
+    nativeCoin: {
+      address: '0x000',
+      name: 'Energy web token',
+      symbol: 'EWT',
+      decimals: 18,
+      logoUri: EwcLogo,
+    },
+  }
+}
+
+export default mainnet
