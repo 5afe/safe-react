b2716c021fb7faa804564c4615f5497b6f2df841
nicosampler <nf.dominguez.87@gmail.com>
2020-10-22 13:06:28 -0300

    rollback ewc filename

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -2,7 +2,7 @@ import local from './local'
 import mainnet from './mainnet'
 import rinkeby from './rinkeby'
 import xdai from './xdai'
-import energyWebChain from './energyWebChain'
+import energy_web_chain from './energy_web_chain'
 import volta from './volta'
 
 export default {
@@ -10,6 +10,6 @@ export default {
   mainnet,
   rinkeby,
   xdai,
-  energy_web_chain: energyWebChain,
+  energy_web_chain,
   volta
 }
