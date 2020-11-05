387ebe68fb69295f0eb4a901d3190d9640168491
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2020-11-05 16:02:24 +0100

    Updates bnc-notify version and adds support for GridPlus Lattice [internal] (#1569)
    * Adds support for GridPlus Lattice and bumps `bnc-onboard` version.
    For more information on the GridPlus Lattice, see https://gridplus.io/lattice
    
    * Disable Lattice wallet from incompatible chains
    
    Co-authored-by: Alex Miller <asmiller1989@gmail.com>
    Co-authored-by: Fernando <fernando.greco@gmail.com>

--- src/components/AppLayout/Header/components/WalletIcon/icons/index.ts
+++ src/components/AppLayout/Header/components/WalletIcon/icons/index.ts
@@ -3,6 +3,7 @@ import metamaskIcon from './icon-metamask.png'
 import walletConnectIcon from './icon-wallet-connect.svg'
 import trezorIcon from './icon-trezor.svg'
 import ledgerIcon from './icon-ledger.svg'
+import latticeIcon from './icon-lattice.svg'
 import dapperIcon from './icon-dapper.png'
 import fortmaticIcon from './icon-fortmatic.svg'
 import portisIcon from './icon-portis.svg'
@@ -41,6 +42,10 @@ const WALLET_ICONS: WalletObjectsProps<IconValue> = {
   [WALLET_PROVIDER.LEDGER]: {
     src: ledgerIcon,
     height: 25,
+  },
+  [WALLET_PROVIDER.LATTICE]: {
+    src: latticeIcon,
+    height: 41,
   },
   [WALLET_PROVIDER.DAPPER]: {
     src: dapperIcon,
