aace12004a8c838cab18bde19da1dbf301fb5e7a
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-05 19:14:05 +0400

    wip error for multiple versions

--- src/routes/safe/components/Apps/communicator.ts
+++ src/routes/safe/components/Apps/communicator.ts
@@ -1,10 +1,16 @@
 import { MutableRefObject } from 'react'
+import { Methods } from '@gnosis.pm/safe-apps-sdk'
 
 class AppCommunicator {
   private iframe: HTMLIFrameElement
+  private handlers = new Map()
 
   constructor(iframeRef: MutableRefObject<HTMLIFrameElement>) {
     this.iframe = iframeRef.current
+  }
+
+  on(method: Methods, handler: () => Promise<unknown> | unknown): void {
+    this.handlers.set(method, handler)
   }
 }
 
