0cd8b9557fe30eb05fba8cec21fb94790a8b9acd
fernandomg <fernando.greco@gmail.com>
2020-11-06 08:29:58 -0300

    simplify `toTokenUnit`

--- src/logic/tokens/utils/humanReadableValue.ts
+++ src/logic/tokens/utils/humanReadableValue.ts
@@ -7,13 +7,5 @@ export const humanReadableValue = (value: number | string, decimals = 18): strin
 export const fromTokenUnit = (amount: number | string, decimals: string | number): string =>
   new BigNumber(amount).times(`1e-${decimals}`).toFixed()
 
-export const toTokenUnit = (amount: number | string, decimals: string | number): string => {
-  const amountBN = new BigNumber(amount).times(`1e${decimals}`)
-  const [, amountDecimalPlaces] = amount.toString().split('.')
-
-  if (amountDecimalPlaces?.length > +decimals) {
-    return amountBN.toFixed(0, BigNumber.ROUND_DOWN)
-  }
-
-  return amountBN.toFixed()
-}
+export const toTokenUnit = (amount: number | string, decimals: string | number): string =>
+  new BigNumber(amount).times(`1e${decimals}`).toFixed(0, BigNumber.ROUND_DOWN)
