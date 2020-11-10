95e8ebede7c5e648ad06871e9b7280fd7c19c716
Agustin Pane <agustin.pane@gmail.com>
2020-11-10 13:23:49 -0300

    Replaces getAddressesListFromAddressBook with addressBookAddressesListSelector

--- src/logic/addressBook/utils/index.ts
+++ src/logic/addressBook/utils/index.ts
@@ -55,9 +55,6 @@ export const saveAddressBook = async (addressBook: AddressBookState): Promise<vo
   }
 }
 
-export const getAddressesListFromAddressBook = (addressBook: AddressBookState): string[] =>
-  addressBook.map((entry) => entry.address)
-
 type GetNameFromAddressBookOptions = {
   filterOnlyValidName: boolean
 }
