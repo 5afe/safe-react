da31053689628c408fab9a3df6fa303b7aeff858
Agustín Longoni <agustin.longoni@altoros.com>
2020-11-12 11:21:38 -0300

    add transition animation to skeleton

--- src/routes/safe/components/Apps/components/AppCard/index.tsx
+++ src/routes/safe/components/Apps/components/AppCard/index.tsx
@@ -35,26 +35,179 @@ const IconImg = styled.img<{ size: 'md' | 'lg' }>`
   object-fit: contain;
 `
 
-const Skeleton = styled.div``
-
 const AppIconSK = styled.div`
   height: 60px;
   width: 60px;
   border-radius: 30px;
   margin: 0 auto;
   background-color: lightgrey;
+  background: linear-gradient(84deg, lightgrey, transparent);
+  background-size: 400% 400%;
+
+  -webkit-animation: gradient-SK 1.5s ease infinite;
+  -moz-animation: gradient-SK 1.5s ease infinite;
+  -o-animation: gradient-SK 1.5s ease infinite;
+  animation: gradient-SK 1.5s ease infinite;
+
+  @-webkit-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-moz-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-o-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
 `
 const TitleSK = styled.div`
   height: 24px;
   width: 160px;
   margin: 24px auto;
   background-color: lightgrey;
+  background: linear-gradient(84deg, lightgrey, transparent);
+  background-size: 400% 400%;
+
+  -webkit-animation: gradient-SK 1.5s ease infinite;
+  -moz-animation: gradient-SK 1.5s ease infinite;
+  -o-animation: gradient-SK 1.5s ease infinite;
+  animation: gradient-SK 1.5s ease infinite;
+
+  @-webkit-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-moz-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-o-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
 `
 const DescriptionSK = styled.div`
   height: 16px;
   width: 200px;
-  margin: 8px auto;
   background-color: lightgrey;
+  background: linear-gradient(84deg, lightgrey, transparent);
+  background-size: 400% 400%;
+
+  -webkit-animation: gradient-SK 1.5s ease infinite;
+  -moz-animation: gradient-SK 1.5s ease infinite;
+  -o-animation: gradient-SK 1.5s ease infinite;
+  animation: gradient-SK 1.5s ease infinite;
+
+  @-webkit-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-moz-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @-o-keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
+  @keyframes gradient-SK {
+    0% {
+      background-position: 0% 54%;
+    }
+    50% {
+      background-position: 100% 47%;
+    }
+    100% {
+      background-position: 0% 54%;
+    }
+  }
 `
 
 type Props = {
@@ -88,12 +241,10 @@ const Apps = ({
   if (isLoading) {
     return (
       <StyledApps className={className}>
-        <Skeleton>
-          <AppIconSK />
-          <TitleSK />
-          <DescriptionSK />
-          <DescriptionSK />
-        </Skeleton>
+        <AppIconSK />
+        <TitleSK />
+        <DescriptionSK />
+        <DescriptionSK />
       </StyledApps>
     )
   }
