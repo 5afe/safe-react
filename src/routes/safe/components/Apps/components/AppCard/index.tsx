49accc957496403346f1e214fb57705b6222d65d
Agustín Longoni <agustin.longoni@altoros.com>
2020-11-16 14:40:46 -0300

    remove margin for cards

--- src/routes/safe/components/Apps/components/AppCard/index.tsx
+++ src/routes/safe/components/Apps/components/AppCard/index.tsx
@@ -14,7 +14,6 @@ const AppCard = styled.div`
   box-shadow: 1px 2px 10px 0 ${({ theme }) => fade(theme.colors.shadow.color, 0.18)};
   border-radius: 8px;
   padding: 24px;
-  margin: 10px;
   background-color: ${({ theme }) => theme.colors.white};
   height: 232px;
   cursor: default !important;
