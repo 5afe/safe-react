e9ab1c3a6730aded584a41fd8ca393ad42646d56
fernandomg <fernando.greco@gmail.com>
2020-11-20 13:13:12 -0300

    add module selector

--- src/routes/safe/container/selector.ts
+++ src/routes/safe/container/selector.ts
@@ -2,13 +2,27 @@ import { List, Map } from 'immutable'
 import { createSelector } from 'reselect'
 
 import { Token } from 'src/logic/tokens/store/model/token'
-import { tokensSelector } from 'src/logic/tokens/store/selectors'
+import { tokenListSelector, tokensSelector } from 'src/logic/tokens/store/selectors'
 import { getEthAsToken } from 'src/logic/tokens/utils/tokenHelpers'
-import { isUserAnOwner } from 'src/logic/wallets/ethAddresses'
+import { isUserAnOwner, sameAddress } from 'src/logic/wallets/ethAddresses'
 import { userAccountSelector } from 'src/logic/wallets/store/selectors'
 
-import { safeActiveTokensSelector, safeBalancesSelector, safeSelector } from 'src/logic/safe/store/selectors'
+import {
+  safeActiveTokensSelector,
+  safeBalancesSelector,
+  safeParamAddressFromStateSelector,
+  safeSelector,
+} from 'src/logic/safe/store/selectors'
 import { SafeRecord } from 'src/logic/safe/store/models/safe'
+import { AppReduxState } from 'src/store'
+import { MODULE_TRANSACTIONS_REDUCER_ID } from 'src/logic/safe/store/reducer/moduleTransactions'
+import { ModuleTxServiceModel } from 'src/logic/safe/store/actions/transactions/fetchTransactions/loadModuleTransactions'
+import {
+  SafeModuleTransaction,
+  TransactionStatus,
+  TransactionTypes,
+} from 'src/logic/safe/store/models/types/transaction'
+import { SPENDING_LIMIT_MODULE_ADDRESS } from 'src/utils/constants'
 
 export const grantedSelector = createSelector(
   userAccountSelector,
@@ -46,5 +60,57 @@ export const extendedSafeTokensSelector = createSelector(
     }
 
     return extendedTokens.toList()
+  },
+)
+
+const moduleTransactionsSelector = (state: AppReduxState) => state[MODULE_TRANSACTIONS_REDUCER_ID]
+
+export const modulesTransactionsBySafeSelector = createSelector(
+  moduleTransactionsSelector,
+  safeParamAddressFromStateSelector,
+  (moduleTransactions, safeAddress): ModuleTxServiceModel[] => {
+    // no module tx for the current safe so far
+    if (!moduleTransactions || !safeAddress || !moduleTransactions[safeAddress]) {
+      return []
+    }
+
+    return moduleTransactions[safeAddress]
+  },
+)
+
+export const safeModuleTransactionsSelector = createSelector(
+  tokenListSelector,
+  modulesTransactionsBySafeSelector,
+  (tokens, safeModuleTransactions): SafeModuleTransaction[] => {
+    return safeModuleTransactions.map((moduleTx) => {
+      // if not spendingLimit module tx, then it's an generic module tx
+      const type = sameAddress(moduleTx.module, SPENDING_LIMIT_MODULE_ADDRESS)
+        ? TransactionTypes.SPENDING_LIMIT
+        : TransactionTypes.MODULE
+
+      // TODO: this is strictly attached to Spending Limit Module.
+      //  This has to be moved nearest the module info rendering.
+      // add token info to the model, so data can be properly displayed in the UI
+      let tokenInfo
+      if (type === TransactionTypes.SPENDING_LIMIT) {
+        if (moduleTx.data) {
+          // if `data` is defined, then it's a token transfer
+          tokenInfo = tokens.find(({ address }) => sameAddress(address, moduleTx.to))
+        } else {
+          // if `data` is not defined, then it's an ETH transfer
+          // ETH does not exist in the list of tokens, so we recreate the record here
+          tokenInfo = getEthAsToken(0)
+        }
+      }
+
+      return {
+        ...moduleTx,
+        safeTxHash: moduleTx.transactionHash,
+        executionTxHash: moduleTx.transactionHash,
+        status: TransactionStatus.SUCCESS,
+        tokenInfo,
+        type,
+      }
+    })
   },
 )
