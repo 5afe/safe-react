c5d6f1140f23a6350fbf73cceeefc8ff6a64de04
fernandomg <fernando.greco@gmail.com>
2020-11-19 21:41:57 -0300

    refactor methods decodes

--- src/logic/contracts/methodIds.ts
+++ src/logic/contracts/methodIds.ts
@@ -1,78 +1,172 @@
+import {
+  DataDecoded,
+  SAFE_METHOD_ID_TO_NAME,
+  SAFE_METHODS_NAMES,
+  SPENDING_LIMIT_METHOD_ID_TO_NAME,
+  SPENDING_LIMIT_METHODS_NAMES,
+  TOKEN_TRANSFER_METHOD_ID_TO_NAME,
+  TOKEN_TRANSFER_METHODS_NAMES,
+} from 'src/logic/safe/store/models/types/transactions.d'
 import { web3ReadOnly as web3 } from 'src/logic/wallets/getWeb3'
-import { DataDecoded, METHOD_TO_ID } from 'src/routes/safe/store/models/types/transactions.d'
+
+type DecodeInfoProps = {
+  paramsHash: string
+  params: Record<string, string>
+}
+
+const decodeInfo = ({ paramsHash, params }: DecodeInfoProps): DataDecoded['parameters'] => {
+  const decodedParameters = web3.eth.abi.decodeParameters(Object.values(params), paramsHash)
+
+  return Object.keys(params).map((name, index) => ({
+    name,
+    type: params[name],
+    value: decodedParameters[index],
+  }))
+}
 
 export const decodeParamsFromSafeMethod = (data: string): DataDecoded | null => {
-  const [methodId, params] = [data.slice(0, 10) as keyof typeof METHOD_TO_ID | string, data.slice(10)]
+  const [methodId, paramsHash] = [data.slice(0, 10), data.slice(10)]
+  const method = SAFE_METHODS_NAMES[methodId]
 
-  switch (methodId) {
-    // swapOwner
-    case '0xe318b52b': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['uint', 'address', 'address'], params) as string[]
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: 'oldOwner', type: 'address', value: decodedParameters[1] },
-          { name: 'newOwner', type: 'address', value: decodedParameters[2] },
-        ],
+  switch (method) {
+    case SAFE_METHODS_NAMES.SWAP_OWNER: {
+      const params = {
+        prevOwner: 'address',
+        oldOwner: 'address',
+        newOwner: 'address',
       }
+
+      // we only need to return the addresses that has been swapped, no need for the `prevOwner`
+      const [, oldOwner, newOwner] = decodeInfo({ paramsHash, params })
+
+      return { method, parameters: [oldOwner, newOwner] }
     }
 
-    // addOwnerWithThreshold
-    case '0x0d582f13': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['address', 'uint'], params)
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: 'owner', type: 'address', value: decodedParameters[0] },
-          { name: '_threshold', type: 'uint', value: decodedParameters[1] },
-        ],
+    case SAFE_METHODS_NAMES.ADD_OWNER_WITH_THRESHOLD: {
+      const params = {
+        owner: 'address',
+        _threshold: 'uint',
       }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
-    // removeOwner
-    case '0xf8dc5dd9': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['address', 'address', 'uint'], params)
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: 'owner', type: 'address', value: decodedParameters[1] },
-          { name: '_threshold', type: 'uint', value: decodedParameters[2] },
-        ],
+    case SAFE_METHODS_NAMES.REMOVE_OWNER: {
+      const params = {
+        prevOwner: 'address',
+        owner: 'address',
+        _threshold: 'uint',
       }
+
+      // we only need to return the removed owner and the new threshold, no need for the `prevOwner`
+      const [, oldOwner, threshold] = decodeInfo({ paramsHash, params })
+
+      return { method, parameters: [oldOwner, threshold] }
     }
 
-    // changeThreshold
-    case '0x694e80c3': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['uint'], params)
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: '_threshold', type: 'uint', value: decodedParameters[0] },
-        ],
+    case SAFE_METHODS_NAMES.CHANGE_THRESHOLD: {
+      const params = {
+        _threshold: 'uint',
       }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
-    // enableModule
-    case '0x610b5925': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['address'], params)
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: 'module', type: 'address', value: decodedParameters[0] },
-        ],
+    case SAFE_METHODS_NAMES.ENABLE_MODULE: {
+      const params = {
+        module: 'address',
       }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
-    // disableModule
-    case '0xe009cfde': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['address', 'address'], params)
-      return {
-        method: METHOD_TO_ID[methodId],
-        parameters: [
-          { name: 'prevModule', type: 'address', value: decodedParameters[0] },
-          { name: 'module', type: 'address', value: decodedParameters[1] },
-        ],
+    case SAFE_METHODS_NAMES.DISABLE_MODULE: {
+      const params = {
+        prevModule: 'address',
+        module: 'address',
       }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
+    }
+
+    default:
+      return null
+  }
+}
+
+export const isSetAllowanceMethod = (data: string): boolean => {
+  const methodId = data.slice(0, 10)
+  return SPENDING_LIMIT_METHOD_ID_TO_NAME[methodId] === SPENDING_LIMIT_METHODS_NAMES.SET_ALLOWANCE
+}
+
+export const isDeleteAllowanceMethod = (data: string): boolean => {
+  const methodId = data.slice(0, 10)
+  return SPENDING_LIMIT_METHOD_ID_TO_NAME[methodId] === SPENDING_LIMIT_METHODS_NAMES.DELETE_ALLOWANCE
+}
+
+export const decodeParamsFromSpendingLimit = (data: string): DataDecoded | null => {
+  const [methodId, paramsHash] = [data.slice(0, 10), data.slice(10)]
+  const method = SPENDING_LIMIT_METHOD_ID_TO_NAME[methodId]
+
+  switch (method) {
+    case SPENDING_LIMIT_METHODS_NAMES.ADD_DELEGATE: {
+      const params = {
+        delegate: 'address',
+      }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
+    }
+
+    case SPENDING_LIMIT_METHODS_NAMES.SET_ALLOWANCE: {
+      const params = {
+        delegate: 'address',
+        token: 'address',
+        allowanceAmount: 'uint96',
+        resetTimeMin: 'uint16',
+        resetBaseMin: 'uint32',
+      }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
+    }
+
+    case SPENDING_LIMIT_METHODS_NAMES.EXECUTE_ALLOWANCE_TRANSFER: {
+      const params = {
+        safe: 'address',
+        token: 'address',
+        to: 'address',
+        amount: 'uint96',
+        paymentToken: 'address',
+        payment: 'uint96',
+        delegate: 'address',
+        signature: 'bytes',
+      }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
+    }
+
+    case SPENDING_LIMIT_METHODS_NAMES.DELETE_ALLOWANCE: {
+      const params = {
+        delegate: 'address',
+        token: 'address',
+      }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
     default:
@@ -81,57 +175,53 @@ export const decodeParamsFromSafeMethod = (data: string): DataDecoded | null => 
 }
 
 const isSafeMethod = (methodId: string): boolean => {
-  return !!METHOD_TO_ID[methodId]
+  return !!SAFE_METHOD_ID_TO_NAME[methodId]
 }
 
-export const decodeMethods = (data: string): DataDecoded | null => {
-  if(!data.length) {
+const isSpendingLimitMethod = (methodId: string): boolean => {
+  return !!SPENDING_LIMIT_METHOD_ID_TO_NAME[methodId]
+}
+
+export const decodeMethods = (data: string | null): DataDecoded | null => {
+  if (!data?.length) {
     return null
   }
 
-  const [methodId, params] = [data.slice(0, 10), data.slice(10)]
+  const [methodId, paramsHash] = [data.slice(0, 10), data.slice(10)]
 
   if (isSafeMethod(methodId)) {
     return decodeParamsFromSafeMethod(data)
   }
 
-  switch (methodId) {
-    // a9059cbb - transfer(address,uint256)
-    case '0xa9059cbb': {
-      const decodeParameters = web3.eth.abi.decodeParameters(['address', 'uint'], params)
-      return {
-        method: 'transfer',
-        parameters: [
-          { name: 'to', type: '', value: decodeParameters[0] },
-          { name: 'value', type: '', value: decodeParameters[1] },
-        ],
+  if (isSpendingLimitMethod(methodId)) {
+    return decodeParamsFromSpendingLimit(data)
+  }
+
+  const method = TOKEN_TRANSFER_METHOD_ID_TO_NAME[methodId]
+
+  switch (method) {
+    case TOKEN_TRANSFER_METHODS_NAMES.TRANSFER: {
+      const params = {
+        to: 'address',
+        value: 'uint',
       }
+
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
-    // 23b872dd - transferFrom(address,address,uint256)
-    case '0x23b872dd': {
-      const decodeParameters = web3.eth.abi.decodeParameters(['address', 'address', 'uint'], params)
-      return {
-        method: 'transferFrom',
-        parameters: [
-          { name: 'from', type: '', value: decodeParameters[0] },
-          { name: 'to', type: '', value: decodeParameters[1] },
-          { name: 'value', type: '', value: decodeParameters[2] },
-        ],
+    case TOKEN_TRANSFER_METHODS_NAMES.TRANSFER_FROM:
+    case TOKEN_TRANSFER_METHODS_NAMES.SAFE_TRANSFER_FROM: {
+      const params = {
+        from: 'address',
+        to: 'address',
+        value: 'uint',
       }
-    }
 
-    // 42842e0e - safeTransferFrom(address,address,uint256)
-    case '0x42842e0e': {
-      const decodedParameters = web3.eth.abi.decodeParameters(['address', 'address', 'uint'], params)
-      return {
-        method: 'safeTransferFrom',
-        parameters: [
-          { name: 'from', type: '', value: decodedParameters[0] },
-          { name: 'to', type: '', value: decodedParameters[1] },
-          { name: 'value', type: '', value: decodedParameters[2] },
-        ],
-      }
+      const parameters = decodeInfo({ paramsHash, params })
+
+      return { method, parameters }
     }
 
     default:
