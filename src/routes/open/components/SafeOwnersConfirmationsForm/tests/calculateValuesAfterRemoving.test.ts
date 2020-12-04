fdc843e590b7724dcfcb8181654a2fbf307dc426
fernandomg <fernando.greco@gmail.com>
2020-12-04 19:27:00 -0300

    prettier:fix

--- src/routes/open/components/SafeOwnersConfirmationsForm/tests/calculateValuesAfterRemoving.test.ts
+++ src/routes/open/components/SafeOwnersConfirmationsForm/tests/calculateValuesAfterRemoving.test.ts
@@ -34,18 +34,16 @@ describe('calculateValuesAfterRemoving', () => {
       owner2Address: '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b',
     }
 
-
     // When
     const newFormContent = calculateValuesAfterRemoving(1, formContent)
 
     // Then
     expect(newFormContent).toStrictEqual({
-        name: 'My Safe',
-        owner0Name: 'Owner 0',
-        owner0Address: '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1',
-        owner1Name: 'Owner 2',
-        owner1Address: '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b',
-      },
-    )
+      name: 'My Safe',
+      owner0Name: 'Owner 0',
+      owner0Address: '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1',
+      owner1Name: 'Owner 2',
+      owner1Address: '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b',
+    })
   })
 })
