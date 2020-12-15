7e78c59c6e960d447f3a18bf53e216f2a42401e6
Agustin Pane <agustin.pane@gmail.com>
2020-12-15 15:50:50 -0300

    Simplify shouldSafeStoreBeUpdated using isEqual from lodash

--- src/logic/safe/utils/shouldSafeStoreBeUpdated.ts
+++ src/logic/safe/utils/shouldSafeStoreBeUpdated.ts
@@ -1,5 +1,5 @@
 import { SafeRecordProps } from 'src/logic/safe/store/models/safe'
-import { sameString } from 'src/utils/strings'
+
 import isEqual from 'lodash.isequal'
 
 export const shouldSafeStoreBeUpdated = (
@@ -8,101 +8,5 @@ export const shouldSafeStoreBeUpdated = (
 ): boolean => {
   if (!oldSafeProps) return true
 
-  const {
-    address,
-    name,
-    modules,
-    spendingLimits,
-    nonce,
-    threshold,
-    featuresEnabled,
-    ethBalance,
-    latestIncomingTxBlock,
-    currentVersion,
-    owners,
-    activeTokens,
-    activeAssets,
-    blacklistedAssets,
-    blacklistedTokens,
-    needsUpdate,
-    recurringUser,
-    balances,
-  } = oldSafeProps
-
-  if (!sameString(address, newSafeProps.address) && newSafeProps.hasOwnProperty('address')) {
-    return true
-  }
-
-  if (!sameString(name, newSafeProps.name) && newSafeProps.hasOwnProperty('name')) {
-    return true
-  }
-
-  if (threshold !== newSafeProps.threshold && newSafeProps.hasOwnProperty('threshold')) {
-    return true
-  }
-
-  if (!sameString(ethBalance, newSafeProps.ethBalance) && newSafeProps.hasOwnProperty('ethBalance')) {
-    return true
-  }
-
-  if (!owners?.equals(newSafeProps.owners) && newSafeProps.hasOwnProperty('owners')) {
-    return true
-  }
-
-  if (!isEqual(modules, newSafeProps.modules) && newSafeProps.hasOwnProperty('modules')) {
-    return true
-  }
-
-  if (!isEqual(spendingLimits, newSafeProps.spendingLimits) && newSafeProps.hasOwnProperty('spendingLimits')) {
-    return true
-  }
-
-  if (!activeTokens?.equals(newSafeProps.activeTokens) && newSafeProps.hasOwnProperty('activeTokens')) {
-    return true
-  }
-
-  if (!activeAssets?.equals(newSafeProps.activeAssets) && newSafeProps.hasOwnProperty('activeAssets')) {
-    return true
-  }
-
-  if (!blacklistedTokens?.equals(newSafeProps.blacklistedTokens) && newSafeProps.hasOwnProperty('blacklistedTokens')) {
-    return true
-  }
-
-  if (!blacklistedAssets?.equals(newSafeProps.blacklistedAssets) && newSafeProps.hasOwnProperty('blacklistedAssets')) {
-    return true
-  }
-
-  if (!balances?.equals(newSafeProps.balances) && newSafeProps.hasOwnProperty('balances')) {
-    return true
-  }
-
-  if (nonce !== newSafeProps.nonce && newSafeProps.hasOwnProperty('nonce')) {
-    return true
-  }
-
-  if (
-    latestIncomingTxBlock !== newSafeProps.latestIncomingTxBlock &&
-    newSafeProps.hasOwnProperty('latestIncomingTxBlock')
-  ) {
-    return true
-  }
-
-  if (recurringUser !== newSafeProps.recurringUser && newSafeProps.hasOwnProperty('recurringUser')) {
-    return true
-  }
-
-  if (!sameString(currentVersion, newSafeProps.currentVersion) && newSafeProps.hasOwnProperty('currentVersion')) {
-    return true
-  }
-
-  if (needsUpdate !== newSafeProps.needsUpdate && newSafeProps.hasOwnProperty('needsUpdate')) {
-    return true
-  }
-
-  if (!isEqual(featuresEnabled, newSafeProps.featuresEnabled) && newSafeProps.hasOwnProperty('featuresEnabled')) {
-    return true
-  }
-
-  return false
+  return !isEqual(oldSafeProps, newSafeProps)
 }
