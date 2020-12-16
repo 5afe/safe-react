84371b36248a7f51bdbb78a6fb4e4ae3c1d0bcf9
fernandomg <fernando.greco@gmail.com>
2020-12-16 18:42:52 -0300

    remove unused var

--- src/logic/currencyValues/store/actions/fetchSelectedCurrency.ts
+++ src/logic/currencyValues/store/actions/fetchSelectedCurrency.ts
@@ -3,7 +3,7 @@ import { ThunkDispatch } from 'redux-thunk'
 
 import { setSelectedCurrency } from 'src/logic/currencyValues/store/actions/setSelectedCurrency'
 import { AVAILABLE_CURRENCIES } from 'src/logic/currencyValues/store/model/currencyValues'
-import { CurrencyPayloads, CurrentCurrencyPayload } from 'src/logic/currencyValues/store/reducer/currencyValues'
+import { CurrentCurrencyPayload } from 'src/logic/currencyValues/store/reducer/currencyValues'
 import { loadSelectedCurrency } from 'src/logic/currencyValues/store/utils/currencyValuesStorage'
 import { AppReduxState } from 'src/store'
 
