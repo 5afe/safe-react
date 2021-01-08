b86e5a5d6122b9993661ce12f0964ea7277d5722
fernandomg <fernando.greco@gmail.com>
2021-01-08 15:09:02 -0300

    wip: infinite-scroll
    Co-authored-by: Mati Dastugue <matiasdastugue@gmail.com>

--- src/routes/safe/components/GatewayTransactions/HistoryTxList.tsx
+++ src/routes/safe/components/GatewayTransactions/HistoryTxList.tsx
@@ -1,30 +1,74 @@
 import { Loader } from '@gnosis.pm/safe-react-components'
 import { format } from 'date-fns'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components'
 
+import { loadPagedTransactions } from 'src/logic/safe/store/actions/transactions/fetchTransactions/loadGatewayTransactions'
+import { addHistoryTransactions } from 'src/logic/safe/store/actions/transactions/gatewayTransactions'
+import { safeParamAddressFromStateSelector } from 'src/logic/safe/store/selectors'
 import { useHistoryTransactions } from './hooks/useHistoryTransactions'
 import { H2, StyledTransactions, StyledTransactionsGroup } from './styled'
 import { TxRow } from './TxRow'
 
-export const HistoryTxList = (): ReactElement => {
+const usePagedTransactions = () => {
   const { count, transactions } = useHistoryTransactions()
+
+  const dispatch = useDispatch()
+  const safeAddress = useSelector(safeParamAddressFromStateSelector)
+  const [hasMore, setHasMore] = useState(true)
+
+  const fetchMoreData = async () => {
+    const values = await loadPagedTransactions(safeAddress)
+
+    if (values) {
+      dispatch(addHistoryTransactions({ safeAddress, values }))
+    } else {
+      setHasMore(false)
+    }
+  }
+
+  return { count, transactions, hasMore, fetchMoreData }
+}
+
+const ScrollableContainer = styled.div`
+  height: calc(100vh - 225px);
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+`
+
+export const HistoryTxList = (): ReactElement => {
+  const { count, fetchMoreData, hasMore, transactions } = usePagedTransactions()
 
   if (count === 0) {
     return <Loader size="lg" />
   }
 
   return (
-    <>
-      {transactions.map(([timestamp, txs]) => (
-        <StyledTransactionsGroup key={timestamp}>
-          <H2>{format(Number(timestamp), 'MMM d, yyyy')}</H2>
-          <StyledTransactions>
-            {txs.map((transaction) => (
-              <TxRow key={transaction.id} transaction={transaction} />
-            ))}
-          </StyledTransactions>
-        </StyledTransactionsGroup>
-      ))}
-    </>
+    <ScrollableContainer id="scrollableDiv">
+      <InfiniteScroll
+        style={{ overflow: 'hidden' }}
+        dataLength={transactions.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<Loader size="md" />}
+        endMessage="all set"
+        scrollThreshold="120px"
+        scrollableTarget="scrollableDiv"
+      >
+        {transactions?.map(([timestamp, txs]) => (
+          <StyledTransactionsGroup key={timestamp}>
+            <H2>{format(Number(timestamp), 'MMM d, yyyy')}</H2>
+            <StyledTransactions>
+              {txs.map((transaction) => (
+                <TxRow key={transaction.id} transaction={transaction} />
+              ))}
+            </StyledTransactions>
+          </StyledTransactionsGroup>
+        ))}
+      </InfiniteScroll>
+    </ScrollableContainer>
   )
 }
