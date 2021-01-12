882bbdc9c4bb36e2c27a97f52c29c5c8eaaf7c54
fernandomg <fernando.greco@gmail.com>
2021-01-12 16:22:20 -0300

    wip: group txs by nonce
    [skip ci]

--- src/routes/safe/components/GatewayTransactions/QueueTxList.tsx
+++ src/routes/safe/components/GatewayTransactions/QueueTxList.tsx
@@ -1,8 +1,58 @@
+import { Text } from '@gnosis.pm/safe-react-components'
 import React, { ReactElement } from 'react'
+import styled from 'styled-components'
 
 import { TransactionDetails } from 'src/logic/safe/store/models/types/gateway.d'
-import { TxQueueRow } from 'src/routes/safe/components/GatewayTransactions/TxQueueRow'
+import { TxQueueRow, TxQueueGroupedRow } from 'src/routes/safe/components/GatewayTransactions/TxQueueRow'
 import { H2, StyledTransactions, StyledTransactionsGroup } from './styled'
+
+const Disclaimer = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.text};
+  display: grid;
+  grid-template-columns: 0.5fr 3fr 3fr 1fr 2fr 2fr 2fr;
+  border-radius: 5px;
+  margin: 8px;
+  padding: 8px;
+  width: calc(100% - 32px);
+
+  .nonce {
+    grid-column-start: 1;
+  }
+
+  .disclaimer {
+    grid-column-start: 2;
+    grid-column-end: span 6;
+  }
+`
+
+const GroupedTransactions = styled.div`
+  display: grid;
+  grid-template-columns: 0.5fr 3fr 3fr 1fr 2fr 2fr 2fr;
+  width: 100%;
+
+  .tx-row {
+    grid-column-start: 2;
+    grid-column-end: span 6;
+    border: 0;
+
+    &:first-child {
+      border: 0;
+    }
+
+    &.Mui-expanded:not(:last-child) {
+      border-bottom: 2px solid ${({ theme }) => theme.colors.separator};
+    }
+
+    .MuiAccordionSummary-root.Mui-expanded {
+      background-color: transparent;
+    }
+
+    .tx-status {
+      justify-self: end;
+    }
+  }
+`
 
 type QueueTxListProps = {
   txLocation: 'queued.next' | 'queued.queued'
@@ -17,9 +67,34 @@ export const QueueTxList = ({ txLocation, transactions }: QueueTxListProps): Rea
       <H2>{title}</H2>
       <StyledTransactions>
         {transactions.map(([nonce, txs]) =>
-          txs.map((transaction) => (
-            <TxQueueRow key={`${nonce}-${transaction.id}`} transaction={transaction} txLocation={txLocation} />
-          )),
+          txs.length > 1 ? (
+            <>
+              <Disclaimer>
+                <Text size="lg" className="nonce">
+                  {nonce}
+                </Text>
+                <Text size="lg" className="disclaimer">
+                  These transactions conflict as they use the same nonce. Executing one will automatically replace the
+                  other(s).{' '}
+                  <Text size="lg" as="span" color="primary">
+                    Learn more
+                  </Text>
+                </Text>
+              </Disclaimer>
+              <GroupedTransactions key={nonce}>
+                {txs.map((transaction) => (
+                  <TxQueueGroupedRow
+                    className="tx-row"
+                    key={`${nonce}-${transaction.id}`}
+                    transaction={transaction}
+                    txLocation={txLocation}
+                  />
+                ))}
+              </GroupedTransactions>
+            </>
+          ) : (
+            <TxQueueRow transaction={txs[0]} txLocation={txLocation} />
+          ),
         )}
       </StyledTransactions>
     </StyledTransactionsGroup>
--- src/routes/safe/components/GatewayTransactions/TxQueueRow.tsx
+++ src/routes/safe/components/GatewayTransactions/TxQueueRow.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react'
 
 import { Transaction } from 'src/logic/safe/store/models/types/gateway'
 import { useTransactionDetails } from './hooks/useTransactionDetails'
-import { TxQueuedCollapsed } from './TxQueueCollapsed'
+import { TxQueuedCollapsed, TxQueuedGroupedCollapsed } from './TxQueueCollapsed'
 import { NoPaddingAccordion } from './styled'
 import { TxDetails } from './TxDetails'
 
@@ -26,6 +26,36 @@ export const TxQueueRow = ({
     >
       <AccordionSummary>
         <TxQueuedCollapsed transaction={transaction} />
+      </AccordionSummary>
+      <AccordionDetails>
+        <TxDetails data={data} loading={loading} />
+      </AccordionDetails>
+    </NoPaddingAccordion>
+  )
+}
+
+export const TxQueueGroupedRow = ({
+  className,
+  transaction,
+  txLocation,
+}: {
+  className: string
+  transaction: Transaction
+  txLocation: 'queued.next' | 'queued.queued'
+}): ReactElement => {
+  const { data, loading } = useTransactionDetails(transaction.id, txLocation)
+
+  return (
+    <NoPaddingAccordion
+      className={className}
+      TransitionProps={{
+        mountOnEnter: false,
+        unmountOnExit: true,
+        appear: true,
+      }}
+    >
+      <AccordionSummary>
+        <TxQueuedGroupedCollapsed transaction={transaction} />
       </AccordionSummary>
       <AccordionDetails>
         <TxDetails data={data} loading={loading} />
