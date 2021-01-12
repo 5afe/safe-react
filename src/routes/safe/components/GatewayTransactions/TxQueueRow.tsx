773435d9f39b6d99d66065d9c5dd327d4bf8d905
fernandomg <fernando.greco@gmail.com>
2021-01-12 20:46:03 -0300

    wip: implement tree view for grouped transactions

--- src/routes/safe/components/GatewayTransactions/TxQueueRow.tsx
+++ src/routes/safe/components/GatewayTransactions/TxQueueRow.tsx
@@ -2,7 +2,6 @@ import { AccordionDetails, AccordionSummary } from '@gnosis.pm/safe-react-compon
 import React, { ReactElement } from 'react'
 
 import { Transaction } from 'src/logic/safe/store/models/types/gateway'
-import { useTransactionDetails } from './hooks/useTransactionDetails'
 import { TxQueuedCollapsed, TxQueuedGroupedCollapsed } from './TxQueueCollapsed'
 import { NoPaddingAccordion } from './styled'
 import { TxDetails } from './TxDetails'
@@ -31,16 +30,13 @@ export const TxQueueRow = ({
 )
 
 export const TxQueueGroupedRow = ({
-  className,
   transaction,
   txLocation,
 }: {
-  className: string
   transaction: Transaction
   txLocation: 'queued.next' | 'queued.queued'
 }): ReactElement => (
   <NoPaddingAccordion
-    className={className}
     TransitionProps={{
       mountOnEnter: false,
       unmountOnExit: true,
