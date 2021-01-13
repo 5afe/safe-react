d03fa6d6f9d8756522139bf49a3948531abb9678
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-13 14:58:56 +0400

    add declaration for styled-components theme

--- src/types/definitions.d.ts
+++ src/types/definitions.d.ts
@@ -1,3 +1,8 @@
+import 'styled-components'
+import { theme } from '@gnosis.pm/safe-react-components'
+
+type Theme = typeof theme
+
 export {}
 declare global {
   interface Window {
@@ -10,3 +15,7 @@ declare global {
 }
 declare module '@openzeppelin/contracts/build/contracts/ERC721'
 declare module 'currency-flags/dist/currency-flags.min.css'
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {} // eslint-disable-line
+}
