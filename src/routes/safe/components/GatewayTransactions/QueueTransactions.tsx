ee7b56ec87492f57cb4059e66cc5acb46234659b
fernandomg <fernando.greco@gmail.com>
2021-01-14 14:55:41 -0300

    make queued transactions list scrollable

--- src/routes/safe/components/GatewayTransactions/QueueTransactions.tsx
+++ src/routes/safe/components/GatewayTransactions/QueueTransactions.tsx
@@ -1,17 +1,19 @@
 import React, { ReactElement } from 'react'
 
+import { ScrollableTransactionsContainer } from 'src/routes/safe/components/GatewayTransactions/styled'
 import { useQueueTransactions } from './hooks/useQueueTransactions'
 import { QueueTxList } from './QueueTxList'
 
 export const QueueTransactions = (): ReactElement => {
   const { next, queue } = useQueueTransactions()
+  const totalTransactions = next.count + queue.count
 
-  return next.count + queue.count === 0 ? (
+  return totalTransactions === 0 ? (
     <div>No txs</div>
   ) : (
-    <>
+    <ScrollableTransactionsContainer>
       {next.count !== 0 && <QueueTxList txLocation="queued.next" transactions={next.transactions} />}
       {queue.count !== 0 && <QueueTxList txLocation="queued.queued" transactions={queue.transactions} />}
-    </>
+    </ScrollableTransactionsContainer>
   )
 }
