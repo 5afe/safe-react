95ff9e087df902bdad38a73651eb5fb1d906e85e
Agustin Pane <agustin.pane@gmail.com>
2021-01-25 14:38:30 -0300

    Adds EditableTxParameters within UpdateSafeModal

--- src/routes/safe/components/Settings/UpdateSafeModal/index.tsx
+++ src/routes/safe/components/Settings/UpdateSafeModal/index.tsx
@@ -1,7 +1,7 @@
 import IconButton from '@material-ui/core/IconButton'
 import Close from '@material-ui/icons/Close'
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { styles } from './style'
 
@@ -15,10 +15,14 @@ import { getUpgradeSafeTransactionHash } from 'src/logic/safe/utils/upgradeSafe'
 import createTransaction from 'src/logic/safe/store/actions/createTransaction'
 import { makeStyles } from '@material-ui/core'
 import { TransactionFees } from 'src/components/TransactionsFees'
-import { useEstimateTransactionGas } from 'src/logic/hooks/useEstimateTransactionGas'
+import { EstimationStatus, useEstimateTransactionGas } from 'src/logic/hooks/useEstimateTransactionGas'
 import { MULTI_SEND_ADDRESS } from 'src/logic/contracts/safeContracts'
 import { DELEGATE_CALL } from 'src/logic/safe/transactions'
 import { EMPTY_DATA } from 'src/logic/wallets/ethTransactions'
+import { TxParametersDetail } from 'src/routes/safe/components/Transactions/helpers/TxParametersDetail'
+import { EditableTxParameters } from 'src/routes/safe/components/Transactions/helpers/EditableTxParameters'
+import { safeThresholdSelector } from 'src/logic/safe/store/selectors'
+import { TxParameters } from 'src/routes/safe/container/hooks/useTransactionParameters'
 
 const useStyles = makeStyles(styles)
 
@@ -31,6 +35,7 @@ export const UpdateSafeModal = ({ onClose, safeAddress }: Props): React.ReactEle
   const classes = useStyles()
   const dispatch = useDispatch()
   const [multiSendCallData, setMultiSendCallData] = useState(EMPTY_DATA)
+  const threshold = useSelector(safeThresholdSelector) || 1
 
   useEffect(() => {
     const calculateUpgradeSafeModal = async () => {
@@ -40,7 +45,7 @@ export const UpdateSafeModal = ({ onClose, safeAddress }: Props): React.ReactEle
     calculateUpgradeSafeModal()
   }, [safeAddress])
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (txParameters: TxParameters) => {
     // Call the update safe method
     dispatch(
       createTransaction({
@@ -48,6 +53,9 @@ export const UpdateSafeModal = ({ onClose, safeAddress }: Props): React.ReactEle
         to: MULTI_SEND_ADDRESS,
         valueInWei: '0',
         txData: multiSendCallData,
+        txNonce: txParameters.safeNonce,
+        safeTxGas: txParameters.safeTxGas ? Number(txParameters.safeTxGas) : undefined,
+        ethParameters: txParameters,
         notifiedTransaction: 'STANDARD_TX',
         operation: DELEGATE_CALL,
       }),
@@ -61,65 +69,91 @@ export const UpdateSafeModal = ({ onClose, safeAddress }: Props): React.ReactEle
     isExecution,
     isCreation,
     isOffChainSignature,
+    gasPriceFormatted,
+    gasLimit,
   } = useEstimateTransactionGas({
     txData: multiSendCallData,
     txRecipient: safeAddress,
   })
 
+  // @todo (agustin) refactor inside EditableTxParameters
+  const getParametersStatus = () => (threshold > 1 ? 'ETH_DISABLED' : 'ENABLED')
+
   return (
-    <>
-      <Row align="center" className={classes.heading} grow>
-        <Paragraph className={classes.headingText} noMargin weight="bolder">
-          Update to new Safe version
-        </Paragraph>
-        <IconButton disableRipple onClick={onClose}>
-          <Close className={classes.close} />
-        </IconButton>
-      </Row>
-      <Hairline />
-      <GnoForm onSubmit={handleSubmit}>
-        {() => (
-          <>
-            <Block className={classes.modalContent}>
-              <Row>
-                <Paragraph>
-                  Update now to take advantage of new features and the highest security standards available.
-                </Paragraph>
-                <Block>
-                  This update includes:
-                  <ul>
-                    <li>Compatibility with new asset types (ERC-721 / ERC-1155)</li>
-                    <li>Improved interoperability with modules</li>
-                    <li>Minor security improvements</li>
-                  </ul>
+    <EditableTxParameters
+      ethGasLimit={gasLimit}
+      ethGasPrice={gasPriceFormatted}
+      parametersStatus={getParametersStatus()}
+    >
+      {(txParameters, toggleEditMode) => (
+        <>
+          <Row align="center" className={classes.heading} grow>
+            <Paragraph className={classes.headingText} noMargin weight="bolder">
+              Update to new Safe version
+            </Paragraph>
+            <IconButton disableRipple onClick={onClose}>
+              <Close className={classes.close} />
+            </IconButton>
+          </Row>
+          <Hairline />
+          <GnoForm onSubmit={() => handleSubmit(txParameters)}>
+            {() => (
+              <>
+                <Block className={classes.modalContent}>
+                  <Row>
+                    <Paragraph>
+                      Update now to take advantage of new features and the highest security standards available.
+                    </Paragraph>
+                    <Block>
+                      This update includes:
+                      <ul>
+                        <li>Compatibility with new asset types (ERC-721 / ERC-1155)</li>
+                        <li>Improved interoperability with modules</li>
+                        <li>Minor security improvements</li>
+                      </ul>
+                    </Block>
+                    <Paragraph>
+                      You will need to confirm this update just like any other transaction. This means other owners will
+                      have to confirm the update in case more than one confirmation is required for this Safe.
+                    </Paragraph>
+                  </Row>
+                  {/* Tx Parameters */}
+                  <TxParametersDetail
+                    txParameters={txParameters}
+                    onEdit={toggleEditMode}
+                    compact={false}
+                    parametersStatus={getParametersStatus()}
+                  />
+                  <Row>
+                    <TransactionFees
+                      gasCostFormatted={gasCostFormatted}
+                      isExecution={isExecution}
+                      isCreation={isCreation}
+                      isOffChainSignature={isOffChainSignature}
+                      txEstimationExecutionStatus={txEstimationExecutionStatus}
+                    />
+                  </Row>
                 </Block>
-                <Paragraph>
-                  You will need to confirm this update just like any other transaction. This means other owners will
-                  have to confirm the update in case more than one confirmation is required for this Safe.
-                </Paragraph>
-              </Row>
-              <Row>
-                <TransactionFees
-                  gasCostFormatted={gasCostFormatted}
-                  isExecution={isExecution}
-                  isCreation={isCreation}
-                  isOffChainSignature={isOffChainSignature}
-                  txEstimationExecutionStatus={txEstimationExecutionStatus}
-                />
-              </Row>
-            </Block>
-            <Hairline />
-            <Row align="center" className={classes.buttonRow}>
-              <Button minWidth={140} onClick={onClose}>
-                Back
-              </Button>
-              <Button color="primary" minWidth={140} type="submit" variant="contained">
-                Update Safe
-              </Button>
-            </Row>
-          </>
-        )}
-      </GnoForm>
-    </>
+                <Hairline />
+                <Row align="center" className={classes.buttonRow}>
+                  <Button minWidth={140} onClick={onClose}>
+                    Back
+                  </Button>
+                  <Button
+                    color="primary"
+                    minWidth={140}
+                    type="submit"
+                    variant="contained"
+                    disabled={!multiSendCallData || txEstimationExecutionStatus === EstimationStatus.LOADING}
+                  >
+                    Update Safe
+                  </Button>
+                </Row>
+              </>
+            )}
+          </GnoForm>
+        </>
+      )}
+    </EditableTxParameters>
   )
 }
