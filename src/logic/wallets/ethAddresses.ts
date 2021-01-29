012b41a3f793c898d8798d13982e78a2e4c9f707
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-01-29 13:07:47 +0100

    Feature - Add .crypto domain resolution (#1764)
    * (Feature) - Support for .crypto resolution
    
    * Fix EthereumAddress validator error msg
    
    * Extract unstoppable domains logic to an util
    
    * Add singleton to UnstoppableDomains instance
    
    * Shorten address or domain validation error message
    
    Co-authored-by: sudoryan <rleinbox@gmail.com>

--- src/logic/wallets/ethAddresses.ts
+++ src/logic/wallets/ethAddresses.ts
@@ -47,3 +47,5 @@ export const isUserAnOwnerOfAnySafe = (safes: List<SafeRecord> | SafeRecord[], u
   safes.some((safe: SafeRecord) => isUserAnOwner(safe, userAccount))
 
 export const isValidEnsName = (name: string): boolean => /^([\w-]+\.)+(eth|test|xyz|luxe|ewc)$/.test(name)
+
+export const isValidCryptoDomainName = (name: string): boolean => /^([\w-]+\.)+(crypto)$/.test(name)
