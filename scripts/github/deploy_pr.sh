0c62f352af63622b1f39524415f00bea0b4d1531
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-02-17 19:13:53 +0100

    Print aws path vars

--- scripts/github/deploy_pr.sh
+++ scripts/github/deploy_pr.sh
@@ -5,6 +5,8 @@ function deploy_pr {
   PULL_REQUEST_NUMBER="pr$PR_NUMBER"
   REVIEW_FEATURE_FOLDER="$REPO_NAME_ALPHANUMERIC/$PULL_REQUEST_NUMBER"
   APP_PATH="./build"
+  echo REVIEW_BUCKET_NAME
+  echo REVIEW_FEATURE_FOLDER
   
   # Deploy app project
   aws s3 sync $APP_PATH s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER}/app --delete
