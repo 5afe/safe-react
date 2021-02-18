d0d9376e0f5acb8520cf1ec6a5af7417ea4ef098
fernandomg <fernando.greco@gmail.com>
2021-02-18 20:30:12 -0300

    build an infinite scroll wrapper for transactions based on `InfiniteScroll`

---
+++ src/routes/safe/components/Transactions/GatewayTransactions/TxsInfiniteScroll.tsx
@@ -0,0 +1,32 @@
+import { Loader } from '@gnosis.pm/safe-react-components'
+import React, { ReactElement, ReactNode } from 'react'
+
+import { INFINITE_SCROLL_CONTAINER, InfiniteScroll } from 'src/components/InfiniteScroll'
+import { HorizontallyCentered, ScrollableTransactionsContainer } from './styled'
+
+type TransactionsInfiniteScrollProps = {
+  children: ReactNode
+  next: () => Promise<void>
+  hasMore: boolean
+  isLoading: boolean
+}
+
+export const TxsInfiniteScroll = ({
+  children,
+  next,
+  hasMore,
+  isLoading,
+}: TransactionsInfiniteScrollProps): ReactElement => {
+  return (
+    <InfiniteScroll next={next} hasMore={hasMore}>
+      <ScrollableTransactionsContainer id={INFINITE_SCROLL_CONTAINER}>
+        {children}
+        <HorizontallyCentered isVisible={isLoading}>
+          <Loader size="md" />
+        </HorizontallyCentered>
+      </ScrollableTransactionsContainer>
+    </InfiniteScroll>
+  )
+}
+
+export { InfiniteScrollContext as TxsInfiniteScrollContext } from 'src/components/InfiniteScroll'
