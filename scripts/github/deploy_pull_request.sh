ffbb2021a8f6dc6114308aa4e449e7cfe6cbeee4
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-02-18 18:00:20 +0100

    Rename some strings

--- scripts/github/deploy_pull_request.sh
+++ scripts/github/deploy_pull_request.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-function deploy_pr {
+function deploy_pull_request {
   # Pull request number with "pr" prefix
   PULL_REQUEST_NUMBER="pr$PR_NUMBER"
+
+  # Only alphanumeric characters. Example safe-react -> safereact
   REVIEW_FEATURE_FOLDER="$REPO_NAME_ALPHANUMERIC/$PULL_REQUEST_NUMBER"
+
+  # App build path
   APP_PATH="./build"
   
-  # Deploy app project
+  # Deploy pull request
   aws s3 sync $APP_PATH s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER}/${REACT_APP_NETWORK}/app --delete
 }
 
@@ -14,6 +18,7 @@ function deploy_pr {
 # - Pull requests
 # - Security env variables are available. PR from forks don't have them.
 if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "Running the script"
-    deploy_pr
+    echo "This is the tag:"
+    echo $TRAVIS_TAG
+    deploy_pull_request
 fi
