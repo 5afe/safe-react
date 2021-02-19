fca93e1de057189f9e3e2cabad5d865dad231e33
fernandomg <fernando.greco@gmail.com>
2021-02-19 11:39:10 -0300

    allow to pass config to the InfiniteScroll component
     - also changed default bottom margin so the txs loading starts a bit earlier

--- src/components/InfiniteScroll/index.tsx
+++ src/components/InfiniteScroll/index.tsx
@@ -9,7 +9,7 @@ import React, {
   useEffect,
   useState,
 } from 'react'
-import { useInView } from 'react-intersection-observer'
+import { InViewHookResponse, useInView } from 'react-intersection-observer'
 
 export const INFINITE_SCROLL_CONTAINER = 'infinite-scroll-container'
 
@@ -37,14 +37,16 @@ type InfiniteScrollProps = {
   children: ReactNode
   hasMore: boolean
   next: () => Promise<void>
+  config?: InViewHookResponse
 }
 
-export const InfiniteScroll = ({ children, hasMore, next }: InfiniteScrollProps): ReactElement => {
+export const InfiniteScroll = ({ children, hasMore, next, config }: InfiniteScrollProps): ReactElement => {
   const { ref, inView } = useInView({
     threshold: 0,
     root: document.querySelector(`#${INFINITE_SCROLL_CONTAINER}`),
-    rootMargin: '0px 0px 200px 0px',
+    rootMargin: '0px 0px 80% 0px',
     triggerOnce: true,
+    ...config,
   })
 
   useEffect(() => {
