d4405588e39dba72c8d70654e906651cc7fea55a
Agustín Longoni <agustin.longoni@altoros.com>
2021-02-25 16:28:44 -0300

    fix padding for buttons

--- src/components/Stepper/Controls/index.tsx
+++ src/components/Stepper/Controls/index.tsx
@@ -3,11 +3,11 @@ import * as React from 'react'
 import Button from 'src/components/layout/Button'
 import Col from 'src/components/layout/Col'
 import Row from 'src/components/layout/Row'
-import { boldFont, sm, md } from 'src/theme/variables'
+import { boldFont, sm, lg } from 'src/theme/variables'
 
 const controlStyle = {
   backgroundColor: 'white',
-  padding: md,
+  padding: lg,
   borderRadius: sm,
 }
 
