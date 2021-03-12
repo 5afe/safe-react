d86297d2af23b8c2e0244e9fa85311aac902d4da
Agustín Longoni <agustin.longoni@altoros.com>
2021-03-12 16:09:44 -0300

    add standart address format on Remove Modal and fix styles

--- src/routes/safe/components/Settings/RemoveSafeModal/index.tsx
+++ src/routes/safe/components/Settings/RemoveSafeModal/index.tsx
@@ -1,21 +1,16 @@
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
 import Close from '@material-ui/icons/Close'
-import OpenInNew from '@material-ui/icons/OpenInNew'
-import classNames from 'classnames'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getExplorerInfo } from 'src/config'
+import { EthHashInfo } from '@gnosis.pm/safe-react-components'
 
 import { styles } from './style'
 
-import Identicon from 'src/components/Identicon'
 import Modal from 'src/components/Modal'
 import Block from 'src/components/layout/Block'
 import Button from 'src/components/layout/Button'
-import Col from 'src/components/layout/Col'
 import Hairline from 'src/components/layout/Hairline'
-import Link from 'src/components/layout/Link'
 import Paragraph from 'src/components/layout/Paragraph'
 import Row from 'src/components/layout/Row'
 import {
@@ -23,16 +18,10 @@ import {
   safeNameSelector,
   safeParamAddressFromStateSelector,
 } from 'src/logic/safe/store/selectors'
-import { md, secondary } from 'src/theme/variables'
 import { WELCOME_ADDRESS } from 'src/routes/routes'
 import { removeLocalSafe } from 'src/logic/safe/store/actions/removeLocalSafe'
 import { sameAddress } from 'src/logic/wallets/ethAddresses'
 import { saveDefaultSafe } from 'src/logic/safe/utils'
-
-const openIconStyle = {
-  height: md,
-  color: secondary,
-}
 
 const useStyles = makeStyles(styles)
 
@@ -47,8 +36,8 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
   const safeName = useSelector(safeNameSelector)
   const defaultSafe = useSelector(defaultSafeSelector)
   const dispatch = useDispatch()
-  const explorerInfo = getExplorerInfo(safeAddress)
-  const { url } = explorerInfo()
+  /* const explorerInfo = getExplorerInfo(safeAddress)
+  const { url } = explorerInfo() */
 
   const onRemoveSafeHandler = async () => {
     await dispatch(removeLocalSafe(safeAddress))
@@ -82,7 +71,15 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
       <Hairline />
       <Block className={classes.container}>
         <Row className={classes.owner}>
-          <Col align="center" xs={1}>
+          <EthHashInfo
+            hash={safeAddress}
+            name={safeName}
+            showIdenticon
+            showCopyBtn
+            /* explorerUrl={getExplorerInfo(selectedEntry.address)} */
+          />
+
+          {/* <Col align="center" xs={1}>
             <Identicon address={safeAddress} diameter={32} />
           </Col>
           <Col xs={11}>
@@ -99,11 +96,10 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
                 </Link>
               </Block>
             </Block>
-          </Col>
+          </Col> */}
         </Row>
-        <Hairline />
         <Row className={classes.description}>
-          <Paragraph noMargin>
+          <Paragraph size="lg" noMargin>
             Removing a Safe only removes it from your interface. <b>It does not delete the Safe</b>. You can always add
             it back using the Safe&apos;s address.
           </Paragraph>
@@ -111,7 +107,7 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
       </Block>
       <Hairline />
       <Row align="center" className={classes.buttonRow}>
-        <Button minHeight={42} minWidth={140} onClick={onClose}>
+        <Button minHeight={42} minWidth={140} onClick={onClose} color="secondary">
           Cancel
         </Button>
         <Button
