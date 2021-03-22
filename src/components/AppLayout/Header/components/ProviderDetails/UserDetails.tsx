ea192aeddc2e296c66639b7c2849a35193bffbe8
Agustín Longoni <agustin.longoni@altoros.com>
2021-03-22 18:06:37 -0300

    add Card component to UserDetails

--- src/components/AppLayout/Header/components/ProviderDetails/UserDetails.tsx
+++ src/components/AppLayout/Header/components/ProviderDetails/UserDetails.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Dot from '@material-ui/icons/FiberManualRecord'
 import classNames from 'classnames'
 import * as React from 'react'
-import { EthHashInfo, Identicon } from '@gnosis.pm/safe-react-components'
+import { EthHashInfo, Identicon, Card } from '@gnosis.pm/safe-react-components'
 
 import Spacer from 'src/components/Spacer'
 import Block from 'src/components/layout/Block'
@@ -18,6 +18,7 @@ import { getExplorerInfo } from 'src/config'
 import { KeyRing } from 'src/components/AppLayout/Header/components/KeyRing'
 import { CircleDot } from '../CircleDot'
 import { createStyles } from '@material-ui/core'
+import styled from 'styled-components'
 
 import WalletIcon from '../../assets/wallet.svg'
 
@@ -91,6 +92,9 @@ const styles = createStyles({
   },
 })
 
+const StyledCard = styled(Card)`
+  padding: 0px;
+`
 type Props = {
   connected: boolean
   network: ETHEREUM_NETWORK
@@ -116,7 +120,7 @@ export const UserDetails = ({
   const classes = useStyles()
 
   return (
-    <>
+    <StyledCard>
       <Block className={classes.container}>
         <Row align="center" className={classes.identicon} margin="md">
           {connected ? (
@@ -190,6 +194,6 @@ export const UserDetails = ({
           </Paragraph>
         </Button>
       </Row>
-    </>
+    </StyledCard>
   )
 }
