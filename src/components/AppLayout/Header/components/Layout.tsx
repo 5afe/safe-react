8fcdb34fc58cb12ba0c425e2b3fe5de1f6b13f78
Agustín Longoni <agustin.longoni@altoros.com>
2021-03-22 16:55:41 -0300

    fix header height and remove commented code / border variable (unsused)

--- src/components/AppLayout/Header/components/Layout.tsx
+++ src/components/AppLayout/Header/components/Layout.tsx
@@ -12,7 +12,7 @@ import Spacer from 'src/components/Spacer'
 import Col from 'src/components/layout/Col'
 import Img from 'src/components/layout/Img'
 import Row from 'src/components/layout/Row'
-import { border, headerHeight, md, screenSm, sm } from 'src/theme/variables'
+import { headerHeight, md, screenSm, sm } from 'src/theme/variables'
 import { useStateHandler } from 'src/logic/hooks/useStateHandler'
 
 import SafeLogo from '../assets/gnosis-safe-multisig-logo.svg'
@@ -29,8 +29,6 @@ const styles = () => ({
   summary: {
     alignItems: 'center',
     backgroundColor: 'white',
-    borderBottom: `solid 2px ${border}`,
-    boxShadow: '0 2px 4px 0 rgba(212, 212, 211, 0.59)',
     flexWrap: 'nowrap',
     height: headerHeight,
     position: 'fixed',
