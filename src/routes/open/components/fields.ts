a818b895fbaf3453c7af16830c955e68ff5c06de
fernandomg <fernando.greco@gmail.com>
2021-03-25 13:18:36 -0300

    extract owner order padding to a helper function

--- src/routes/open/components/fields.ts
+++ src/routes/open/components/fields.ts
@@ -1,4 +1,5 @@
 import { LoadFormValues } from 'src/routes/load/container/Load'
+import { padOwnerIndex } from 'src/routes/open/utils/padOwnerIndex'
 import { CreateSafeValues } from 'src/routes/open/utils/safeDataExtractor'
 
 export const FIELD_NAME = 'name'
@@ -7,8 +8,8 @@ export const FIELD_OWNERS = 'owners'
 export const FIELD_SAFE_NAME = 'safeName'
 export const FIELD_CREATION_PROXY_SALT = 'safeCreationSalt'
 
-export const getOwnerNameBy = (index: number): string => `owner${index.toString().padStart(4, '0')}Name`
-export const getOwnerAddressBy = (index: number): string => `owner${index.toString().padStart(4, '0')}Address`
+export const getOwnerNameBy = (index: number): string => `owner${padOwnerIndex(index)}Name`
+export const getOwnerAddressBy = (index: number): string => `owner${padOwnerIndex(index)}Address`
 
 export const getNumOwnersFrom = (values: CreateSafeValues | LoadFormValues): number => {
   const accounts = Object.keys(values)
