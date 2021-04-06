b2c14f5fc021dbc73a7ce05e98a3ea9a4c413ee7
fudongbai <296179868@qq.com>
2021-04-06 21:55:58 +0800

    fix wrong url

--- src/config/networks/mainnet.ts
+++ src/config/networks/mainnet.ts
@@ -8,7 +8,7 @@ const baseConfig: EnvironmentSettings = {
     url: 'https://ethgasstation.info/json/ethgasAPI.json',
     gasParameter: 'average',
   },
-  rpcServiceUrl: 'https://bsc-dataseed.binance.org/',
+  rpcServiceUrl: 'https://bsc-dataseed.binance.org',
   networkExplorerName: 'BscScan',
   networkExplorerUrl: 'https://bscscan.com',
   networkExplorerApiUrl: 'https://api.bscscan.com/api',
