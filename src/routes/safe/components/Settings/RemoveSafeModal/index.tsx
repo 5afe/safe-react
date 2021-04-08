2c1a7cb3d9fba368c11a961d5f5b80a115f3ed52
Agustín Longoni <agustin.longoni@altoros.com>
2021-04-08 14:07:16 -0300

    Remove Safe modal - replace EthHashInfo

--- src/routes/safe/components/Settings/RemoveSafeModal/index.tsx
+++ src/routes/safe/components/Settings/RemoveSafeModal/index.tsx
@@ -1,21 +1,18 @@
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
 import Close from '@material-ui/icons/Close'
-import OpenInNew from '@material-ui/icons/OpenInNew'
-import classNames from 'classnames'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getExplorerInfo } from 'src/config'
 
 import { styles } from './style'
 
-import { Identicon } from '@gnosis.pm/safe-react-components'
+import { EthHashInfo } from '@gnosis.pm/safe-react-components'
 import Modal from 'src/components/Modal'
 import Block from 'src/components/layout/Block'
 import Button from 'src/components/layout/Button'
 import Col from 'src/components/layout/Col'
 import Hairline from 'src/components/layout/Hairline'
-import Link from 'src/components/layout/Link'
 import Paragraph from 'src/components/layout/Paragraph'
 import Row from 'src/components/layout/Row'
 import {
@@ -23,16 +20,10 @@ import {
   safeNameSelector,
   safeParamAddressFromStateSelector,
 } from 'src/logic/safe/store/selectors'
-import { md, secondary } from 'src/theme/variables'
 import { WELCOME_ADDRESS } from 'src/routes/routes'
 import { removeLocalSafe } from 'src/logic/safe/store/actions/removeLocalSafe'
 import { sameAddress } from 'src/logic/wallets/ethAddresses'
 import { saveDefaultSafe } from 'src/logic/safe/utils'
-
-const openIconStyle = {
-  height: md,
-  color: secondary,
-}
 
 const useStyles = makeStyles(styles)
 
@@ -47,8 +38,6 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
   const safeName = useSelector(safeNameSelector)
   const defaultSafe = useSelector(defaultSafeSelector)
   const dispatch = useDispatch()
-  const explorerInfo = getExplorerInfo(safeAddress)
-  const { url } = explorerInfo()
 
   const onRemoveSafeHandler = async () => {
     await dispatch(removeLocalSafe(safeAddress))
@@ -82,28 +71,19 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
       <Hairline />
       <Block className={classes.container}>
         <Row className={classes.owner}>
-          <Col align="center" xs={1}>
-            <Identicon address={safeAddress} size="md" />
-          </Col>
-          <Col xs={11}>
-            <Block className={classNames(classes.name, classes.userName)}>
-              <Paragraph noMargin size="lg" weight="bolder">
-                {safeName}
-              </Paragraph>
-              <Block className={classes.user} justify="center">
-                <Paragraph color="disabled" noMargin size="md">
-                  {safeAddress}
-                </Paragraph>
-                <Link className={classes.open} target="_blank" to={url}>
-                  <OpenInNew style={openIconStyle} />
-                </Link>
-              </Block>
-            </Block>
+          <Col align="center" xs={12}>
+            <EthHashInfo
+              hash={safeAddress}
+              name={safeName}
+              showAvatar
+              showCopyBtn
+              explorerUrl={getExplorerInfo(safeAddress)}
+            />
           </Col>
         </Row>
         <Hairline />
         <Row className={classes.description}>
-          <Paragraph noMargin>
+          <Paragraph noMargin size="lg">
             Removing a Safe only removes it from your interface. <b>It does not delete the Safe</b>. You can always add
             it back using the Safe&apos;s address.
           </Paragraph>
@@ -111,7 +91,7 @@ export const RemoveSafeModal = ({ isOpen, onClose }: RemoveSafeModalProps): Reac
       </Block>
       <Hairline />
       <Row align="center" className={classes.buttonRow}>
-        <Button minHeight={42} minWidth={140} onClick={onClose}>
+        <Button minHeight={42} minWidth={140} onClick={onClose} color="secondary">
           Cancel
         </Button>
         <Button
--- src/routes/safe/components/Settings/RemoveSafeModal/style.ts
+++ src/routes/safe/components/Settings/RemoveSafeModal/style.ts
@@ -1,11 +1,11 @@
 import { createStyles } from '@material-ui/core/styles'
-import { background, error, lg, md, sm } from 'src/theme/variables'
+import { error, lg, md, sm } from 'src/theme/variables'
 
 export const styles = createStyles({
   heading: {
     boxSizing: 'border-box',
     justifyContent: 'space-between',
-    maxHeight: '75px',
+    minHeight: '75px',
     padding: `${sm} ${lg}`,
   },
   container: {
@@ -27,29 +27,11 @@ export const styles = createStyles({
     backgroundColor: error,
     height: '42px',
   },
-  name: {
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  },
-  userName: {
-    whiteSpace: 'nowrap',
-  },
   owner: {
-    backgroundColor: background,
     padding: md,
     alignItems: 'center',
   },
-  user: {
-    justifyContent: 'left',
-  },
   description: {
     padding: md,
-  },
-  open: {
-    paddingLeft: sm,
-    width: 'auto',
-    '&:hover': {
-      cursor: 'pointer',
-    },
   },
 })
