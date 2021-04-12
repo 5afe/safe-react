27c3f99c1ceb9145d85c43f46f375e2a90c6b563
Mati Dastugue <mdastugu@amazon.com>
2021-04-12 11:32:59 -0300

    Reduce height of Assets list

--- src/routes/safe/components/Balances/Coins/index.tsx
+++ src/routes/safe/components/Balances/Coins/index.tsx
@@ -97,7 +97,7 @@ const Coins = (props: Props): React.ReactElement => {
         data={filteredData}
         defaultFixed
         defaultOrderBy={BALANCE_TABLE_ASSET_ID}
-        defaultRowsPerPage={100}
+        defaultRowsPerPage={15}
         label="Balances"
         size={filteredData.size}
       >
