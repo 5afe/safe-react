66786dc5408d30c1496f66f972a3dde738f3dbfd
nicosampler <nf.dominguez.87@gmail.com>
2021-04-16 16:08:41 -0300

    rollback mainnet baseconfig

--- src/config/networks/mainnet.ts
+++ src/config/networks/mainnet.ts
@@ -2,8 +2,7 @@ import EtherLogo from 'src/config/assets/token_eth.svg'
 import { EnvironmentSettings, ETHEREUM_NETWORK, NetworkConfig } from 'src/config/networks/network.d'
 
 const baseConfig: EnvironmentSettings = {
-  //clientGatewayUrl: 'https://safe-client.mainnet.staging.gnosisdev.com/v1',
-  clientGatewayUrl: 'https://safe-client.mainnet.gnosis.io/v1',
+  clientGatewayUrl: 'https://safe-client.mainnet.staging.gnosisdev.com/v1',
   txServiceUrl: 'https://safe-transaction.mainnet.staging.gnosisdev.com/api/v1',
   safeAppsUrl: 'https://safe-apps.dev.gnosisdev.com',
   gasPriceOracle: {
