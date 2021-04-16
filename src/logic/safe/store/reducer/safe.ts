b559f75543340da02782107794f3da463b5e811e
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-04-16 12:45:00 +0200

    Remove unnecessary prop

--- src/logic/safe/store/reducer/safe.ts
+++ src/logic/safe/store/reducer/safe.ts
@@ -25,12 +25,10 @@ export const buildSafe = (storedSafe: SafeRecordProps): SafeRecordProps => {
   const names = storedSafe.owners.map((owner) => owner.name)
   const addresses = storedSafe.owners.map((owner) => checksumAddress(owner.address))
   const owners = buildOwnersFrom(Array.from(names), Array.from(addresses))
-  const balances = storedSafe.balances
 
   return {
     ...storedSafe,
     owners,
-    balances: balances || [],
     latestIncomingTxBlock: 0,
     modules: null,
   }
