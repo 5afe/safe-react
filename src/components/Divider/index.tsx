1accf53c560a0e540916b01e893997b435f1e4fc
Agustín Longoni <agustin.longoni@altoros.com>
2021-04-16 16:10:36 -0300

    add Divider and Icon component (SRC) to Divider (SR)

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Icon, Divider as DividerSRC } from '@gnosis.pm/safe-react-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 4px 0;
+
+  svg {
+    margin: 0 12px 0 4px;
+  }
+`
+const StyledDivider = styled(DividerSRC)`
+  width: 100%;
+`
+
+type Props = {
+  withArrow?: boolean
+}
+
+const Divider = ({ withArrow }: Props) => (
+  <Wrapper>
+    {withArrow && <Icon type="arrowDown" size="md" />}
+    <StyledDivider />
+  </Wrapper>
+)
+
+export default Divider
