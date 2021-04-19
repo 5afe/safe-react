16b37657f04f350b340d41c6817a84acb08b0266
Agustín Longoni <agustin.longoni@altoros.com>
2021-04-19 16:51:16 -0300

    Send collectible (from address book) - replace Divider

--- src/routes/safe/components/Balances/SendModal/screens/SendCollectible/index.tsx
+++ src/routes/safe/components/Balances/SendModal/screens/SendCollectible/index.tsx
@@ -5,6 +5,7 @@ import Close from '@material-ui/icons/Close'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
+import Divider from 'src/components/Divider'
 import CopyBtn from 'src/components/CopyBtn'
 import GnoForm from 'src/components/forms/GnoForm'
 import Identicon from 'src/components/Identicon'
@@ -25,10 +26,7 @@ import { AddressBookInput } from 'src/routes/safe/components/Balances/SendModal/
 import { NFTToken } from 'src/logic/collectibles/sources/collectibles.d'
 import { getExplorerInfo } from 'src/config'
 import { sameAddress } from 'src/logic/wallets/ethAddresses'
-import { sm } from 'src/theme/variables'
 import { sameString } from 'src/utils/strings'
-
-import ArrowDown from 'src/routes/safe/components/Balances/SendModal/screens/assets/arrow-down.svg'
 
 import { CollectibleSelectField } from './CollectibleSelectField'
 import { styles } from './style'
@@ -161,14 +159,7 @@ const SendCollectible = ({
               <WhenFieldChanges field="assetAddress" set="nftTokenId" to={''} />
               <Block className={classes.formContainer}>
                 <SafeInfo />
-                <Row margin="md">
-                  <Col xs={1}>
-                    <img alt="Arrow Down" src={ArrowDown} style={{ marginLeft: sm }} />
-                  </Col>
-                  <Col center="xs" layout="column" xs={11}>
-                    <Hairline />
-                  </Col>
-                </Row>
+                <Divider withArrow />
                 {selectedEntry && selectedEntry.address ? (
                   <div
                     onKeyDown={(e) => {
