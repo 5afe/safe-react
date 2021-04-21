8c23bc183ba8ccd2baed379600ba7c419443b2aa
Agustín Longoni <agustin.longoni@altoros.com>
2021-04-21 19:21:14 -0300

    fix modal heading height on Add new owner modal

--- src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/OwnerForm/style.ts
+++ src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/OwnerForm/style.ts
@@ -6,7 +6,7 @@ export const styles = createStyles({
     padding: `${sm} ${lg}`,
     justifyContent: 'flex-start',
     boxSizing: 'border-box',
-    maxHeight: '75px',
+    height: '74px',
   },
   annotation: {
     color: secondaryText,
