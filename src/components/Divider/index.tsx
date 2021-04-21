59cd96c1f13f4c6e481de82d9ee260c5ffd84e22
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-04-21 18:35:43 +0200

    Fix EsLint warning

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 import { Icon, Divider as DividerSRC } from '@gnosis.pm/safe-react-components'
 
@@ -19,7 +19,7 @@ type Props = {
   withArrow?: boolean
 }
 
-const Divider = ({ withArrow }: Props) => (
+const Divider = ({ withArrow }: Props): ReactElement => (
   <Wrapper>
     {withArrow && <Icon type="arrowDown" size="md" />}
     <StyledDivider />
