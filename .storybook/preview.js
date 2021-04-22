a36232b8049f30a053270078f3229ad9057b2653
katspaugh <katspaugh@gmail.com>
2021-04-22 09:48:57 +0200

    Fix the storybook: add a redux provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { addDecorator } from '@storybook/react'
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { theme } from '@gnosis.pm/safe-react-components'
 
+import { aNewStore } from 'src/store'
 import averta from 'src/assets/fonts/Averta-normal.woff2'
 import avertaBold from 'src/assets/fonts/Averta-ExtraBold.woff2'
 
@@ -20,7 +22,9 @@ addDecorator((storyFn) => (
   <ThemeProvider theme={theme}>
     <MemoryRouter>
       <GlobalStyles />
-      {storyFn()}
+      <Provider store={aNewStore()}>
+        {storyFn()}
+      </Provider>
     </MemoryRouter>
   </ThemeProvider>
 ))
