89110563c31a5edf1cd5f5b65a187c6f46f7ad2f
nicosampler <nf.dominguez.87@gmail.com>
2021-04-30 20:25:33 -0300

    Fix: remove auto chacksum address from InputAddress

--- src/components/forms/validator.ts
+++ src/components/forms/validator.ts
@@ -68,7 +68,7 @@ export const mustBeEthereumAddress = memoize(
     const startsWith0x = address?.startsWith('0x')
     const isAddress = getWeb3().utils.isAddress(address)
 
-    const errorMessage = `Input must be a valid Ethereum address${
+    const errorMessage = `Must be a valid checksummed address${
       isFeatureEnabled(FEATURES.DOMAIN_LOOKUP) ? ', ENS or Unstoppable domain' : ''
     }`
 
