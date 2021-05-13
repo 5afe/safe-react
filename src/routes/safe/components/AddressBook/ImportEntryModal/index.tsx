6f7a2dff9b72271588c2af735ea46318b5cf1942
unknown <juampibermani@gmail.com>
2021-05-13 14:11:52 -0300

    Added entry validations

--- src/routes/safe/components/AddressBook/ImportEntryModal/index.tsx
+++ src/routes/safe/components/AddressBook/ImportEntryModal/index.tsx
@@ -7,35 +7,73 @@ import { Modal } from 'src/components/Modal'
 import Row from 'src/components/layout/Row'
 import { CSVReader } from 'react-papaparse'
 import { AddressBookEntry } from 'src/logic/addressBook/model/addressBook'
+import { checksumAddress } from 'src/utils/checksumAddress'
+import { getWeb3 } from 'src/logic/wallets/getWeb3'
 
 export const IMPORT_ENTRY_BTN_ID = 'import-entry-btn-id'
+const WRONG_FILE_EXTENSION_ERROR = 'Only CSV files are allowed'
+const FILE_SIZE_TOO_BIG = 'The size of the file is over 1mb'
 
 const ImportEntryModalComponent = ({ importEntryModalHandler, isOpen, onClose }) => {
   const [csvLoaded, setCsvLoaded] = useState(false)
+  const [importError, setImportError] = useState('')
   const [addressList, setAddressList] = useState<AddressBookEntry[]>([])
 
   const handleImportEntrySubmit = () => {
     importEntryModalHandler(addressList)
   }
 
-  const handleOnDrop = (data) => {
-    setCsvLoaded(true)
-    const list = data.map((entry) => {
-      return {
-        address: entry.data[0],
-        name: entry.data[1],
+  const handleOnDrop = (data, file) => {
+    if (
+      file.type !== 'application/vnd.ms-excel' &&
+      file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' &&
+      file.type !== '	text/csv'
+    ) {
+      setImportError(WRONG_FILE_EXTENSION_ERROR)
+    } else if (file.size >= 1000000) {
+      setImportError(FILE_SIZE_TOO_BIG)
+    } else {
+      setCsvLoaded(true)
+      const list: { address: string; name: string }[] = []
+      const slicedData = data.slice(1)
+      setImportError('')
+      for (let index = 0; index < slicedData.length; index++) {
+        const entry = slicedData[index]
+        if (entry.data.length !== 2) {
+          console.log(entry.data)
+          setImportError(`Invalid amount of columns on row ${index + 2}`)
+          break
+        } else if (typeof entry.data[0] !== 'string' || typeof entry.data[1] !== 'string') {
+          setImportError(`Invalid entry on row ${index + 2}`)
+          break
+        } else {
+          // Verify address properties
+          const address = entry.data[0].toLowerCase()
+          try {
+            checksumAddress(address)
+          } catch (error) {
+            setImportError(`Invalid address on row ${index + 2}`)
+            break
+          }
+          list.push({
+            address: getWeb3().utils.toChecksumAddress(address),
+            name: entry.data[1],
+          })
+        }
       }
-    })
-    setAddressList(list.slice(1))
+      if (importError === '') {
+        setAddressList(list)
+      }
+    }
   }
 
-  const handleOnError = (err) => {
-    console.log(err)
+  const handleOnError = (error) => {
+    setImportError(error.message)
   }
 
-  const handleOnRemoveFile = (data) => {
+  const handleOnRemoveFile = () => {
     setCsvLoaded(false)
-    console.log(data)
+    setImportError('')
   }
 
   return (
@@ -49,6 +87,7 @@ const ImportEntryModalComponent = ({ importEntryModalHandler, isOpen, onClose })
           <CSVReader onDrop={handleOnDrop} onError={handleOnError} addRemoveButton onRemoveFile={handleOnRemoveFile}>
             <span>Drop CSV file here or click to upload.</span>
           </CSVReader>
+          {importError !== '' && <Text size="xl">{importError}</Text>}
         </Row>
       </Modal.Body>
       <Modal.Footer>
@@ -56,7 +95,7 @@ const ImportEntryModalComponent = ({ importEntryModalHandler, isOpen, onClose })
           cancelButtonProps={{ onClick: () => onClose() }}
           confirmButtonProps={{
             color: 'primary',
-            disabled: !csvLoaded,
+            disabled: !csvLoaded || importError !== '',
             onClick: handleImportEntrySubmit,
             text: 'Import',
           }}
