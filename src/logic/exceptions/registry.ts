ea9ef35c4fef40c64f7659f997da93e6122c3b50
katspaugh <katspaugh@gmail.com>
2021-05-14 10:18:41 +0200

    Add a comment on how to add new errors

--- src/logic/exceptions/registry.ts
+++ src/logic/exceptions/registry.ts
@@ -3,6 +3,12 @@ export interface ExceptionContent {
   uiMessage?: string
 }
 
+/**
+ * When creating a new error type, please try to group them semantically
+ * with the existing errors in the same hundred. For example, if it's
+ * related to fetching data from the backend, add it to the 6xx errors.
+ * This is not a hard requirement, just a useful convention.
+ */
 const registry: Record<number, ExceptionContent> = {
   0: {
     description: 'No such error code',
