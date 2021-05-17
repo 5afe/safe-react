318b69366800ee270f41e16d4cafe025cf757df2
fernandomg <fernando.greco@gmail.com>
2021-05-16 21:06:08 -0300

    update AB model file

--- src/logic/addressBook/model/addressBook.ts
+++ src/logic/addressBook/model/addressBook.ts
@@ -1,16 +1,20 @@
 import { ETHEREUM_NETWORK } from 'src/config/networks/network.d'
 import { getNetworkId } from 'src/config'
 
+export const ADDRESS_BOOK_DEFAULT_NAME = 'UNKNOWN'
+
 export type AddressBookEntry = {
   address: string // the contact address
   name: string // human-readable name
   chainId: ETHEREUM_NETWORK // see https://chainid.network
 }
 
+const networkId = getNetworkId()
+
 export const makeAddressBookEntry = ({
   address = '',
   name = '',
-  chainId = getNetworkId(),
+  chainId = networkId,
 }: {
   address: string
   name?: string
