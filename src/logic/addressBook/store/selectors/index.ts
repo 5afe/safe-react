fbdc83fe91f1bc4e45cf1614d8da7cb24803fc89
fernandomg <fernando.greco@gmail.com>
2021-05-18 21:51:55 -0300

    create AB map-like selector
    chainId -> address -> name

--- src/logic/addressBook/store/selectors/index.ts
+++ src/logic/addressBook/store/selectors/index.ts
@@ -7,6 +7,29 @@ import { Overwrite } from 'src/types/helpers'
 
 export const addressBookSelector = (state: AppReduxState): AppReduxState['addressBook'] => state['addressBook']
 
+type AddressBookMap = {
+  [chainId: number]: {
+    [address: string]: [name: string]
+  }
+}
+
+export const addressBookMapSelector = createSelector(
+  [addressBookSelector],
+  (addressBook): AddressBookMap => {
+    const addressBookMap = {}
+
+    addressBook.forEach(({ address, name, chainId }) => {
+      if (!addressBookMap[chainId]) {
+        addressBookMap[chainId] = { [address]: name }
+      } else {
+        addressBookMap[chainId][address] = name
+      }
+    })
+
+    return addressBookMap
+  },
+)
+
 export const addressBookAddressesListSelector = createSelector([addressBookSelector], (addressBook): string[] =>
   addressBook.map(({ address }) => address),
 )
