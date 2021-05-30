85b1addfde899f142ea2f9c07b12df14d620e34f
fernandomg <fernando.greco@gmail.com>
2021-05-30 14:02:04 -0300

    prevent using `checksumAddress` inside reducer

--- src/logic/addressBook/store/reducer/index.ts
+++ src/logic/addressBook/store/reducer/index.ts
@@ -4,7 +4,6 @@ import { AddressBookEntry, AddressBookState } from 'src/logic/addressBook/model/
 import { ADDRESS_BOOK_ACTIONS } from 'src/logic/addressBook/store/actions'
 import { getEntryIndex, isValidAddressBookName } from 'src/logic/addressBook/utils'
 import { AppReduxState } from 'src/store'
-import { checksumAddress } from 'src/utils/checksumAddress'
 
 export const ADDRESS_BOOK_REDUCER_ID = 'addressBook'
 
@@ -14,15 +13,12 @@ export default handleActions<AppReduxState['addressBook'], Payloads>(
   {
     [ADDRESS_BOOK_ACTIONS.ADD_OR_UPDATE]: (state, action: Action<AddressBookEntry>) => {
       const newState = [...state]
-      const { address, ...rest } = action.payload
+      const addressBookEntry = action.payload
 
-      if (!isValidAddressBookName(rest.name)) {
+      if (!isValidAddressBookName(addressBookEntry.name)) {
         // prevent adding an invalid name
         return newState
       }
-
-      // always checksum the address before storing it
-      const addressBookEntry = { address: checksumAddress(address), ...rest }
 
       const entryIndex = getEntryIndex(newState, addressBookEntry)
 
@@ -56,18 +52,14 @@ export default handleActions<AppReduxState['addressBook'], Payloads>(
         // exclude those entries with invalid name
         .filter(({ name }) => isValidAddressBookName(name))
         .forEach((addressBookEntry) => {
-          const { address, ...rest } = addressBookEntry
-
-          // always checksum the address before storing it
-          const newAddressBookEntry = { address: checksumAddress(address), ...rest }
-          const entryIndex = getEntryIndex(newState, newAddressBookEntry)
+          const entryIndex = getEntryIndex(newState, addressBookEntry)
 
           if (entryIndex >= 0) {
             // update
-            newState[entryIndex] = newAddressBookEntry
+            newState[entryIndex] = addressBookEntry
           } else {
             // add
-            newState.push(newAddressBookEntry)
+            newState.push(addressBookEntry)
           }
         })
 
