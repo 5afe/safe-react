fe55a2de4fb19b55f6d5b4da907cf62599618b12
fernandomg <fernando.greco@gmail.com>
2021-05-31 15:27:09 -0300

    invert check order for `isValidAddress`

--- src/utils/isValidAddress.ts
+++ src/utils/isValidAddress.ts
@@ -3,8 +3,8 @@ import { isAddress, isHexStrict } from 'web3-utils'
 export const isValidAddress = (address?: string): boolean => {
   if (address) {
     // `isAddress` do not require the string to start with `0x`
-    // `isHexStrict` forces the address to start with `0x`
-    return isAddress(address) && isHexStrict(address)
+    // `isHexStrict` ensures the address to start with `0x` aside from being a valid hex string
+    return isHexStrict(address) && isAddress(address)
   }
 
   return false
