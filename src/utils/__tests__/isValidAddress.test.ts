a3d940b7f6d8518dbf9c138f496d2ba1084367fd
fernandomg <fernando.greco@gmail.com>
2021-05-31 10:00:37 -0300

    add cases for invalid addresses

--- src/utils/__tests__/isValidAddress.test.ts
+++ src/utils/__tests__/isValidAddress.test.ts
@@ -1,7 +1,13 @@
 import { isValidAddress } from '../isValidAddress'
 
 describe('isValidAddress', () => {
-  it('Returns false for a non valid address', () => {
+  it('Returns false for an empty string', () => {
+    expect(isValidAddress('')).toBeFalsy()
+  })
+  it('Returns false when address is `undefined`', () => {
+    expect(isValidAddress(undefined)).toBeFalsy()
+  })
+  it('Returns false for `0x123`', () => {
     expect(isValidAddress('0x123')).toBeFalsy()
   })
   it('Returns false for a valid address without `0x` prefix', () => {
