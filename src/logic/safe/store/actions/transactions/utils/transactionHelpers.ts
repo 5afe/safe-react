e9806b0918f4e7fc34603b5f9b24f0bc21858a80
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-06-15 21:09:58 +0200

    Fix missing tests

--- src/logic/safe/store/actions/transactions/utils/transactionHelpers.ts
+++ src/logic/safe/store/actions/transactions/utils/transactionHelpers.ts
@@ -1,37 +1,7 @@
 import { TypedDataUtils } from 'eth-sig-util'
 
-import { EMPTY_DATA } from 'src/logic/wallets/ethTransactions'
-import { Transaction, TxArgs } from 'src/logic/safe/store/models/types/transaction'
-import {
-  BatchProcessTxsProps,
-  TxServiceModel,
-} from 'src/logic/safe/store/actions/transactions/fetchTransactions/loadOutgoingTransactions'
+import { TxArgs } from 'src/logic/safe/store/models/types/transaction'
 import { getEip712MessageTypes, generateTypedDataFrom } from 'src/logic/safe/transactions/offchainSigner/EIP712Signer'
-
-export const isEmptyData = (data?: string | null): boolean => {
-  return !data || data === EMPTY_DATA
-}
-
-export const isMultiSendTransaction = (tx: BuildTx['tx']): boolean => {
-  return !isEmptyData(tx.data) && tx.data?.substring(0, 10) === '0x8d80ff0a' && Number(tx.value) === 0
-}
-
-export const isUpgradeTransaction = (tx: BuildTx['tx']): boolean => {
-  return (
-    !isEmptyData(tx.data) &&
-    isMultiSendTransaction(tx) &&
-    tx.data?.substr(308, 8) === '7de7edef' && // 7de7edef - changeMasterCopy (308, 8)
-    tx.data?.substr(550, 8) === 'f08a0323' // f08a0323 - setFallbackHandler (550, 8)
-  )
-}
-
-export type ServiceTx = TxServiceModel | TxToMock
-
-export type BuildTx = BatchProcessTxsProps & {
-  tx: ServiceTx | Transaction
-}
-
-export type TxToMock = TxArgs & Partial<TxServiceModel>
 
 export const generateSafeTxHash = async (safeAddress: string, safeVersion: string, txArgs: TxArgs): Promise<string> => {
   const typedData = await generateTypedDataFrom({ safeAddress, safeVersion, ...txArgs })
--- src/logic/safe/utils/safeVersion.test.ts
+++ src/logic/safe/utils/safeVersion.test.ts
@@ -1,51 +1,22 @@
-import { getSafeSingletonDeployment } from '@gnosis.pm/safe-deployments'
-import { AbiItem } from 'web3-utils'
-import Web3 from 'web3'
-
-import { GnosisSafe } from 'src/types/contracts/GnosisSafe.d'
 import { checkIfSafeNeedsUpdate } from 'src/logic/safe/utils/safeVersion'
 
 describe('Check safe version', () => {
   it('Calls checkIfSafeNeedUpdate, should return true if the safe version is bellow the target one', async () => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(''))
-    const safeSingletonDeployment = getSafeSingletonDeployment({
-      version: '1.0.0',
-    })
-    const safeInstance = (new web3.eth.Contract(safeSingletonDeployment?.abi as AbiItem[]) as unknown) as GnosisSafe
-
-    // eslint-disable-next-line
-    // @ts-ignore
-    safeInstance.methods.VERSION = () => ({ call: async () => '1.0.0' })
+    const safeVersion = '1.0.0'
     const targetVersion = '1.1.1'
-    const { needUpdate } = await checkIfSafeNeedsUpdate(safeInstance, targetVersion)
+    const { needUpdate } = await checkIfSafeNeedsUpdate(safeVersion, targetVersion)
     expect(needUpdate).toEqual(true)
   })
   it('Calls checkIfSafeNeedUpdate, should return false if the safe version is over the target one', async () => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(''))
-    const safeSingletonDeployment = getSafeSingletonDeployment({
-      version: '1.3.0',
-    })
-    const safeInstance = (new web3.eth.Contract(safeSingletonDeployment?.abi as AbiItem[]) as unknown) as GnosisSafe
-
-    // eslint-disable-next-line
-    // @ts-ignore
-    safeInstance.methods.VERSION = () => ({ call: async () => '1.3.0' })
+    const safeVersion = '1.3.0'
     const targetVersion = '1.1.1'
-    const { needUpdate } = await checkIfSafeNeedsUpdate(safeInstance, targetVersion)
+    const { needUpdate } = await checkIfSafeNeedsUpdate(safeVersion, targetVersion)
     expect(needUpdate).toEqual(false)
   })
   it('Calls checkIfSafeNeedUpdate, should return false if the safe version is equal the target one', async () => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(''))
-    const safeSingletonDeployment = getSafeSingletonDeployment({
-      version: '1.1.1',
-    })
-    const safeInstance = (new web3.eth.Contract(safeSingletonDeployment?.abi as AbiItem[]) as unknown) as GnosisSafe
-
-    // eslint-disable-next-line
-    // @ts-ignore
-    safeInstance.methods.VERSION = () => ({ call: async () => '1.1.1' })
+    const safeVersion = '1.1.1'
     const targetVersion = '1.1.1'
-    const { needUpdate } = await checkIfSafeNeedsUpdate(safeInstance, targetVersion)
+    const { needUpdate } = await checkIfSafeNeedsUpdate(safeVersion, targetVersion)
     expect(needUpdate).toEqual(false)
   })
 })
