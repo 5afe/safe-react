52cfff0a953fc35111175db456543b6b2f99fea0
Agustín Longoni <agustin.longoni@altoros.com>
2021-06-16 06:39:50 -0300

    Add hover state to Icons (#2433)
    * delete border on modal footer
    
    * space between button on Export modal
    
    * add hover state to icon-buttons
    
    * fix buttons QR and Explore
    
    * add hover state and fix margins
    
    * fix hover color
    
    * upgrade SRC dependency
    
    * fix margins on sidebar
    
    * upgrade SRC dependency
    
    * fix size and align circleDropdown
    
    * upgrade yarn.lock

--- src/components/ButtonHelper/index.tsx
+++ src/components/ButtonHelper/index.tsx
@@ -8,9 +8,17 @@ const UnStyledButton = styled.button`
   padding: 0;
   font: inherit;
   cursor: pointer;
-  outline-color: ${({ theme }) => theme.colors.icon};
+  border-radius: 50%;
+  transition: background-color 0.2s ease-in-out;
+  outline-color: transparent;
+  height: 24px;
+  width: 24px;
   display: flex;
+  justify-content: center;
   align-items: center;
+  :hover {
+    background-color: ${({ theme }) => theme.colors.separator};
+  }
 `
 type Props = {
   onClick?: () => void
