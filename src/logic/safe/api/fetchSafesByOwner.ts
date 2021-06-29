f4ec4d9b5a92e74285ecd5b5b7f3eaf7cb7d9dbf
katspaugh <katspaugh@gmail.com>
2021-06-29 14:22:39 +0200

    Fix the endpoint

--- src/logic/safe/api/fetchSafesByOwner.ts
+++ src/logic/safe/api/fetchSafesByOwner.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getTxServiceUrl } from 'src/config'
 
 export const fetchSafesByOwner = async (ownerAddress: string): Promise<string[]> => {
-  const url = `${getTxServiceUrl()}/owners/${ownerAddress}`
+  const url = `${getTxServiceUrl()}/owners/${ownerAddress}/safes`
   const res = await axios.get<{ safes: string[] }>(url)
   return res.data.safes
 }
