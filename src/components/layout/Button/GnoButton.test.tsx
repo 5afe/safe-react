c7d5664f893e52ae2627269c66aeb21dab3b6f1c
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-08-19 14:42:59 +0200

    Added unit tests to GnoButton component

---
+++ src/components/layout/Button/GnoButton.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import GnoButton from './index'
+
+describe('<GnoButton>', () => {
+  it('Should render GnoButton', () => {
+    render(<GnoButton testId="gno-button-id" />)
+
+    const gnoButtonNode = screen.getByTestId('gno-button-id')
+
+    expect(gnoButtonNode).toBeInTheDocument()
+  })
+
+  it('Should trigger onClick event when clicks on the button', () => {
+    const onClickSpy = jest.fn()
+
+    render(<GnoButton testId="gno-button-id" onClick={onClickSpy} />)
+
+    const gnoButtonNode = screen.getByTestId('gno-button-id')
+
+    expect(onClickSpy).not.toHaveBeenCalled()
+
+    fireEvent.click(gnoButtonNode)
+
+    expect(onClickSpy).toHaveBeenCalled()
+  })
+})
