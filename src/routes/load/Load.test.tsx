02939fbf4552b45531fbfcfe29c78570dd91fe19
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-08-24 17:31:05 +0200

    Added not found Safe Address unhappy path to load safe tests

--- src/routes/load/Load.test.tsx
+++ src/routes/load/Load.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 import { getClientGatewayUrl } from 'src/config'
 import { mockedEndpoints } from 'src/setupTests'
 import { renderWithProviders } from 'src/utils/test-utils'
@@ -87,6 +87,44 @@ describe('<Load>', () => {
       const errorTextNode = screen.getByText('Must be a valid address, ENS or Unstoppable domain')
 
       expect(errorTextNode).toBeInTheDocument()
+    })
+
+    it('should show and error if the Safe Address is not found', async () => {
+      // simulating a 404 error by returning a rejected promise
+      mockedEndpoints.getSafeInfo.mockImplementation(() =>
+        Promise.reject({
+          code: 42,
+          message: '',
+        }),
+      )
+
+      const networkId = '4'
+
+      const customState = {
+        providers: {
+          name: 'MetaMask',
+          loaded: true,
+          available: true,
+          account: '0x680cde08860141F9D223cE4E620B10Cd6741037E',
+          network: networkId,
+          smartContractWallet: false,
+          hardwareWallet: false,
+        },
+      }
+
+      const validSafeAddress = '0x680cde08860141F9D223cE4E620B10Cd6741037E'
+
+      renderWithProviders(<Load />, customState)
+
+      const safeAddressInputNode = screen.getByTestId('load-safe-address-field')
+
+      fireEvent.change(safeAddressInputNode, { target: { value: validSafeAddress } })
+
+      await waitFor(() => {
+        const errorTextNode = screen.getByText('Address given is not a valid Safe address')
+
+        expect(errorTextNode).toBeInTheDocument()
+      })
     })
   })
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto'
 import '@testing-library/jest-dom/extend-expect'
+import * as sdkGatewayEndpoints from '@gnosis.pm/safe-react-gateway-sdk'
 import { mockGetSafeInfoResponse } from './logic/safe/utils/mocks/getSafeMock'
 
 function mockedGetRandomValues(buf) {
@@ -31,28 +32,29 @@ jest.mock('bnc-onboard', () =>
   })),
 )
 
-export let mockedEndpoints = {
+jest.mock('@gnosis.pm/safe-react-gateway-sdk', () => ({
+  __esModule: true,
+  Operation: jest.fn(),
+  TokenType: jest.fn(),
+  TransactionStatus: jest.fn(),
+  TransferDirection: jest.fn(),
+  getBalances: jest.fn(),
+  getCollectibles: jest.fn(),
+  getFiatCurrencies: jest.fn(),
   getSafeInfo: jest.fn(),
+  getTransactionDetails: jest.fn(),
+  getTransactionHistory: jest.fn(),
+  getTransactionQueue: jest.fn(),
+  postTransaction: jest.fn(),
+}))
+
+export let mockedEndpoints = {}
+
+function mockAllEndpointsByDefault() {
+  mockedEndpoints.getSafeInfo = sdkGatewayEndpoints.getSafeInfo.mockImplementation(
+    () => new Promise((resolve) => resolve(mockGetSafeInfoResponse)),
+  )
 }
-
-jest.mock('@gnosis.pm/safe-react-gateway-sdk', () => {
-  mockedEndpoints.getSafeInfo = jest.fn(() => new Promise((resolve) => resolve(mockGetSafeInfoResponse)))
-
-  return {
-    Operation: jest.fn(),
-    TokenType: jest.fn(),
-    TransactionStatus: jest.fn(),
-    TransferDirection: jest.fn(),
-    getBalances: jest.fn(),
-    getCollectibles: jest.fn(),
-    getFiatCurrencies: jest.fn(),
-    getSafeInfo: mockedEndpoints.getSafeInfo,
-    getTransactionDetails: jest.fn(),
-    getTransactionHistory: jest.fn(),
-    getTransactionQueue: jest.fn(),
-    postTransaction: jest.fn(),
-  }
-})
 
 // to avoid failing tests in some environments
 const NumberFormat = Intl.NumberFormat
@@ -88,6 +90,7 @@ afterEach(() => {
 })
 
 beforeEach(() => {
+  mockAllEndpointsByDefault()
   delete window.location
   window.location = new URL('http://localhost/') // Restore default location
 })
