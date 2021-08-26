a215cbdfdbab7b727361f97529c20ee173e149be
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-08-26 11:10:13 +0200

    Remove unused selectors

--- src/logic/config/store/selectors/index.ts
+++ src/logic/config/store/selectors/index.ts
@@ -2,20 +2,11 @@ import { createSelector } from 'reselect'
 import { ETHEREUM_NETWORK } from 'src/config/networks/network.d'
 
 import { AppReduxState } from 'src/store'
-import { NativeCurrency } from 'src/logic/config/model/networkConfig'
 
 export const networkConfigState = (state: AppReduxState): AppReduxState['networkConfig'] => state['networkConfig']
 
 export const currentChainId = createSelector([networkConfigState], (networkConfig): ETHEREUM_NETWORK => {
   return networkConfig.chainId
-})
-
-export const currentChainName = createSelector([networkConfigState], (networkConfig): string => {
-  return networkConfig.chainName
-})
-
-export const currentNativeCurrency = createSelector([networkConfigState], (networkConfig): NativeCurrency => {
-  return networkConfig.nativeCurrency
 })
 
 export const theme = createSelector([networkConfigState], (networkConfig): AppReduxState['networkConfig']['theme'] => {
