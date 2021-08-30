2d5d60a3c7933192776600e0dbd62cc36225c335
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-08-30 11:54:07 +0200

    Added new stepper component with context api

--- src/routes/new-load/steps/LoadSafeOwnersStep.tsx
+++ src/routes/new-load/steps/LoadSafeOwnersStep.tsx
@@ -1,6 +1,15 @@
 import React, { ReactElement } from 'react'
+import { useForm } from 'react-final-form'
+import { useStepper } from 'src/components/NewStepper/stepperContext'
 
 function LoadSafeOwnersStep(): ReactElement {
+  const loadSafeForm = useForm()
+  const loadSafeStepper = useStepper()
+
+  const formValues = loadSafeForm.getState().values
+
+  console.log('FORM VALUES !', formValues)
+  console.log('STEPPER VALUES', loadSafeStepper)
   return <div>Second step: LoadSafeOwnersStep</div>
 }
 
