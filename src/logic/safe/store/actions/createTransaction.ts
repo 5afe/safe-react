08a6331e71a337ae1e357f00a027fc73235869d0
katspaugh <katspaugh@gmail.com>
2021-08-31 11:15:44 +0200

    Fix a lint warning

--- src/logic/safe/store/actions/createTransaction.ts
+++ src/logic/safe/store/actions/createTransaction.ts
@@ -55,7 +55,7 @@ type ErrorEventHandler = () => void
 
 export const METAMASK_REJECT_CONFIRM_TX_ERROR_CODE = 4001
 
-export const isKeystoneError = (err: Error) => {
+export const isKeystoneError = (err: Error): boolean => {
   return err.message.startsWith('#ktek_error')
 }
 
