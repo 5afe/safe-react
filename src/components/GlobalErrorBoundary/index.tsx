e365c8f15803cef36945baf00343db8c266a8b3c
Aaron Cook <aaron.cook@gnosis.pm>
2021-09-02 12:59:17 +0200

    Fix React import

--- src/components/GlobalErrorBoundary/index.tsx
+++ src/components/GlobalErrorBoundary/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 import { Text, Link, Icon, FixedIcon, Title } from '@gnosis.pm/safe-react-components'
 import { IS_PRODUCTION } from 'src/utils/constants'
@@ -61,58 +62,58 @@ const GlobalErrorBoundaryFallback: FallbackRender = ({ error, componentStack }) 
     window.location.reload()
 
     return null
-  } else {
-    localStorage.removeItem(lastReloadKey)
-
-    return (
-      <Wrapper>
-        <Content>
-          <Title size="md">Something went wrong, please try again.</Title>
-          <FixedIcon type="networkError" />
-          {IS_PRODUCTION && (
-            <div>
-              <Text size="xl" as="span">
-                In case the problem persists, please reach out to us via{' '}
-              </Text>
-              <LinkWrapper>
-                <a target="_blank" href="email: mailto:safe@gnosis.io" rel="noopener noreferrer">
-                  <Text color="primary" size="lg" as="span">
-                    Email
-                  </Text>
-                </a>
-                <Icon type="externalLink" color="primary" size="sm" />
-              </LinkWrapper>
-              or{' '}
-              <LinkWrapper>
-                <a target="_blank" href="https://discordapp.com/invite/FPMRAwK" rel="noopener noreferrer">
-                  <Text color="primary" size="lg" as="span">
-                    Discord
-                  </Text>
-                </a>
-                <Icon type="externalLink" color="primary" size="sm" />
-              </LinkWrapper>
-            </div>
-          )}
-          {!IS_PRODUCTION && (
-            <>
-              <Text size="xl" color="error">
-                {error.toString()}
-              </Text>
-              <Text size="md" color="error">
-                {componentStack}
-              </Text>
-            </>
-          )}
-          <Link size="lg" color="primary" href="/app/">
-            <LinkContent>
-              <Icon size="md" type="home" color="primary" />
-              Go to Home
-            </LinkContent>
-          </Link>
-        </Content>
-      </Wrapper>
-    )
   }
+
+  localStorage.removeItem(lastReloadKey)
+
+  return (
+    <Wrapper>
+      <Content>
+        <Title size="md">Something went wrong, please try again.</Title>
+        <FixedIcon type="networkError" />
+        {IS_PRODUCTION && (
+          <div>
+            <Text size="xl" as="span">
+              In case the problem persists, please reach out to us via{' '}
+            </Text>
+            <LinkWrapper>
+              <a target="_blank" href="email: mailto:safe@gnosis.io" rel="noopener noreferrer">
+                <Text color="primary" size="lg" as="span">
+                  Email
+                </Text>
+              </a>
+              <Icon type="externalLink" color="primary" size="sm" />
+            </LinkWrapper>
+            or{' '}
+            <LinkWrapper>
+              <a target="_blank" href="https://discordapp.com/invite/FPMRAwK" rel="noopener noreferrer">
+                <Text color="primary" size="lg" as="span">
+                  Discord
+                </Text>
+              </a>
+              <Icon type="externalLink" color="primary" size="sm" />
+            </LinkWrapper>
+          </div>
+        )}
+        {!IS_PRODUCTION && (
+          <>
+            <Text size="xl" color="error">
+              {error.toString()}
+            </Text>
+            <Text size="md" color="error">
+              {componentStack}
+            </Text>
+          </>
+        )}
+        <Link size="lg" color="primary" href="/app/">
+          <LinkContent>
+            <Icon size="md" type="home" color="primary" />
+            Go to Home
+          </LinkContent>
+        </Link>
+      </Content>
+    </Wrapper>
+  )
 }
 
 export default GlobalErrorBoundaryFallback
