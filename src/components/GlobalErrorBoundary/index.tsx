9cb31eee8d2c0d7bfc7fa9127b574461d7e75bf9
Aaron Cook <aaron.cook@gnosis.pm>
2021-09-02 12:52:57 +0200

    Fix ESLint

--- src/components/GlobalErrorBoundary/index.tsx
+++ src/components/GlobalErrorBoundary/index.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useLayoutEffect } from 'react'
 import styled from 'styled-components'
 import { Text, Link, Icon, FixedIcon, Title } from '@gnosis.pm/safe-react-components'
-
 import { IS_PRODUCTION } from 'src/utils/constants'
-import { ErrorBoundaryProps, FallbackRender } from '@sentry/react/dist/errorboundary'
+import { FallbackRender } from '@sentry/react/dist/errorboundary'
 
 const Wrapper = styled.div`
   width: 100%;
@@ -44,76 +42,77 @@ const LinkContent = styled.div`
   }
 `
 
-const GlobalErrorBoundaryFallback = ({ error, componentStack }: any) => {
+const GlobalErrorBoundaryFallback: FallbackRender = ({ error, componentStack }) => {
   //  When loading app during release, chunk load failure may occur
   const chunkFailedMessage = /Loading chunk [\d]+ failed/
   const isChunkError = error?.message && chunkFailedMessage.test(error.message)
 
   const time = new Date().getTime()
-  const key = 'lastReload'
-  const lastReloadString = localStorage.getItem(key)
+
+  const lastReloadKey = 'lastReload'
+  const lastReloadString = localStorage.getItem(lastReloadKey)
   const lastReload = lastReloadString && JSON.parse(lastReloadString)
 
   // Expires 10 secs after last chunk failure reload
   const isExpired = time > +lastReload + 10000
 
   if (isChunkError && isExpired) {
-    localStorage.setItem(key, JSON.stringify(time))
+    localStorage.setItem(lastReloadKey, JSON.stringify(time))
     window.location.reload()
 
     return null
   } else {
-    localStorage.removeItem(key)
-  }
+    localStorage.removeItem(lastReloadKey)
 
-  return (
-    <Wrapper>
-      <Content>
-        <Title size="md">Something went wrong, please try again.</Title>
-        <FixedIcon type="networkError" />
-        {IS_PRODUCTION && (
-          <div>
-            <Text size="xl" as="span">
-              In case the problem persists, please reach out to us via{' '}
-            </Text>
-            <LinkWrapper>
-              <a target="_blank" href="email: mailto:safe@gnosis.io" rel="noopener noreferrer">
-                <Text color="primary" size="lg" as="span">
-                  Email
-                </Text>
-              </a>
-              <Icon type="externalLink" color="primary" size="sm" />
-            </LinkWrapper>
-            or{' '}
-            <LinkWrapper>
-              <a target="_blank" href="https://discordapp.com/invite/FPMRAwK" rel="noopener noreferrer">
-                <Text color="primary" size="lg" as="span">
-                  Discord
-                </Text>
-              </a>
-              <Icon type="externalLink" color="primary" size="sm" />
-            </LinkWrapper>
-          </div>
-        )}
-        {!IS_PRODUCTION && (
-          <>
-            <Text size="xl" color="error">
-              {error.toString()}
-            </Text>
-            <Text size="md" color="error">
-              {componentStack}
-            </Text>
-          </>
-        )}
-        <Link size="lg" color="primary" href="/app/">
-          <LinkContent>
-            <Icon size="md" type="home" color="primary" />
-            Go to Home
-          </LinkContent>
-        </Link>
-      </Content>
-    </Wrapper>
-  )
+    return (
+      <Wrapper>
+        <Content>
+          <Title size="md">Something went wrong, please try again.</Title>
+          <FixedIcon type="networkError" />
+          {IS_PRODUCTION && (
+            <div>
+              <Text size="xl" as="span">
+                In case the problem persists, please reach out to us via{' '}
+              </Text>
+              <LinkWrapper>
+                <a target="_blank" href="email: mailto:safe@gnosis.io" rel="noopener noreferrer">
+                  <Text color="primary" size="lg" as="span">
+                    Email
+                  </Text>
+                </a>
+                <Icon type="externalLink" color="primary" size="sm" />
+              </LinkWrapper>
+              or{' '}
+              <LinkWrapper>
+                <a target="_blank" href="https://discordapp.com/invite/FPMRAwK" rel="noopener noreferrer">
+                  <Text color="primary" size="lg" as="span">
+                    Discord
+                  </Text>
+                </a>
+                <Icon type="externalLink" color="primary" size="sm" />
+              </LinkWrapper>
+            </div>
+          )}
+          {!IS_PRODUCTION && (
+            <>
+              <Text size="xl" color="error">
+                {error.toString()}
+              </Text>
+              <Text size="md" color="error">
+                {componentStack}
+              </Text>
+            </>
+          )}
+          <Link size="lg" color="primary" href="/app/">
+            <LinkContent>
+              <Icon size="md" type="home" color="primary" />
+              Go to Home
+            </LinkContent>
+          </Link>
+        </Content>
+      </Wrapper>
+    )
+  }
 }
 
 export default GlobalErrorBoundaryFallback
--- src/utils/wrapInSuspense.tsx
+++ src/utils/wrapInSuspense.tsx
@@ -1,5 +1,6 @@
 import React, { SuspenseProps } from 'react'
 
-export const wrapInSuspense = (component: Required<SuspenseProps['children']>, fallback: SuspenseProps['fallback'] = null) => (
-  <React.Suspense fallback={fallback}>{component}</React.Suspense>
-)
+export const wrapInSuspense = (
+  component: Required<SuspenseProps['children']>,
+  fallback: SuspenseProps['fallback'] = null,
+) => <React.Suspense fallback={fallback}>{component}</React.Suspense>
