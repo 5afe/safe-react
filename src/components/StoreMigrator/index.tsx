345d4f8c495f506e6a8bcf18c14368f7b5e3e5a9
juampibermani <juampibermani@gmail.com>
2021-09-06 14:01:17 -0300

    Migration from unified app to network apps

--- src/components/StoreMigrator/index.tsx
+++ src/components/StoreMigrator/index.tsx
@@ -1,72 +1,78 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getNetworks } from 'src/config'
+import { AddressBookEntry, makeAddressBookEntry } from 'src/logic/addressBook/model/addressBook'
+import { addressBookAddOrUpdate } from 'src/logic/addressBook/store/actions'
+import { logError, Errors } from 'src/logic/exceptions/CodedException'
 import { MIGRATION_ADDRESS } from 'src/routes/routes'
-import { saveToStorage } from 'src/utils/storage'
 
-const MIGRATION_PATH_BASE_URL = 'http://localhost:3001/#'
-const MIGRATION_PATH = `${MIGRATION_PATH_BASE_URL}${MIGRATION_ADDRESS}`
-const APPS_STORAGE_MIGRATION_DONE = 'APPS_STORAGE_MIGRATION_DONE'
+const mergeAddressBooks = (addressBookEntries: AddressBookEntry[], dispatch) => {
+  addressBookEntries.forEach((addressBookEntry) => {
+    // Save addressBookEntries
+    dispatch(addressBookAddOrUpdate(makeAddressBookEntry(addressBookEntry)))
+  })
+}
 
 const StoreMigrator: React.FC = () => {
-  const [migrationDone, setMigrationDone] = useState(false)
-  const [checkMigrationDone] = useState(false)
-  const [loaded, setLoaded] = useState(false)
+  const [currentNetwork, setCurrentNetwork] = useState(0)
+  const dispatch = useDispatch()
+  const networks = getNetworks()
 
-  // Redirect if migration already happened -- Commented until we expose the unified app
-  /*
+  // Recieve the data to be migrated and save it into the localstorage
   useEffect(() => {
-    const loadStorageMigrationDone = async () => {
-      const storageMigrationDone = await loadFromStorage(APPS_STORAGE_MIGRATION_DONE)
-      if (storageMigrationDone) {
-        window.location.href = MIGRATION_PATH_BASE_URL
-        setCheckMigrationDone(true)
-      }
-    }
-
-    loadStorageMigrationDone()
-  }, [])
-  */
-
-  // Migrate local storage
-  useEffect(() => {
-    const migrateLocalStorage = (event) => {
-      if (event.data.migrateReady) {
-        const migrationIframe = (document.getElementById('targetWindow') as any).contentWindow
-        if (migrationIframe) {
-          migrationIframe.postMessage(
-            {
-              migrate: true,
-              payload: JSON.stringify(localStorage),
-            },
-            MIGRATION_PATH_BASE_URL, // TODO Replace with target origin
-          )
+    const saveEventData = (event) => {
+      const isTrustedOrigin = networks.some((network) => {
+        return network.safeUrl.includes(event.origin)
+      })
+      const isRightOrigin = event.origin !== self.origin && isTrustedOrigin
+      if (event.data.migrate && isRightOrigin) {
+        try {
+          const payload = JSON.parse(event.data.payload)
+          Object.keys(payload).forEach((key) => {
+            const payloadEntry = JSON.parse(payload[key])
+            if (key === 'SAFE__addressBook') {
+              mergeAddressBooks(payloadEntry, dispatch)
+            } else if (key.startsWith('_immortal|v2_')) {
+              // Save entry in localStorage
+              localStorage.setItem(key, payloadEntry)
+            }
+          })
+          const migrationIframe = (document.getElementById('targetWindow') as HTMLIFrameElement).contentWindow
+          if (migrationIframe) {
+            migrationIframe.postMessage(
+              {
+                migrated: true,
+              },
+              networks[currentNetwork].safeUrl,
+            )
+          }
+          setCurrentNetwork(currentNetwork + 1)
+        } catch (error) {
+          logError(Errors._612, error.message)
         }
       }
-      if (event.data.migrateDone) {
-        saveToStorage(APPS_STORAGE_MIGRATION_DONE, true)
-        setMigrationDone(true)
+      if (event.data.migrated && isRightOrigin) {
+        setCurrentNetwork(currentNetwork + 1)
       }
     }
 
-    window.addEventListener('message', migrateLocalStorage, false)
+    window.addEventListener('message', saveEventData, false)
+    return window.removeEventListener('message', saveEventData, false)
+  }, [currentNetwork, dispatch, networks])
 
-    return window.removeEventListener('message', migrateLocalStorage)
-  }, [])
-
-  // Load migration target
+  const isSingleNetworkApp = networks.some((network) => {
+    return network.safeUrl.includes(self.origin)
+  })
+  // Migrate local storage
   useEffect(() => {
-    if (!loaded && checkMigrationDone && !MIGRATION_PATH_BASE_URL.includes(self.origin)) {
-      const migrationIframe = window.open(MIGRATION_PATH, 'targetWindow')
-      if (migrationIframe) {
-        setLoaded(true)
-      }
+    if (!isSingleNetworkApp && currentNetwork < networks.length) {
+      const urlToMigrate = `${networks[currentNetwork].safeUrl}${MIGRATION_ADDRESS}`
+      window.open(urlToMigrate, 'targetWindow')
     }
-  }, [loaded, checkMigrationDone])
+  }, [currentNetwork, isSingleNetworkApp, networks])
+
   return (
-    <div>
-      {!migrationDone && !MIGRATION_PATH_BASE_URL.includes(self.origin) && (
-        <iframe width="0px" height="0px" name="targetWindow" id="targetWindow"></iframe>
-      )}
-    </div>
+    <div>{!isSingleNetworkApp && <iframe width="0px" height="0px" name="targetWindow" id="targetWindow"></iframe>}</div>
   )
 }
 
--- src/routes/migration/container/index.tsx
+++ src/routes/migration/container/index.tsx
@@ -1,67 +1,49 @@
 import React, { useEffect } from 'react'
-import { AddressBookEntry, makeAddressBookEntry } from 'src/logic/addressBook/model/addressBook'
-import { addressBookAddOrUpdate } from 'src/logic/addressBook/store/actions'
-import { useDispatch } from 'react-redux'
-import { Errors, logError } from 'src/logic/exceptions/CodedException'
-// import { getNetworks } from 'src/config'
+import { loadFromStorage, saveToStorage } from 'src/utils/storage'
 
-const mergeAddressBooks = (addressBookEntries: AddressBookEntry[], dispatch) => {
-  addressBookEntries.forEach((addressBookEntry) => {
-    // Save addressBookEntries
-    dispatch(addressBookAddOrUpdate(makeAddressBookEntry(addressBookEntry)))
-  })
+const APPS_STORAGE_MIGRATION_DONE = 'APPS_STORAGE_MIGRATION_DONE'
+
+type MigrationMessage = {
+  migrate: boolean
+  payload: string
+}
+
+type MigratedMessage = {
+  migrated: boolean
 }
 
 const MigrationScreen: React.FC = () => {
-  const dispatch = useDispatch()
-
-  // Recieve the data to be migrated and save it into the localstorage
   useEffect(() => {
-    const saveEventData = (event) => {
-      // Uncomment once we send it to prod
-      /*const networks = getNetworks()
-        const isTrustedOrigin = networks.some((network) => {
-          return network.safeUrl.includes(event.origin)
-        })
-        */
-      // Also add isTrustedOrigin to the if clause
-      if (event.data.migrate && event.origin !== self.origin) {
-        try {
-          const payload = JSON.parse(event.data.payload)
-          Object.keys(payload).forEach((key) => {
-            const payloadEntry = JSON.parse(payload[key])
-            if (key === 'SAFE__addressBook') {
-              mergeAddressBooks(payloadEntry, dispatch)
-            } else if (key.startsWith('_immortal|v2_')) {
-              // Save entry in localStorage
-              localStorage.setItem(key, payloadEntry)
-            }
-          })
-          // Commented until we expose the unified app
-          /*window.parent.postMessage(
-            {
-              migrateDone: true,
-            },
-            event.origin,
-          )*/
-        } catch (error) {
-          logError(Errors._612, error.message)
+    const loadStorageMigrationDone = async () => {
+      const storageMigrationDone = await loadFromStorage(APPS_STORAGE_MIGRATION_DONE)
+      if (storageMigrationDone) {
+        const message: MigratedMessage = {
+          migrated: true,
         }
+        window.parent.postMessage(message, parent.origin)
       }
     }
 
+    loadStorageMigrationDone()
+  }, [])
+
+  useEffect(() => {
+    const saveEventData = (event) => {
+      if (event.data.migrated && event.origin === parent.origin) {
+        saveToStorage(APPS_STORAGE_MIGRATION_DONE, true)
+      }
+    }
     window.addEventListener('message', saveEventData, false)
     return window.removeEventListener('message', saveEventData, false)
-  }, [dispatch])
+  }, [])
 
   // Once the app loads completely we ping the parent to start the migration
   useEffect(() => {
-    window.parent.postMessage(
-      {
-        migrateReady: true,
-      },
-      '*',
-    )
+    const message: MigrationMessage = {
+      migrate: true,
+      payload: JSON.stringify(localStorage),
+    }
+    window.parent.postMessage(message, parent.origin)
   }, [])
   return <div>Migrator</div>
 }
