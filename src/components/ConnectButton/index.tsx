1c8c852af102da13f61b49134b7652dd23ed9ef8
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-09-09 17:54:50 +0200

    restored the check if web3 has been instantiated

--- src/components/ConnectButton/index.tsx
+++ src/components/ConnectButton/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import Button from 'src/components/layout/Button'
 import { getNetworkId } from 'src/config'
+import { getWeb3 } from 'src/logic/wallets/getWeb3'
 import onboard from 'src/logic/wallets/onboard'
 import { shouldSwitchNetwork, switchNetwork } from 'src/logic/wallets/utils/network'
 
@@ -24,7 +25,8 @@ export const onboardUser = async (): Promise<boolean> => {
   // before calling walletSelect you want to check if web3 has been instantiated
   // which indicates that a wallet has already been selected
   // and web3 has been instantiated with that provider
-  const walletSelected = await onboard().walletSelect()
+  const web3 = getWeb3()
+  const walletSelected = web3 ? true : await onboard().walletSelect()
   return walletSelected && checkWallet()
 }
 
