78e3681eb9fc8e08fa61766e8ada5297029d8e40
juampibermani <juampibermani@gmail.com>
2021-09-14 15:15:47 -0300

    Target.origin updated for postMessage

--- src/routes/migration/container/index.tsx
+++ src/routes/migration/container/index.tsx
@@ -16,7 +16,7 @@ const MigrationScreen: React.FC = () => {
         migrate: true,
         payload: JSON.stringify(payload),
       }
-      window.parent.postMessage(message, '*')
+      window.parent.postMessage(message, 'https://pr2695--safereact.review.gnosisdev.com')
     }
 
     loadStorageMigrationDone()
