729e8842a837a5c8f8bc12dde05cf2d1fec445f6
Mikhail <mmvsha73@gmail.com>
2021-09-15 18:34:07 +0200

    rough test cases

---
+++ src/routes/safe/components/Apps/components/AppList.test.tsx
@@ -0,0 +1,11 @@
+import AppList from './AppList'
+
+describe('Safe Apps -> AppList -> Search', () => {
+  it('Shows apps matching the search query', () => {})
+
+  it('Shows app matching the name first for a query that matches in name and description of multiple apps', () => {})
+
+  it('Shows "no apps found" message when not able to find apps matching the query', () => {})
+
+  it('Clears the search result when you press on clear button and shows all apps again', () => {})
+})
