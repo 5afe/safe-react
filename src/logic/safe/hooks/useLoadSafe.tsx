f27f3ce7be4fe59cd874cdb76b9d1d772da37bed
juampibermani <30930241+juampibermani@users.noreply.github.com>
2021-09-21 12:38:56 -0300

    Fetch selectedCurrency on safeload (#2742)

--- src/logic/safe/hooks/useLoadSafe.tsx
+++ src/logic/safe/hooks/useLoadSafe.tsx
@@ -7,6 +7,7 @@ import { fetchSafe } from 'src/logic/safe/store/actions/fetchSafe'
 import fetchTransactions from 'src/logic/safe/store/actions/transactions/fetchTransactions'
 import { Dispatch } from 'src/logic/safe/store/actions/types.d'
 import { updateAvailableCurrencies } from 'src/logic/currencyValues/store/actions/updateAvailableCurrencies'
+import { fetchSelectedCurrency } from 'src/logic/currencyValues/store/actions/fetchSelectedCurrency'
 
 export const useLoadSafe = (safeAddress?: string): boolean => {
   const dispatch = useDispatch<Dispatch>()
@@ -15,6 +16,7 @@ export const useLoadSafe = (safeAddress?: string): boolean => {
   useEffect(() => {
     const fetchData = async () => {
       if (safeAddress) {
+        await dispatch(fetchSelectedCurrency())
         await dispatch(fetchLatestMasterContractVersion())
         await dispatch(fetchSafe(safeAddress))
         setIsSafeLoaded(true)
