3301e0bc79ee3948ffb0565283da93961657169e
iamacook <aaron.cook@gnosis.pm>
2021-09-23 09:27:13 +0200

    Fix effect loop

--- src/logic/safe/hooks/useLoadSafe.tsx
+++ src/logic/safe/hooks/useLoadSafe.tsx
@@ -15,7 +15,6 @@ export const useLoadSafe = (safeAddress?: string): boolean => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsSafeLoaded(false)
       if (safeAddress) {
         await dispatch(fetchSelectedCurrency())
         await dispatch(fetchLatestMasterContractVersion())
