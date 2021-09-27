697051644a0e5a1024eab39a0c196a2570acc3ec
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-09-27 15:04:07 +0200

    Added new design on Welcome page cards

--- src/routes/welcome/Welcome.tsx
+++ src/routes/welcome/Welcome.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import { Button, Card, Title, Text } from '@gnosis.pm/safe-react-components'
+import Divider from '@material-ui/core/Divider'
 import styled from 'styled-components'
 
 import Page from 'src/components/layout/Page'
@@ -18,45 +19,48 @@ function Welcome(): ReactElement {
           Gnosis Safe is the most trusted platform to manage digital assets. <br /> Here is how to get started:
         </Title>
         <CardsContainer>
-          {/* Create Safe Card */}
           <StyledCard>
-            <Title size="sm" strong withoutMargin>
-              Create Safe
-            </Title>
-            <CardDescriptionContainer>
-              <Text size="xl">Create a new Safe Multisig that is controlled by one or multiple owners.</Text>
-              <Text size="xl">You will be required to pay a network fee for creating your new Safe.</Text>
-            </CardDescriptionContainer>
-            <Button size="lg" color="primary" variant="contained" component={Link} to={OPEN_ADDRESS}>
-              <Text size="xl" color="white">
-                + Create new Safe
-              </Text>
-            </Button>
-          </StyledCard>
-          {/* Load Safe Card */}
-          <StyledCard>
-            <Title size="sm" strong withoutMargin>
-              Load Existing Safe
-            </Title>
-            <CardDescriptionContainer>
-              <Text size="xl">
-                Already have a Safe or want to access it from a different device? Easily load your Safe MultiSig using
-                your Safe address.
-              </Text>
-            </CardDescriptionContainer>
-            <Button
-              variant="bordered"
-              iconType="safe"
-              iconSize="sm"
-              size="lg"
-              color="secondary"
-              component={Link}
-              to={LOAD_ADDRESS}
-            >
-              <Text size="xl" color="secondary">
-                Add existing Safe
-              </Text>
-            </Button>
+            {/* Create Safe */}
+            <CardContentContainer>
+              <Title size="sm" strong withoutMargin>
+                Create Safe
+              </Title>
+              <CardDescriptionContainer>
+                <Text size="xl">Create a new Safe Multisig that is controlled by one or multiple owners.</Text>
+                <Text size="xl">You will be required to pay a network fee for creating your new Safe.</Text>
+              </CardDescriptionContainer>
+              <Button size="lg" color="primary" variant="contained" component={Link} to={OPEN_ADDRESS}>
+                <Text size="xl" color="white">
+                  + Create new Safe
+                </Text>
+              </Button>
+            </CardContentContainer>
+            <Divider orientation="vertical" flexItem />
+            <CardContentContainer>
+              {/* Load Safe */}
+              <Title size="sm" strong withoutMargin>
+                Load Existing Safe
+              </Title>
+              <CardDescriptionContainer>
+                <Text size="xl">
+                  Already have a Safe or want to access it from a different device? Easily load your Safe MultiSig using
+                  your Safe address.
+                </Text>
+              </CardDescriptionContainer>
+              <Button
+                variant="bordered"
+                iconType="safe"
+                iconSize="sm"
+                size="lg"
+                color="secondary"
+                component={Link}
+                to={LOAD_ADDRESS}
+              >
+                <StyledButtonLabel size="xl" color="secondary">
+                  Add existing Safe
+                </StyledButtonLabel>
+              </Button>
+            </CardContentContainer>
           </StyledCard>
         </CardsContainer>
       </Block>
@@ -68,17 +72,26 @@ export default Welcome
 
 const CardsContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   height: 300px;
-  max-width: 1200px;
+  max-width: 850px;
 `
 
 const StyledCard = styled(Card)`
   display: flex;
-  flex: 0 1 36%;
+  flex: 0 1 100%;
+  padding: 0;
+`
+
+const CardContentContainer = styled.div`
+  flex: 1 1 50%;
+  display: flex;
   flex-direction: column;
   padding: 24px;
   align-items: flex-start;
+`
+
+const StyledButtonLabel = styled(Text)`
+  min-width: 130px;
 `
 
 const CardDescriptionContainer = styled.div`
