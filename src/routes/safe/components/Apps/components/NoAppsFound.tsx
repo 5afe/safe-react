5f887177077f12ac98fd1766bf0f37763ade1a92
Mikhail <mmvsha73@gmail.com>
2021-09-27 13:48:11 +0200

    update not found screen

--- src/routes/safe/components/Apps/components/NoAppsFound.tsx
+++ src/routes/safe/components/Apps/components/NoAppsFound.tsx
@@ -1,21 +1,16 @@
 import { Text, Button } from '@gnosis.pm/safe-react-components'
-import InfoIcon from '@material-ui/icons/Info'
 import styled from 'styled-components'
 
 const NoAppsFoundTextContainer = styled.div`
   max-width: 650px;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  flex-wrap: wrap;
   margin-top: ${({ theme }) => theme.margin.xxl};
   margin-bottom: ${({ theme }) => theme.margin.xxl};
 
   & > p {
-    flex: 1 90%;
-  }
-
-  & > svg {
-    flex: 0 7%;
+    text-align: center;
   }
 `
 
@@ -31,7 +26,6 @@ type Props = {
 
 const NoAppsFound = ({ query, onWalletConnectSearch }: Props): React.ReactElement => (
   <NoAppsFoundTextContainer>
-    <InfoIcon />
     <Text size="xl">
       No apps found matching <b>{query}</b>. Connect to dApps that haven&apos;t yet been integrated with the Safe using
       the WalletConnect App.
