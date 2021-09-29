55d62a998f7d933913cfcc92c3faf7b0f3e5198d
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-09-29 17:18:03 +0200

    removed old Load Safe, Create Safe and Stepper code

--- src/routes/CreateNewSafePage/components/SafeCreationProcess.tsx
+++ src/routes/CreateNewSafePage/components/SafeCreationProcess.tsx
@@ -9,7 +9,6 @@ import styled from 'styled-components'
 import { getSafeDeploymentTransaction } from 'src/logic/contracts/safeContracts'
 import { txMonitor } from 'src/logic/safe/transactions/txMonitor'
 import { userAccountSelector } from 'src/logic/wallets/store/selectors'
-import { FIELD_CREATION_PROXY_SALT } from 'src/routes/open/components/fields'
 import { SafeDeployment } from 'src/routes/opening'
 import { SAFE_ROUTES, WELCOME_ADDRESS } from 'src/routes/routes'
 import { history } from 'src/store'
@@ -26,6 +25,7 @@ import {
   FIELD_NEW_SAFE_CREATION_TX_HASH,
   FIELD_CREATE_SUGGESTED_SAFE_NAME,
   FIELD_CREATE_CUSTOM_SAFE_NAME,
+  FIELD_NEW_SAFE_PROXY_SALT,
 } from '../fields/createSafeFields'
 import { getSafeInfo } from 'src/logic/safe/utils/safeInformation'
 import { buildSafe } from 'src/logic/safe/store/actions/fetchSafe'
@@ -70,7 +70,7 @@ function SafeCreationProcess(): ReactElement {
         const confirmations = safeCreationFormValues[FIELD_NEW_SAFE_THRESHOLD]
         const ownerFields = safeCreationFormValues[FIELD_SAFE_OWNERS_LIST]
         const ownerAddresses = ownerFields.map(({ addressFieldName }) => safeCreationFormValues[addressFieldName])
-        const safeCreationSalt = safeCreationFormValues[FIELD_CREATION_PROXY_SALT]
+        const safeCreationSalt = safeCreationFormValues[FIELD_NEW_SAFE_PROXY_SALT]
         const gasLimit = safeCreationFormValues[FIELD_NEW_SAFE_GAS_LIMIT]
         const deploymentTx = getSafeDeploymentTransaction(ownerAddresses, confirmations, safeCreationSalt)
 
