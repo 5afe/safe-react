836158f7a09464c07a2205dfa1c9c9597fc6d13d
iamacook <aaron.cook@gnosis.pm>
2021-10-13 16:44:27 +0200

    fix: Always setNetwork in NetworkSelector

--- src/components/AppLayout/Header/components/NetworkSelector.tsx
+++ src/components/AppLayout/Header/components/NetworkSelector.tsx
@@ -16,6 +16,8 @@ import Col from 'src/components/layout/Col'
 import { screenSm, sm } from 'src/theme/variables'
 import { getNetworkConfigById } from 'src/config'
 import { ReturnValue } from 'src/logic/hooks/useStateHandler'
+import { ETHEREUM_NETWORK } from 'src/config/networks/network'
+import { setNetwork } from 'src/logic/config/utils'
 import { NETWORK_ROOT_ROUTES, ROOT_ROUTE } from 'src/routes/routes'
 import { useSelector } from 'react-redux'
 import { currentChainId } from 'src/logic/config/store/selectors'
@@ -84,10 +86,10 @@ const NetworkSelector = ({ open, toggle, clickAway }: NetworkSelectorProps): Rea
   const chainId = useSelector(currentChainId)
 
   const onNetworkSwitch = useCallback(
-    (e: React.SyntheticEvent) => {
+    (e: React.SyntheticEvent, networkId: ETHEREUM_NETWORK) => {
       e.preventDefault()
       clickAway()
-      // setNetwork occurs via history.listen
+      setNetwork(networkId)
       history.push(ROOT_ROUTE)
     },
     [clickAway, history],
@@ -118,7 +120,7 @@ const NetworkSelector = ({ open, toggle, clickAway }: NetworkSelectorProps): Rea
                 <List className={classes.network} component="div">
                   {NETWORK_ROOT_ROUTES.map(({ id, route }) => (
                     <Fragment key={id}>
-                      <StyledLink onClick={onNetworkSwitch} href={route}>
+                      <StyledLink onClick={(e) => onNetworkSwitch(e, id)} href={route}>
                         <NetworkLabel networkInfo={getNetworkConfigById(id)?.network} />
 
                         {chainId === id && <Icon type="check" size="md" color="primary" />}
