b84dd54a126bde83b02d0c9e05dd3a08ff3d28db
Diogo Soares <diogo.soares@gnosis.pm>
2021-10-14 17:54:21 +0200

    Use core SDK to remove safe owner

--- src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/index.tsx
+++ src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/index.tsx
@@ -3,16 +3,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { OwnerData } from 'src/routes/safe/components/Settings/ManageOwners/dataFetcher'
 
 import { CheckOwner } from './screens/CheckOwner'
+import { userAccountSelector } from 'src/logic/wallets/store/selectors'
 import { ReviewRemoveOwnerModal } from './screens/Review'
 import { ThresholdForm } from './screens/ThresholdForm'
 
 import Modal from 'src/components/Modal'
-import { SENTINEL_ADDRESS, getGnosisSafeInstanceAt } from 'src/logic/contracts/safeContracts'
 import { TX_NOTIFICATION_TYPES } from 'src/logic/safe/transactions'
 import { createTransaction } from 'src/logic/safe/store/actions/createTransaction'
 import { currentSafe } from 'src/logic/safe/store/selectors'
 import { Dispatch } from 'src/logic/safe/store/actions/types.d'
 import { TxParameters } from 'src/routes/safe/container/hooks/useTransactionParameters'
+import { getSafeSDK } from 'src/logic/wallets/getWeb3'
 
 type OwnerValues = OwnerData & {
   threshold: string
@@ -21,18 +22,14 @@ type OwnerValues = OwnerData & {
 export const sendRemoveOwner = async (
   values: OwnerValues,
   safeAddress: string,
-  safeVersion: string,
   ownerAddressToRemove: string,
   dispatch: Dispatch,
   txParameters: TxParameters,
+  connectedWalletAddress: string,
 ): Promise<void> => {
-  const gnosisSafe = getGnosisSafeInstanceAt(safeAddress, safeVersion)
-  const safeOwners = await gnosisSafe.methods.getOwners().call()
-  const index = safeOwners.findIndex(
-    (ownerAddress) => ownerAddress.toLowerCase() === ownerAddressToRemove.toLowerCase(),
-  )
-  const prevAddress = index === 0 ? SENTINEL_ADDRESS : safeOwners[index - 1]
-  const txData = gnosisSafe.methods.removeOwner(prevAddress, ownerAddressToRemove, values.threshold).encodeABI()
+  const sdk = await getSafeSDK(connectedWalletAddress, safeAddress)
+  const safeTx = await sdk.getRemoveOwnerTx(ownerAddressToRemove, +values.threshold)
+  const txData = safeTx.data.data
 
   dispatch(
     createTransaction({
@@ -58,7 +55,8 @@ export const RemoveOwnerModal = ({ isOpen, onClose, owner }: RemoveOwnerProps): 
   const [activeScreen, setActiveScreen] = useState('checkOwner')
   const [values, setValues] = useState<OwnerValues>({ ...owner, threshold: '' })
   const dispatch = useDispatch()
-  const { address: safeAddress = '', currentVersion: safeVersion = '' } = useSelector(currentSafe) ?? {}
+  const { address: safeAddress = '' } = useSelector(currentSafe) ?? {}
+  const connectedWalletAddress = useSelector(userAccountSelector)
 
   useEffect(
     () => () => {
@@ -87,7 +85,7 @@ export const RemoveOwnerModal = ({ isOpen, onClose, owner }: RemoveOwnerProps): 
 
   const onRemoveOwner = (txParameters: TxParameters) => {
     onClose()
-    sendRemoveOwner(values, safeAddress, safeVersion, owner.address, dispatch, txParameters)
+    sendRemoveOwner(values, safeAddress, owner.address, dispatch, txParameters, connectedWalletAddress)
   }
 
   return (
