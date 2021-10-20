cfdab40cde3dda13ca5596bac3b116881fb8adc6
Daniel Somoza <daniel.somoza@gnosis.pm>
2021-10-20 15:24:47 +0200

    removed updatePinnedSafeApps useCallback and Saving to the storage decoupled from the state

--- src/routes/safe/components/Apps/hooks/appList/usePinnedSafeApps.ts
+++ src/routes/safe/components/Apps/hooks/appList/usePinnedSafeApps.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { loadFromStorage, saveToStorage } from 'src/utils/storage'
 import { PINNED_SAFE_APP_IDS } from '../../utils'
 import { FETCH_STATUS } from 'src/utils/requests'
@@ -11,13 +11,8 @@ type ReturnType = {
 }
 
 const usePinnedSafeApps = (remoteSafeApps: SafeApp[], remoteAppsFetchStatus: FETCH_STATUS): ReturnType => {
-  const [pinnedSafeAppIds, setPinnedSafeAppIds] = useState<string[]>([])
+  const [pinnedSafeAppIds, updatePinnedSafeApps] = useState<string[]>([])
   const [loaded, setLoaded] = useState(false)
-
-  const updatePinnedSafeApps = useCallback((newPinnedSafeAppIds: string[]) => {
-    setPinnedSafeAppIds(newPinnedSafeAppIds)
-    saveToStorage(PINNED_SAFE_APP_IDS, newPinnedSafeAppIds)
-  }, [])
 
   useEffect(() => {
     const loadPinnedAppIds = async () => {
@@ -25,17 +20,25 @@ const usePinnedSafeApps = (remoteSafeApps: SafeApp[], remoteAppsFetchStatus: FET
 
       const isRemoteSafeAppsListLoaded = remoteAppsFetchStatus === FETCH_STATUS.SUCCESS
       if (isRemoteSafeAppsListLoaded) {
+        // we remove pinned Safe Apps that are not included in the remote list, see #2847
         const filteredPinnedAppsIds = pinnedAppIds.filter((pinnedAppId) =>
           remoteSafeApps.some((app) => app.id === pinnedAppId),
         )
-        saveToStorage(PINNED_SAFE_APP_IDS, filteredPinnedAppsIds)
-        setPinnedSafeAppIds(filteredPinnedAppsIds)
+        updatePinnedSafeApps(filteredPinnedAppsIds)
         setLoaded(true)
       }
     }
 
     loadPinnedAppIds()
   }, [remoteAppsFetchStatus, remoteSafeApps])
+
+  // we only update pinned apps in the localStorage when remote Apps are loaded
+  useEffect(() => {
+    const isRemoteSafeAppsListLoaded = remoteAppsFetchStatus === FETCH_STATUS.SUCCESS
+    if (isRemoteSafeAppsListLoaded) {
+      saveToStorage(PINNED_SAFE_APP_IDS, pinnedSafeAppIds)
+    }
+  }, [pinnedSafeAppIds, remoteAppsFetchStatus])
 
   return { pinnedSafeAppIds, loaded, updatePinnedSafeApps }
 }
