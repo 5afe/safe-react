ade9c69c979b0769ca98244ed8e5c2e2d021de6c
iamacook <aaron.cook@gnosis.pm>
2021-11-04 10:12:31 +0100

    fix: Reorder imports

--- src/components/layout/Heading/index.tsx
+++ src/components/layout/Heading/index.tsx
@@ -1,9 +1,8 @@
 import classNames from 'classnames/bind'
 import * as React from 'react'
 
-import styles from './index.module.scss'
-
 import { capitalize } from 'src/utils/css'
+import styles from './index.module.scss'
 
 const cx = classNames.bind(styles)
 
