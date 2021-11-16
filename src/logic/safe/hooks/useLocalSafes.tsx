93389efc6a40ba39757bc3a266fabb4490e10ec8
Aaron Cook <aaron.cook@gnosis.pm>
2021-11-16 16:41:14 +0000

    Fix: checksum sidebar addresses (#3008)
    * fix: Checksum sidebar addresses
    
    * chore: Remove owned safe checksum
    
    * fix: Add checksum err handling + optimisations
    
    * fix: Checksum addresses
    
    * fix: Add safe
    
    * build: Filter local + add other network safes
    
    * chore: Move catch to checksum

--- src/logic/safe/hooks/useLocalSafes.tsx
+++ src/logic/safe/hooks/useLocalSafes.tsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react'
 
 import { ETHEREUM_NETWORK } from 'src/config/networks/network.d'
-import { SafeRecordProps } from '../store/models/safe'
+import { SafeRecordWithNames } from '../store/selectors'
 import { getLocalNetworkSafesById } from '../utils'
 
-type EmptyLocalSafes = Record<ETHEREUM_NETWORK, never[]>
-type LocalSafes = Record<ETHEREUM_NETWORK, SafeRecordProps[] | never[]>
+type LocalSafes = Record<ETHEREUM_NETWORK, SafeRecordWithNames[]>
 
-const getEmptyLocalSafes = (): EmptyLocalSafes => {
+const getEmptyLocalSafes = (): LocalSafes => {
   const networkIds = Object.values(ETHEREUM_NETWORK)
-  return networkIds.reduce((safes, networkId) => ({ ...safes, [networkId]: [] }), {} as EmptyLocalSafes)
+  return networkIds.reduce((safes, networkId) => ({ ...safes, [networkId]: [] }), {} as LocalSafes)
 }
 
 const useLocalSafes = (): LocalSafes => {
