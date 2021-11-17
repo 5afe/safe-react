3bd9b668dd63820b9715febeedf32f18d9d00fa5
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-11-17 15:07:32 +0100

    Fix: update local safes in sidebar (#3017)

--- src/logic/safe/utils/safeInformation.ts
+++ src/logic/safe/utils/safeInformation.ts
@@ -2,9 +2,6 @@ import { getSafeInfo as fetchSafeInfo, SafeInfo } from '@gnosis.pm/safe-react-ga
 
 import { Errors, CodedException } from 'src/logic/exceptions/CodedException'
 import { getClientGatewayUrl, getNetworkId } from 'src/config'
-import { sameAddress } from 'src/logic/wallets/ethAddresses'
-import { SafeRecordProps } from '../store/models/safe'
-import { SafeRecordWithNames } from '../store/selectors'
 
 const GATEWAY_ERROR = /1337|42/
 
@@ -16,6 +13,3 @@ export const getSafeInfo = async (safeAddress: string): Promise<SafeInfo> => {
     throw new CodedException(safeNotFound ? Errors._605 : Errors._613, e.message)
   }
 }
-
-export const isSafeAdded = (addedSafes: SafeRecordWithNames[] | SafeRecordProps[], address: string): boolean =>
-  addedSafes.some((safe: SafeRecordWithNames | SafeRecordProps) => sameAddress(safe.address, address))
