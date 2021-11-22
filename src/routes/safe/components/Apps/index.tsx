6db25bb2322dfb35a8410c222c72743ea50cf1bf
Yago Pérez Vázquez <yagoperez@outlook.com>
2021-11-22 10:35:27 +0100

    When cancelling the apps disclaimer goback instead showing assets

--- src/routes/safe/components/Apps/index.tsx
+++ src/routes/safe/components/Apps/index.tsx
@@ -1,10 +1,6 @@
 import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 import { useSafeAppUrl } from 'src/logic/hooks/useSafeAppUrl'
-import { generateSafeRoute, SAFE_ROUTES } from 'src/routes/routes'
-import { getCurrentShortChainName } from 'src/config'
-import { currentSafe } from 'src/logic/safe/store/selectors'
 import AppFrame from 'src/routes/safe/components/Apps/components/AppFrame'
 import AppsList from 'src/routes/safe/components/Apps/components/AppsList'
 import LegalDisclaimer from 'src/routes/safe/components/Apps/components/LegalDisclaimer'
@@ -12,17 +8,15 @@ import { useLegalConsent } from 'src/routes/safe/components/Apps/hooks/useLegalC
 
 const Apps = (): React.ReactElement => {
   const history = useHistory()
-  const { address: safeAddress } = useSelector(currentSafe)
   const { getAppUrl } = useSafeAppUrl()
   const url = getAppUrl()
   const { consentReceived, onConsentReceipt } = useLegalConsent()
 
-  const redirectToBalance = () =>
-    history.push(generateSafeRoute(SAFE_ROUTES.ASSETS_BALANCES, { shortName: getCurrentShortChainName(), safeAddress }))
+  const goBack = () => history.goBack()
 
   if (url) {
     if (!consentReceived) {
-      return <LegalDisclaimer onCancel={redirectToBalance} onConfirm={onConsentReceipt} />
+      return <LegalDisclaimer onCancel={goBack} onConfirm={onConsentReceipt} />
     }
 
     return <AppFrame appUrl={url} />
