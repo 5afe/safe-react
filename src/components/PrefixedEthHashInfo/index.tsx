9c0964ffcf41c5d0a1dba298615078b3d16a4eca
Diogo Soares <diogo.soares@gnosis.pm>
2021-11-22 16:37:28 +0100

    Strip network prefix from the Address when calling EthHashInfo

--- src/components/PrefixedEthHashInfo/index.tsx
+++ src/components/PrefixedEthHashInfo/index.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from 'react'
 import { useSelector } from 'react-redux'
 import { copyShortNameSelector, showShortNameSelector } from 'src/logic/appearance/selectors'
 import { extractShortChainName } from 'src/routes/routes'
+import getAddressWithoutNetworkPrefix from 'src/utils/getAddressWithoutNetworkPrefix'
 
 type Props = Omit<Parameters<typeof EthHashInfo>[0], 'shouldShowShortName' | 'shouldCopyShortName'>
 
@@ -12,7 +13,7 @@ const PrefixedEthHashInfo = ({ hash, ...rest }: Props): ReactElement => {
 
   return (
     <EthHashInfo
-      hash={hash}
+      hash={getAddressWithoutNetworkPrefix(hash)}
       shortName={extractShortChainName()}
       shouldShowShortName={showChainPrefix}
       shouldCopyShortName={copyChainPrefix}
