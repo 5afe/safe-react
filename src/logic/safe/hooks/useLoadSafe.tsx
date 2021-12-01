918d68eb01b918f2994cb26bdc3de912d414560c
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-12-01 10:58:16 +0100

    Fix: avoid loading safe multiple times
    Lint

--- src/logic/safe/hooks/useLoadSafe.tsx
+++ src/logic/safe/hooks/useLoadSafe.tsx
@@ -1,37 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import addViewedSafe from 'src/logic/currentSession/store/actions/addViewedSafe'
 import fetchLatestMasterContractVersion from 'src/logic/safe/store/actions/fetchLatestMasterContractVersion'
 import { fetchSafe } from 'src/logic/safe/store/actions/fetchSafe'
-import fetchTransactions from 'src/logic/safe/store/actions/transactions/fetchTransactions'
 import { Dispatch } from 'src/logic/safe/store/actions/types.d'
 import { updateAvailableCurrencies } from 'src/logic/currencyValues/store/actions/updateAvailableCurrencies'
 import { currentChainId } from 'src/logic/config/store/selectors'
+import { fetchSafeTokens } from 'src/logic/tokens/store/actions/fetchSafeTokens'
 
-export const useLoadSafe = (safeAddress?: string): boolean => {
+export const useLoadSafe = (safeAddress?: string): void => {
   const dispatch = useDispatch<Dispatch>()
   const chainId = useSelector(currentChainId)
-  const [isSafeLoaded, setIsSafeLoaded] = useState(false)
 
   useEffect(() => {
-    setIsSafeLoaded(false)
-  }, [safeAddress])
+    if (!safeAddress) return
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (safeAddress) {
-        await dispatch(fetchLatestMasterContractVersion())
-        await dispatch(fetchSafe(safeAddress, isSafeLoaded))
-
-        setIsSafeLoaded(true)
-        await dispatch(updateAvailableCurrencies())
-        await dispatch(fetchTransactions(chainId, safeAddress))
-        dispatch(addViewedSafe(safeAddress))
-      }
-    }
-    fetchData()
-  }, [chainId, dispatch, safeAddress, isSafeLoaded])
-
-  return isSafeLoaded
+    dispatch(fetchLatestMasterContractVersion())
+    dispatch(fetchSafe(safeAddress))
+    dispatch(fetchSafeTokens(safeAddress))
+    dispatch(updateAvailableCurrencies())
+    dispatch(addViewedSafe(safeAddress))
+  }, [dispatch, safeAddress, chainId])
 }
