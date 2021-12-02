5dcdd183f676774cce3137e2afb7fecbffce180c
iamacook <aaron.cook@gnosis.pm>
2021-12-02 16:17:24 +0100

    chore: Add multisend contract label

--- src/routes/safe/components/Transactions/TxList/TxInfoMultiSend.tsx
+++ src/routes/safe/components/Transactions/TxList/TxInfoMultiSend.tsx
@@ -3,6 +3,7 @@ import { MultiSend } from '@gnosis.pm/safe-react-gateway-sdk'
 
 import PrefixedEthHashInfo from 'src/components/PrefixedEthHashInfo'
 import { getExplorerInfo } from 'src/config'
+import { InfoDetails } from './InfoDetails'
 
 // Does not use AddressInfo as to not allow address book data display
 // as we use backend data to verify the deligate call
@@ -11,14 +12,16 @@ const TxInfoMultiSend = ({ txInfo }: { txInfo: MultiSend }): ReactElement => {
   const name = txInfo.to?.name || undefined
   const customAvatar = txInfo.to?.logoUri || undefined
   return (
-    <PrefixedEthHashInfo
-      hash={hash}
-      name={name}
-      customAvatar={customAvatar}
-      showAvatar
-      showCopyBtn
-      explorerUrl={getExplorerInfo(hash)}
-    />
+    <InfoDetails title="Multisend contract:">
+      <PrefixedEthHashInfo
+        hash={hash}
+        name={name}
+        customAvatar={customAvatar}
+        showAvatar
+        showCopyBtn
+        explorerUrl={getExplorerInfo(hash)}
+      />
+    </InfoDetails>
   )
 }
 
