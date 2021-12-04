a40a4fa602097b7c727683549e7fcdaa7399cea9
iamacook <aaron.cook@gnosis.pm>
2021-12-04 11:22:17 +0100

    test: Basic WC QR code

--- src/components/AppLayout/Header/components/ProviderDetails/ConnectDetails.tsx
+++ src/components/AppLayout/Header/components/ProviderDetails/ConnectDetails.tsx
@@ -1,54 +1,73 @@
-import { withStyles } from '@material-ui/core/styles'
 import { ReactElement } from 'react'
-
-import ConnectButton from 'src/components/ConnectButton'
-
-import Block from 'src/components/layout/Block'
-import Paragraph from 'src/components/layout/Paragraph'
-import Row from 'src/components/layout/Row'
-import { KeyRing } from 'src/components/AppLayout/Header/components/KeyRing'
 import { Card } from '@gnosis.pm/safe-react-components'
 import styled from 'styled-components'
+import Divider from '@material-ui/core/Divider'
+import QRCode from 'qrcode.react'
 
-const styles = () => ({
-  logo: {
-    justifyContent: 'center',
-  },
-  text: {
-    letterSpacing: '-0.6px',
-    flexGrow: 1,
-    textAlign: 'center',
-  },
-  connect: {
-    textAlign: 'center',
-    marginTop: '60px',
-  },
-  connectText: {
-    letterSpacing: '1px',
-  },
-  img: {
-    margin: '0px 2px',
-  },
-})
+import ConnectButton from 'src/components/ConnectButton'
+import Paragraph from 'src/components/layout/Paragraph'
+import { KeyRing } from 'src/components/AppLayout/Header/components/KeyRing'
+import useWalletConnect from '../../hooks/useWalletConnect'
+import { getChainById } from 'src/config'
 
 const StyledCard = styled(Card)`
-  padding: 20px;
+  display: flex;
+  flex-direction: 'row';
+  flex: 0 1 100%;
+  padding: 0;
 `
-const ConnectDetails = ({ classes }): ReactElement => (
-  <StyledCard>
-    <Row align="center" margin="lg">
-      <Paragraph className={classes.text} noMargin size="xl" weight="bolder">
-        Connect a Wallet
-      </Paragraph>
-    </Row>
+const CardContainer = styled.div`
+  flex: 1 1 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 240px;
+  padding: 24px;
+  gap: 20px;
+`
 
-    <Row className={classes.logo}>
-      <KeyRing center circleSize={60} dotRight={20} dotSize={20} dotTop={50} keySize={28} mode="error" />
-    </Row>
-    <Block className={classes.connect}>
-      <ConnectButton data-testid="heading-connect-btn" />
-    </Block>
-  </StyledCard>
-)
+const StyledParagraph = styled(Paragraph)`
+  letter-spacing: -0.6px;
+`
 
-export default withStyles(styles as any)(ConnectDetails)
+const ConnectDetails = (): ReactElement => {
+  const { uri, chainId, address } = useWalletConnect()
+
+  const { shortName } = getChainById(chainId)
+  const prefixedSafeAddress = `${shortName}:${address}`
+
+  return (
+    <StyledCard>
+      <CardContainer>
+        <StyledParagraph noMargin size="xl" weight="bolder">
+          Connect to Wallet
+        </StyledParagraph>
+        <KeyRing center circleSize={60} dotRight={20} dotSize={20} dotTop={50} keySize={28} mode="error" />
+        <ConnectButton data-testid="heading-connect-btn" />
+      </CardContainer>
+      <Divider orientation="vertical" flexItem />
+      <CardContainer>
+        <StyledParagraph noMargin size="xl" weight="bolder">
+          Connect to Mobile
+        </StyledParagraph>
+        {!chainId ? (
+          <>
+            <QRCode size={100} value={uri} />
+            <a href="https://apps.apple.com/us/app/gnosis-safe/id1515759131">
+              <img
+                src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1599436800&h=93244e063e3bdf5b5b9f93aff647da09"
+                alt="Download on the App Store"
+                style={{ height: 35 }}
+              />
+            </a>
+          </>
+        ) : (
+          <>{prefixedSafeAddress}</>
+        )}
+      </CardContainer>
+    </StyledCard>
+  )
+}
+
+export default ConnectDetails
