3a26dca0b637da57038fdf40c05e8b4d92b47adb
Aaron Cook <aaron.cook@gnosis.pm>
2021-12-08 17:50:20 +0100

    chore: Don't show explorer for native currency (#3132)
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- src/routes/safe/components/Balances/AssetTableCell/index.tsx
+++ src/routes/safe/components/Balances/AssetTableCell/index.tsx
@@ -8,20 +8,24 @@ import Paragraph from 'src/components/layout/Paragraph'
 import { setImageToPlaceholder } from 'src/routes/safe/components/Balances/utils'
 import { getExplorerInfo } from 'src/config'
 import { BalanceData } from '../dataFetcher'
+import { getNativeCurrencyAddress } from 'src/config/utils'
 
 const StyledParagraph = styled(Paragraph)`
   margin-left: 10px;
   margin-right: 10px;
 `
 
-const AssetTableCell = ({ asset }: { asset: BalanceData['asset'] }): ReactElement => (
-  <Block justify="left">
-    <Img alt={asset.name} height={26} onError={setImageToPlaceholder} src={asset.logoUri} />
-    <StyledParagraph noMargin size="lg">
-      {asset.name}
-    </StyledParagraph>
-    <ExplorerButton explorerUrl={getExplorerInfo(asset.address)} />
-  </Block>
-)
+const AssetTableCell = ({ asset }: { asset: BalanceData['asset'] }): ReactElement => {
+  const isNativeCurrency = asset.address === getNativeCurrencyAddress()
+  return (
+    <Block justify="left">
+      <Img alt={asset.name} height={26} onError={setImageToPlaceholder} src={asset.logoUri} />
+      <StyledParagraph noMargin size="lg">
+        {asset.name}
+      </StyledParagraph>
+      {!isNativeCurrency && <ExplorerButton explorerUrl={getExplorerInfo(asset.address)} />}
+    </Block>
+  )
+}
 
 export default AssetTableCell
