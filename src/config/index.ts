be27af16717255340f555dc49e5895ad63b0ca35
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-12-10 15:44:43 +0100

    Fix: take initial chain id from URL or LS (#3162)
    * Fix: take initial chain id from URL or LS
    
    * Fix types
    
    * Set chain id from URL after chains are loaded
    
    * Remove history listener
    
    * Restore initialConfigState
    
    * Restore initialConfigState

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,31 +6,28 @@ import {
   RpcUri,
   GasPriceFixed,
 } from '@gnosis.pm/safe-react-gateway-sdk'
-import { CONFIG_REDUCER_ID, initialConfigState } from 'src/logic/config/store/reducer'
 
 import {
+  DEFAULT_CHAIN_ID,
   ETHERSCAN_API_KEY,
   INFURA_TOKEN,
-  LS_NAMESPACE,
-  LS_SEPARATOR,
   SAFE_APPS_RPC_TOKEN,
   TX_SERVICE_VERSION,
 } from 'src/utils/constants'
 import { ChainId, ChainName, ShortName } from './chain.d'
 import { emptyChainInfo, getChains } from './cache/chains'
 import { evalTemplate } from './utils'
+import local from 'src/utils/storage/local'
+import { ConfigState } from 'src/logic/config/store/reducer/reducer.d'
 
-export const getInitialChainId = () => {
-  const LOCAL_CONFIG_KEY = `${LS_NAMESPACE}${LS_SEPARATOR}${CONFIG_REDUCER_ID}`
-  const DEFAULT_CHAIN_ID = initialConfigState.chainId
+export const LOCAL_CONFIG_KEY = 'config'
 
-  const localChainId = localStorage.getItem(LOCAL_CONFIG_KEY)
-
-  try {
-    return localChainId ? JSON.parse(localChainId)?.chainId : DEFAULT_CHAIN_ID
-  } catch {
-    return DEFAULT_CHAIN_ID
-  }
+/**
+ * Determine the initial chain id
+ */
+const getInitialChainId = (): ChainId => {
+  const localItem = local.getItem<ConfigState>(LOCAL_CONFIG_KEY)
+  return localItem?.chainId || DEFAULT_CHAIN_ID
 }
 
 let _chainId = getInitialChainId()
---
+++ src/logic/config/store/reducer/reducer.d.ts
@@ -0,0 +1,7 @@
+import { ChainId } from 'src/config/chain.d'
+
+export type ConfigState = {
+  chainId: ChainId
+}
+
+export type ConfigPayload = ChainId
