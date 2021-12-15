f0b9226f9df4e9d630ccc7aacd60a744cc07cf57
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-12-15 15:43:43 +0100

    Use LS_NAMESPACE

--- src/components/AppLayout/Sidebar/DebugToggle/index.tsx
+++ src/components/AppLayout/Sidebar/DebugToggle/index.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import FormControlLabel from '@material-ui/core/FormControlLabel/FormControlLabel'
 import { Switch } from '@gnosis.pm/safe-react-components'
 import useStoredState from 'src/utils/storage/useStoredState'
+import { LS_USE_PROD_CGW } from 'src/utils/constants'
 
 const StyledContainer = styled.div`
   padding-top: 10px;
@@ -9,7 +10,7 @@ const StyledContainer = styled.div`
 `
 
 const DebugToggle = (): React.ReactElement => {
-  const [enabled = false, setEnabled] = useStoredState<boolean>('useProdGateway')
+  const [enabled = false, setEnabled] = useStoredState<boolean>(LS_USE_PROD_CGW)
 
   const onToggle = () => {
     setEnabled((prev: boolean) => !prev)
