95002dfd455bc0521434e12db5fe0906b0d1e3cb
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-12-17 11:10:08 +0100

    Fix status flicker

--- src/logic/hooks/useLocalTxStatus.ts
+++ src/logic/hooks/useLocalTxStatus.ts
@@ -3,9 +3,20 @@ import { TransactionStatus } from '@gnosis.pm/safe-react-gateway-sdk'
 import { Transaction } from 'src/logic/safe/store/models/types/gateway.d'
 import { AppReduxState } from 'src/store'
 import { selectTxStatus } from 'src/logic/safe/store/selectors/txStatus'
+import { useState } from 'react'
+import { useDebounce } from './useDebounce'
 
 const useLocalTxStatus = (transaction: Transaction): TransactionStatus => {
-  return useSelector((state: AppReduxState) => selectTxStatus(state, transaction))
+  const storedStatus = useSelector((state: AppReduxState) => selectTxStatus(state, transaction))
+  const [localStatus, setLocalStatus] = useState(storedStatus)
+
+  useDebounce(() => {
+    if (storedStatus) {
+      setLocalStatus(storedStatus)
+    }
+  }, 100)
+
+  return localStatus
 }
 
 export default useLocalTxStatus
