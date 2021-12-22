68cc33c63a3e1f0ccc8aa82a7ce8b5a27026ce37
iamacook <aaron.cook@gnosis.pm>
2021-12-22 15:07:52 +0100

    chore: Remove storage listener

--- src/components/AppLayout/Sidebar/DebugToggle/index.tsx
+++ src/components/AppLayout/Sidebar/DebugToggle/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import FormControlLabel from '@material-ui/core/FormControlLabel/FormControlLabel'
 import { Switch } from '@gnosis.pm/safe-react-components'
-import useStoredState from 'src/utils/storage/useStoredState'
+import useCachedState from 'src/utils/storage/useCachedState'
 import { LS_USE_PROD_CGW } from 'src/utils/constants'
 
 const StyledContainer = styled.div`
@@ -10,7 +10,7 @@ const StyledContainer = styled.div`
 `
 
 const DebugToggle = (): React.ReactElement => {
-  const [enabled = false, setEnabled] = useStoredState<boolean>(LS_USE_PROD_CGW)
+  const [enabled = false, setEnabled] = useCachedState<boolean>(LS_USE_PROD_CGW)
 
   const onToggle = () => {
     setEnabled((prev: boolean) => !prev)
