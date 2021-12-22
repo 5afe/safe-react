889b618cf7887e96a30d541d9cd362e5dbfca1f3
Aaron Cook <aaron.cook@gnosis.pm>
2021-12-22 16:28:34 +0100

    Fix: deeplinked, out of nonce order transactions allow confirmation  (#3210)
    * fix: Define undefined labels + poll after deeplink
    
    * fix: Update/remove redundant comments

--- src/logic/safe/store/reducer/gatewayTransactions.ts
+++ src/logic/safe/store/reducer/gatewayTransactions.ts
@@ -119,6 +119,10 @@ export const gatewayTransactionsReducer = handleActions<GatewayTransactionsState
           return
         }
 
+        if (!label) {
+          label = newNext[txNonce] ? 'next' : 'queued'
+        }
+
         const newTx = value.transaction
         if (label === 'queued') {
           if (newQueued?.[txNonce]) {
