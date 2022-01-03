67301c672007c1c5e07e4d255b6a541303e3c84a
Diogo Soares <diogo.soares@gnosis.pm>
2022-01-03 12:25:09 +0100

    Set the default chain id if app redirects due to wrong URL shortName

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -3,6 +3,7 @@ import { getShortName } from 'src/config'
 import { getChains } from 'src/config/cache/chains'
 import { setChainId } from 'src/logic/config/utils'
 import { hasPrefixedSafeAddressInUrl, extractPrefixedSafeAddress, history, WELCOME_ROUTE } from 'src/routes/routes'
+import { DEFAULT_CHAIN_ID } from './constants'
 
 export const switchNetworkWithUrl = ({ pathname }: LocationDescriptorObject): void => {
   if (!hasPrefixedSafeAddressInUrl()) {
@@ -19,6 +20,7 @@ export const switchNetworkWithUrl = ({ pathname }: LocationDescriptorObject): vo
   const chainId = getChains().find((chain) => chain.shortName === shortName)?.chainId
 
   if (!chainId) {
+    setChainId(DEFAULT_CHAIN_ID)
     history.push(WELCOME_ROUTE)
     return
   }
