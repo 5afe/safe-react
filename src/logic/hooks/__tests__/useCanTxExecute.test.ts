ffc1bfc60ea4eb094bb9226dfa354bdd78384543
Diogo Soares <diogo.soares@gnosis.pm>
2022-01-07 18:41:27 +0100

    refactor: refactor the useCanExecuteType to test the logic

---
+++ src/logic/hooks/__tests__/useCanTxExecute.test.ts
@@ -0,0 +1,207 @@
+import { calculateCanTxExecute } from '../useCanTxExecute'
+
+describe('useCanTxExecute tests', () => {
+  describe('calculateCanTxExecute test', () => {
+    beforeEach(() => {
+      txType = ''
+      hasQueueNextTx = false
+      currentSafeNonce = 8
+      recommendedNonce = 8
+    })
+    // to be overriden as necessary
+    let threshold
+    let preApprovingOwner
+    let txConfirmations
+    let txType
+    let currentSafeNonce
+    let recommendedNonce
+    let hasQueueNextTx
+    it(`should return false there are queued txs`, () => {
+      // given
+      hasQueueNextTx = true
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(false)
+    })
+    it(`should return true if the safe threshold is 1 and no queued txs`, () => {
+      // given
+      threshold = 1
+      currentSafeNonce = 8
+      recommendedNonce = 8
+      hasQueueNextTx = false
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(true)
+    })
+    it(`should return false if the safe threshold is 1 and there are queued txs`, () => {
+      // given
+      threshold = 1
+      currentSafeNonce = 8
+      recommendedNonce = 8
+      hasQueueNextTx = true
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(false)
+    })
+
+    it(`should return false if the safe threshold is 1, there are no queued txs but recommendedNonce does not match safeNonce`, () => {
+      // given
+      threshold = 1
+      currentSafeNonce = 8
+      recommendedNonce = 10
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(false)
+    })
+    it(`should return true if the safe threshold is reached for the transaction`, () => {
+      // given
+      threshold = 3
+      txConfirmations = 3
+
+      console.log(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(true)
+    })
+    it(`should return true if the transaction is spendingLimit`, () => {
+      // given
+      txType = 'spendingLimit'
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(true)
+    })
+    it.skip(`should return false if the number of confirmations meets the threshold but txNonce > safeNonce`, () => {
+      // given
+      threshold = 5
+      txConfirmations = 5
+      currentSafeNonce = 8
+      recommendedNonce = 10
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(false)
+    })
+    it(`should return false if the number of confirmations does not meet the threshold and there is no preApprovingOwner`, () => {
+      // given
+      threshold = 5
+      txConfirmations = 4
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(false)
+    })
+    it(`should return true if the number of confirmations is one bellow the threshold but there is a preApprovingOwner`, () => {
+      // given
+      threshold = 5
+      preApprovingOwner = '0x29B1b813b6e84654Ca698ef5d7808E154364900B'
+      txConfirmations = 4
+
+      // when
+      const result = calculateCanTxExecute(
+        currentSafeNonce,
+        hasQueueNextTx,
+        preApprovingOwner,
+        threshold,
+        txConfirmations,
+        txType,
+        recommendedNonce,
+      )
+
+      // then
+      expect(result).toBe(true)
+    })
+  })
+})
--- src/logic/hooks/useCanTxExecute.tsx
+++ src/logic/hooks/useCanTxExecute.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { extractSafeAddress } from 'src/routes/routes'
 import { sameString } from 'src/utils/strings'
@@ -12,34 +13,57 @@ type UseCanTxExecuteType = (
   txType?: string,
 ) => boolean
 
-// Review default values
-const useCanTxExecute: UseCanTxExecuteType = (threshold, preApprovingOwner = '', txConfirmations = 0, txType) => {
-  const nextQueuedTx = useSelector(nextTransactions)
-  const hasQueueNextTx = nextQueuedTx && Object.keys(nextQueuedTx).length > 0
-
-  const safeAddress = extractSafeAddress()
-  const recommendedNonce = useGetRecommendedNonce(safeAddress)
-  const { nonce: currentSafeNonce } = useSelector(currentSafe)
-
+export const calculateCanTxExecute = (
+  currentSafeNonce: number,
+  hasQueueNextTx = false,
+  preApprovingOwner = '',
+  threshold: number,
+  txConfirmations = 0,
+  txType = '',
+  recommendedNonce?: number,
+): boolean => {
   // Do not display "Execute checkbox" if there's a tx in the next queue
   if (hasQueueNextTx) return false
 
-  // Single owner Safe and transaction is the next nonce
+  // Single owner Safe AND transaction is the next nonce
   const isSingleOwnerNextTx = threshold === 1 && recommendedNonce === currentSafeNonce
 
-  if (
-    isSingleOwnerNextTx ||
-    sameString(txType, 'spendingLimit') ||
-    (txConfirmations !== undefined && txConfirmations >= threshold)
-  ) {
+  if (isSingleOwnerNextTx || sameString(txType, 'spendingLimit') || txConfirmations >= threshold) {
     return true
   }
 
+  // When having a preApprovingOwner it is needed one less confirmation to execute the tx
   if (preApprovingOwner && txConfirmations) {
     return txConfirmations + 1 === threshold
   }
 
   return false
+}
+
+// Review default values
+const useCanTxExecute: UseCanTxExecuteType = (threshold, preApprovingOwner = '', txConfirmations = 0, txType) => {
+  const [canTxExecute, setCanTxExecute] = useState(false)
+  const nextQueuedTx = useSelector(nextTransactions)
+  const hasQueueNextTx = nextQueuedTx && Object.keys(nextQueuedTx).length > 0
+
+  const safeAddress = extractSafeAddress()
+  const recommendedNonce = useGetRecommendedNonce(safeAddress) // to be changed. should be txNonce
+  const { nonce: currentSafeNonce } = useSelector(currentSafe)
+
+  useEffect(() => {
+    const result = calculateCanTxExecute(
+      currentSafeNonce,
+      hasQueueNextTx,
+      preApprovingOwner,
+      threshold,
+      txConfirmations,
+      txType,
+      recommendedNonce,
+    )
+    setCanTxExecute(result)
+  }, [currentSafeNonce, hasQueueNextTx, preApprovingOwner, recommendedNonce, threshold, txConfirmations, txType])
+
+  return canTxExecute
 }
 
 export default useCanTxExecute
