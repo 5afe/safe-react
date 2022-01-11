4357def6ce9b5463370fa5c104652ede5998874b
iamacook <aaron.cook@gnosis.pm>
2022-01-11 16:13:54 +0100

    fix: Show circle for PENDING_FAILED txs

--- src/routes/safe/components/Transactions/TxList/TxExpandedActions.tsx
+++ src/routes/safe/components/Transactions/TxList/TxExpandedActions.tsx
@@ -4,9 +4,10 @@ import { ReactElement } from 'react'
 import { useSelector } from 'react-redux'
 
 import { currentSafeNonce } from 'src/logic/safe/store/selectors'
-import { LocalTransactionStatus, Transaction } from 'src/logic/safe/store/models/types/gateway.d'
+import { Transaction } from 'src/logic/safe/store/models/types/gateway.d'
 import { useActionButtonsHandlers } from 'src/routes/safe/components/Transactions/TxList/hooks/useActionButtonsHandlers'
 import useLocalTxStatus from 'src/logic/hooks/useLocalTxStatus'
+import { isAwaitingExecution } from './utils'
 
 type TxExpandedActionsProps = {
   transaction: Transaction
@@ -24,10 +25,7 @@ export const TxExpandedActions = ({ transaction }: TxExpandedActionsProps): Reac
   } = useActionButtonsHandlers(transaction)
   const nonce = useSelector(currentSafeNonce)
   const txStatus = useLocalTxStatus(transaction)
-  const isAwaitingExecution = [
-    LocalTransactionStatus.AWAITING_EXECUTION,
-    LocalTransactionStatus.PENDING_FAILED,
-  ].includes(txStatus)
+  const isAwaitingEx = isAwaitingExecution(txStatus)
 
   const onExecuteOrConfirm = (event) => {
     handleOnMouseLeave()
@@ -35,7 +33,7 @@ export const TxExpandedActions = ({ transaction }: TxExpandedActionsProps): Reac
   }
 
   const getConfirmTooltipTitle = () => {
-    if (isAwaitingExecution) {
+    if (isAwaitingEx) {
       return (transaction.executionInfo as MultisigExecutionInfo)?.nonce === nonce
         ? 'Execute'
         : `Transaction with nonce ${nonce} needs to be executed first`
@@ -58,7 +56,7 @@ export const TxExpandedActions = ({ transaction }: TxExpandedActionsProps): Reac
             onMouseLeave={handleOnMouseLeave}
             className="primary"
           >
-            {isAwaitingExecution ? 'Execute' : 'Confirm'}
+            {isAwaitingEx ? 'Execute' : 'Confirm'}
           </Button>
         </span>
       </Tooltip>
