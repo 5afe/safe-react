031eb0b4a315b904d6876ce0802b7a1c34f43f94
Aaron Cook <aaron.cook@gnosis.pm>
2022-01-11 17:45:03 +0100

    Add non-owner error message for Safe Apps (#3272)
    * fix: Add non-owner error message for Safe Apps
    
    * chore: Cleanup code layout
    
    * fix: Unicode issue

--- src/components/TransactionFailText/index.tsx
+++ src/components/TransactionFailText/index.tsx
@@ -9,6 +9,7 @@ import InfoIcon from 'src/assets/icons/info_red.svg'
 import { useSelector } from 'react-redux'
 import { currentSafeThreshold } from 'src/logic/safe/store/selectors'
 import { shouldSwitchWalletChain } from 'src/logic/wallets/store/selectors'
+import { grantedSelector } from 'src/routes/safe/container/selector'
 
 const styles = createStyles({
   executionWarningRow: {
@@ -34,28 +35,31 @@ export const TransactionFailText = ({
   const classes = useStyles()
   const threshold = useSelector(currentSafeThreshold)
   const isWrongChain = useSelector(shouldSwitchWalletChain)
+  const isGranted = useSelector(grantedSelector)
 
   if (txEstimationExecutionStatus !== EstimationStatus.FAILURE) {
     return null
   }
 
-  let errorMessage = 'To save gas costs, avoid creating the transaction.'
+  let errorDesc = 'To save gas costs, avoid creating the transaction.'
   if (isExecution) {
-    errorMessage =
+    errorDesc =
       threshold && threshold > 1
         ? `To save gas costs, reject this transaction`
         : `To save gas costs, avoid executing the transaction.`
   }
 
+  const error = isGranted
+    ? `This transaction will most likely fail. ${errorDesc}`
+    : isWrongChain
+    ? 'Your wallet is connected to the wrong chain.'
+    : "You are currently not an owner of this Safe and won't be able to submit this transaction."
+
   return (
     <Row align="center">
       <Paragraph color="error" className={classes.executionWarningRow}>
         <Img alt="Info Tooltip" height={16} src={InfoIcon} className={classes.warningIcon} />
-        {isWrongChain ? (
-          <>Your wallet is connected to the wrong chain.</>
-        ) : (
-          <>This transaction will most likely fail. {errorMessage}</>
-        )}
+        {error}
       </Paragraph>
     </Row>
   )
