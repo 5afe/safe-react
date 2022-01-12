47e4d0f0c4437d54bc62352fe40bb7d9c8f3b9ed
iamacook <aaron.cook@gnosis.pm>
2022-01-12 10:27:58 +0100

    chore: Cleanup code + add types

--- src/logic/wallets/store/actions/removeProvider.ts
+++ src/logic/wallets/store/actions/removeProvider.ts
@@ -3,12 +3,13 @@ import { createAction } from 'redux-actions'
 
 import onboard from 'src/logic/wallets/onboard'
 import { resetWeb3 } from 'src/logic/wallets/getWeb3'
+import { ProviderState } from '../reducer/provider'
 
 export const REMOVE_PROVIDER = 'REMOVE_PROVIDER'
 
 const removeProvider = createAction(REMOVE_PROVIDER)
 
-export default (payload?: { keepStorageKey: boolean }) =>
+export default (payload?: Pick<ProviderState, 'keepStorageKey'>) =>
   (dispatch: Dispatch): void => {
     onboard().walletReset()
     resetWeb3()
--- src/logic/wallets/store/middlewares/providerWatcher.ts
+++ src/logic/wallets/store/middlewares/providerWatcher.ts
@@ -1,10 +1,12 @@
+import { Dispatch } from 'redux'
 import closeSnackbar from 'src/logic/notifications/store/actions/closeSnackbar'
 import { getAccountFrom, getChainIdFrom, getWeb3 } from 'src/logic/wallets/getWeb3'
 import { fetchProvider } from 'src/logic/wallets/store/actions'
 import { ADD_PROVIDER } from 'src/logic/wallets/store/actions/addProvider'
 import { REMOVE_PROVIDER } from 'src/logic/wallets/store/actions/removeProvider'
-
+import { store as reduxStore } from 'src/store/index'
 import { loadFromStorage, removeFromStorage, saveToStorage } from 'src/utils/storage'
+import { ProviderState } from '../reducer/provider'
 
 const watchedActions = [ADD_PROVIDER, REMOVE_PROVIDER]
 
@@ -16,51 +18,54 @@ export const loadLastUsedProvider = async (): Promise<string | undefined> => {
   return lastUsedProvider
 }
 
-let watcherInterval
-const providerWatcherMware = (store) => (next) => async (action) => {
-  const handledAction = next(action)
+let watcherInterval: NodeJS.Timer
+const providerWatcherMware =
+  (store: ReturnType<typeof reduxStore>) =>
+  (next: Dispatch) =>
+  async (action: { type: string; payload: ProviderState }) => {
+    const handledAction = next(action)
 
-  if (watchedActions.includes(action.type)) {
-    switch (action.type) {
-      case ADD_PROVIDER: {
-        const currentProviderProps = action.payload.toJS()
+    if (watchedActions.includes(action.type)) {
+      switch (action.type) {
+        case ADD_PROVIDER: {
+          const currentProviderProps = action.payload.toJS()
 
-        if (watcherInterval) {
-          clearInterval(watcherInterval)
-        }
+          if (watcherInterval) {
+            clearInterval(watcherInterval)
+          }
 
-        saveToStorage(LAST_USED_PROVIDER_KEY, currentProviderProps.name)
+          saveToStorage(LAST_USED_PROVIDER_KEY, currentProviderProps.name)
 
-        watcherInterval = setInterval(async () => {
-          const web3 = getWeb3()
-          const account = await getAccountFrom(web3)
-          const network = await getChainIdFrom(web3)
+          watcherInterval = setInterval(async () => {
+            const web3 = getWeb3()
+            const network = await getChainIdFrom(web3)
 
-          const networkChanged = currentProviderProps.network !== network
+            if (currentProviderProps.network === network) {
+              return
+            }
 
-          if (networkChanged) {
             store.dispatch(closeSnackbar({ dismissAll: true }))
-          }
 
-          if (currentProviderProps.account !== account || networkChanged) {
-            store.dispatch(fetchProvider(currentProviderProps.name))
-          }
-        }, 2000)
+            const account = await getAccountFrom(web3)
+            if (currentProviderProps.account !== account) {
+              store.dispatch(fetchProvider(currentProviderProps.name))
+            }
+          }, 2000)
 
-        break
-      }
-      case REMOVE_PROVIDER:
-        clearInterval(watcherInterval)
-        if (!action.payload?.keepStorageKey) {
-          removeFromStorage(LAST_USED_PROVIDER_KEY)
+          break
         }
-        break
-      default:
-        break
+        case REMOVE_PROVIDER:
+          clearInterval(watcherInterval)
+          if (!action.payload?.keepStorageKey) {
+            removeFromStorage(LAST_USED_PROVIDER_KEY)
+          }
+          break
+        default:
+          break
+      }
     }
-  }
 
-  return handledAction
-}
+    return handledAction
+  }
 
 export default providerWatcherMware
