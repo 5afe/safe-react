5d78ee4c36bef35dbe66596862a0a27078ca3a94
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-01-14 12:56:00 +0100

    Fix: xDai -> Gnosis Chain

--- src/config/cache/chains.ts
+++ src/config/cache/chains.ts
@@ -8,7 +8,9 @@ let chains: ChainInfo[] = []
 export const getChains = (): ChainInfo[] => chains
 
 export const loadChains = async () => {
-  const { results = [] } = await getChainsConfig(GATEWAY_URL, { limit: 100 })
+  // @FIXME: just for testing the Gnosis Chain
+  const GATEWAY_URL_S = 'https://safe-client.staging.gnosisdev.com' || GATEWAY_URL
+  const { results = [] } = await getChainsConfig(GATEWAY_URL_S, { limit: 100 })
   chains = results
   // Set the initail web3 provider after loading chains
   setWeb3ReadOnly()
--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,29 +1,20 @@
-import { LocationDescriptorObject } from 'history'
-import { getShortName } from 'src/config'
+import { _getChainId } from 'src/config'
 import { getChains } from 'src/config/cache/chains'
 import { setChainId } from 'src/logic/config/utils'
-import { hasPrefixedSafeAddressInUrl, extractPrefixedSafeAddress, history, WELCOME_ROUTE } from 'src/routes/routes'
-import { DEFAULT_CHAIN_ID } from './constants'
+import { hasPrefixedSafeAddressInUrl, extractPrefixedSafeAddress } from 'src/routes/routes'
 
-export const switchNetworkWithUrl = ({ pathname }: LocationDescriptorObject): void => {
-  if (!hasPrefixedSafeAddressInUrl()) {
-    return
-  }
+export const setChainIdFromUrl = (pathname: string): boolean => {
+  if (!hasPrefixedSafeAddressInUrl()) return false
 
   const { shortName } = extractPrefixedSafeAddress(pathname)
-  const currentShortName = getShortName()
-
-  if (shortName === currentShortName) {
-    return
-  }
-
   const chainId = getChains().find((chain) => chain.shortName === shortName)?.chainId
 
-  if (!chainId) {
-    setChainId(DEFAULT_CHAIN_ID)
-    history.push(WELCOME_ROUTE)
-    return
+  if (chainId) {
+    if (chainId !== _getChainId()) {
+      setChainId(chainId)
+    }
+    return true
   }
 
-  setChainId(chainId)
+  return false
 }
