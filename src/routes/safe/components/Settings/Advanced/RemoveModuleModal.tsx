27345967c759174330df33c5ebf3874c2d9e1025
Diogo Soares <diogo.soares@gnosis.pm>
2022-01-16 19:49:02 +0100

    refactor RemoveModuleModal and RemoveLimitModal modals

--- src/routes/safe/components/Settings/Advanced/RemoveModuleModal.tsx
+++ src/routes/safe/components/Settings/Advanced/RemoveModuleModal.tsx
@@ -6,25 +6,20 @@ import Col from 'src/components/layout/Col'
 import Hairline from 'src/components/layout/Hairline'
 import Paragraph from 'src/components/layout/Paragraph'
 import Row from 'src/components/layout/Row'
-import Modal, { ButtonStatus, Modal as GenericModal } from 'src/components/Modal'
+import Modal from 'src/components/Modal'
 import PrefixedEthHashInfo from 'src/components/PrefixedEthHashInfo'
-import { ReviewInfoText } from 'src/components/ReviewInfoText'
 import { getExplorerInfo } from 'src/config'
 import { getDisableModuleTxData } from 'src/logic/safe/utils/modules'
 import { createTransaction } from 'src/logic/safe/store/actions/createTransaction'
-
 import { ModulePair } from 'src/logic/safe/store/models/safe'
 import { currentSafe } from 'src/logic/safe/store/selectors'
 import { TX_NOTIFICATION_TYPES } from 'src/logic/safe/transactions'
-
-import { useStyles } from './style'
 import { Errors, logError } from 'src/logic/exceptions/CodedException'
-import { EstimationStatus, useEstimateTransactionGas } from 'src/logic/hooks/useEstimateTransactionGas'
-import { useEstimationStatus } from 'src/logic/hooks/useEstimationStatus'
-import { TxParametersDetail } from 'src/routes/safe/components/Transactions/helpers/TxParametersDetail'
-import { EditableTxParameters } from 'src/routes/safe/components/Transactions/helpers/EditableTxParameters'
 import { TxParameters } from 'src/routes/safe/container/hooks/useTransactionParameters'
 import { ModalHeader } from 'src/routes/safe/components/Balances/SendModal/screens/ModalHeader'
+import { TxParamsState } from 'src/routes/safe/components/Transactions/helpers/TxParamsState'
+
+import { useStyles } from './style'
 
 interface RemoveModuleModalProps {
   onClose: () => void
@@ -37,31 +32,7 @@ export const RemoveModuleModal = ({ onClose, selectedModulePair }: RemoveModuleM
   const { address: safeAddress, currentVersion: safeVersion } = useSelector(currentSafe)
   const [txData, setTxData] = useState('')
   const dispatch = useDispatch()
-  const [manualSafeTxGas, setManualSafeTxGas] = useState('0')
-  const [manualGasPrice, setManualGasPrice] = useState<string | undefined>()
-  const [manualGasLimit, setManualGasLimit] = useState<string | undefined>()
-
   const [, moduleAddress] = selectedModulePair
-
-  const {
-    gasCostFormatted,
-    txEstimationExecutionStatus,
-    isExecution,
-    isOffChainSignature,
-    isCreation,
-    gasLimit,
-    gasEstimation,
-    gasPriceFormatted,
-  } = useEstimateTransactionGas({
-    txData,
-    txRecipient: safeAddress,
-    txAmount: '0',
-    safeTxGas: manualSafeTxGas,
-    manualGasPrice,
-    manualGasLimit,
-  })
-
-  const [buttonStatus] = useEstimationStatus(txEstimationExecutionStatus)
 
   useEffect(() => {
     const txData = getDisableModuleTxData(selectedModulePair, safeAddress, safeVersion)
@@ -87,30 +58,6 @@ export const RemoveModuleModal = ({ onClose, selectedModulePair }: RemoveModuleM
     }
   }
 
-  const closeEditModalCallback = (txParameters: TxParameters) => {
-    const oldGasPrice = gasPriceFormatted
-    const newGasPrice = txParameters.ethGasPrice
-    const oldSafeTxGas = gasEstimation
-    const newSafeTxGas = txParameters.safeTxGas
-
-    if (newGasPrice && oldGasPrice !== newGasPrice) {
-      setManualGasPrice(txParameters.ethGasPrice)
-    }
-
-    if (txParameters.ethGasLimit && gasLimit !== txParameters.ethGasLimit) {
-      setManualGasLimit(txParameters.ethGasLimit)
-    }
-
-    if (newSafeTxGas && oldSafeTxGas !== newSafeTxGas) {
-      setManualSafeTxGas(newSafeTxGas)
-    }
-  }
-
-  let confirmButtonText = 'Remove'
-  if (ButtonStatus.LOADING === buttonStatus) {
-    confirmButtonText = txEstimationExecutionStatus === EstimationStatus.LOADING ? 'Estimating' : 'Removing'
-  }
-
   return (
     <Modal
       description="Remove the selected Module"
@@ -119,73 +66,28 @@ export const RemoveModuleModal = ({ onClose, selectedModulePair }: RemoveModuleM
       title="Remove Module"
       open
     >
-      <EditableTxParameters
-        isOffChainSignature={isOffChainSignature}
-        isExecution={isExecution}
-        ethGasLimit={gasLimit}
-        ethGasPrice={gasPriceFormatted}
-        safeTxGas={gasEstimation}
-        closeEditModalCallback={closeEditModalCallback}
-      >
-        {(txParameters, toggleEditMode) => {
-          return (
-            <>
-              <ModalHeader onClose={onClose} title="Remove Guard" />
-              <Hairline />
-              <Block>
-                <Row className={classes.modalOwner}>
-                  <Col align="center" xs={1}>
-                    <PrefixedEthHashInfo
-                      hash={moduleAddress}
-                      showCopyBtn
-                      showAvatar
-                      explorerUrl={getExplorerInfo(moduleAddress)}
-                    />
-                  </Col>
-                </Row>
-                <Row className={classes.modalDescription}>
-                  <Paragraph noMargin size="lg">
-                    After removing this module, any feature or app that uses this module might no longer work. If this
-                    Safe requires more then one signature, the module removal will have to be confirmed by other owners
-                    as well.
-                  </Paragraph>
-                </Row>
-              </Block>
-              <Block className={classes.accordionContainer}>
-                {/* Tx Parameters */}
-                <TxParametersDetail
-                  txParameters={txParameters}
-                  onEdit={toggleEditMode}
-                  isTransactionCreation={isCreation}
-                  isTransactionExecution={isExecution}
-                  isOffChainSignature={isOffChainSignature}
-                />
-              </Block>
-              <Row className={classes.modalDescription}>
-                <ReviewInfoText
-                  gasCostFormatted={gasCostFormatted}
-                  isCreation={isCreation}
-                  isExecution={isExecution}
-                  isOffChainSignature={isOffChainSignature}
-                  safeNonce={txParameters.safeNonce}
-                  txEstimationExecutionStatus={txEstimationExecutionStatus}
-                />
-              </Row>
-              <GenericModal.Footer withoutBorder={buttonStatus !== ButtonStatus.LOADING}>
-                <GenericModal.Footer.Buttons
-                  cancelButtonProps={{ onClick: onClose }}
-                  confirmButtonProps={{
-                    color: 'error',
-                    onClick: () => removeSelectedModule(txParameters),
-                    status: buttonStatus,
-                    text: confirmButtonText,
-                  }}
-                />
-              </GenericModal.Footer>
-            </>
-          )
-        }}
-      </EditableTxParameters>
+      <TxParamsState txData={txData} onSubmit={removeSelectedModule} submitText="Remove">
+        <ModalHeader onClose={onClose} title="Remove Guard" />
+        <Hairline />
+        <Block>
+          <Row className={classes.modalOwner}>
+            <Col align="center" xs={1}>
+              <PrefixedEthHashInfo
+                hash={moduleAddress}
+                showCopyBtn
+                showAvatar
+                explorerUrl={getExplorerInfo(moduleAddress)}
+              />
+            </Col>
+          </Row>
+          <Row className={classes.modalDescription}>
+            <Paragraph noMargin size="lg">
+              After removing this module, any feature or app that uses this module might no longer work. If this Safe
+              requires more then one signature, the module removal will have to be confirmed by other owners as well.
+            </Paragraph>
+          </Row>
+        </Block>
+      </TxParamsState>
     </Modal>
   )
 }
--- src/routes/safe/components/Settings/SpendingLimit/RemoveLimitModal.tsx
+++ src/routes/safe/components/Settings/SpendingLimit/RemoveLimitModal.tsx
@@ -2,27 +2,21 @@ import { ReactElement, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import Col from 'src/components/layout/Col'
-import Row from 'src/components/layout/Row'
 import Hairline from 'src/components/layout/Hairline'
-import { ButtonStatus, Modal } from 'src/components/Modal'
-import { ReviewInfoText } from 'src/components/ReviewInfoText'
+import { Modal } from 'src/components/Modal'
 import { ModalHeader } from 'src/routes/safe/components/Balances/SendModal/screens/ModalHeader'
-import { useEstimationStatus } from 'src/logic/hooks/useEstimationStatus'
-import { EstimationStatus, useEstimateTransactionGas } from 'src/logic/hooks/useEstimateTransactionGas'
 import useTokenInfo from 'src/logic/safe/hooks/useTokenInfo'
 import { createTransaction } from 'src/logic/safe/store/actions/createTransaction'
 import { TX_NOTIFICATION_TYPES } from 'src/logic/safe/transactions'
 import { getDeleteAllowanceTxData } from 'src/logic/safe/utils/spendingLimits'
 import { fromTokenUnit } from 'src/logic/tokens/utils/humanReadableValue'
-import { EditableTxParameters } from 'src/routes/safe/components/Transactions/helpers/EditableTxParameters'
-import { TxParametersDetail } from 'src/routes/safe/components/Transactions/helpers/TxParametersDetail'
 import { TxParameters } from 'src/routes/safe/container/hooks/useTransactionParameters'
 import { SPENDING_LIMIT_MODULE_ADDRESS } from 'src/utils/constants'
 import { getResetTimeOptions } from './FormFields/ResetTime'
 import { AddressInfo, ResetTimeInfo, TokenInfo } from './InfoDisplay'
 import { SpendingLimitTable } from './LimitsTable/dataFetcher'
-import { useStyles } from './style'
 import { extractSafeAddress } from 'src/routes/routes'
+import { TxParamsState } from 'src/routes/safe/components/Transactions/helpers/TxParamsState'
 
 interface RemoveSpendingLimitModalProps {
   onClose: () => void
@@ -31,16 +25,10 @@ interface RemoveSpendingLimitModalProps {
 }
 
 export const RemoveLimitModal = ({ onClose, spendingLimit, open }: RemoveSpendingLimitModalProps): ReactElement => {
-  const classes = useStyles()
-
   const tokenInfo = useTokenInfo(spendingLimit.spent.tokenAddress)
-
   const safeAddress = extractSafeAddress()
   const [txData, setTxData] = useState('')
   const dispatch = useDispatch()
-  const [manualSafeTxGas, setManualSafeTxGas] = useState('0')
-  const [manualGasPrice, setManualGasPrice] = useState<string | undefined>()
-  const [manualGasLimit, setManualGasLimit] = useState<string | undefined>()
 
   useEffect(() => {
     const {
@@ -50,26 +38,6 @@ export const RemoveLimitModal = ({ onClose, spendingLimit, open }: RemoveSpendin
     const txData = getDeleteAllowanceTxData({ beneficiary, tokenAddress })
     setTxData(txData)
   }, [spendingLimit])
-
-  const {
-    gasCostFormatted,
-    txEstimationExecutionStatus,
-    isExecution,
-    isOffChainSignature,
-    isCreation,
-    gasLimit,
-    gasEstimation,
-    gasPriceFormatted,
-  } = useEstimateTransactionGas({
-    txData,
-    txRecipient: SPENDING_LIMIT_MODULE_ADDRESS,
-    txAmount: '0',
-    safeTxGas: manualSafeTxGas,
-    manualGasPrice,
-    manualGasLimit,
-  })
-
-  const [buttonStatus] = useEstimationStatus(txEstimationExecutionStatus)
 
   const removeSelectedSpendingLimit = (txParameters: TxParameters) => {
     try {
@@ -96,30 +64,6 @@ export const RemoveLimitModal = ({ onClose, spendingLimit, open }: RemoveSpendin
   const resetTimeLabel =
     getResetTimeOptions().find(({ value }) => +value === +spendingLimit.resetTime.resetTimeMin)?.label ?? ''
 
-  const closeEditModalCallback = (txParameters: TxParameters) => {
-    const oldGasPrice = gasPriceFormatted
-    const newGasPrice = txParameters.ethGasPrice
-    const oldSafeTxGas = gasEstimation
-    const newSafeTxGas = txParameters.safeTxGas
-
-    if (newGasPrice && oldGasPrice !== newGasPrice) {
-      setManualGasPrice(txParameters.ethGasPrice)
-    }
-
-    if (txParameters.ethGasLimit && gasLimit !== txParameters.ethGasLimit) {
-      setManualGasLimit(txParameters.ethGasLimit)
-    }
-
-    if (newSafeTxGas && oldSafeTxGas !== newSafeTxGas) {
-      setManualSafeTxGas(newSafeTxGas)
-    }
-  }
-
-  let confirmButtonText = 'Remove'
-  if (ButtonStatus.LOADING === buttonStatus) {
-    confirmButtonText = txEstimationExecutionStatus === EstimationStatus.LOADING ? 'Estimating' : 'Removing'
-  }
-
   return (
     <Modal
       handleClose={onClose}
@@ -127,72 +71,33 @@ export const RemoveLimitModal = ({ onClose, spendingLimit, open }: RemoveSpendin
       title="Remove spending limit"
       description="Remove the selected spending limit"
     >
-      <EditableTxParameters
-        isOffChainSignature={isOffChainSignature}
-        isExecution={isExecution}
-        ethGasLimit={gasLimit}
-        ethGasPrice={gasPriceFormatted}
-        safeTxGas={gasEstimation}
-        closeEditModalCallback={closeEditModalCallback}
+      <TxParamsState
+        txData={txData}
+        txTo={SPENDING_LIMIT_MODULE_ADDRESS}
+        onSubmit={removeSelectedSpendingLimit}
+        submitText="Remove"
       >
-        {(txParameters, toggleEditMode) => {
-          return (
-            <>
-              <ModalHeader onClose={onClose} title="Remove spending limit" />
-              <Hairline />
+        <ModalHeader onClose={onClose} title="Remove spending limit" />
+        <Hairline />
 
-              <Modal.Body>
-                <Col margin="lg">
-                  <AddressInfo title="Beneficiary" address={spendingLimit.beneficiary} />
-                </Col>
-                <Col margin="lg">
-                  {tokenInfo && (
-                    <TokenInfo
-                      amount={fromTokenUnit(spendingLimit.spent.amount, tokenInfo.decimals)}
-                      title="Amount"
-                      token={tokenInfo}
-                    />
-                  )}
-                </Col>
-                <Col margin="lg">
-                  <ResetTimeInfo title="Reset Time" label={resetTimeLabel} />
-                </Col>
-                {/* Tx Parameters */}
-                <TxParametersDetail
-                  txParameters={txParameters}
-                  onEdit={toggleEditMode}
-                  isTransactionCreation={isCreation}
-                  isTransactionExecution={isExecution}
-                  isOffChainSignature={isOffChainSignature}
-                />
-              </Modal.Body>
-
-              <Row className={classes.modalDescription}>
-                <ReviewInfoText
-                  gasCostFormatted={gasCostFormatted}
-                  isCreation={isCreation}
-                  isExecution={isExecution}
-                  isOffChainSignature={isOffChainSignature}
-                  safeNonce={txParameters.safeNonce}
-                  txEstimationExecutionStatus={txEstimationExecutionStatus}
-                />
-              </Row>
-
-              <Modal.Footer withoutBorder={buttonStatus !== ButtonStatus.LOADING}>
-                <Modal.Footer.Buttons
-                  cancelButtonProps={{ onClick: onClose }}
-                  confirmButtonProps={{
-                    color: 'error',
-                    onClick: () => removeSelectedSpendingLimit(txParameters),
-                    status: buttonStatus,
-                    text: confirmButtonText,
-                  }}
-                />
-              </Modal.Footer>
-            </>
-          )
-        }}
-      </EditableTxParameters>
+        <Modal.Body>
+          <Col margin="lg">
+            <AddressInfo title="Beneficiary" address={spendingLimit.beneficiary} />
+          </Col>
+          <Col margin="lg">
+            {tokenInfo && (
+              <TokenInfo
+                amount={fromTokenUnit(spendingLimit.spent.amount, tokenInfo.decimals)}
+                title="Amount"
+                token={tokenInfo}
+              />
+            )}
+          </Col>
+          <Col margin="lg">
+            <ResetTimeInfo title="Reset Time" label={resetTimeLabel} />
+          </Col>
+        </Modal.Body>
+      </TxParamsState>
     </Modal>
   )
 }
