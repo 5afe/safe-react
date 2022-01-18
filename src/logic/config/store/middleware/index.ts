e90fc5406e651b463256c4bc637896aa4aa66b5e
iamacook <aaron.cook@gnosis.pm>
2022-01-18 10:12:04 +0100

    build: Separate local `chainId` from store

--- src/logic/config/store/middleware/index.ts
+++ src/logic/config/store/middleware/index.ts
@@ -1,5 +1,4 @@
 import { Action } from 'redux'
-import { _setChainId } from 'src/config'
 
 import { clearCurrentSession } from 'src/logic/currentSession/store/actions/clearCurrentSession'
 import loadCurrentSessionFromStorage from 'src/logic/currentSession/store/actions/loadCurrentSessionFromStorage'
@@ -7,20 +6,16 @@ import { clearSafeList } from 'src/logic/safe/store/actions/clearSafeList'
 import loadSafesFromStorage from 'src/logic/safe/store/actions/loadSafesFromStorage'
 import { Dispatch } from 'src/logic/safe/store/actions/types'
 import { CONFIG_ACTIONS } from '../actions'
-import { currentChainId } from '../selectors'
 import { store as reduxStore } from 'src/store'
 
 export const configMiddleware =
-  ({ dispatch, getState }: typeof reduxStore) =>
+  ({ dispatch }: typeof reduxStore) =>
   (next: Dispatch) =>
   async (action: Action<typeof CONFIG_ACTIONS.SET_CHAIN_ID>) => {
     const handledAction = next(action)
 
-    const state = getState()
     switch (action.type) {
       case CONFIG_ACTIONS.SET_CHAIN_ID: {
-        _setChainId(currentChainId(state))
-
         dispatch(clearSafeList())
         dispatch(clearCurrentSession())
         dispatch(loadSafesFromStorage())
