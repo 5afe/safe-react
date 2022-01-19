5a41caa8000d246c418a459e91c1162585a09b1f
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-01-19 19:08:07 +0100

    Fix css margin

--- src/routes/safe/components/Balances/Collectibles/index.tsx
+++ src/routes/safe/components/Balances/Collectibles/index.tsx
@@ -21,6 +21,7 @@ const useStyles = makeStyles(
     },
     cardOuter: {
       boxShadow: '1px 2px 10px 0 rgba(212, 212, 211, 0.59)',
+      marginBottom: '15px',
     },
     gridRow: {
       boxSizing: 'border-box',
