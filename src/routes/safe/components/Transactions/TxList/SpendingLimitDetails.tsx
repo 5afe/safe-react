5ae3851802aaf6b8c5cff7a7b69bbcf0651aadd2
Diogo Soares <diogo.soares@gnosis.pm>
2022-01-19 18:42:12 +0100

    Updgrade Spending Limit TxList details

--- src/routes/safe/components/Transactions/TxList/SpendingLimitDetails.tsx
+++ src/routes/safe/components/Transactions/TxList/SpendingLimitDetails.tsx
@@ -9,6 +9,7 @@ import { getResetTimeOptions } from 'src/routes/safe/components/Settings/Spendin
 import { AddressInfo, ResetTimeInfo, TokenInfo } from 'src/routes/safe/components/Settings/SpendingLimit/InfoDisplay'
 import { TransactionData, TransactionInfo } from '@gnosis.pm/safe-react-gateway-sdk'
 import { getTxTo } from './utils'
+import { md } from 'src/theme/variables'
 
 const SET_ALLOWANCE = 'setAllowance'
 const DELETE_ALLOWANCE = 'deleteAllowance'
@@ -25,8 +26,25 @@ export const isSpendingLimitMethod = (method?: string): boolean => {
   return isSetAllowance(method) || isDeleteAllowance(method)
 }
 
-const SpendingLimitRow = styled.div`
-  margin-bottom: 16px;
+const StyledSpendingLimitDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${md};
+
+  & > * > *:nth(2)-child {
+    margin-top: 4px;
+  }
+`
+
+const StyledDetailsTitle = styled(Text)`
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`
+
+const StyledInfoBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 `
 
 type SpendingLimitProps = {
@@ -50,29 +68,33 @@ export const ModifySpendingLimitDetails = ({ txData, txInfo }: SpendingLimitProp
   const txTo = getTxTo({ txInfo })
 
   return (
-    <>
-      <SpendingLimitRow>
-        <Text size="xl" strong>
-          Modify spending limit:
-        </Text>
-      </SpendingLimitRow>
-      <SpendingLimitRow>
+    <StyledSpendingLimitDetails>
+      <StyledDetailsTitle size="sm" strong color="placeHolder">
+        Modify spending limit:
+      </StyledDetailsTitle>
+      <StyledInfoBlock>
         <AddressInfo
           title="Beneficiary"
           address={(beneficiary as string) || txTo?.value || '0x'}
           name={txTo?.name || undefined}
           logoUri={txTo?.logoUri || undefined}
+          color="placeHolder"
         />
-      </SpendingLimitRow>
-      <SpendingLimitRow>
+      </StyledInfoBlock>
+      <StyledInfoBlock>
         {tokenInfo && (
-          <TokenInfo amount={fromTokenUnit(amount as string, tokenInfo.decimals)} title="Amount" token={tokenInfo} />
+          <TokenInfo
+            amount={fromTokenUnit(amount as string, tokenInfo.decimals)}
+            title="Amount"
+            token={tokenInfo}
+            color="placeHolder"
+          />
         )}
-      </SpendingLimitRow>
-      <SpendingLimitRow>
-        <ResetTimeInfo title="Reset Time" label={resetTimeLabel} />
-      </SpendingLimitRow>
-    </>
+      </StyledInfoBlock>
+      <StyledInfoBlock>
+        <ResetTimeInfo title="Reset Time" label={resetTimeLabel} color="placeHolder" />
+      </StyledInfoBlock>
+    </StyledSpendingLimitDetails>
   )
 }
 
@@ -86,21 +108,19 @@ export const DeleteSpendingLimitDetails = ({ txData, txInfo }: SpendingLimitProp
   const txTo = getTxTo({ txInfo })
 
   return (
-    <>
-      <SpendingLimitRow>
-        <Text size="xl" strong>
-          Delete spending limit:
-        </Text>
-      </SpendingLimitRow>
-      <SpendingLimitRow>
+    <StyledSpendingLimitDetails>
+      <StyledDetailsTitle size="sm" strong color="placeHolder">
+        Delete spending limit:
+      </StyledDetailsTitle>
+      <StyledInfoBlock>
         <AddressInfo
           title="Beneficiary"
           address={(beneficiary as string) || txTo?.value || '0x'}
           name={txTo?.name || undefined}
           logoUri={txTo?.logoUri || undefined}
         />
-      </SpendingLimitRow>
-      <SpendingLimitRow>{tokenInfo && <TokenInfo amount="" title="Token" token={tokenInfo} />}</SpendingLimitRow>
-    </>
+      </StyledInfoBlock>
+      <StyledInfoBlock>{tokenInfo && <TokenInfo amount="" title="Token" token={tokenInfo} />}</StyledInfoBlock>
+    </StyledSpendingLimitDetails>
   )
 }
