e78cf6a2edc474ab732bee672cc72337196c0d28
Usame Algan <usame.algan@gnosis.pm>
2022-01-21 15:24:25 +0100

    style: Adjust execute checkbox font size

--- src/components/ExecuteCheckbox/index.tsx
+++ src/components/ExecuteCheckbox/index.tsx
@@ -1,21 +1,28 @@
 import { ReactElement } from 'react'
 import { Tooltip } from '@gnosis.pm/safe-react-components'
 import { Checkbox, FormControlLabel } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { styles } from './style'
 import Row from 'src/components/layout/Row'
 import Img from 'src/components/layout/Img'
 import InfoIcon from 'src/assets/icons/info.svg'
+
+const useStyles = makeStyles(styles)
 
 interface ExecuteCheckboxProps {
   onChange: (val: boolean) => unknown
 }
 
 const ExecuteCheckbox = ({ onChange }: ExecuteCheckboxProps): ReactElement => {
+  const classes = useStyles()
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     onChange(e.target.checked)
   }
   return (
     <Row style={{ alignItems: 'center' }}>
       <FormControlLabel
+        classes={{ label: classes.label }}
         control={<Checkbox defaultChecked color="secondary" onChange={handleChange} />}
         label="Execute transaction"
         data-testid="execute-checkbox"
