0b50ae46d05ce90c81deb9095eaa46acb7175fae
Diogo Soares <diogo.soares@gnosis.pm>
2022-01-23 22:36:48 +0100

    Generate creation entities jsx

--- src/routes/safe/components/Transactions/TxList/TxInfoCreation.tsx
+++ src/routes/safe/components/Transactions/TxList/TxInfoCreation.tsx
@@ -1,88 +1,64 @@
-import { Text } from '@gnosis.pm/safe-react-components'
 import { ReactElement } from 'react'
 import styled from 'styled-components'
+import { Text } from '@gnosis.pm/safe-react-components'
+import { Creation } from '@gnosis.pm/safe-react-gateway-sdk'
 
 import { getExplorerInfo } from 'src/config'
 import { formatDateTime } from 'src/utils/date'
 import { Transaction } from 'src/logic/safe/store/models/types/gateway.d'
 import { NOT_AVAILABLE } from './utils'
 import { TxDetailsContainer } from './styled'
-import { Creation } from '@gnosis.pm/safe-react-gateway-sdk'
-import { useKnownAddress } from './hooks/useKnownAddress'
+import { KnownAddressType, useKnownAddress } from './hooks/useKnownAddress'
 import PrefixedEthHashInfo from 'src/components/PrefixedEthHashInfo'
 import { TxDataRow } from './TxDataRow'
 import { md } from 'src/theme/variables'
 
 const StyledTxCreationAddress = styled.div`
   margin-bottom: ${md};
+
+  &:last-of-type {
+    margin-bottom: 0px;
+  }
 `
 
 export const TxInfoCreation = ({ transaction }: { transaction: Transaction }): ReactElement => {
   const txInfo = transaction.txInfo as Creation
   const timestamp = transaction.timestamp
 
-  const creator = useKnownAddress(txInfo.creator)
-  const factory = useKnownAddress(txInfo.factory)
-  const implementation = useKnownAddress(txInfo.implementation)
+  const generateCreatorTxData = (
+    title,
+    creationEntity: Creation['creator' | 'factory' | 'implementation'],
+    address: KnownAddressType,
+  ): ReactElement => (
+    <StyledTxCreationAddress>
+      <Text size="xl" strong>
+        {title}:
+      </Text>
+      {creationEntity ? (
+        <PrefixedEthHashInfo
+          textSize="xl"
+          hash={creationEntity.value}
+          showCopyBtn
+          explorerUrl={getExplorerInfo(creationEntity.value)}
+          name={address?.name || undefined}
+          customAvatar={address?.logoUri || undefined}
+          showAvatar
+        />
+      ) : (
+        <Text size="xl" as="span">
+          {NOT_AVAILABLE}
+        </Text>
+      )}
+    </StyledTxCreationAddress>
+  )
 
   return (
     <TxDetailsContainer>
       <div className="tx-creation">
         <div>
-          <StyledTxCreationAddress>
-            <Text size="xl" strong>
-              Creator:
-            </Text>
-            <PrefixedEthHashInfo
-              textSize="xl"
-              hash={txInfo.creator.value}
-              showCopyBtn
-              explorerUrl={getExplorerInfo(txInfo.creator.value)}
-              name={creator.name || undefined}
-              customAvatar={creator.logoUri || undefined}
-              showAvatar
-            />
-          </StyledTxCreationAddress>
-          <StyledTxCreationAddress>
-            <Text size="xl" strong>
-              Factory:
-            </Text>
-            {txInfo.factory ? (
-              <PrefixedEthHashInfo
-                textSize="xl"
-                hash={txInfo.factory.value}
-                showCopyBtn
-                explorerUrl={getExplorerInfo(txInfo.factory.value)}
-                name={factory?.name || undefined}
-                customAvatar={factory?.logoUri || undefined}
-                showAvatar
-              />
-            ) : (
-              <Text size="xl" as="span">
-                {NOT_AVAILABLE}
-              </Text>
-            )}
-          </StyledTxCreationAddress>
-          <StyledTxCreationAddress>
-            <Text size="xl" strong>
-              Mastercopy:
-            </Text>
-            {txInfo.implementation ? (
-              <PrefixedEthHashInfo
-                textSize="xl"
-                hash={txInfo.implementation.value}
-                showCopyBtn
-                explorerUrl={getExplorerInfo(txInfo.implementation.value)}
-                name={implementation?.name || undefined}
-                customAvatar={implementation?.logoUri || undefined}
-                showAvatar
-              />
-            ) : (
-              <Text size="xl" as="span">
-                {NOT_AVAILABLE}
-              </Text>
-            )}
-          </StyledTxCreationAddress>
+          {generateCreatorTxData('Creator', txInfo.creator, useKnownAddress(txInfo.creator))}
+          {generateCreatorTxData('Factory', txInfo.factory, useKnownAddress(txInfo.factory))}
+          {generateCreatorTxData('Mastercopy', txInfo.implementation, useKnownAddress(txInfo.implementation))}
         </div>
         <div>
           <TxDataRow title="Transaction hash:" value={txInfo.transactionHash} inlineType="hash" />
