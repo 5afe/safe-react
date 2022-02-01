f7f6bd61fbddbcb878215f323a8cebd16e9d1f48
iamacook <aaron.cook@gnosis.pm>
2022-02-01 09:30:04 +0100

    fix: cleanup code

--- src/routes/safe/components/Transactions/TxList/TxOwners.tsx
+++ src/routes/safe/components/Transactions/TxList/TxOwners.tsx
@@ -1,6 +1,10 @@
 import { ReactElement, useState } from 'react'
-import styled from 'styled-components'
-import { Step, StepConnector, StepContent, StepLabel, Stepper } from '@material-ui/core'
+import styled, { AnyStyledComponent } from 'styled-components'
+import Step from '@material-ui/core/Step'
+import StepConnector from '@material-ui/core/StepConnector'
+import StepContent from '@material-ui/core/StepContent'
+import StepLabel from '@material-ui/core/StepLabel'
+import Stepper from '@material-ui/core/Stepper'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import ControlPointIcon from '@material-ui/icons/ControlPoint'
 import RadioButtonUncheckedOutlinedIcon from '@material-ui/icons/RadioButtonUncheckedOutlined'
@@ -10,10 +14,12 @@ import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined'
 import { ExpandedTxDetails, isMultiSigExecutionDetails } from 'src/logic/safe/store/models/types/gateway.d'
 import { AddressInfo } from 'src/routes/safe/components/Transactions/TxList/AddressInfo'
 import { isCancelTxDetails } from 'src/routes/safe/components/Transactions/TxList/utils'
-import { black300, primary400 } from 'src/theme/variables'
+import { black300, gray500, primary400 } from 'src/theme/variables'
 
 // Icons
-const getStyledIcon = (icon: typeof ControlPointIcon): typeof ControlPointIcon => {
+
+// All icons from MUI share the same type
+const getStyledIcon = (icon: typeof ControlPointIcon): AnyStyledComponent => {
   return styled(icon)`
     height: 20px;
     width: 20px;
@@ -22,16 +28,13 @@ const getStyledIcon = (icon: typeof ControlPointIcon): typeof ControlPointIcon =
 }
 
 const TxCreationIcon = getStyledIcon(ControlPointIcon)
-
 const TxRejectionIcon = getStyledIcon(CancelOutlinedIcon)
-
 const CheckIcon = getStyledIcon(CheckCircleIcon)
 
 const CircleIcon = styled(getStyledIcon(RadioButtonUncheckedOutlinedIcon))`
   stroke: currentColor;
   stroke-width: 1px;
 `
-
 const DotIcon = styled(FiberManualRecordIcon)`
   height: 14px;
   width: 14px;
@@ -44,11 +47,11 @@ const StyledStepper = styled(Stepper)`
 `
 
 const StyledStepConnector = styled(StepConnector)`
-  padding: 0;
+  padding: 3px 0;
 
   .MuiStepConnector-line {
     margin-left: -1px;
-    border-color: gray500;
+    border-color: ${gray500};
     border-left-width: 2px;
     min-height: 14px;
   }
@@ -71,32 +74,19 @@ type StyledStepProps = {
   color: StepColor
 }
 const StyledStep = styled(Step)<StyledStepProps>`
-  height: max-content;
-
   .MuiStepLabel-label {
     font-weight: ${({ bold = false }) => (bold ? 'bold' : 'normal')};
     color: ${({ color }) => getStepColor(color)};
   }
 
-  .MuiStepLabel-completed {
-    color: ${getStepColor('green')};
-  }
-
   .MuiStepLabel-iconContainer {
     color: ${({ color }) => getStepColor(color)};
     align-items: center;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    background-color: white;
   }
+`
 
-  .MuiStepLabel-active {
-    color: ${getStepColor('orange')};
-  }
-
-  .MuiStepContent-root {
-    color: ${getStepColor('gray')};
-  }
+const StyledStepContent = styled(StepContent)`
+  color: ${black300};
 `
 
 export const TxOwners = ({
@@ -106,12 +96,12 @@ export const TxOwners = ({
   txDetails: ExpandedTxDetails
   isPending: boolean
 }): ReactElement | null => {
-  const { txInfo, detailedExecutionInfo } = txDetails
-
-  const [hideConfirmations, showConfirmations] = useState<boolean>(false)
+  const [showConfirmations, setShowConfirmations] = useState<boolean>(false)
   const toggleHide = () => {
-    showConfirmations((prev) => !prev)
+    setShowConfirmations((prev) => !prev)
   }
+
+  const { txInfo, detailedExecutionInfo } = txDetails
 
   if (!detailedExecutionInfo || !isMultiSigExecutionDetails(detailedExecutionInfo)) {
     return null
@@ -122,7 +112,7 @@ export const TxOwners = ({
   const isExecuted = !!detailedExecutionInfo.executor
 
   return (
-    <StyledStepper orientation="vertical" nonLinear activeStep={-1} connector={<StyledStepConnector />}>
+    <StyledStepper orientation="vertical" nonLinear connector={<StyledStepConnector />}>
       {isCancelTxDetails(txInfo) ? (
         <StyledStep bold color="green">
           <StepLabel icon={<TxRejectionIcon />}>On-chain rejection created</StepLabel>
@@ -135,12 +125,12 @@ export const TxOwners = ({
       <StyledStep bold color={isConfirmed ? 'green' : 'orange'}>
         <StepLabel icon={isConfirmed ? <CheckIcon /> : <CircleIcon />}>
           Confirmations{' '}
-          <span style={{ color: '#B2BBC0', fontWeight: 'normal' }}>
+          <span style={{ color: black300, fontWeight: 'normal' }}>
             ({`${detailedExecutionInfo.confirmations.length} of ${detailedExecutionInfo.confirmationsRequired}`})
           </span>
         </StepLabel>
       </StyledStep>
-      {!hideConfirmations &&
+      {showConfirmations &&
         detailedExecutionInfo.confirmations.map(({ signer }) => (
           <StyledStep key={signer.value} bold color="green">
             <StepLabel icon={<DotIcon />}>
@@ -155,8 +145,8 @@ export const TxOwners = ({
         ))}
       {detailedExecutionInfo.confirmations.length > 0 && (
         <StyledStep color="green">
-          <StepLabel icon={<DotIcon strokeWidth={14} />} onClick={toggleHide} style={{ cursor: 'pointer' }}>
-            {hideConfirmations ? 'Show all' : 'Hide all'}
+          <StepLabel icon={<DotIcon />} onClick={toggleHide} style={{ cursor: 'pointer' }}>
+            {showConfirmations ? 'Show all' : 'Hide all'}
           </StepLabel>
         </StyledStep>
       )}
@@ -164,8 +154,8 @@ export const TxOwners = ({
         <StepLabel icon={isExecuted ? <CheckIcon /> : <CircleIcon />}>
           {isExecuted ? 'Executed' : isPending ? 'Executing' : 'Execution'}
         </StepLabel>
-        {!isConfirmed && !isPending && (
-          <StepContent>
+        {!isPending && !isExecuted && (
+          <StyledStepContent>
             {detailedExecutionInfo.executor ? (
               <AddressInfo
                 address={detailedExecutionInfo.executor.value}
@@ -176,7 +166,7 @@ export const TxOwners = ({
             ) : (
               'Can be executed once the threshold is reached'
             )}
-          </StepContent>
+          </StyledStepContent>
         )}
       </StyledStep>
     </StyledStepper>
