4459ea0f13b92cb8545923a5e64af70666627350
Aaron Cook <aaron.cook@gnosis.pm>
2022-02-02 14:57:57 +0100

    feat: track pending transactions by `txId` (#3410)

--- src/logic/safe/store/selectors/pendingTransactions.ts
+++ src/logic/safe/store/selectors/pendingTransactions.ts
@@ -2,11 +2,7 @@ import { TransactionStatus } from '@gnosis.pm/safe-react-gateway-sdk'
 import { createSelector } from 'reselect'
 
 import { AppReduxState } from 'src/store'
-import {
-  isMultiSigExecutionDetails,
-  LocalTransactionStatus,
-  Transaction,
-} from 'src/logic/safe/store/models/types/gateway.d'
+import { LocalTransactionStatus, Transaction } from 'src/logic/safe/store/models/types/gateway.d'
 import { PendingTransactionsState, PENDING_TRANSACTIONS_ID } from 'src/logic/safe/store/reducer/pendingTransactions'
 import { currentChainId } from 'src/logic/config/store/selectors'
 import { ChainId } from 'src/config/chain'
@@ -25,29 +21,16 @@ const pendingTxsByChain = createSelector(
 
 export const isTxPending = createSelector(
   pendingTxsByChain,
-  (_: AppReduxState, safeTxHash: string) => safeTxHash,
-  (pendingTxs: PendingTransactionsState[ChainId], safeTxHash: string): boolean => {
-    return pendingTxs ? !!pendingTxs?.[safeTxHash] : false
+  (_: AppReduxState, id: string) => id,
+  (pendingTxs: PendingTransactionsState[ChainId], id: string): boolean => {
+    return pendingTxs ? !!pendingTxs?.[id] : false
   },
 )
-
-// @FIXME: this is a dirty hack.
-// Ask backend to add safeTxHash in tx list items.
-export const getSafeTxHashFromId = (id: string): string => {
-  return id.split('_').pop() || ''
-}
 
 export const selectTxStatus = createSelector(
   pendingTxsByChain,
   (_: AppReduxState, tx: Transaction) => tx,
   (pendingTxs: PendingTransactionsState[ChainId], tx: Transaction): TransactionStatus => {
-    const { detailedExecutionInfo } = tx.txDetails || {}
-
-    const safeTxHash =
-      detailedExecutionInfo && isMultiSigExecutionDetails(detailedExecutionInfo)
-        ? detailedExecutionInfo.safeTxHash
-        : getSafeTxHashFromId(tx.id)
-
-    return !!pendingTxs?.[safeTxHash] ? LocalTransactionStatus.PENDING : tx.txStatus
+    return !!pendingTxs?.[tx.id] ? LocalTransactionStatus.PENDING : tx.txStatus
   },
 )
