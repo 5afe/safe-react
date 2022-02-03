257c8fb4cdf88973f147c2a069ff399186e9e3f5
iamacook <aaron.cook@gnosis.pm>
2022-02-03 11:50:46 +0100

    feat: create `Track` HoC

---
+++ src/components/Track/index.ts
@@ -0,0 +1,20 @@
+import { ReactElement, cloneElement } from 'react'
+
+const TRACK_ID_PROP = 'data-track-id'
+const TRACK_PAYLOAD_PROP = 'data-track-payload'
+
+type Props = {
+  children: ReactElement
+  [TRACK_ID_PROP]: string
+  [TRACK_PAYLOAD_PROP]: unknown
+}
+
+const Track = ({ children, [TRACK_ID_PROP]: trackId, [TRACK_PAYLOAD_PROP]: trackPayload }: Props): ReactElement => {
+  return cloneElement(children, {
+    ...children.props,
+    [TRACK_ID_PROP]: trackId,
+    [TRACK_PAYLOAD_PROP]: trackPayload ? JSON.stringify(trackPayload) : undefined,
+  })
+}
+
+export default Track
