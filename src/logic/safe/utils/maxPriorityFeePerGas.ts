2e423a776fc48723e14cfd5113103efadca402cf
iamacook <aaron.cook@gnosis.pm>
2022-02-08 09:36:36 +0100

    fix: don't round, median reward, catch w/ default

--- src/logic/safe/utils/maxPriorityFeePerGas.ts
+++ src/logic/safe/utils/maxPriorityFeePerGas.ts
@@ -10,22 +10,25 @@ export const clampMaxPrioFeePerGas = (maxPriorityFeePerGas: number, maxFeePerGas
     return Math.max(MIN_MAX_PRIO_FEE, MAX_MAX_PRIO_FEE)
   }
 
-  // We must round as BN does not support decimals
-  return Math.round(maxPriorityFeePerGas)
+  return maxPriorityFeePerGas
 }
 
+const DEFAULT_MAX_PRIO_FEE = 2.5e9 // 2.5 GWEI
 export const getMedianMaxPrioFee = async (): Promise<number> => {
   const HISTORICAL_BLOCKS = 4
 
   const web3 = getWeb3ReadOnly()
 
-  // [0] slow, [1] average, [2] fast reward percentiles ([3] pending)
-  const blocks = await web3.eth.getFeeHistory(HISTORICAL_BLOCKS, 'pending', [25, 50, 75])
+  try {
+    // [0] slow, [1] average, [2] fast reward percentiles ([3] pending)
+    const blocks = await web3.eth.getFeeHistory(HISTORICAL_BLOCKS, 'pending', [25, 50, 75])
 
-  const baseFeePerGas = hexToNumber(blocks.baseFeePerGas[1])
-  const maxPriorityFeePerGas =
-    blocks.reward.reduce((acc, reward) => acc + hexToNumber(reward[1]), 0) / blocks.reward.length
-  const maxFeePerGas = baseFeePerGas + maxPriorityFeePerGas
+    const baseFeePerGas = hexToNumber(blocks.baseFeePerGas[1])
+    const maxPriorityFeePerGas = hexToNumber(blocks.reward[1][1])
+    const maxFeePerGas = baseFeePerGas + maxPriorityFeePerGas
 
-  return clampMaxPrioFeePerGas(maxPriorityFeePerGas, maxFeePerGas)
+    return clampMaxPrioFeePerGas(maxPriorityFeePerGas, maxFeePerGas)
+  } catch {
+    return DEFAULT_MAX_PRIO_FEE
+  }
 }
