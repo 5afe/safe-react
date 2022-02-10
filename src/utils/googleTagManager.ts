bee065e05ba076e7bc00c6ff80839e29a72a7fb1
Aaron Cook <aaron.cook@gnosis.pm>
2022-02-10 10:18:50 +0100

    fix: migrate anonymous page tracking to GTM (#3426)
    * fix: migrate pageview tracking to GTM
    
    * fix: cleanup event emission

--- src/utils/googleTagManager.ts
+++ src/utils/googleTagManager.ts
@@ -1,4 +1,9 @@
+import { useEffect } from 'react'
 import TagManager, { TagManagerArgs } from 'react-gtm-module'
+import { matchPath } from 'react-router-dom'
+import { Location } from 'history'
+
+import { ADDRESSED_ROUTE, history, SAFE_ADDRESS_SLUG, SAFE_ROUTES, TRANSACTION_ID_SLUG } from 'src/routes/routes'
 
 import {
   GOOGLE_TAG_MANAGER_ID,
@@ -7,6 +12,27 @@ import {
   IS_PRODUCTION,
   GOOGLE_TAG_MANAGER_DEVELOPMENT_AUTH,
 } from 'src/utils/constants'
+
+const getAnonymizedLocation = ({ pathname, search, hash }: Location = history.location): string => {
+  const ANON_SAFE_ADDRESS = 'SAFE_ADDRESS'
+  const ANON_TX_ID = 'TRANSACTION_ID'
+
+  let anonPathname = pathname
+
+  // Anonymize safe address
+  const safeAddressMatch = matchPath(pathname, { path: ADDRESSED_ROUTE })
+  if (safeAddressMatch) {
+    anonPathname = anonPathname.replace(safeAddressMatch.params[SAFE_ADDRESS_SLUG], ANON_SAFE_ADDRESS)
+  }
+
+  // Anonymise transaction id
+  const txIdMatch = matchPath(pathname, { path: SAFE_ROUTES.TRANSACTIONS_SINGULAR })
+  if (txIdMatch) {
+    anonPathname = anonPathname.replace(txIdMatch.params[TRANSACTION_ID_SLUG], ANON_TX_ID)
+  }
+
+  return anonPathname + search + hash
+}
 
 type GTMEnvironment = 'LIVE' | 'LATEST' | 'DEVELOPMENT'
 type GTMEnvironmentArgs = Required<Pick<TagManagerArgs, 'auth' | 'preview'>>
@@ -26,6 +52,14 @@ const GTM_ENV_AUTH: Record<GTMEnvironment, GTMEnvironmentArgs> = {
   },
 }
 
+enum GTM_EVENTS {
+  PAGE_VIEW = 'pageview',
+}
+
+enum GTM_DATA_LAYER_VARS {
+  PAGE = 'page', // Tracked alongside 'pageview' event
+}
+
 export const loadGoogleTagManager = (): void => {
   const GTM_ENVIRONMENT = IS_PRODUCTION ? GTM_ENV_AUTH.LIVE : GTM_ENV_AUTH.DEVELOPMENT
 
@@ -37,5 +71,27 @@ export const loadGoogleTagManager = (): void => {
   TagManager.initialize({
     gtmId: GOOGLE_TAG_MANAGER_ID,
     ...GTM_ENVIRONMENT,
+    dataLayer: {
+      // Page Safe was loaded at
+      [GTM_DATA_LAYER_VARS.PAGE]: getAnonymizedLocation(),
+    },
   })
+}
+
+export const useGTMPageTracking = (): void => {
+  useEffect(() => {
+    const unsubscribe = history.listen((location) => {
+      TagManager.dataLayer({
+        dataLayer: {
+          // Must emit (custom) event in order to trigger page tracking
+          event: GTM_EVENTS.PAGE_VIEW,
+          [GTM_DATA_LAYER_VARS.PAGE]: getAnonymizedLocation(location),
+        },
+      })
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 }
