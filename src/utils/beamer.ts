3b72395354124e0584253c166a2ed072047651cb
Diogo Soares <diogo.soares@gnosis.pm>
2022-02-14 11:20:23 +0000

    fix: Add Beamer params and methods type definitions

--- src/utils/beamer.ts
+++ src/utils/beamer.ts
@@ -1,17 +1,22 @@
 import { BEAMER_ID } from './constants'
 import { MutableRefObject } from 'react'
+import { BeamerConfig } from 'src/types/Beamer'
 
 const BEAMER_URL = 'https://app.getbeamer.com/js/beamer-embed.js'
 
+const APP_ID = BEAMER_ID
+const baseConfig: BeamerConfig = {
+  product_id: APP_ID,
+}
+
 export const loadBeamer = async (scriptRef: MutableRefObject<HTMLScriptElement | undefined>): Promise<void> => {
-  const APP_ID = BEAMER_ID
   if (!APP_ID) {
     console.error('[Beamer] - In order to use Beamer you need to add an appID')
     return
   }
 
   window.beamer_config = {
-    product_id: APP_ID,
+    ...baseConfig,
     selector: 'whats-new-button',
     display: 'left',
     button: false,
@@ -35,6 +40,7 @@ export const closeBeamer = (scriptRef: MutableRefObject<HTMLScriptElement | unde
   if (!window.Beamer || !scriptRef.current) return
   window.Beamer.destroy()
   window.Beamer.update({
+    ...baseConfig,
     selector: undefined,
   })
   scriptRef.current.remove()
