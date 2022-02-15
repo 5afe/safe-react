5419bb60209999d0eabaf0d38ec4ff13eeb5a35f
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-02-15 16:01:58 +0100

    Fix: suggest upgrading Safe in the estimation error message

--- src/components/TransactionFailText/index.tsx
+++ src/components/TransactionFailText/index.tsx
@@ -6,10 +6,12 @@ import Img from 'src/components/layout/Img'
 import InfoIcon from 'src/assets/icons/info_red.svg'
 
 import { useSelector } from 'react-redux'
-import { currentSafeThreshold } from 'src/logic/safe/store/selectors'
+import { currentSafeCurrentVersion, currentSafeThreshold } from 'src/logic/safe/store/selectors'
 import { shouldSwitchWalletChain } from 'src/logic/wallets/store/selectors'
 import { grantedSelector } from 'src/routes/safe/container/selector'
 import { EstimationStatus } from 'src/logic/hooks/useEstimateTransactionGas'
+import { hasFeature } from 'src/logic/safe/utils/safeVersion'
+import { FEATURES } from '@gnosis.pm/safe-react-gateway-sdk'
 
 const styles = createStyles({
   executionWarningRow: {
@@ -38,6 +40,7 @@ export const TransactionFailText = ({
   const threshold = useSelector(currentSafeThreshold)
   const isWrongChain = useSelector(shouldSwitchWalletChain)
   const isGranted = useSelector(grantedSelector)
+  const safeVersion = useSelector(currentSafeCurrentVersion)
 
   if (estimationStatus !== EstimationStatus.FAILURE && !(isCreation && !isGranted)) {
     return null
@@ -54,8 +57,14 @@ export const TransactionFailText = ({
   const defaultMsg = `This transaction will most likely fail. ${errorDesc}`
   const notOwnerMsg = `You are currently not an owner of this Safe and won't be able to submit this transaction.`
   const wrongChainMsg = 'Your wallet is connected to the wrong chain.'
+  const updateSafeMessage = `This transaction will most likely fail. Please consider updating your Safe version (Settings → Safe Details → Update Safe).`
 
-  const error = isGranted ? defaultMsg : isWrongChain ? wrongChainMsg : isCreation ? notOwnerMsg : defaultMsg
+  let error = isGranted ? defaultMsg : isWrongChain ? wrongChainMsg : isCreation ? notOwnerMsg : defaultMsg
+
+  // For legacy Safes, esp. on Gnosis Chain
+  if (estimationStatus === EstimationStatus.FAILURE && !hasFeature(FEATURES.SAFE_TX_GAS_OPTIONAL, safeVersion)) {
+    error = updateSafeMessage
+  }
 
   return (
     <Row align="center">
