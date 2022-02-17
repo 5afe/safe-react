22c160ac9dc989782c6e9e4b0165af71171a4b02
iamacook <aaron.cook@gnosis.pm>
2022-02-17 13:28:52 +0000

    fix: propose once `txHash` + store with pending

--- src/logic/safe/store/actions/createTransaction.ts
+++ src/logic/safe/store/actions/createTransaction.ts
@@ -105,7 +105,7 @@ export class TxSender {
   txId: string
 
   // On transaction completion (either confirming or executing)
-  async onComplete(signature?: string, confirmCallback?: ConfirmEventHandler): Promise<void> {
+  async onComplete(signature?: string, confirmCallback?: ConfirmEventHandler, txHash?: string): Promise<void> {
     const { txArgs, safeTxHash, txProps, dispatch, notifications, isFinalization } = this
 
     // Propose the tx to the backend
@@ -121,10 +121,9 @@ export class TxSender {
       }
     }
 
-    // If threshold reached except for last sig, and owner chooses to execute the created tx immediately
-    // we retrieve txId of newly created tx from the proposal response
-    if (isFinalization && txDetails) {
-      dispatch(addPendingTransaction({ id: txDetails.txId }))
+    const id = txDetails?.txId || this.txId
+    if (isFinalization && id && txHash) {
+      dispatch(addPendingTransaction({ id, txHash }))
     }
 
     notifications.closePending()
@@ -184,22 +183,21 @@ export class TxSender {
     )
   }
 
-  async sendTx(): Promise<string> {
-    const { txArgs, isFinalization, from, safeTxHash, txProps, dispatch, txId } = this
+  async sendTx(confirmCallback?: ConfirmEventHandler): Promise<string> {
+    const { txArgs, isFinalization, from, safeTxHash, txProps } = this
 
     const tx = isFinalization ? getExecutionTransaction(txArgs) : getApprovalTransaction(this.safeInstance, safeTxHash)
     const sendParams = createSendParams(from, txProps.ethParameters || {})
 
-    // When signing on-chain don't mark as pending as it is never removed
-    if (isFinalization) {
-      // Finalising existing transaction (txId exists)
-      if (txId) {
-        dispatch(addPendingTransaction({ id: txId }))
-      }
-      aboutToExecuteTx.setNonce(txArgs.nonce)
-    }
-
-    return await tx.send(sendParams).then(({ transactionHash }) => transactionHash)
+    return await tx
+      .send(sendParams)
+      .once('transactionHash', (txHash) => {
+        if (isFinalization) {
+          aboutToExecuteTx.setNonce(txArgs.nonce)
+        }
+        this.onComplete(undefined, confirmCallback, txHash)
+      })
+      .then(({ transactionHash }) => transactionHash)
   }
 
   async submitTx(
@@ -224,8 +222,7 @@ export class TxSender {
 
     // On-chain signature or execution
     try {
-      await this.sendTx()
-      this.onComplete(undefined, confirmCallback)
+      await this.sendTx(confirmCallback)
     } catch (err) {
       this.onError(err, errorCallback)
     }
