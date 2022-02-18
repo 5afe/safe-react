9388f725245d4224c275377cadb68885f591b94a
Aaron <aaron@clabs.co>
2022-02-18 12:21:52 -0800

    block out testnets until we have deployments

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -1,11 +1,11 @@
 import local from './local'
 import mainnet from './mainnet'
-import alfajores from './alfajores'
-import baklava from './baklava'
+// import alfajores from './alfajores'
+// import baklava from './baklava'
 
 export default {
   local,
   mainnet,
-  alfajores,
-  baklava,
+  // alfajores,
+  // baklava,
 }
