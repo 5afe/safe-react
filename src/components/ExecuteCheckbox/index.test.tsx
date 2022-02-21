c495fe9a6c7e2b83280ab777262aece431b3b5af
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-02-21 13:17:01 +0100

    fix: Remove defaultChecked attribute from exec tx checkbox (#3547)

--- src/components/ExecuteCheckbox/index.test.tsx
+++ src/components/ExecuteCheckbox/index.test.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { fireEvent, render, screen, waitFor, act } from 'src/utils/test-utils'
 import { history } from 'src/routes/routes'
 import ExecuteCheckbox from '.'
@@ -7,8 +8,20 @@ describe('ExecuteCheckbox', () => {
     const onChange = jest.fn()
     history.push('/rin:0xb3b83bf204C458B461de9B0CD2739DB152b4fa5A/balances')
 
+    const ControlledInputWrapper = () => {
+      const [checked, setChecked] = useState<boolean>(true)
+      return (
+        <ExecuteCheckbox
+          checked={checked}
+          onChange={(value) => {
+            setChecked(value)
+            onChange(value)
+          }}
+        />
+      )
+    }
     await act(async () => {
-      render(<ExecuteCheckbox onChange={onChange} />)
+      render(<ControlledInputWrapper />)
     })
 
     await waitFor(() => {
