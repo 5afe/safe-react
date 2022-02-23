2c2de004ff621426158d3deed3aa0f707feb51c9
iamacook <aaron.cook@gnosis.pm>
2022-02-23 11:46:21 +0100

    fix: reset QR code if chain changes

--- src/logic/wallets/pairing/hooks/usePairing.ts
+++ src/logic/wallets/pairing/hooks/usePairing.ts
@@ -1,18 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
 
+import { ChainId } from 'src/config/chain'
+import { currentChainId } from 'src/logic/config/store/selectors'
 import onboard from 'src/logic/wallets/onboard'
 import { initPairing, isPairingConnected, isPairingModule } from 'src/logic/wallets/pairing/utils'
 
+const resetPairing = () => {
+  if (isPairingModule() && !isPairingConnected()) {
+    onboard().walletReset()
+  }
+}
+
 const usePairing = (): void => {
+  const chainId = useSelector(currentChainId)
+  const prevChainId = useRef<ChainId>(chainId)
+
   useEffect(() => {
     if (!isPairingConnected()) {
+      // Reset QR code if chain changed
+      if (chainId !== prevChainId.current) {
+        resetPairing()
+        prevChainId.current = chainId
+      }
+
       initPairing()
     }
 
     return () => {
-      if (isPairingModule() && !isPairingConnected()) {
-        onboard().walletReset()
-      }
+      resetPairing()
     }
   }, [])
 }
