ce4ac697f508143cbe1ddbd9ad72565ab7cd420f
iamacook <aaron.cook@gnosis.pm>
2022-03-14 11:28:17 +0100

    fix: track via listener

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, Fragment } from 'react'
+import { ReactElement, Fragment, useEffect, useRef } from 'react'
 
 import { GTM_EVENT, trackEvent } from 'src/utils/googleTagManager'
 
@@ -10,17 +10,27 @@ type Props = {
 }
 
 const Track = ({ children, ...trackData }: Props): typeof children => {
-  if (children.type === Fragment) {
-    throw new Error('Fragments cannot be tracked.')
-  }
+  const el = useRef<HTMLDivElement>(null)
 
-  const event = {
+  const event: Parameters<typeof trackEvent>[0] = {
     event: GTM_EVENT.CLICK,
     ...trackData,
   }
 
+  useEffect(() => {
+    const handleClick = () => trackEvent(event)
+
+    // We cannot use onClick as events in children do not always bubble up
+    el.current?.addEventListener('click', handleClick)
+    return el.current?.removeEventListener('click', handleClick)
+  }, [el.current])
+
+  if (children.type === Fragment) {
+    throw new Error('Fragments cannot be tracked.')
+  }
+
   return (
-    <div data-track={`${event.category}: ${event.action}`} onClick={() => trackEvent(event)}>
+    <div data-track={`${event.category}: ${event.action}`} ref={el}>
       {children}
     </div>
   )
