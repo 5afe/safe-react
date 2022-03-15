427848e93678965f078c49f18382b5c471d3288f
Aaron Cook <aaron.cook@gnosis.pm>
2022-03-15 12:33:05 +0100

    fix: shrink input label when placeholder exists (#3666)
    * fix: shrink input label when placeholder exists
    
    * fix: remove unnecessary `shrink`

--- src/components/forms/TextField/index.tsx
+++ src/components/forms/TextField/index.tsx
@@ -1,4 +1,4 @@
-import MuiTextField from '@material-ui/core/TextField'
+import MuiTextField, { TextFieldProps } from '@material-ui/core/TextField'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import { lg } from 'src/theme/variables'
@@ -39,6 +39,8 @@ type Props = {
   disabled?: boolean
   rowsMax?: number
   className?: string
+  placeholder?: string
+  InputLabelProps?: TextFieldProps['InputLabelProps']
 }
 
 const TextField = (props: Props): React.ReactElement => {
@@ -50,6 +52,7 @@ const TextField = (props: Props): React.ReactElement => {
     rows,
     testId,
     text,
+    InputLabelProps: _InputLabelProps,
     ...rest
   } = props
   const classes = useStyles()
@@ -74,6 +77,10 @@ const TextField = (props: Props): React.ReactElement => {
     className: `${inputRoot} ${statusClasses}`,
     disableUnderline: disableUnderline,
   }
+  const InputLabelProps = {
+    ..._InputLabelProps,
+    ...(rest?.placeholder && { shrink: true }),
+  }
 
   return (
     <MuiTextField
@@ -81,6 +88,7 @@ const TextField = (props: Props): React.ReactElement => {
       helperText={hasError && showError ? errorMessage : helperText || ''}
       inputProps={inputProps} // blank in order to force to have helper text
       InputProps={inputRootProps}
+      InputLabelProps={InputLabelProps}
       multiline={multiline}
       name={name}
       onChange={onChange}
