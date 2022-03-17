1f4ad55c0912ea2cb1f6c23fdedea28a811ca64e
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-03-17 15:05:14 +0100

    Epic: Dashboard

---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from 'react'
+import styled from 'styled-components'
+import { Breadcrumb, BreadcrumbElement, Menu } from '@gnosis.pm/safe-react-components'
+
+import Page from 'src/components/layout/Page'
+import Row from 'src/components/layout/Row'
+import Col from 'src/components/layout/Col'
+
+const Card = styled.div`
+  background: #fff;
+  padding: 0 20px 30px;
+  border-radius: 8px;
+  flex: 1;
+  max-width: 500px;
+  margin: 10px;
+`
+
+function Home(): ReactElement {
+  return (
+    <Page>
+      <Menu>
+        <Col start="sm" sm={6} xs={12}>
+          <Breadcrumb>
+            <BreadcrumbElement iconType="assets" text="Dashboard" color="primary" />
+          </Breadcrumb>
+        </Col>
+
+        <Col end="sm" sm={6} xs={12} />
+      </Menu>
+
+      <Row>
+        <Card>
+          <h2>Owned Safes</h2>
+        </Card>
+
+        <Card>
+          <h2>Transactions to Sign</h2>
+        </Card>
+
+        <Card>
+          <h2>Gas Fees</h2>
+        </Card>
+      </Row>
+    </Page>
+  )
+}
+
+export default Home
