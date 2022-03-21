629006979cb192adcf8b4156f03c3d0f3126ed8b
iamacook <aaron.cook@gnosis.pm>
2022-03-21 18:10:17 +0100

    fix: fetch singular txs not in store

--- src/routes/safe/components/Transactions/TxList/TxSingularDetails.tsx
+++ src/routes/safe/components/Transactions/TxList/TxSingularDetails.tsx
@@ -1,12 +1,13 @@
 import { ReactElement, useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import { Loader } from '@gnosis.pm/safe-react-components'
-import { shallowEqual, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import { TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
 
 import { isTxQueued } from 'src/logic/safe/store/models/types/gateway.d'
 import {
   extractPrefixedSafeAddress,
+  extractSafeAddress,
   generateSafeRoute,
   SafeRouteSlugs,
   SAFE_ROUTES,
@@ -18,6 +19,13 @@ import { TxLocationContext } from './TxLocationProvider'
 import { AppReduxState } from 'src/store'
 import { logError, Errors } from 'src/logic/exceptions/CodedException'
 import { fetchSafeTransaction } from 'src/logic/safe/transactions/api/fetchSafeTransaction'
+import { makeTxFromDetails } from './utils'
+import {
+  addQueuedTransactions,
+  addHistoryTransactions,
+} from 'src/logic/safe/store/actions/transactions/gatewayTransactions'
+import { HistoryPayload, QueuedPayload } from 'src/logic/safe/store/reducer/gatewayTransactions'
+import { currentChainId } from 'src/logic/config/store/selectors'
 import { QueueTxList } from './QueueTxList'
 import { HistoryTxList } from './HistoryTxList'
 import FetchError from '../../FetchError'
@@ -26,7 +34,9 @@ const TxSingularDetails = (): ReactElement => {
   const { [TRANSACTION_ID_SLUG]: safeTxHash = '' } = useParams<SafeRouteSlugs>()
   const [fetchedTx, setFetchedTx] = useState<TransactionDetails>()
   const [error, setError] = useState<Error>()
+  const dispatch = useDispatch()
   const history = useHistory()
+  const chainId = useSelector(currentChainId)
 
   const indexedTx = useSelector(
     (state: AppReduxState) =>
@@ -69,6 +79,27 @@ const TxSingularDetails = (): ReactElement => {
       isCurrent = false
     }
   }, [history, safeTxHash, setFetchedTx])
+
+  // Add the tx to the store
+  useEffect(() => {
+    if (!fetchedTx) return
+
+    // Format the tx details into a History or Queue-like tx item
+    const listItemTx = makeTxFromDetails(fetchedTx)
+    const payload: HistoryPayload | QueuedPayload = {
+      chainId,
+      safeAddress: extractSafeAddress(),
+      values: [
+        {
+          transaction: listItemTx,
+          type: 'TRANSACTION', // Other types are discarded in reducer
+          conflictType: 'None', // Not used in reducer
+        },
+      ],
+    }
+    // And add it to the corresponding list in the store
+    dispatch(isTxQueued(listItemTx.txStatus) ? addQueuedTransactions(payload) : addHistoryTransactions(payload))
+  }, [fetchedTx, chainId, dispatch])
 
   if (!indexedTx && error) {
     const safeParams = extractPrefixedSafeAddress()
