64456ae119ea499b768c5760491bfdd0b8013adb
Diogo Soares <diogo.soares@gnosis.pm>
2022-03-28 17:26:28 +0200

    feat: use mock data to render pending transactions

--- src/components/PendingTxsList/index.tsx
+++ src/components/PendingTxsList/index.tsx
@@ -1,24 +1,16 @@
-import { Identicon } from '@gnosis.pm/safe-react-components'
-import { getTransactionQueue, TransactionSummary, TransferDirection } from '@gnosis.pm/safe-react-gateway-sdk'
+import { Identicon, Text } from '@gnosis.pm/safe-react-components'
+import { TransactionSummary, TransferDirection } from '@gnosis.pm/safe-react-gateway-sdk'
 import { List } from '@material-ui/core'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement } from 'react'
 import { Link } from 'react-router-dom'
 
 import { CircleDot } from 'src/components/AppLayout/Header/components/CircleDot'
 import { getChainById } from 'src/config'
 import { ChainId } from 'src/config/chain.d'
-import useLocalSafes from 'src/logic/safe/hooks/useLocalSafes'
-import {
-  isMultisigExecutionInfo,
-  isTransactionSummary,
-  isTransferTxInfo,
-  LocalTransactionStatus,
-} from 'src/logic/safe/store/models/types/gateway.d'
+import { isTransferTxInfo } from 'src/logic/safe/store/models/types/gateway.d'
 import { generateSafeRoute, SAFE_ROUTES } from 'src/routes/routes'
-import { grey400, sm } from 'src/theme/variables'
-import { checksumAddress } from 'src/utils/checksumAddress'
-import { GATEWAY_URL } from 'src/utils/constants'
+import { grey400, sm, lg } from 'src/theme/variables'
 import { CustomIconText } from 'src/components/CustomIconText'
 import { getAssetInfo } from 'src/routes/safe/components/Transactions/TxList/utils'
 import { TxInfo } from 'src/routes/safe/components/Transactions/TxList/TxCollapsed'
@@ -29,6 +21,17 @@ import SettingsTxIcon from 'src/routes/safe/components/Transactions/TxList/asset
 import styled from 'styled-components'
 import Spacer from 'src/components/Spacer'
 
+// Replaces the to be implemented BE endpoint
+import mockData from './mockData.json'
+type MockType = {
+  safeAddress: string
+  chainId: ChainId
+  totalPending: number
+  summaries: TransactionSummary[]
+}
+const mock = mockData as unknown as MockType[]
+
+/**
 const getTxsAwaitingConfirmationByChainId = async (
   chainId: string,
   safeAddress: string,
@@ -51,8 +54,11 @@ type TransactionsSummaryPerChain = {
     [safeAddress: string]: TransactionSummary[]
   }
 }
+ */
 
 const PendingTxsList = (): ReactElement => {
+  // Code to be used when using the new BE endpoint
+  /**
   const localSafesWithDetails = useLocalSafes()
   const localSafes: Record<string, string[]> = Object.entries(localSafesWithDetails).reduce(
     (result, [chain, safes]) => {
@@ -67,7 +73,7 @@ const PendingTxsList = (): ReactElement => {
 
   const title = <h2>Transactions to Sign</h2>
 
-  // Fetch txs awaiting confirmations after retrieving the owned safes from the LocalStorage
+  Fetch txs awaiting confirmations after retrieving the owned safes from the LocalStorage
   useEffect(() => {
     if (!isInitialLoad || !Object.keys(localSafes || {}).length) {
       return
@@ -113,41 +119,77 @@ const PendingTxsList = (): ReactElement => {
       </>
     )
   }
+   */
+
+  const transactionsToDisplay = (): MockType[] => {
+    const MAX_PENDING_TXS = 5
+
+    // If there are more than 5 Safes with pending transactions
+    if (mock.length >= MAX_PENDING_TXS) {
+      return mock.slice(0, MAX_PENDING_TXS).map((safe) => ({ ...safe, summaries: [safe.summaries[0]] }))
+    }
+
+    // If there are less than MAX_PENDING_TXS in total
+    const totalPendingTransactions = mock.reduce((acc, val) => acc + val.totalPending, 0)
+    if (totalPendingTransactions <= MAX_PENDING_TXS) {
+      return mock
+    }
+
+    const safesToDisplay: MockType[] = []
+    let countDisplayTransactions = 0
+    let summariesIndex = 0
+
+    // Fill MAX_PENDING_TXS transactions to display
+    while (countDisplayTransactions < MAX_PENDING_TXS) {
+      for (let i = 0; i < mock.length; i++) {
+        if (summariesIndex === 0) {
+          // Deep copy
+          const safe = JSON.parse(JSON.stringify(mock[i]))
+          safe.summaries = [safe.summaries[summariesIndex]]
+          safesToDisplay.push(safe)
+        } else {
+          const nextTxSummary = mock[i].summaries[summariesIndex]
+          nextTxSummary && safesToDisplay[i].summaries.push(nextTxSummary)
+        }
+        countDisplayTransactions++
+      }
+      summariesIndex++
+    }
+    return safesToDisplay
+  }
+
   return (
     <>
-      {title}
+      <h2>Transactions to Sign</h2>
       <List component="div">
-        {Object.entries(txsAwaitingConfirmation)?.map(([chainId, transactionsPerSafe]) => {
+        {transactionsToDisplay().map(({ safeAddress, chainId, totalPending, summaries }) => {
           const { shortName } = getChainById(chainId)
-          return Object.entries(transactionsPerSafe).map(([safeAddress, transactions]) => {
-            if (!transactions.length) return null
-            const url = generateSafeRoute(SAFE_ROUTES.TRANSACTIONS_QUEUE, { safeAddress, shortName })
-
-            return transactions.map((transaction) => {
-              const info = getAssetInfo(transaction.txInfo)
-
-              return (
-                <TransactionToConfirm key={transaction.id} to={url}>
-                  <OverlapDots>
-                    <CircleDot networkId={chainId} />
-                    <div style={{ width: '24px', height: '24px' }}>
-                      <Identicon address={safeAddress} size="sm" />
-                    </div>
-                  </OverlapDots>
-                  <CustomIconText
-                    address="0x"
-                    iconUrl={
-                      isTransferTxInfo(transaction.txInfo) &&
-                      transaction.txInfo.direction === TransferDirection.OUTGOING
-                        ? OutgoingTxIcon
-                        : SettingsTxIcon || undefined
-                    }
-                  />
-                  {info ? <TxInfo info={info} /> : <Spacer />}
-                  <ChevronRightIcon />
-                </TransactionToConfirm>
-              )
-            })
+          const url = generateSafeRoute(SAFE_ROUTES.TRANSACTIONS_QUEUE, { safeAddress, shortName })
+          return summaries.map((transaction) => {
+            const info = getAssetInfo(transaction.txInfo)
+            return (
+              <TransactionToConfirm key={transaction.id} to={url}>
+                <OverlapDots>
+                  <CircleDot networkId={chainId} />
+                  <div style={{ width: `${lg}`, height: `${lg}` }}>
+                    <Identicon address={safeAddress} size="sm" />
+                  </div>
+                </OverlapDots>
+                <CustomIconText
+                  address="0x"
+                  iconUrl={
+                    isTransferTxInfo(transaction.txInfo) && transaction.txInfo.direction === TransferDirection.OUTGOING
+                      ? OutgoingTxIcon
+                      : SettingsTxIcon || undefined
+                  }
+                />
+                {info ? <TxInfo info={info} /> : <Spacer />}
+                <Text color="text" size="lg" as="span" strong>
+                  ({totalPending})
+                </Text>
+                <ChevronRightIcon />
+              </TransactionToConfirm>
+            )
           })
         })}
       </List>
@@ -169,11 +211,11 @@ const OverlapDots = styled.div`
 `
 
 const TransactionToConfirm = styled(Link)`
-  min-width: 250px;
+  min-width: 270px;
   height: 40px;
   display: grid;
   align-items: center;
-  grid-template-columns: 1fr 1fr 4fr 1fr;
+  grid-template-columns: 1fr 1fr 4fr 0.5fr 0.5fr;
   gap: 4px;
   margin: ${sm} auto;
   text-decoration: none;
