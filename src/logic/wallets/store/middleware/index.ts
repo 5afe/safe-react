2d00b51f14453e6d52b8d759a3e0a5ba0c1e5284
Aaron Cook <aaron.cook@gnosis.pm>
2022-03-28 16:24:19 +0200

    fix: initialise web3 and load contracts in middleware (#3727)
    * fix: initialise web3 and contracts in middleware
    
    * fix: add return type

--- src/logic/wallets/store/middleware/index.ts
+++ src/logic/wallets/store/middleware/index.ts
@@ -9,8 +9,11 @@ import { ProviderPayloads } from 'src/logic/wallets/store/reducer'
 import { providerSelector } from '../selectors'
 import { trackEvent } from 'src/utils/googleTagManager'
 import { WALLET_EVENTS } from 'src/utils/events/wallet'
+import { instantiateSafeContracts } from 'src/logic/contracts/safeContracts'
+import { resetWeb3, setWeb3 } from '../../getWeb3'
+import onboard, { removeLastUsedProvider, saveLastUsedProvider } from '../../onboard'
 
-let hasName = false
+let hasWalletName = false
 let hasAccount = false
 let hasNetwork = false
 
@@ -25,16 +28,24 @@ const providerMiddleware =
     // Onboard sends provider details via separate subscriptions: wallet, account, network
     // Payloads from all three need to be combined to be `loaded` and `available`
     if (type === PROVIDER_ACTIONS.WALLET_NAME) {
-      hasName = !!payload
+      hasWalletName = !!payload
     } else if (type === PROVIDER_ACTIONS.ACCOUNT) {
       hasAccount = !!payload
     } else if (type === PROVIDER_ACTIONS.NETWORK) {
       hasNetwork = !!payload
     } else {
+      // Dispatched actions from reducers unrelated to wallet
       return handledAction
     }
 
-    if (!hasName || !hasAccount || !hasNetwork) {
+    // No wallet is connected via onboard
+    if (!hasWalletName && !hasAccount && !hasNetwork) {
+      resetWeb3()
+      removeLastUsedProvider()
+    }
+
+    // Wallet 'partially' connected: only a subset of onboard subscription(s) have fired
+    if (!hasWalletName || !hasAccount || !hasNetwork) {
       return handledAction
     }
 
@@ -45,17 +56,27 @@ const providerMiddleware =
     // Wallet, account and network did not successfully load
     if (!loaded) {
       store.dispatch(enqueueSnackbar(enhanceSnackbarForAction(NOTIFICATIONS.CONNECT_WALLET_ERROR_MSG)))
-
       return handledAction
     }
 
-    if (available && name) {
+    if (!available) {
+      store.dispatch(enqueueSnackbar(enhanceSnackbarForAction(NOTIFICATIONS.UNLOCK_WALLET_MSG)))
+      return handledAction
+    }
+
+    // Instantiate web3/contract
+    const { wallet } = onboard().getState()
+    if (wallet.provider) {
+      setWeb3(wallet.provider)
+      instantiateSafeContracts()
+    }
+
+    if (name) {
+      saveLastUsedProvider(name)
       // Only track when account has been successfully saved to store
       if (payload === account) {
         trackEvent({ ...WALLET_EVENTS.CONNECT, label: name })
       }
-    } else {
-      store.dispatch(enqueueSnackbar(enhanceSnackbarForAction(NOTIFICATIONS.UNLOCK_WALLET_MSG)))
     }
 
     return handledAction
