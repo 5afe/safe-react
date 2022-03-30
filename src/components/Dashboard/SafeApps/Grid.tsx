f1f09c6b3d7e6970609b8e8aed39d1641e2ce714
Diogo Soares <diogo.soares@gnosis.pm>
2022-03-30 08:52:02 +0200

    Add "Explore" Card

--- src/components/Dashboard/SafeApps/Grid.tsx
+++ src/components/Dashboard/SafeApps/Grid.tsx
@@ -1,7 +1,11 @@
 import { ReactElement } from 'react'
 import styled from 'styled-components'
+import { Button } from '@gnosis.pm/safe-react-components'
+import { generatePath, Link } from 'react-router-dom'
+
 import { useAppList } from 'src/routes/safe/components/Apps/hooks/appList/useAppList'
-import Card from './Card'
+import { GENERIC_APPS_ROUTE } from 'src/routes/routes'
+import Card from 'src/components/Dashboard/SafeApps/Card'
 
 const StyledGrid = styled.div`
   display: flex;
@@ -11,6 +15,33 @@ const StyledGrid = styled.div`
 
 const MAX_APPS = 3
 
+const StyledExplorerButton = styled.div`
+  width: 260px;
+  height: 200px;
+  background-color: white;
+  border-radius: 8px;
+  padding: 24px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+
+  > button {
+    width: 200px;
+  }
+`
+
+const StyledIcon = styled.img`
+  width: 160px;
+  height: auto;
+`
+
 const Grid = (): ReactElement => {
   const { allApps, pinnedSafeApps, togglePin } = useAppList()
   // Transactions Builder && Wallet connect
@@ -18,6 +49,8 @@ const Grid = (): ReactElement => {
   const officialApps = allApps.filter((app) => officialAppIds.includes(app.id))
 
   const displayedApps = pinnedSafeApps.concat(officialApps).slice(0, MAX_APPS)
+
+  const path = generatePath(GENERIC_APPS_ROUTE)
 
   return (
     <div>
@@ -35,6 +68,16 @@ const Grid = (): ReactElement => {
             onPin={() => togglePin(safeApp)}
           />
         ))}
+        {displayedApps.length < MAX_APPS && (
+          <StyledExplorerButton>
+            <StyledIcon src="./resources/explore.svg" alt="Explore Safe Apps" />
+            <StyledLink to={path}>
+              <Button size="md" color="primary" variant="contained">
+                Explore
+              </Button>
+            </StyledLink>
+          </StyledExplorerButton>
+        )}
       </StyledGrid>
     </div>
   )
