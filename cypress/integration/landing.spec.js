40cb2dd55272e709a7c86ff6ea5e3a9c1dd89ec0
iamacook <aaron.cook@gnosis.pm>
2022-04-01 10:49:13 +0200

    chore: use `@testing-library/cypress`

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -8,22 +8,20 @@ describe('Landing page', () => {
   it('features discoverability cookies', () => {
     cy.visit('/')
 
-    const cookieBannerId = '[data-testid="cookies-banner-form"]'
+    cy.findByTestId('cookies-banner-form').contains('We use cookies to provide you with the best experience')
 
-    cy.get(cookieBannerId).contains('We use cookies to provide you with the best experience')
+    cy.findByTestId('cookies-banner-form').contains('Accept selection').click()
 
-    cy.get(cookieBannerId).contains('Accept selection').click()
-
-    cy.get(cookieBannerId).should('not.exist')
+    cy.findByTestId('cookies-banner-form').should('not.exist')
 
     cy.contains("What's new").click()
 
-    cy.get(cookieBannerId).contains('We use cookies to provide you with the best experience')
-    cy.get(cookieBannerId).contains('Please accept the community support & updates cookies.')
+    cy.findByTestId('cookies-banner-form').contains('We use cookies to provide you with the best experience')
+    cy.findByTestId('cookies-banner-form').contains('Please accept the community support & updates cookies.')
 
-    cy.get(cookieBannerId).contains('Community support & updates').click()
-    cy.get(cookieBannerId).contains('Accept selection').click()
-    cy.get(cookieBannerId).should('not.exist')
+    cy.findByTestId('cookies-banner-form').contains('Community support & updates').click()
+    cy.findByTestId('cookies-banner-form').contains('Accept selection').click()
+    cy.findByTestId('cookies-banner-form').should('not.exist')
 
     // Open the features discoverability section
     cy.wait(3000) // TODO: wait for Beamer cookies to be set
