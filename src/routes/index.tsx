ece833b3e0786921431dc8016dd81e12ab00e110
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-04-05 15:55:26 +0200

    feat: Add Safe route for dashboard (#3759)
    * feat: Add safe specific dashboard route
    
    * fix: Redirect user to Dashboard when adding/loading/removing a safe
    
    * fix: Remove old dashboard route
    
    * fix: Move Dashboard component inside SafeContainer to access transactions
    
    * fix: Remove null fallback for wrapInSuspense, remove unnecessary React reference

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,13 +14,14 @@ import {
   LOAD_SAFE_ROUTE,
   getNetworkRootRoutes,
   extractSafeAddress,
-  HOME_ROUTE,
+  SAFE_ROUTES,
+  generateSafeRoute,
 } from './routes'
 import { setChainId } from 'src/logic/config/utils'
 import { setChainIdFromUrl } from 'src/utils/history'
 import { usePageTracking } from 'src/utils/googleTagManager'
+import { getShortName } from 'src/config'
 
-const Home = React.lazy(() => import('./Home'))
 const Welcome = React.lazy(() => import('./welcome/Welcome'))
 const CreateSafePage = React.lazy(() => import('./CreateSafePage/CreateSafePage'))
 const LoadSafePage = React.lazy(() => import('./LoadSafePage/LoadSafePage'))
@@ -81,14 +82,19 @@ const Routes = (): React.ReactElement => {
           }
 
           if (defaultSafe) {
-            return <Redirect to={HOME_ROUTE} />
+            return (
+              <Redirect
+                to={generateSafeRoute(SAFE_ROUTES.DASHBOARD, {
+                  shortName: getShortName(),
+                  safeAddress: defaultSafe,
+                })}
+              />
+            )
           }
 
           return <Redirect to={WELCOME_ROUTE} />
         }}
       />
-
-      <Route component={Home} exact path={HOME_ROUTE} />
 
       <Route component={Welcome} exact path={WELCOME_ROUTE} />
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -41,13 +41,13 @@ export const LOAD_SPECIFIC_SAFE_ROUTE = `/load/:${SAFE_ADDRESS_SLUG}?` // ? = op
 
 // Routes independant of safe/network
 export const ROOT_ROUTE = '/'
-export const HOME_ROUTE = '/home'
 export const WELCOME_ROUTE = '/welcome'
 export const OPEN_SAFE_ROUTE = '/open'
 export const LOAD_SAFE_ROUTE = generatePath(LOAD_SPECIFIC_SAFE_ROUTE) // By providing no slug, we get '/load'
 
 // [SAFE_SECTION_SLUG], [SAFE_SUBSECTION_SLUG] populated safe routes
 export const SAFE_ROUTES = {
+  DASHBOARD: `${ADDRESSED_ROUTE}/home`,
   ASSETS_BALANCES: `${ADDRESSED_ROUTE}/balances`, // [SAFE_SECTION_SLUG] === 'balances'
   ASSETS_BALANCES_COLLECTIBLES: `${ADDRESSED_ROUTE}/balances/collectibles`, // [SAFE_SUBSECTION_SLUG] === 'collectibles'
   TRANSACTIONS: `${ADDRESSED_ROUTE}/transactions`,
