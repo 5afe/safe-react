72e57f413ba9c50788d7334d54cfcc84d66b526f
iamacook <aaron.cook@gnosis.pm>
2022-04-06 20:31:46 +0200

    test: define env for Cypress testing

---
+++ scripts/cypress.js
@@ -0,0 +1,36 @@
+const rl = require('readline')
+const { exec } = require('child_process')
+
+const DEPLOYMENTS = {
+  d: 'https://safe-team.dev.gnosisdev.com/app',
+  s: 'https://safe-team.staging.gnosisdev.com/app',
+  p: 'https://gnosis-safe.io/app',
+}
+
+const readline = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const cmd = `cypress ${process.argv[2]}`
+
+function launchCypressEnv() {
+  readline.question(
+    'Which environment do you want to test: (l)ocal, (d)evelopment, (s)taging or (p)roduction? ',
+    (env) => {
+      const key = env?.[0].toLowerCase()
+
+      if (!key || key === 'l') {
+        exec(cmd)
+      } else if (Object.keys(DEPLOYMENTS).includes(key)) {
+        exec(`${cmd} --config baseUrl=${DEPLOYMENTS[key]}`)
+      } else {
+        console.log(`\nInvalid environment.`)
+      }
+
+      readline.close()
+    },
+  )
+}
+
+launchCypressEnv()
