b56cab3d25d4bed8571163fa1f0d72f85b2b65be
iamacook <aaron.cook@gnosis.pm>
2022-04-07 20:08:03 +0200

    fix: modify script to accept optional env arg

--- scripts/cypress.js
+++ scripts/cypress.js
@@ -1,36 +1,12 @@
-const rl = require('readline')
 const { exec } = require('child_process')
 
 const DEPLOYMENTS = {
-  d: 'https://safe-team.dev.gnosisdev.com/app',
-  s: 'https://safe-team.staging.gnosisdev.com/app',
-  p: 'https://gnosis-safe.io/app',
+  dev: 'https://safe-team.dev.gnosisdev.com/app',
+  staging: 'https://safe-team.staging.gnosisdev.com/app',
+  prod: 'https://gnosis-safe.io/app',
 }
 
-const readline = rl.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+const command = `cypress ${process.argv[2]}`
+const env = process.argv?.[3]
 
-const cmd = `cypress ${process.argv[2]}`
-
-function launchCypressEnv() {
-  readline.question(
-    'Which environment do you want to test: (l)ocal, (d)evelopment, (s)taging or (p)roduction? ',
-    ([env = 'l']) => {
-      const key = env.toLowerCase()
-
-      if (key === 'l') {
-        exec(cmd)
-      } else if (Object.keys(DEPLOYMENTS).includes(key)) {
-        exec(`${cmd} --config baseUrl=${DEPLOYMENTS[key]}`)
-      } else {
-        console.log(`\nInvalid environment.`)
-      }
-
-      readline.close()
-    },
-  )
-}
-
-launchCypressEnv()
+exec(env ? `${command} --config baseUrl=${DEPLOYMENTS[env]}` : command)
