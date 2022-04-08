846be315e6d8123ac019ebbe8301979f3cb42f63
Aaron Cook <aaron.cook@gnosis.pm>
2022-04-08 14:54:21 +0200

    chore: add cypress env commands (#3752)
    * chore: add cypress env commands
    
    * test: define env for Cypress testing
    
    * fix: use fallback
    
    * fix: destructure env
    
    * fix: modify script to accept optional env arg

---
+++ scripts/cypress.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process')
+
+const DEPLOYMENTS = {
+  dev: 'https://safe-team.dev.gnosisdev.com/app',
+  staging: 'https://safe-team.staging.gnosisdev.com/app',
+  prod: 'https://gnosis-safe.io/app',
+}
+
+const command = `cypress ${process.argv[2]}`
+const env = process.argv?.[3]
+
+exec(env ? `${command} --config baseUrl=${DEPLOYMENTS[env]}` : command)
