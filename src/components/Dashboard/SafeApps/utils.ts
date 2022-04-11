47d2cef59fab171f82f7f0ae41b6f0e8ed5e5e7c
Diogo Soares <diogo.soares@gnosis.pm>
2022-04-11 18:08:55 +0200

    chore: Add comments to the sorting formula

--- src/components/Dashboard/SafeApps/utils.ts
+++ src/components/Dashboard/SafeApps/utils.ts
@@ -1,7 +1,7 @@
 import { AppTrackData } from 'src/routes/safe/components/Apps/trackAppUsageCount'
 
-const TXCOUNT_WEIGHT = 2
-const OPENCOUNT_WEIGHT = 1
+const TX_COUNT_WEIGHT = 2
+const OPEN_COUNT_WEIGHT = 1
 const MORE_RECENT_MULTIPLIER = 2
 const LESS_RECENT_MULTIPLIER = 1
 
@@ -10,13 +10,15 @@ export const rankTrackedSafeApps = (apps: AppTrackData): string[] => {
 
   return appsMap
     .sort((a, b) => {
+      // The more recently used app gets a bigger score/relevancy multiplier
       const aTimeMultiplier =
         a[1].timestamp.valueOf() - b[1].timestamp.valueOf() > 0 ? MORE_RECENT_MULTIPLIER : LESS_RECENT_MULTIPLIER
       const bTimeMultiplier =
         b[1].timestamp.valueOf() - a[1].timestamp.valueOf() > 0 ? MORE_RECENT_MULTIPLIER : LESS_RECENT_MULTIPLIER
 
-      const aScore = (TXCOUNT_WEIGHT * a[1].txCount + OPENCOUNT_WEIGHT * a[1].openCount) * aTimeMultiplier
-      const bScore = (TXCOUNT_WEIGHT * b[1].txCount + OPENCOUNT_WEIGHT * b[1].openCount) * bTimeMultiplier
+      // The sorting score is a weighted function where the OPEN_C0UNT weights differently than the TX_COUNT
+      const aScore = (TX_COUNT_WEIGHT * a[1].txCount + OPEN_COUNT_WEIGHT * a[1].openCount) * aTimeMultiplier
+      const bScore = (TX_COUNT_WEIGHT * b[1].txCount + OPEN_COUNT_WEIGHT * b[1].openCount) * bTimeMultiplier
 
       return bScore - aScore
     })
