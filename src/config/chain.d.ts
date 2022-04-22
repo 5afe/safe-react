8ea9075d3edba008558dc390b83c97f8740fb3f6
Daniel Russian <danielrussian@gmail.com>
2022-04-22 15:37:44 -0300

    Setting the Evmos testnet as default network

--- src/config/chain.d.ts
+++ src/config/chain.d.ts
@@ -13,6 +13,7 @@ export const CHAIN_ID: Record<ChainName, ChainId> = {
   ETHEREUM: '1',
   RINKEBY: '4',
   VOLTA: '73799',
+  EVMOS_TESTNET: '9000',
 }
 
 // Values match that required of onboard and returned by CGW
