8d05af3a68ada09e5eb5738a6c1c7cb677ee1773
iamacook <aaron.cook@gnosis.pm>
2022-04-25 12:24:17 +0200

    fix: stricter linting across whole repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,15 +16,24 @@ module.exports = {
     },
   },
   rules: {
+    // Errors:
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/no-danger': 'error', // Prevent usage of dangerous JSX props
+    'react/self-closing-comp': ['error', { component: true, html: true }],
+    'react/void-dom-elements-no-children': 'error', // No passing of children to void DOM elements
+    'react/jsx-boolean-value': 'error', // Boolean attributes notation in JSX
+    'react/jsx-key': 'error', // Missing iterators/collection literals keys
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    // Disabled:
+    'react/react-in-jsx-scope': 'off', // React 17+ doesn't require the explicit import of React
+    '@typescript-eslint/no-empty-function': 'off', // Sometimes we use empty functions as context defaults
+    /**
+     * The following was added by legacy devs to suppress warnings
+     */
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
   },
   settings: {
     react: {
