84195c5cc2fb4da9a420e8afb3b15175088c01f2
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-05-16 14:09:30 +0200

    Fix typo in var name

--- cypress/integration/intercom.spec.js
+++ cypress/integration/intercom.spec.js
@@ -1,5 +1,5 @@
 const RINKEBY_TEST_SAFE = 'rin:0xFfDC1BcdeC18b1196e7FA04246295DE3A17972Ac'
-const intercomIfame = 'iframe[id="intercom-frame"]'
+const intercomIframe = 'iframe[id="intercom-frame"]'
 const intercomButton = '[aria-label="Open Intercom Messenger"]'
 const fakeIntercomButton = 'img[alt="Open Intercom"]'
 
@@ -19,7 +19,7 @@ describe('Intercom and cookie prefs', () => {
     cy.contains('a', 'Accept all').click()
 
     // Intercom is now active
-    cy.get(intercomIfame).should('exist')
+    cy.get(intercomIframe).should('exist')
     cy.get(fakeIntercomButton).should('not.exist')
     cy.get(intercomButton).click()
     cy.get('#intercom-container').should('exist')
@@ -33,7 +33,7 @@ describe('Intercom and cookie prefs', () => {
 
     // Go to Settings and change the cookie settings
     cy.visit(`/${RINKEBY_TEST_SAFE}/settings`)
-    cy.get(intercomIfame).should('exist')
+    cy.get(intercomIframe).should('exist')
     cy.contains('button', 'Preferences').click()
     cy.contains('Community support & updates').click()
     cy.contains('a', 'Accept selection').click()
