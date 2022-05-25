e115a19a530de44a7c8ed6ee81f050b2385dfd8b
Diogo Soares <diogo.soares@gnosis.pm>
2022-05-25 11:34:16 +0200

    display a table with the delegates

--- src/routes/safe/components/Settings/Delegates/index.tsx
+++ src/routes/safe/components/Settings/Delegates/index.tsx
@@ -1,6 +1,7 @@
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+import { Table, TableHeader, TableRow, Text } from '@gnosis.pm/safe-react-components'
 
 import Block from 'src/components/layout/Block'
 import Heading from 'src/components/layout/Heading'
@@ -8,6 +9,22 @@ import Paragraph from 'src/components/layout/Paragraph/index'
 import { lg } from 'src/theme/variables'
 import { currentSafeWithNames } from 'src/logic/safe/store/selectors'
 import { getChainInfo } from 'src/config'
+import { checksumAddress } from 'src/utils/checksumAddress'
+
+type Page<T> = {
+  next?: string
+  previous?: string
+  results: Array<T>
+}
+
+type Delegate = {
+  safe?: string
+  delegate: string
+  delegator: string
+  label: string
+}
+
+type DelegateResponse = Page<Delegate>
 
 const StyledBlock = styled(Block)`
   minheight: 420px;
@@ -20,8 +37,18 @@ const StyledHeading = styled(Heading)`
 
 const Delegates = (): ReactElement => {
   const { address: safeAddress } = useSelector(currentSafeWithNames)
-  const [delegatesList, setDelegatesList] = useState([])
+  const [delegatesList, setDelegatesList] = useState<DelegateResponse['results']>([])
   const { transactionService } = getChainInfo()
+
+  const headerCells: TableHeader[] = useMemo(
+    () => [
+      { id: 'delegate', label: 'Delegate' },
+      { id: 'delegator', label: 'Delegator' },
+      { id: 'label', label: 'Label' },
+    ],
+    [],
+  )
+  const rows: TableRow[] = useMemo(() => [], [])
 
   useEffect(() => {
     if (!safeAddress || !transactionService) return
@@ -34,11 +61,28 @@ const Delegates = (): ReactElement => {
       })
   }, [safeAddress, transactionService])
 
+  useEffect(() => {
+    if (delegatesList.length) {
+      let index = 0
+      for (const obj of delegatesList) {
+        rows.push({
+          id: `${index++}`,
+          cells: [
+            { id: 'delegate', content: <Text size="xl">{checksumAddress(obj.delegate)}</Text> },
+            { id: 'delegator', content: <Text size="xl">{checksumAddress(obj.delegator)}</Text> },
+            { id: 'label', content: <Text size="xl">{obj.label}</Text> },
+          ],
+        })
+      }
+    }
+  }, [delegatesList, rows])
+
   return (
     <StyledBlock>
       <StyledHeading tag="h2">Manage Safe Delegates</StyledHeading>
       <Paragraph>Get, add and delete delegates.</Paragraph>
       <pre>{JSON.stringify(delegatesList, undefined, 2)}</pre>
+      <Table headers={headerCells} rows={rows} />
     </StyledBlock>
   )
 }
