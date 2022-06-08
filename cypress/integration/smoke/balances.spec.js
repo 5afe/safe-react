09a66b717f763b821719e34edcb7a1ceb33b7c9b
Diogo Soares <diogo.soares@gnosis.pm>
2022-06-08 16:50:10 +0200

    test: assert balances are well formatted

--- cypress/integration/smoke/balances.spec.js
+++ cypress/integration/smoke/balances.spec.js
@@ -6,6 +6,16 @@ const SAFE_TEST = '0x8675B754342754A30A2AeF474D114d8460bca19b'
 const ASSETS_LENGTH = 26
 
 describe('Assets > Coins', () => {
+  const number = 123456.789
+  const localeNumber = number.toLocaleString()
+  // Find localeNumber thousand separator and decimal separator
+  const [thousandSeparator, decimalSeparator] = localeNumber.match(/[^0-9]/g)
+
+  // Fiat balance regex
+  const fiatRegex = new RegExp(`([0-9]{1,3}${thousandSeparator})*[0-9]{1,3}${decimalSeparator}[0-9]{2}`)
+  // Token balance regex
+  const tokenRegex = new RegExp(`([0-9]{1,3}${thousandSeparator})*[0-9]{1,3}${decimalSeparator}[0-9]{5}`)
+
   beforeEach(() => {
     // Open the Safe used for testing
     cy.visit(`/eth:${SAFE_TEST}/balances`)
@@ -13,18 +23,45 @@ describe('Assets > Coins', () => {
 
   describe('Should have all table entries', () => {
     it('Should have 26 entries in the table', () => {
-      cy.get(assetsTableContainer).find(balanceRowTestId).should('have.length', ASSETS_LENGTH)
+      cy.get(balanceRowTestId).should('have.length', ASSETS_LENGTH)
     })
 
     it('Should have Wrapped Ether', () => {
-      cy.get(assetsTableContainer).find(balanceRowTestId).contains('Wrapped Ether')
+      cy.get(balanceRowTestId).contains('Wrapped Ether')
     })
     it('Should have Dai Stablecoin', () => {
-      cy.get(assetsTableContainer).find(balanceRowTestId).contains('Dai Stablecoin')
+      cy.get(balanceRowTestId).contains('Dai Stablecoin')
     })
 
     it('Should have MakerDAO', () => {
-      cy.get(assetsTableContainer).find(balanceRowTestId).contains('MakerDAO')
+      cy.get(balanceRowTestId).contains('MakerDAO')
+    })
+  })
+
+  describe('Values should be well formatted', () => {
+    it('Token balance should be formated as per tokenRegex', () => {
+      // Balance should comply with the tokenRegex
+      cy.get(assetsTableContainer)
+        .find(balanceRowTestId)
+        .eq(1)
+        .find('td')
+        .eq(1)
+        .should(($div) => {
+          const text = $div.text()
+          expect(text).to.match(tokenRegex)
+        })
+    })
+    it('Fiat balance should be formated as per fiatRegex', () => {
+      // Balance should comply with the fiatRegex
+      cy.get(assetsTableContainer)
+        .find(balanceRowTestId)
+        .eq(1)
+        .find('td')
+        .eq(2)
+        .should(($div) => {
+          const text = $div.text()
+          expect(text).to.match(fiatRegex)
+        })
     })
   })
 
