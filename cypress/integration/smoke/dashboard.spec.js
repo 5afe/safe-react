b6fadede7dd4cca4c687324912e6fa3dabfd4755
Diogo Soares <diogo.soares@gnosis.pm>
2022-06-12 18:50:14 +0200

    tests: pass execution environment to cypress

--- cypress/integration/smoke/dashboard.spec.js
+++ cypress/integration/smoke/dashboard.spec.js
@@ -1,6 +1,9 @@
 const SAFE = 'rin:0xB5ef359e8eBDAd1cd7695FFEF3f6F6D7d5e79B08'
 
 describe('Dashboard', () => {
+  const safeAppsUrl =
+    Cypress.env('CYPRESS_ENV') === 'prod' ? 'https://apps.gnosis-safe.io' : 'https://safe-apps.dev.gnosisdev.com'
+
   before(() => {
     // Go to the test Safe home page
     cy.visit(`/${SAFE}/home`)
@@ -9,7 +12,7 @@ describe('Dashboard', () => {
 
   it('should display the dashboard title', () => {
     cy.contains('main h1', 'Dashboard')
-  }) 
+  })
 
   it('should display the overview widget', () => {
     cy.contains('main p', SAFE).should('exist')
@@ -20,9 +23,10 @@ describe('Dashboard', () => {
   })
 
   it('should display the mobile banner', () => {
-    const appStoreLink = 'https://apps.apple.com/app/apple-store/id1515759131?pt=119497694&ct=Web%20App%20Dashboard&mt=8'
+    const appStoreLink =
+      'https://apps.apple.com/app/apple-store/id1515759131?pt=119497694&ct=Web%20App%20Dashboard&mt=8'
     cy.get(`a[href="${appStoreLink}"]`).should('exist')
-    
+
     cy.get('button[aria-label="Close mobile banner"]').click()
     cy.contains('button', 'Already use it!')
     cy.contains('button', 'Not interested').click()
@@ -34,16 +38,10 @@ describe('Dashboard', () => {
     cy.contains('main', 'This Safe has no queued transactions').should('not.exist')
 
     // Queued txns
-    cy.contains(
-      `main a[href="/app/${SAFE}/transactions/queue"]`,
-      '0' + 'addOwnerWithThreshold' + '1/1'
-    ).should('exist')
+    cy.contains(`main a[href="/app/${SAFE}/transactions/queue"]`, '0' + 'addOwnerWithThreshold' + '1/1').should('exist')
 
-    cy.contains(
-      `main a[href="/app/${SAFE}/transactions/queue"]`,
-      '2' + 'Send' + '-0.001 USDC' + '1/1'
-    ).should('exist')
-  
+    cy.contains(`main a[href="/app/${SAFE}/transactions/queue"]`, '2' + 'Send' + '-0.001 USDC' + '1/1').should('exist')
+
     cy.contains(`a[href="/app/${SAFE}/transactions/queue"]`, 'View All')
   })
 
@@ -52,13 +50,11 @@ describe('Dashboard', () => {
 
     // Tx Builder app
     cy.contains('main p', 'Use Transaction Builder')
-    cy.get(`a[href="/app/${SAFE}/apps?appUrl=https://safe-apps.dev.gnosisdev.com/tx-builder"]`)
-      .should('exist')
+    cy.get(`a[href="/app/${SAFE}/apps?appUrl=${safeAppsUrl}/tx-builder"]`).should('exist')
 
     // WalletConnect app
     cy.contains('main p', 'Use WalletConnect')
-    cy.get(`a[href="/app/${SAFE}/apps?appUrl=https://safe-apps.dev.gnosisdev.com/wallet-connect"]`)
-      .should('exist')
+    cy.get(`a[href="/app/${SAFE}/apps?appUrl=${safeAppsUrl}/wallet-connect"]`).should('exist')
 
     // Featured apps have a Safe-specific link
     cy.get(`main a[href^="/app/${SAFE}/apps?appUrl=http"]`).should('have.length', 2)
