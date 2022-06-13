e5d5c2c2cee4299af930931fc6f99936160f52a3
Diogo Soares <diogo.soares@gnosis.pm>
2022-06-13 12:02:10 +0200

    change it titles. Improve assertions readability.

--- cypress/integration/smoke/balances.spec.js
+++ cypress/integration/smoke/balances.spec.js
@@ -2,62 +2,61 @@ const assetsTableContainer = '.MuiTableContainer-root'
 const balanceRowTestId = '[data-testid=balance-row]'
 const receiveModalClass = '.receive-modal'
 
-const SAFE_TEST = '0x8675B754342754A30A2AeF474D114d8460bca19b'
+const TEST_SAFE = '/eth:0x8675B754342754A30A2AeF474D114d8460bca19b'
 const ASSETS_LENGTH = 26
 
 describe('Assets > Coins', () => {
-  const number = 123456.789
-  const localeNumber = number.toLocaleString()
-  // Find localeNumber thousand separator and decimal separator
-  const [thousandSeparator, decimalSeparator] = localeNumber.match(/[^0-9]/g)
-
   // Fiat balance regex
-  const fiatRegex = new RegExp(`([0-9]{1,3}${thousandSeparator})*[0-9]{1,3}${decimalSeparator}[0-9]{2}`)
+  const fiatRegex = new RegExp(`([0-9]{1,3},)*[0-9]{1,3}.[0-9]{2}`)
   // Token balance regex
-  const tokenRegex = new RegExp(`([0-9]{1,3}${thousandSeparator})*[0-9]{1,3}${decimalSeparator}[0-9]{5}`)
+  const tokenRegex = new RegExp(`([0-9]{1,3},)*[0-9]{1,3}.[0-9]{5}`)
 
-  beforeEach(() => {
+  before(() => {
     // Open the Safe used for testing
-    cy.visit(`/eth:${SAFE_TEST}/balances`)
+    cy.visit(`${TEST_SAFE}/balances`)
+    cy.contains('a', 'Accept selection').click()
   })
 
-  describe('Should have all table entries', () => {
-    it('Should have 26 entries in the table', () => {
+  describe('should have different tokens', () => {
+    it('should have 26 entries in the table', () => {
       cy.get(balanceRowTestId).should('have.length', ASSETS_LENGTH)
     })
 
-    it('Should have Wrapped Ether', () => {
+    it('should have Wrapped Ether', () => {
       cy.get(balanceRowTestId).contains('Wrapped Ether')
     })
-    it('Should have Dai Stablecoin', () => {
+    it('should have Dai Stablecoin', () => {
       cy.get(balanceRowTestId).contains('Dai Stablecoin')
     })
 
-    it('Should have MakerDAO', () => {
+    it('should have MakerDAO', () => {
       cy.get(balanceRowTestId).contains('MakerDAO')
     })
   })
 
-  describe('Values should be well formatted', () => {
-    it('Token balance should be formated as per tokenRegex', () => {
+  describe('values should be formatted as per locale', () => {
+    const TOKEN_AMOUNT_COLUMN = 1
+    const FIAT_AMOUNT_COLUMN = 2
+
+    it('should have Token balance formated as per tokenRegex', () => {
       // Balance should comply with the tokenRegex
       cy.get(assetsTableContainer)
         .find(balanceRowTestId)
         .eq(1)
         .find('td')
-        .eq(1)
+        .eq(TOKEN_AMOUNT_COLUMN)
         .should(($div) => {
           const text = $div.text()
           expect(text).to.match(tokenRegex)
         })
     })
-    it('Fiat balance should be formated as per fiatRegex', () => {
+    it('should have Fiat balance formated as per fiatRegex', () => {
       // Balance should comply with the fiatRegex
       cy.get(assetsTableContainer)
         .find(balanceRowTestId)
         .eq(1)
         .find('td')
-        .eq(2)
+        .eq(FIAT_AMOUNT_COLUMN)
         .should(($div) => {
           const text = $div.text()
           expect(text).to.match(fiatRegex)
@@ -65,8 +64,8 @@ describe('Assets > Coins', () => {
     })
   })
 
-  describe('Should open assets modals', () => {
-    it('Should open the Receive assets modal', () => {
+  describe('should open assets modals', () => {
+    it('should open the Receive assets modal', () => {
       // Assets table container should exist
       cy.get(assetsTableContainer).should('exist')
 
