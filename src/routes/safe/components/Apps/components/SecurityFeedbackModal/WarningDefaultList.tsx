15204eeeb6c604b1e50ddd10454057098cb17f6e
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-06-20 10:34:48 +0200

    Align checkbox to the right

--- src/routes/safe/components/Apps/components/SecurityFeedbackModal/WarningDefaultList.tsx
+++ src/routes/safe/components/Apps/components/SecurityFeedbackModal/WarningDefaultList.tsx
@@ -1,5 +1,8 @@
-import { Checkbox, Text } from '@gnosis.pm/safe-react-components'
 import { useState } from 'react'
+import styled from 'styled-components'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { Text } from '@gnosis.pm/safe-react-components'
 import { StyledTitle } from './styles'
 
 type WarningDefaultListProps = {
@@ -22,14 +25,27 @@ const WarningDefaultList = ({ onHideWarning }: WarningDefaultListProps): React.R
       </Text>
       <br />
       <Text size="lg">Check the link you are using and ensure it comes from a trusted source</Text>
-      <Checkbox
-        name="Warning message preference"
+      <StyledFormControlLabel
+        control={
+          <Checkbox
+            checked={toggleHideWarning}
+            onChange={handleToggleWarningPreference}
+            name="Warning message preference"
+          />
+        }
         label="Do not show this warning again"
-        checked={toggleHideWarning}
-        onChange={handleToggleWarningPreference}
       />
     </>
   )
 }
+
+const StyledFormControlLabel = styled(FormControlLabel)`
+  && {
+    margin-top: 15px;
+    flex-direction: row-reverse;
+    justify-self: flex-end;
+    display: flex;
+  }
+`
 
 export default WarningDefaultList
