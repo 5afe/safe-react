08cc265621fae2950fb36a57db9bba8a6757e7cf
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-06-27 17:20:49 +0200

    Rewite disclaimer texts

--- src/routes/safe/components/Apps/components/SecurityFeedbackModal/LegalDisclaimer.tsx
+++ src/routes/safe/components/Apps/components/SecurityFeedbackModal/LegalDisclaimer.tsx
@@ -1,4 +1,5 @@
-import { Text } from '@gnosis.pm/safe-react-components'
+import { Text, Link } from '@gnosis.pm/safe-react-components'
+import styled from 'styled-components'
 import { StyledTitle } from './styles'
 
 const LegalDisclaimer = (): JSX.Element => (
@@ -8,19 +9,25 @@ const LegalDisclaimer = (): JSX.Element => (
     </StyledTitle>
     <Text size="lg">
       You are now accessing third-party apps, which we do not own, control, maintain or audit. We are not liable for any
-      loss you may suffer in connection with interacting with the apps, which is at your own risk. You must read our
-      Terms, which contain more detailed provisions binding on you relating to the apps.
+      loss you may suffer in connection with interacting with the apps, which is at your own risk.
     </Text>
     <br />
+    <Text size="lg">
+      You must read our Terms, which contain more detailed provisions binding on you relating to the apps.
+    </Text>
     <br />
     <Text size="lg">
       I have read and understood the{' '}
-      <a href="https://gnosis-safe.io/terms" rel="noopener noreferrer" target="_blank">
+      <StyledLink href="https://gnosis-safe.io/terms" size="lg" rel="noopener noreferrer" target="_blank" style={{}}>
         Terms
-      </a>{' '}
+      </StyledLink>{' '}
       and this Disclaimer, and agree to be bound by them.
     </Text>
   </>
 )
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 export default LegalDisclaimer
