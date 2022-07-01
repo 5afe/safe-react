706971a210a500386f4c41fd607d70360eb526d2
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-01 18:15:53 +0200

    Send funds modal displays correctly

--- cypress/integration/tx_modal.spec.js
+++ cypress/integration/tx_modal.spec.js
@@ -9,9 +9,71 @@ describe('Tx Modal', () => {
     cy.contains('a', 'Accept selection').click()
   })
 
-  it('should open the modal when clicking New Transaction', () => {
-    cy.get('[aria-describedby="Send Tokens Form"]').should('not.exist')
-    cy.contains('button', 'New Transaction').click()
-    cy.get('[aria-describedby="Send Tokens Form"]').should('be.visible')
+  describe('Send funds modal', () => {
+    it('should open the modal when clicking New Transaction', () => {
+      // Modal should be blosed
+      cy.get('[aria-describedby="Send Tokens Form"]').should('not.exist')
+
+      // Opens Tx Modal
+      cy.contains('New Transaction').click()
+      cy.get('[aria-describedby="Send Tokens Form"]').should('be.visible')
+      cy.get('.smaller-modal-window').contains('Send')
+    })
+
+    it('should close the modal by clicking outside of the modal', () => {
+      // Modal is open
+      cy.get('.smaller-modal-window').contains('Send')
+
+      // Click outside of the .smaller-modal-window to close the modal
+      cy.get('.smaller-modal-window').then(($target) => {
+        let coords = $target[0].getBoundingClientRect()
+        const { x, y } = coords
+        cy.get('body').click(x - 10, y - 10)
+      })
+
+      // Modal is closed
+      cy.get('[aria-describedby="Send Tokens Form"]').should('not.exist')
+    })
+
+    it.only('should display Send Funds modal with all the form elements', () => {
+      // Opens Tx Modal
+      cy.contains('New Transaction').click()
+
+      // Click on Send Funds
+      cy.contains('Send funds').click()
+
+      // Send Funds modal is open
+      cy.contains('Send funds').should('be.visible')
+      cy.contains('Step 1 of 2').should('be.visible')
+
+      // It contains the form elements
+      cy.get('form').within(() => {
+        // Sending from the current Safe address
+        const [chainPrefix, safeAddress] = TEST_SAFE.split(':')
+        cy.contains(chainPrefix)
+        cy.contains(safeAddress)
+
+        // Current ETH balance
+        cy.contains('Balance:').contains('0.3 ETH')
+
+        // Recipient field
+        cy.get('#address-book-input').should('be.visible')
+
+        // Token selector
+        cy.contains('Select an asset*').should('be.visible')
+
+        // Amount field
+        cy.contains('Amount').should('be.visible')
+      })
+
+      // Review button is disabled
+      cy.get('button[type="submit"]').should('be.disabled')
+
+      // Close modal when clicking Cancel
+      cy.contains('Cancel').click()
+
+      // Modal is closed
+      cy.get('[aria-describedby="Send Tokens Form"]').should('not.exist')
+    })
   })
 })
