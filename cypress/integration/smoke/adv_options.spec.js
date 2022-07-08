ff1edf6bf7617835ccbb91395aec460ebea2afd9
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-08 19:27:46 +0200

    Add owner form validation

--- cypress/integration/smoke/adv_options.spec.js
+++ cypress/integration/smoke/adv_options.spec.js
@@ -3,7 +3,7 @@ const TEST_SAFE = 'rin:0x11Df0fa87b30080d59eba632570f620e37f2a8f7'
 const offset = 7
 
 describe('Advanced Options', () => {
-  let currentNonce = ''
+  let currentNonce = 0
   before(() => {
     cy.connectE2EWallet()
 
@@ -14,7 +14,7 @@ describe('Advanced Options', () => {
     cy.contains('Safe Nonce', { timeout: 5000 })
     // Get current nonce from Settings > Advanced
     cy.findByTestId('current-nonce').then((element) => {
-      currentNonce = element.text()
+      currentNonce = parseInt(element.text())
     })
   })
 
@@ -45,75 +45,88 @@ describe('Advanced Options', () => {
       // Review step
       cy.contains('Review').click()
       cy.contains('p', 'Add new owner').should('be.visible').next().contains('Step 3 of 3').should('be.visible')
-    })
-  })
 
-  describe('Verifying current advanced options values', () => {
-    it('Opens Edit estimation information', () => {
-      cy.findByText('Estimated fee price').click()
-
-      //Waiting for the current estimation.
-      cy.wait(2000)
-      //Checking that gas limit is not a 0, usually a sign that the estimation failed
-      cy.findByText('Gas limit').next().should('not.to.be.empty').and('to.not.equal', '0')
-
-      cy.get('[data-track="modals: Edit estimation"]').findByText('Edit').click()
-      cy.wait(2000) //Wait till the values fully load
-    })
-  })
-  describe('Checking error messages for wrongs values', () => {
-    before(() => {
-      //Adding offset to check the warning message later
-      cy.get('[name="safeNonce"]')
-        .clear()
-        .type(`${currentNonce + offset}`)
-    })
-    //The following values are fixed to get a specific estimation value in ETH
-    it('Should show error for gas limit', () => {
-      cy.get('[name="ethGasLimit"]').clear().type('-100')
-      cy.findByText('Must be greater than or equal to 0')
-      cy.get('[name="ethGasLimit"]').clear().type('200000')
-    })
-
-    it('Should show error for gas price', () => {
-      cy.get('[name="ethGasPrice"]').clear().type('-100')
-      cy.findByText('Must be greater than or equal to 0')
-      cy.get('[name="ethGasPrice"]').clear().type('5')
-    })
-
-    it('Should show error for gas fee and prio fee', () => {
-      cy.get('[name="ethMaxPrioFee"]').clear().type('-100')
-      cy.findByText('Must be greater than or equal to 0')
-      cy.get('[name="ethMaxPrioFee"]').clear().type('7')
-      cy.findByText('Maximum value is 5') //It cannot be higher than gasPrice
-      cy.get('[name="ethMaxPrioFee"]').clear().type('5')
-      cy.wait(1000)
-      cy.findByTestId('submit-tx-btn').click()
-    })
-  })
-
-  describe('Checking error messages for wrongs values', () => {
-    it('Should show the edited values', () => {
-      //Vefifying that in the dropdown all  the values are there
-      cy.findByText('Estimated fee price').click()
-      cy.wait(1000)
-      cy.findByText('Gas limit').next().should('have.text', '200000')
-      cy.findByText('Max fee per gas').next().should('have.text', '5')
-      cy.findByText('Max priority fee').next().should('have.text', '5')
-    })
-
-    it('Should show the out of order nonce warning', () => {
-      cy.findByText('Safe nonce')
+      // Chosen threshold should be in the review modal
+      cy.contains('Any transaction requires the confirmation of:')
+        .should('be.visible')
         .next()
-        .should('have.text', `${currentNonce + offset}`)
-
-      //Verifying the warning message of setting a nonce hihgher than "current nonce"
-      cy.findByText(
-        'transactions will need to be created and executed before this transaction, are you sure you want to do this?',
-      ).contains(`${offset}`)
+        .contains('3 out of 3 owner(s)')
+        .should('be.visible')
     })
-    it('Show the calculation of gaslimit + gas price', () => {
-      cy.findByText('0.002 ETH') //the result of 200k gaslimit, 5 gasPrice and Fee
+
+    it('should open Edit estimation information', () => {
+      // Estimated gas price is loaded
+      cy.contains('Estimated fee price').next().should('not.have.text', '> 0.001 ETH')
+
+      // Open the accordion if gas is loaded
+      cy.contains('Estimated fee price').click()
+
+      //Checking that gas limit is not a 0, usually a sign that the estimation failed
+      cy.contains('Gas limit').next().should('not.to.be.empty').and('to.not.equal', '0')
+
+      cy.get('[data-track="modals: Edit estimation"]').contains('Edit').click()
+    })
+
+    describe('Edit Advanced parameters form', () => {
+      before(() => {
+        //Adding offset to check the warning message later
+        cy.get('label')
+          .contains('Safe nonce')
+          .next()
+          .clear()
+          .type(`${currentNonce + offset}`)
+      })
+
+      //The following values are fixed to get a specific estimation value in ETH
+      it('should show validation errors in the form', () => {
+        // Gas limit
+        cy.get('label').contains('Gas limit').next().clear().type('-100')
+        cy.findByText('Must be greater than or equal to 0')
+        cy.get('label').contains('Gas limit').next().clear().type('200000')
+
+        // Max gas price
+        cy.get('label').contains('Max fee per gas').next().clear().type('-100')
+        cy.findByText('Must be greater than or equal to 0')
+        cy.get('label').contains('Max fee per gas').next().clear().type('5')
+
+        // Max prio fee
+        cy.get('label').contains('Max priority fee').next().clear().type('-100')
+        cy.findByText('Must be greater than or equal to 0')
+        cy.get('label').contains('Max priority fee').next().clear().type('7')
+        cy.findByText('Maximum value is 5') //It cannot be higher than gasPrice
+        cy.get('label').contains('Max priority fee').next().clear().type('5')
+
+        // Accepts the values
+        cy.contains('Confirm').click()
+      })
+
+      it('should show the edited values', () => {
+        //Vefifying that in the dropdown all  the values are there
+        cy.contains('Estimated fee price').click()
+
+        // Verify the accordion details fields kept the edited values
+        cy.contains('Gas limit').next().should('have.text', '200000')
+        cy.contains('Max fee per gas').next().should('have.text', '5')
+        cy.contains('Max priority fee').next().should('have.text', '5')
+      })
+    })
+
+    describe('Displaying error messages for invalid values', () => {
+      it('should show the out of order nonce warning', () => {
+        cy.contains('Safe nonce')
+          .next()
+          .should('have.text', `${currentNonce + offset}`)
+
+        //Verifying the warning message of setting a nonce hihgher than "current nonce"
+        cy.findByText(
+          'transactions will need to be created and executed before this transaction, are you sure you want to do this?',
+        ).contains(`${offset}`)
+      })
+
+      it('should display a correctly calculated gas price estimation', () => {
+        //the result of 200k gaslimit, 5 gasPrice and Fee
+        cy.contains('Estimated fee price').next().should('have.text', '0.002 ETH')
+      })
     })
   })
 })
