24ad1aea661262a8426b576d678bbeefcea6c7fc
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-07-19 18:11:08 +0200

    Improve permissions component

--- src/routes/safe/components/Apps/components/SecurityFeedbackModal/SecurityFeedbackAllowedFeatures.tsx
+++ src/routes/safe/components/Apps/components/SecurityFeedbackModal/SecurityFeedbackAllowedFeatures.tsx
@@ -1,7 +1,8 @@
-import { Checkbox } from '@gnosis.pm/safe-react-components'
+import { Checkbox, Icon, Text } from '@gnosis.pm/safe-react-components'
 import React from 'react'
 import { AllowedFeatures } from '../../types'
-
+import { StyledSecurityTitle } from 'src/routes/safe/components/Apps/components/SecurityFeedbackModal/styles'
+import styled from 'styled-components'
 type SecurityFeedbackAllowedFeaturesProps = {
   features: { feature: AllowedFeatures; checked: boolean }[]
   onFeatureSelectionChange: (feature: AllowedFeatures, checked: boolean) => void
@@ -13,18 +14,26 @@ const SecurityFeedbackAllowedFeatures: React.FC<SecurityFeedbackAllowedFeaturesP
 }): React.ReactElement => {
   return (
     <>
-      <p>Hello SecurityFeedbackAllowedFeatures</p>
-      {features.map(({ feature, checked }, index) => (
-        <Checkbox
-          key={index}
-          name="checkbox"
-          checked={checked}
-          onChange={(_, checked) => onFeatureSelectionChange(feature, checked)}
-          label={feature}
-        />
-      ))}
+      <Icon size="md" type="privacyPolicy" />
+      <StyledSecurityTitle size="lg">Manage the features the Safe App can use</StyledSecurityTitle>
+      <StyledFeaturesContainer>
+        <Text size="xl">This app is requesting permission to use:</Text>
+        <br />
+        {features.map(({ feature, checked }, index) => (
+          <Checkbox
+            key={index}
+            name="checkbox"
+            checked={checked}
+            onChange={(_, checked) => onFeatureSelectionChange(feature, checked)}
+            label={feature}
+          />
+        ))}
+      </StyledFeaturesContainer>
     </>
   )
 }
 
+const StyledFeaturesContainer = styled.div`
+  padding: 20px 20px 0 20px;
+`
 export default SecurityFeedbackAllowedFeatures
