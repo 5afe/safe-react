481c92ce4d90827b50b438e9f1c689c50df83988
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-07-28 13:36:38 +0200

    Improve UI in SafeAppsPermissions

--- src/routes/safe/components/Settings/SafeAppsPermissions/index.tsx
+++ src/routes/safe/components/Settings/SafeAppsPermissions/index.tsx
@@ -4,9 +4,11 @@ import styled from 'styled-components'
 import { grey400, lg } from 'src/theme/variables'
 import Heading from 'src/components/layout/Heading'
 import { useSafePermissions, useBrowserPermissions } from 'src/routes/safe/components/Apps/hooks/permissions'
-import { Box, Grid, Link } from '@material-ui/core'
-import { Checkbox, Text } from '@gnosis.pm/safe-react-components'
+import { Grid, Checkbox, FormControlLabel } from '@material-ui/core'
+import { Text, Link } from '@gnosis.pm/safe-react-components'
 import { AllowedFeatures, PermissionStatus } from '../../Apps/types'
+
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
 
 const SafeAppsPermissions = (): ReactElement => {
   const {
@@ -30,55 +32,70 @@ const SafeAppsPermissions = (): ReactElement => {
   const handleBrowserPermissionsChange = (origin: string, feature: AllowedFeatures, checked: boolean) =>
     updateBrowserPermission(origin, feature, checked)
 
+  const handleAllowAll = (event: React.MouseEvent, origin: string) => {
+    event.preventDefault()
+    console.log(origin)
+  }
+
+  const handleClearAll = (event: React.MouseEvent, origin: string) => {
+    event.preventDefault()
+    console.log(origin)
+  }
+
   return (
     <StyledContainer>
       <Heading tag="h2">Safe Apps Permissions</Heading>
-      <Box flexDirection="column">
-        {domains.map((domain) => (
-          <StyledPermissionsCard item key={domain}>
-            <StyledPermissionsCardBody container direction="row" alignItems="center">
-              <Grid item>
-                <Text size="lg">{domain}</Text>
-              </Grid>
+      <br />
+      {domains.map((domain) => (
+        <StyledPermissionsCard item key={domain}>
+          <StyledPermissionsCardBody container>
+            <StyledSafeAppInfo item xs={5}>
+              <Text size="lg">{domain}</Text>
+            </StyledSafeAppInfo>
+            <Grid container item xs={7}>
               {safePermissions[domain]?.map(({ parentCapability, caveats }) => {
                 return (
-                  <Grid item key={parentCapability}>
-                    <Checkbox
+                  <Grid key={parentCapability} item xs={12} sm={12} lg={4} xl={3}>
+                    <PermissionsCheckbox
                       name={parentCapability}
-                      checked={!isUserRestricted(caveats)}
-                      onChange={(_, checked) => handleSafePermissionsChange(domain, parentCapability, checked)}
                       label={parentCapability}
+                      onChange={(_, checked: boolean) => handleSafePermissionsChange(domain, parentCapability, checked)}
+                      checked={!isUserRestricted(caveats)}
                     />
                   </Grid>
                 )
               })}
               {browserPermissions[domain]?.map(({ feature, status }) => {
                 return (
-                  <Grid item key={feature}>
-                    <Checkbox
+                  <Grid key={feature} item xs={12} sm={6} lg={4} xl={3}>
+                    <PermissionsCheckbox
                       name={feature.toString()}
+                      label={capitalize(feature.toString())}
+                      onChange={(_, checked: boolean) => handleBrowserPermissionsChange(domain, feature, checked)}
                       checked={status === PermissionStatus.GRANTED ? true : false}
-                      onChange={(_, checked) => handleBrowserPermissionsChange(domain, feature, checked)}
-                      label={feature}
                     />
                   </Grid>
                 )
               })}
-            </StyledPermissionsCardBody>
-            <StyledPermissionsCardFooter item>
-              <Link href={`https://${domain}`} target="_blank">
-                Allow all
-              </Link>
-              <Link href={`https://${domain}`} target="_blank">
-                Clear all
-              </Link>
-            </StyledPermissionsCardFooter>
-          </StyledPermissionsCard>
-        ))}
-      </Box>
+            </Grid>
+          </StyledPermissionsCardBody>
+          <StyledPermissionsCardFooter container item justifyContent="flex-end">
+            <StyledLink href="#" size="lg" onClick={(event) => handleAllowAll(event, domain)}>
+              Allow all
+            </StyledLink>
+            <StyledLink href="#" color="error" size="lg" onClick={(event) => handleClearAll(event, domain)}>
+              Clear all
+            </StyledLink>
+          </StyledPermissionsCardFooter>
+        </StyledPermissionsCard>
+      ))}
     </StyledContainer>
   )
 }
+
+const PermissionsCheckbox = ({ label, checked, onChange, name }) => (
+  <StyledFormControlLabel control={<Checkbox checked={checked} onChange={onChange} name={name} />} label={label} />
+)
 
 const StyledContainer = styled(Block)`
   padding: ${lg};
@@ -91,12 +108,28 @@ const StyledPermissionsCard = styled(Grid)`
 `
 
 const StyledPermissionsCardBody = styled(Grid)`
-  padding: 24px;
+  padding: 15px 24px;
   border-bottom: 2px solid ${grey400};
 `
 
 const StyledPermissionsCardFooter = styled(Grid)`
-  padding: 12px;
+  padding: 12px 24px;
+`
+
+const StyledSafeAppInfo = styled(Grid)`
+  padding: 9px 0;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin-left: 16px;
+`
+
+const StyledFormControlLabel = styled(FormControlLabel)`
+  flex: 1;
+  .MuiIconButton-root:not(.Mui-checked) {
+    color: ${({ theme }) => theme.colors.inputDisabled};
+  }
 `
 
 export default SafeAppsPermissions
