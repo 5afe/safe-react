7a3028bdec6c8a255353e5f0bef5a5d1140a6111
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-08-17 09:55:40 +0200

    Change hasAnyKey param type for avoid null check

--- src/routes/safe/components/Apps/hooks/permissions/usePermissions.ts
+++ src/routes/safe/components/Apps/hooks/permissions/usePermissions.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import local from 'src/utils/storage/local'
 
-const hasAnyKey = (obj: any) => !!Object.keys(obj).length
+const hasAnyKey = (obj: Record<string, any>) => !!Object.keys(obj).length
 
 const usePermissions = <T>(initialValue: T, storageKey: string): [T, (state: T) => void] => {
   const [permissions, setPermissions] = useState<T>(() => local.getItem(storageKey) || initialValue)
