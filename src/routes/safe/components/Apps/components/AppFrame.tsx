f1f8c5a74a8714e7814b71d8ab7dae62006734c2
Soptq <32592090+soptq@users.noreply.github.com>
2022-08-18 16:20:14 +0800

    fix: remove testing code

--- src/routes/safe/components/Apps/components/AppFrame.tsx
+++ src/routes/safe/components/Apps/components/AppFrame.tsx
@@ -264,12 +264,9 @@ const AppFrame = ({ appUrl }: Props): ReactElement => {
     })
 
     communicator?.on(Methods.signMessage, async (msg) => {
-      // const { message } = msg.data.params as SignMessageParams
-      const message =
-        '{"types":{"Person":[{"name":"name","type":"string"},{"name":"wallet","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person"},{"name":"contents","type":"string"}]},"primaryType":"Mail","domain":{"name":"Ether Mail","version":"1","chainId":1,"verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"},"message":{"from":{"name":"Cow","wallet":"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"},"to":{"name":"Bob","wallet":"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"},"contents":"Hello, Bob!"}}'
+      const { message } = msg.data.params as SignMessageParams
 
-      openSignMessageModal(message, msg.data.id, Methods.signTypedMessage)
-      // openSignMessageModal(message, msg.data.id, Methods.signMessage)
+      openSignMessageModal(message, msg.data.id, Methods.signMessage)
     })
 
     communicator?.on(Methods.signTypedMessage, async (msg) => {
