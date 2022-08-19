fc875b2b7e150123f1822d25655fd4638ac02c66
vien.nguyen2-tiki <vien.nguyen2@tiki.vn>
2022-08-19 14:45:31 +0700

    fix: remove Intercom popup

--- src/components/CookiesBanner/index.tsx
+++ src/components/CookiesBanner/index.tsx
@@ -10,7 +10,6 @@ import { closeCookieBanner, openCookieBanner } from 'src/logic/cookies/store/act
 import { cookieBannerState } from 'src/logic/cookies/store/selectors'
 import { loadFromCookie, saveCookie } from 'src/logic/cookies/utils'
 import { mainFontFamily, md, primary, screenSm } from 'src/theme/variables'
-import { closeIntercom, isIntercomLoaded, loadIntercom } from 'src/utils/intercom'
 import AlertRedIcon from './assets/alert-red.svg'
 import IntercomIcon from './assets/intercom.png'
 import { useSafeAppUrl } from 'src/logic/hooks/useSafeAppUrl'
@@ -276,16 +275,16 @@ const CookiesBanner = isDesktop
       }, [localAnalytics])
 
       // Toggle Intercom
-      useEffect(() => {
-        if (isSafeAppView || !localSupportAndUpdates) {
-          isIntercomLoaded() && closeIntercom()
-          return
-        }
+      //   useEffect(() => {
+      //     if (isSafeAppView || !localSupportAndUpdates) {
+      //       isIntercomLoaded() && closeIntercom()
+      //       return
+      //     }
 
-        if (!isSafeAppView && localSupportAndUpdates) {
-          !isIntercomLoaded() && loadIntercom()
-        }
-      }, [localSupportAndUpdates, isSafeAppView])
+      //     if (!isSafeAppView && localSupportAndUpdates) {
+      //       !isIntercomLoaded() && loadIntercom()
+      //     }
+      //   }, [localSupportAndUpdates, isSafeAppView])
 
       // Toggle Beamer
       useEffect(() => {
