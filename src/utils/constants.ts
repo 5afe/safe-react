6b66212ae5b230e63a04eb12542fe42677a37f6b
katspaugh <katspaugh@gmail.com>
2022-10-25 16:33:07 +0200

    Fix: update CGW URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -57,7 +57,7 @@ export const isProdGateway = (): boolean => {
 
 export const GATEWAY_URL =
   process.env.REACT_APP_GATEWAY_URL ||
-  (IS_PRODUCTION || isProdGateway() ? 'https://safe-client.gnosis.io' : 'https://safe-client.staging.5afe.dev')
+  (IS_PRODUCTION || isProdGateway() ? 'https://safe-client.safe.global' : 'https://safe-client.staging.5afe.dev')
 
 // Safe Token
 export const SAFE_TOKEN_ADDRESSES: { [chainId: string]: string } = {
